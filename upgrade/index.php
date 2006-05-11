0b00f789ea4066a901f1f8f00f4cfbc68314a381
Yannis Exidaridis <jexi@noc.uoa.gr>
2006-05-11 11:00:39 +0000

    Βελτίωση στο interface

--- upgrade/index.php
+++ upgrade/index.php
@@ -6,16 +6,18 @@ $nameTools = 'Αναβάθμιση';
 
 begin_page();
 ?>
+	<table cellpadding="8" cellspacing="0" border="0" width="100%">
 	<form method="post" action="upgrade.php">
-	<p align="left">Για να δείτε τις αλλαγές-βελτιώσεις της καινούριας έκδοσης 
-	του e-Class κάντε κλικ <a href="CHANGES.txt" target=_blank>εδώ</a>.</p>
-	<p align="left">Για να προχωρήσετε στην αναβάθμιση της βάσης δεδομένων,
-	δώστε το όνομα χρήστη και το συνθηματικό του διαχειριστή της πλατφόρμας:</p>
-	<p align="left">
-	<?= $langUsername ?>: <input type="text" name="login" size="20"><br><br>
-	<?= $langPass ?>:   <input type="password" name="password" size="20"><br><br>
-	<input type="submit" name="submit" value="Αναβάθμιση Βάσης Δεδομένων">
-	</p>
+	<tr><td colspan="2" bgcolor=<?= $color2 ?>>
+	Για να δείτε τις αλλαγές-βελτιώσεις της καινούριας έκδοσης του e-Class κάντε κλικ <a href="CHANGES.txt" target=_blank>εδώ</a>.</td></tr>
+	<tr><td colspan="2" bgcolor=<?= $color2 ?>>Για να προχωρήσετε στην αναβάθμιση της βάσης δεδομένων,
+	δώστε το όνομα χρήστη και το συνθηματικό του διαχειριστή της πλατφόρμας:</td></tr>
+	<tr><td bgcolor=<?= $color2 ?>><?= $langUsername ?>:</td><td bgcolor=<?= $color2 ?>>
+	<input type="text" name="login" size="20"></td></tr>
+	<tr><td bgcolor=<?= $color2 ?>><?= $langPass ?>:</td>
+	<td bgcolor=<?= $color2 ?>><input type="password" name="password" size="20"></td></tr>
+	<tr><td align="center" colspan="2" bgcolor=<?= $color2 ?>><input type="submit" name="submit" value="Αναβάθμιση Βάσης Δεδομένων"></td></tr>
+	</table>
 <?
 end_page();
 ?>
