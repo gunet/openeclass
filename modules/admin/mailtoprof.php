ee8cceb970bd8039a709cd696afebbae5d873afa
Stratos Karatzidis <kstratos>
2006-05-19 10:59:16 +0000

    Εφαρμογή νέου template, δυνατότητα αποστολής σε όλους τους χρήστες ή μονο στους καθηγητές

--- modules/admin/mailtoprof.php
+++ modules/admin/mailtoprof.php
@@ -1,23 +1,27 @@
-<?
+<?php
 $langFiles = array('gunet','registration','admin');
-include '../../include/init.php';
+include '../../include/baseTheme.php';
 @include('check_admin.inc');
 include('../../include/sendMail.inc.php');
 
 $nameTools=$sendinfomail;
-$navigation[]= array ("url"=>"index.php", "name"=> "$langAdmin");
-begin_page();
+$tool_content = "";
 
-echo "<tr><td>";
 $conn = mysql_connect("$mysqlServer", "$mysqlUser", "$mysqlPassword");
 if (!mysql_select_db("$mysqlMainDb", $conn)) {
-        die("Cannot select database $mysqlMainDb.\n");
-        }
+	die("Cannot select database $mysqlMainDb.\n");
+}
+
+$tool_content .= "<table width=\"99%\"><tbody><tr><td>";
 
 
-if (isset($sendmail) && $sendmail == 1) {
+if (isset($submit) && ($body_mail!="")) {
 	
-	$sql=mysql_query("SELECT DISTINCT email FROM user where statut='1'");
+	if ($sendTo=="0") {
+		$sql=mysql_query("SELECT DISTINCT email FROM user");
+	} elseif ($sendTo=="1") {
+		$sql=mysql_query("SELECT DISTINCT email FROM user where statut='1'");
+	}
 	while ($m = mysql_fetch_array($sql)) {
 		$to = $m[0];
 		$emailsubject = $infoabouteclass;
@@ -30,28 +34,24 @@ $langEmail : $emailAdministrator
 ";
 		if (!send_mail($siteName, $emailAdministrator, '', $to,
 			$emailsubject, $emailbody, $charset)) {
-				echo "<h4>Σφάλμα κατά την αποστολή e-mail στη διεύθυνση '$to'!</h4>";
+				$tool_content .= "<h4>Σφάλμα κατά την αποστολή e-mail στη διεύθυνση '$to'!</h4>";
 		}
 	}
-	echo "<h4>$emailsuccess</h4>";
-	echo "<p><center><a href=\"index.php\">Επιστροφή</a></p></center>";
-	$sendmail=0;
-
+	$tool_content .= "<h4>$emailsuccess</h4></td></tr><tbody></table>";
 } else {
 
-?>
-
-<h5><?echo $typeyourmessage;?></h5>
-
-<form action="mailtoprof.php" method="post">
-<textarea name="body_mail" rows="10" cols="60"></textarea>
+	$tool_content .= "<h5>".$typeyourmessage."</h5>";
+	$tool_content .= "<form action=\"".$_SERVER[PHP_SELF]."\" method=\"post\">
+<textarea name=\"body_mail\" rows=\"10\" cols=\"60\"></textarea>
 <br><br>
-<input type="checkbox" name="sendmail" value="1" checked="yes">
-&nbsp;Αποστολή μηνύματος<br><br><input type="submit" name="submit" value="Αποστολή"></input>
-</form>
-<p><center><a href="index.php">Επιστροφή</a></p></center>
-</body>
-</html>
-<?
-	}
+Αποστολή μηνύματος προς <select name=\"sendTo\">
+<option value=\"0\">Όλους τους χρήστες</option>
+<option value=\"1\">Μόνο στους εκπαιδευτές</option></select><br><br><input type=\"submit\" name=\"submit\" value=\"Αποστολή\"></input>
+</form></td></tr></tbody></table>";
+
+}
+
+$tool_content .= "<br><center><p><a href=\"index.php\">Επιστροφή</a></p></center>";	
+
+draw($tool_content,3,'admin');
 ?>
