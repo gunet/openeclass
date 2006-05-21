1841bde5f8fab76d29312fa35ecdd1042492c6af
Evelthon Prodromou <eprodromou@upnet.gr>
2006-05-21 15:04:44 +0000

    The content of eclass 2.0 UI when a general error occurs (index.php)

---
+++ not_installed.php
@@ -0,0 +1,29 @@
+<?PHP
+
+//not installed
+
+$tool_content .= "
+	
+	<body bgcolor='white'>
+	<center>
+	<table cellpadding='6' cellspacing='0' border='0' width='650' bgcolor='#E6E6E6'>
+	<tr bgcolor='navy'><td valign='top' align='center'>
+	<font color='white' face='arial, helvetica'>Πλατφόρμα Ασύγχρονης Τηλεκπαίδευσης GUNet e-Class</font>
+	</td></tr><tr><td>&nbsp;</td></tr>
+	<tr bgcolor='#E6E6E6'>
+	<td>
+	<b>Η πλατφόρμα ασύγχρονης τηλεκπαίδευσης δεν λειτουργεί !</b>
+	<p>Πιθανοί λόγοι: 
+	<ul>
+	<li>Χρησιμοποιείτε την πλατφόρμα για πρώτη φορά.<br> Σε αυτή την περίπτωση κάντε κλίκ στον 
+	<a href=\"./install/\">Οδηγό Εγκατάστασης</a> για να ξεκινήσετε το πρόγραμμα 
+	εγκατάστασης.</li>
+	<li>Το αρχείο <tt>config.php</tt> δεν υπάρχει ή δεν μπορεί να διαβαστεί.</li>
+	<li>Η MySQL δεν λειτουργεί (επικοινωνήστε με το διαχειριστή του συστήματος).</li>
+	</ul></p> 
+	</td>
+        </tr>
+	</table>";
+	exit();
+
+?>
