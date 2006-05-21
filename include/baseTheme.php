7d34cfa678a8ca36c705ae10b847840015e52a2a
Evelthon Prodromou <eprodromou@upnet.gr>
2006-05-21 14:24:17 +0000

    Added a test icon to the navbar Created breadcrumb

--- include/baseTheme.php
+++ include/baseTheme.php
@@ -12,10 +12,13 @@
 
 
 include('init.php');
-//echo $langAdmin;
-include('../../template/template.inc');
-include('tools.php');
 
+//template path for modules
+//include('../../template/template.inc');
+
+//template path for logged out + logged in
+include($relPath."template/template.inc");
+include('tools.php');
 
 function getTools($menuTypeID){
 
@@ -32,17 +35,20 @@ function getTools($menuTypeID){
 	 * 
 	 */
 function draw($toolContent, $menuTypeID, $tool_css = null, $head_content = null, $body_action = null){
-	global $langUser, $prenom, $nom, $langLogout, $intitule,  $nameTools, $langHelp, $language, $helpTopic, $require_help;
-	$toolArr = getTools($menuTypeID);
-//	dumpArray($toolArr);
+	global $langUser, $prenom, $nom, $langLogout, $intitule,  $nameTools, $langHelp;
+	global $language, $helpTopic, $require_help, $homePage;
+	global $relPath, $urlServer;
+	global $page_name, $page_navi,$currentCourseID, $siteName;
 
-//	echo "<br>" . count($toolArr[0][1]);
+	$toolArr = getTools($menuTypeID);
+	//	dumpArray($toolArr);
+	//echo MODULE_ID_GROUPS;
 	$numOfToolGroups = count($toolArr);
 
-	//	$url_intro = $this->eclBase->varVal("get", "url_server");
-	//	$url_intro .= $this->eclBase->varVal("get", "current_course_id");
 
-	$t = new Template("../../template/classic");
+
+	//	$t = new Template("../../template/classic");
+	$t = new Template($relPath ."template/classic");
 
 	$t->set_file('fh', "theme.html");
 
@@ -67,14 +73,15 @@ function draw($toolContent, $menuTypeID, $tool_css = null, $head_content = null,
 			$numOfTools = count($toolArr[$i][1]);
 			//echo "draw";
 
-//			$t->set_var('TOOL_LINK', $url_intro);
-//			$t->set_var('TOOL_TEXT', 'Eisagwgh Ma8hmatos');
-//			$t->parse('leftNavLink', 'leftNavLinkBlock', true);
+			//			$t->set_var('TOOL_LINK', $url_intro);
+			//			$t->set_var('TOOL_TEXT', 'Eisagwgh Ma8hmatos');
+			//			$t->parse('leftNavLink', 'leftNavLinkBlock', true);
 
 			for($j=0; $j< $numOfTools; $j++){
 
 				$t->set_var('TOOL_LINK', $toolArr[$i][2][$j]);
 				$t->set_var('TOOL_TEXT', $toolArr[$i][1][$j]);
+				$t->set_var('TOOL_ICON', 'agenda');
 				$t->parse('leftNavLink', 'leftNavLinkBlock', true);
 
 			}
@@ -86,43 +93,114 @@ function draw($toolContent, $menuTypeID, $tool_css = null, $head_content = null,
 
 			$t->clear_var('leftNavLink'); //clear inner block
 		}
-	
+
 		$t->set_var('TOOL_CONTENT', $toolContent);
-		
+
 		$t->set_var('LANG_USER', $langUser);
 		$t->set_var('USER_NAME', $prenom);
 		$t->set_var('USER_SURNAME', $nom);
 		$t->set_var('LANG_LOGOUT', $langLogout);
-		
+		//TODO: set a var for logout link url!
 		$t->set_var('LESSON_TITLE', $intitule);
 		$t->set_var('TOOL_NAME',  $nameTools);
-		
-		
-		if (isset($tool_css)){
-			
-			$t->set_var('TOOL_CSS', "<link href=\"../../modules/$tool_css/tool.css\" rel=\"stylesheet\" type=\"text/css\" />");
+
+		$t->set_var('LOGOUT_LINK',  $relPath);
+
+		//START breadcrumb
+		//		$t->set_var('BREAD_TEXT',  "Η αρχική μου σελίδα");
+		//		$t->set_var('BREAD_START_LINK',  $urlServer);
+
+		if (!$page_navi) $page_navi = $navigation;
+		if (!$page_name) $page_name = $nameTools;
+
+		$t->set_block('mainBlock', 'breadCrumbHomeBlock', 'breadCrumbHome');
+		$t->set_var('BREAD_TEXT',  $siteName);
+		if (!$homePage) {
+			$t->set_var('BREAD_HREF_FRONT',  '<a href="{BREAD_START_LINK}">');
+			$t->set_var('BREAD_START_LINK',  $urlServer);
+			$t->set_var('BREAD_HREF_END',  '</a>');
 		}
-		
+
+		$t->parse('breadCrumbHome', 'breadCrumbHomeBlock',false);
+
+		$breadIterator=1;
+		$t->set_block('mainBlock', 'breadCrumbStartBlock', 'breadCrumbStart');
+		if (isset($currentCourseID)){
+			$t->set_var('BREAD_HREF_FRONT',  '<a href="{BREAD_LINK}">');
+			$t->set_var('BREAD_LINK',  $urlServer.'courses/'.$currentCourseID.'/index.php');
+			$t->set_var('BREAD_TEXT',  $intitule);
+			$t->set_var('BREAD_ARROW', '&#187;');
+			$t->set_var('BREAD_HREF_END',  '</a>');
+			$t->parse('breadCrumbStart', 'breadCrumbStartBlock',true);
+			$breadIterator++;
+			//			echo "&nbsp;&gt;&nbsp;<a href=\"".$urlServer.
+			//			"courses/$currentCourseID/index.php\" target=\"_top\">". q($intitule) . "</a>\n";
+		}
+		if (isset($page_navi) && is_array($page_navi) && !$homePage){
+			foreach ($page_navi as $step){
+				$t->set_var('BREAD_HREF_FRONT',  '<a href="{BREAD_LINK}">');
+				$t->set_var('BREAD_LINK',  $step["url"]);
+				$t->set_var('BREAD_TEXT',  $step["name"]);
+				$t->set_var('BREAD_ARROW', '&#187;');
+				$t->set_var('BREAD_HREF_END',  '</a>');
+				$t->parse('breadCrumbStart', 'breadCrumbStartBlock',true);
+
+
+//				$t->set_var('BREAD_TEXT',  $intitule);
+//				$t->set_var('BREAD_ARROW', '&#187;');
+
+				$t->parse('breadCrumbStart', 'breadCrumbStartBlock',true);
+				$breadIterator++;
+			}
+		}
+		//		echo "&nbsp;&gt;&nbsp;<a target=\"_top\" href=\""
+		//		.$step["url"]."\" >".q($step["name"])."</a>";
+		if (isset($page_name) && !$homePage) {
+			//			echo "&nbsp;&gt;&nbsp;<b>" . q($page_name) . "</b>";
+			$t->set_var('BREAD_HREF_FRONT',  '');
+			//			$t->set_var('BREAD_LINK',  $urlServer.'courses/'.$currentCourseID.'/index.php');
+			$t->set_var('BREAD_TEXT',  $page_name);
+			$t->set_var('BREAD_ARROW', '&#187;');
+			$t->set_var('BREAD_HREF_END',  '');
+
+			$t->parse('breadCrumbStart', 'breadCrumbStartBlock',true);
+			$breadIterator++;
+		}
+		//		echo $breadIterator;
+		$t->set_block('mainBlock', 'breadCrumbEndBlock', 'breadCrumbEnd');
+		for($breadIterator2=0; $breadIterator2 <= $breadIterator; $breadIterator2++){
+
+			$t->parse('breadCrumbEnd', 'breadCrumbEndBlock',true);
+		}
+
+		//END breadcrumb
+
+		if (isset($tool_css)){
+
+			$t->set_var('TOOL_CSS', "<link href=\"{TOOL_PATH}modules/$tool_css/tool.css\" rel=\"stylesheet\" type=\"text/css\" />");
+		}
+
+		$t->set_var('TOOL_PATH',  $relPath);
 		if (isset($head_content)){
 			$t->set_var('HEAD_EXTRAS', $head_content);
 		}
-		
+
 		if (isset($body_action)){
 			$t->set_var('BODY_ACTION', $body_action);
 		}
-		
-		if ($require_help == true){
-			$help_link = <<<hLink
-		 <a href="../help/help.php?topic=$helpTopic&language=$language>" 
-        onClick="window.open('../help/help.php?topic=$helpTopic&language=$language','MyWindow','toolbar=no,location=no,directories=no,status=yes,menubar=no,scrollbars=yes,resizable=yes,width=350,height=450,left=300,top=10'); 
-        return false;">$langHelp</a>
-hLink;
 
-			$help_link_icon = " <a id=\"help_icon\" href=\"../help/help.php?topic=$helpTopic&language=$language>\" 
-        onClick=\"window.open('../help/help.php?topic=$helpTopic&language=$language','MyWindow','toolbar=no,location=no,directories=no,status=yes,menubar=no,scrollbars=yes,resizable=yes,width=350,height=450,left=300,top=10'); 
+		if ($require_help == true){
+			$help_link = "
+		 <a href=\"".$relPath."modules/help/help.php?topic=$helpTopic&language=$language>\" 
+        onClick=\"window.open('".$relPath."modules/help/help.php?topic=$helpTopic&language=$language','MyWindow','toolbar=no,location=no,directories=no,status=yes,menubar=no,scrollbars=yes,resizable=yes,width=350,height=450,left=300,top=10'); 
+        return false;\">$langHelp</a>
+";
+
+			$help_link_icon = " <a id=\"help_icon\" href=\"".$relPath."modules/help/help.php?topic=$helpTopic&language=$language>\"
+        onClick=\"window.open('".$relPath."modules/help/help.php?topic=$helpTopic&language=$language','MyWindow','toolbar=no,location=no,directories=no,status=yes,menubar=no,scrollbars=yes,resizable=yes,width=350,height=450,left=300,top=10'); 
         return false;\"></a>";
-	
-			
+
+
 			$t->set_var('HELP_LINK', $help_link);
 			$t->set_var('HELP_LINK_ICON', $help_link_icon);
 			$t->set_var('LANG_HELP', $langHelp);
@@ -130,8 +208,8 @@ hLink;
 			$t->set_var('{HELP_LINK}', '');
 			$t->set_var('LANG_HELP', '');
 		}
-		
-		
+
+
 
 		//		At this point all variables are set and we are ready to send the final output
 		//		back to the browser
@@ -156,14 +234,14 @@ hLink;
 	 * @return string
 	 */
 /*function makeToolLink($link){
-	$path = $this->eclBase->varVal("get", "url_server");
-	if (substr($link, 3) != "htt") {
-		$path .= substr($link, 3);
-	} else {
-		$path = substr($link, 3);
-	}
-	//		$path .= substr($link, 3);
-	return $path;
+$path = $this->eclBase->varVal("get", "url_server");
+if (substr($link, 3) != "htt") {
+$path .= substr($link, 3);
+} else {
+$path = substr($link, 3);
+}
+//		$path .= substr($link, 3);
+return $path;
 }*/
 
 /**
