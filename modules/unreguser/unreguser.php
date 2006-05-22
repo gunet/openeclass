89fac43b9e2f67920b989ffc8893d76e380b1f48
Evelthon Prodromou <eprodromou@upnet.gr>
2006-05-22 23:24:58 +0000

    Ported to eclass 2.0

--- modules/unreguser/unreguser.php
+++ modules/unreguser/unreguser.php
@@ -1,59 +1,63 @@
 <?
 $require_login = TRUE;
 $langFiles = 'unreguser';
-include "../../include/init.php";
+
 $nameTools = $langUnregUser;
 $navigation[]= array ("url"=>"../auth/profile.php", "name"=> $langModifProfile);
-	
-$local_style = 'li { font-size: 10pt; }';
 
-begin_page();
+//$local_style = 'li { font-size: 10pt; }';
+include '../../include/baseTheme.php';
+//begin_page();
+$tool_content = "";
 
 if (!isset($doit) or $doit != "yes") {
-	echo "<table cellpadding=3 cellspacing=0 border=0 width=100%>";
-        echo "<tr bgcolor=$color2><td align=center>";
+	$tool_content .=  "<table width=99%><tbody>";
+	$tool_content .=  "<tr><td class=\"caution\">";
 
-	 // admin cannot be deleted
+	// admin cannot be deleted
 	if ($is_admin) {
-		echo $langAdminNo;
-        	echo "<p><a href='../profile/profile.php'>$langBack</a></p>";
+		$tool_content .=  $langAdminNo;
+		$tool_content .=  "<p><a href='../profile/profile.php'>$langBack</a></p>";
 		exit;
 	} else {
-   		$q = db_query ("SELECT code_cours FROM cours_user WHERE user_id = '$uid'") ;
-   		if (mysql_num_rows($q) == 0) {
-           		echo "<h3>$langConfirm</h3>";
-	   		echo "<ul>";
-	   		echo "<li>$langYes: ";
-	   		echo "<a href='$_SERVER[PHP_SELF]?u=$uid&doit=yes'>$langDelete</a>";
-	   		echo "</li>";
-			echo "<br>";
-	   		echo "<li>$langNo: <a href='../profile/profile.php'>$langBack</a>";
-	  		echo "</li></ul>";
-   		} else {
-			echo "<h3>$langNotice</h3>";
-	  		echo $langExplain;
-			echo "<p><a href='../profile/profile.php'>$langBack</a></p><br>";
-        	}
+		$q = db_query ("SELECT code_cours FROM cours_user WHERE user_id = '$uid'") ;
+		if (mysql_num_rows($q) == 0) {
+			$tool_content .=  "<h3>$langConfirm</h3>";
+			$tool_content .=  "<ul>";
+			$tool_content .=  "<li>$langYes: ";
+			$tool_content .=  "<a href='$_SERVER[PHP_SELF]?u=$uid&doit=yes'>$langDelete</a>";
+			$tool_content .=  "</li>";
+			$tool_content .=  "<br>";
+			$tool_content .=  "<li>$langNo: <a href='../profile/profile.php'>$langBack</a>";
+			$tool_content .=  "</li></ul>";
+			$tool_content .= "</td></tr></tbody></table>";
+		} else {
+			$tool_content .=  "<h3>$langNotice</h3>";
+			$tool_content .=  "<p>$langExplain</p>";
+			$tool_content .=  "<p><a href='../profile/profile.php'>$langBack</a></p><br>";
+			$tool_content .= "</td></tr></tbody></table>";
+		}
 	}  //endif is admin
 } else {
 	if (isset($uid)) {
-     		echo "<table cellpadding=3 cellspacing=0 border=0 width=100%>";
-                echo "<br><tr valign=top bgcolor=$color2>";
-		echo "<td align=center>";
-		echo "<font size=3 face=arial, helvetica>";
-     		db_query("DELETE from user WHERE user_id = '$uid'");
+		$tool_content .=  "<table width=99%><tbody>";
+		$tool_content .=  "<tr>";
+		$tool_content .=  "<td class=\"success\">";
+		db_query("DELETE from user WHERE user_id = '$uid'");
 		if (mysql_affected_rows() > 0) {
-       			echo "<p>$langDelSuccess</p>";
-			echo "<p>$langThanks</p>";
+			$tool_content .=  "<p>$langDelSuccess</p>";
+			$tool_content .=  "<p>$langThanks</p>";
+			$tool_content .=  "<br><a href='../../index.php?logout=yes'>$langLogout</a><br>";
 			unset($_SESSION['uid']);
 		} else {
-       			echo "<p>$langError</p>";
-			echo "<p><a href='../profile/profile.php'>$langBack</a></p><br>";
-			exit;
+			$tool_content .=  "<p>$langError</p>";
+			$tool_content .=  "<p><a href='../profile/profile.php'>$langBack</a></p><br>";
+//			exit;
 		}
-	 }
-	echo "<br><a href='../../index.php?logout=yes'>$langLogout</a><br>";
+	}
+	
+	$tool_content .= "</td></tr></tbody></table>";
 }
-end_page();
+draw($tool_content, 1);
 ?>
 
