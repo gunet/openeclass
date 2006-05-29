0223a8850c19a2fa20c38d5aa86190c669677e29
Stratos Karatzidis <kstratos>
2006-05-29 13:45:47 +0000

    New template

--- modules/admin/unreguser.php
+++ modules/admin/unreguser.php
@@ -1,58 +1,74 @@
 <?
 $langFiles = array('gunet','admin');
-include '../../include/init.php';
-@include("check_admin.inc");
+include '../../include/baseTheme.php';
+@include "check_admin.inc";
 
 $nameTools = $langUnregUser;
 $navigation[]= array ("url"=>"index.php", "name"=> $langAdmin);
-begin_page();
 
-if (!isset($doit) or $doit != "yes") {
+$u = isset($_GET['u'])?$_GET['u']:'';
+$doit = isset($_GET['doit'])?$_GET['doit']:'';
+$c = isset($_GET['c'])?$_GET['c']:'';
 
-	echo "
-		<h3>Επιβεβαίωση διαγραφής</h3>
+if((!empty($doit)) && ($doit != "yes")) 
+{
+	$tool_content .= "<h4>Επιβεβαίωση διαγραφής</h4>
 		<p>Θέλετε σίγουρα να διαγράψετε τον χρήστη <em>$un</em>";
-	if (isset($c) and $c != "") echo " από το μάθημα με κωδικό <em>$c</em>";
-	echo ";</p>
+	if(!empty($c)) 
+	{
+		$tool_content .= " από το μάθημα με κωδικό <em>".$c."</em>";
+	}
+	$tool_content .= ";</p>
 		<ul><li>Ναι: 
 			<a href=\"unreguser.php?u=$u&c=$c&doit=yes\">Διαγραφή!</a>
 			<br>&nbsp;</li>
-		<li>Όχι: <a href=\"index.php\">
-			Επιστροφή στη σελίδα διαχείρισης</a></li></ul>
-	";	
-
-
-} else {
+		<li>Όχι: <a href=\"index.php\">Επιστροφή στη σελίδα διαχείρισης</a></li></ul>";	
+} 
+else 
+{
 
 	$conn = mysql_connect($mysqlServer, $mysqlUser, $mysqlPassword);
         if (!mysql_select_db($mysqlMainDb, $conn))
                 die("Cannot select database \"claroline\".\n");
 
-	if ($c==""  and isset($u)) {
-		if ($u == 1) {
-			echo "Σφάλμα! Προσπαθήσατε να διαγράψετε τον χρήστη με user id = 1!";
-			exit;
+	if(empty($c)) 
+	{
+		if ($u == 1) 
+		{
+			$tool_content .= "Σφάλμα! Προσπαθήσατε να διαγράψετε τον χρήστη με user id = 1!";
 		}
 		$sql = mysql_query("DELETE from user WHERE user_id = '$u'");
-
-		if (mysql_affected_rows($conn) > 0) {
-        		echo "<p>Ο χρήστης με id $u διαγράφτηκε.</p>\n";
-		} else {
-        		echo "Σφάλμα κατά τη διαγραφή του χρήστη";
+		if (mysql_affected_rows($conn) > 0) 
+		{
+			$tool_content .= "<p>Ο χρήστης με id $u διαγράφτηκε.</p>\n";
+		} 
+		else 
+		{
+			$tool_content .= "Σφάλμα κατά τη διαγραφή του χρήστη";
 		}
 		mysql_query("DELETE from admin WHERE idUser = '$u'");
-		if (mysql_affected_rows($conn) > 0) {
-        		echo "<p>Ο χρήστης με id $u ήταν διαχειριστής.</p>\n";
-		}
-	} elseif (isset($c) and isset($u)) 
+		if (mysql_affected_rows($conn) > 0) 
 		{
+			$tool_content .= "<p>Ο χρήστης με id $u ήταν διαχειριστής.</p>\n";
+		}
+	} 
+	elseif((!empty($c)) && (!empty($u)))
+	{
 		$sql = mysql_query("DELETE from cours_user WHERE user_id = '$u' and code_cours='$c'");
 		if (mysql_affected_rows($conn) > 0)  
-			echo "<p>Ο χρήστης με id $u διαγράφτηκε από το Μάθημα $c.</p>\n";
-		else
-			echo "Σφάλμα κατά τη διαγραφή του χρήστη";
+		{
+			$tool_content .= "<p>Ο χρήστης με id $u διαγράφτηκε από το Μάθημα $c.</p>\n";
 		}
-	echo "<br>&nbsp;<br><a href=\"index.php\">Επιστροφή στη σελίδα διαχείρισης</a>\n";
+	}
+	else
+	{
+			$tool_content .= "Σφάλμα κατά τη διαγραφή του χρήστη";
+	}
+	$tool_content .= "<br>&nbsp;<br><a href=\"index.php\">Επιστροφή στη σελίδα διαχείρισης</a>\n";
 }	
+
+$tool_content .= "<br><center><p><a href=\"index.php\">Επιστροφή</a></p></center>";
+
+draw($tool_content,3,'admin');
+
 ?>
-</body></html>
