5ae164e862a41503f815d777b3188f4399b4501e
Dionysios G. Synodinos <dsin@noc.ntua.gr>
2006-06-01 12:30:53 +0000

    Updated for new template engine

--- modules/exercice/question_admin.inc.php
+++ modules/exercice/question_admin.inc.php
@@ -1,26 +1,57 @@
 <?php // $Id$
-/*
-      +----------------------------------------------------------------------+
-      | CLAROLINE version 1.4.0 $Revision$                            |
-      +----------------------------------------------------------------------+
-      | Copyright (c) 2001, 2003 Universite catholique de Louvain (UCL)      |
-      +----------------------------------------------------------------------+
-      |   This program is free software; you can redistribute it and/or      |
-      |   modify it under the terms of the GNU General Public License        |
-      |   as published by the Free Software Foundation; either version 2     |
-      |   of the License, or (at your option) any later version.             |
-      +----------------------------------------------------------------------+
-      | Authors: Olivier Brouckaert <oli.brouckaert@skynet.be>               |
-      +----------------------------------------------------------------------+
+/*=============================================================================
+       	GUnet e-Class 2.0 
+        E-learning and Course Management Program  
+================================================================================
+       	Copyright(c) 2003-2006  Greek Universities Network - GUnet
+        Á full copyright notice can be read in "/info/copyright.txt".
+        
+       	Authors:    Costas Tsibanis <k.tsibanis@noc.uoa.gr>
+        	    Yannis Exidaridis <jexi@noc.uoa.gr> 
+      		    Alexandros Diamantidis <adia@noc.uoa.gr> 
+
+        For a full list of contributors, see "credits.txt".  
+     
+        This program is a free software under the terms of the GNU 
+        (General Public License) as published by the Free Software 
+        Foundation. See the GNU License for more details. 
+        The full license can be read in "license.txt".
+     
+       	Contact address: GUnet Asynchronous Teleteaching Group, 
+        Network Operations Center, University of Athens, 
+        Panepistimiopolis Ilissia, 15784, Athens, Greece
+        eMail: eclassadmin@gunet.gr
+==============================================================================*/
+
+/*===========================================================================
+	work.php
+	@last update: 17-4-2006 by Costas Tsibanis
+	@authors list: Dionysios G. Synodinos <synodinos@gmail.com>
+==============================================================================        
+        @Description: Main script for the work tool
+
+ 	This is a tool plugin that allows course administrators - or others with the
+ 	same rights
+
+ 	The user can : - navigate through files and directories.
+                       - upload a file
+                       - delete, copy a file or a directory
+                       - edit properties & content (name, comments, 
+			 html content)
+
+ 	@Comments: The script is organised in four sections.
+
+ 	1) Execute the command called by the user
+           Note (March 2004) some editing functions (renaming, commenting)
+           are moved to a separate page, edit_document.php. This is also
+           where xml and other stuff should be added.
+   	2) Define the directory to display
+  	3) Read files and directories from the directory defined in part 2
+  	4) Display all of that on an HTML page
+ 
+  	@TODO: eliminate code duplication between document/document.php, scormdocument.php
+==============================================================================
 */
-
-		/*>>>>>>>>>>>>>>>>>>>> QUESTION ADMINISTRATION <<<<<<<<<<<<<<<<<<<<*/
-
-/**
- * This script allows to manage a question and its answers
- *
- * It is included from the script admin.php
- */
 
 // ALLOWED_TO_INCLUDE is defined in admin.php
 if(!defined('ALLOWED_TO_INCLUDE'))
@@ -31,96 +62,105 @@ if(!defined('ALLOWED_TO_INCLUDE'))
 // if the question we are modifying is used in several exercises
 if(isset($usedInSeveralExercises))
 {
-?>
 
-<h3>
-  <?php echo $questionName; ?>
-</h3>
+$tool_content .= <<<cData
+	<h3>
+		${questionName}
+	</h3>
 
-<form method="post" action="<?php echo $PHP_SELF; ?>?modifyQuestion=<?php echo $modifyQuestion; ?>&modifyAnswers=<?php echo $modifyAnswers; ?>">
-<table border="0" cellpadding="5">
-<tr>
-  <td>
+		
+		<form method="post" action="${PHP_SELF}?modifyQuestion=${modifyQuestion}&modifyAnswers=${modifyAnswers}">
+		<table border="0" cellpadding="5">
+		<tr>
+		  <td>
+cData;
 
-<?php
 	// submit question
 	if($submitQuestion)
 	{
-?>
 
-	<input type="hidden" name="questionName" value="<?php echo htmlspecialchars($questionName); ?>">
-    <input type="hidden" name="questionDescription" value="<?php echo htmlspecialchars($questionDescription); ?>">
-    <input type="hidden" name="imageUpload_size" value="<?php echo $imageUpload_size; ?>">
-    <input type="hidden" name="deletePicture" value="<?php echo $deletePicture; ?>">
+		$tool_content .= "<input type=\"hidden\" name=\"questionName\" value=\"".htmlspecialchars($questionName)."\">";
+    $tool_content .= "<input type=\"hidden\" name=\"questionDescription\"".
+    	"value=\"".htmlspecialchars($questionDescription)."\">";
+    $tool_content .= <<<cData
+    	<input type="hidden" name="imageUpload_size" value="${imageUpload_size}">
+    	<input type="hidden" name="deletePicture" value="${deletePicture}">
+cData;
 
-<?php
 	}
 	// submit answers
 	else
 	{
 		if($answerType == UNIQUE_ANSWER || $answerType == MULTIPLE_ANSWER)
 		{
-?>
 
-	<input type="hidden" name="correct" value="<?php echo htmlspecialchars(serialize($correct)); ?>">
-	<input type="hidden" name="reponse" value="<?php echo htmlspecialchars(serialize($reponse)); ?>">
-	<input type="hidden" name="comment" value="<?php echo htmlspecialchars(serialize($comment)); ?>">
-	<input type="hidden" name="weighting" value="<?php echo htmlspecialchars(serialize($weighting)); ?>">
-	<input type="hidden" name="nbrAnswers" value="<?php echo $nbrAnswers; ?>">
+	$tool_content .= "<input type=\"hidden\" name=\"correct\" value=\"".	  htmlspecialchars(serialize($correct))	.		"\">";
+	$tool_content .= "<input type=\"hidden\" name=\"reponse\" value=\"".	  htmlspecialchars(serialize($reponse))	.		"\">";
+	$tool_content .= "<input type=\"hidden\" name=\"comment\" value=\"".	  htmlspecialchars(serialize($comment))	.		"\">";
+	$tool_content .= "<input type=\"hidden\" name=\"weighting\" value=\"".  htmlspecialchars(serialize($weighting)).	"\">";
+	$tool_content .= "<input type=\"hidden\" name=\"nbrAnswers\" value=\"". $nbrAnswers."\">";
 
-<?php
 		}
 		elseif($answerType == MATCHING)
 		{
-?>
 
-	<input type="hidden" name="option" value="<?php echo htmlspecialchars(serialize($option)); ?>">
-	<input type="hidden" name="match" value="<?php echo htmlspecialchars(serialize($match)); ?>">
-	<input type="hidden" name="sel" value="<?php echo htmlspecialchars(serialize($sel)); ?>">
-	<input type="hidden" name="weighting" value="<?php echo htmlspecialchars(serialize($weighting)); ?>">
-	<input type="hidden" name="nbrOptions" value="<?php echo $nbrOptions; ?>">
-	<input type="hidden" name="nbrMatches" value="<?php echo $nbrMatches; ?>">
+	$tool_content .= "<input type=\"hidden\" name=\"option\" value=\"".	htmlspecialchars(serialize($option)).				"\">";
+	$tool_content .= "<input type=\"hidden\" name=\"match\" value=\"".	htmlspecialchars(serialize($match)). 				"\">";
+	$tool_content .= "<input type=\"hidden\" name=\"sel\" value=\"".		htmlspecialchars(serialize($sel)). 					"\">";
+	$tool_content .= "<input type=\"hidden\" name=\"weighting\" value=\"".htmlspecialchars(serialize($weighting)).	"\">";
+	$tool_content .= "<input type=\"hidden\" name=\"nbrOptions\" value=\"".$nbrOptions.															"\">";
+	$tool_content .= "<input type=\"hidden\" name=\"nbrMatches\" value=\"".$nbrMatches.															"\">";
 
-<?php
 		}
 		else
 		{
-?>
 
-	<input type="hidden" name="reponse" value="<?php echo htmlspecialchars(serialize($reponse)); ?>">
-	<input type="hidden" name="comment" value="<?php echo htmlspecialchars(serialize($comment)); ?>">
-	<input type="hidden" name="blanks" value="<?php echo htmlspecialchars(serialize($blanks)); ?>">
-	<input type="hidden" name="weighting" value="<?php echo htmlspecialchars(serialize($weighting)); ?>">
-	<input type="hidden" name="setWeighting" value="1">
+	$tool_content .= "<input type=\"hidden\" name=\"reponse\" value=\"".htmlspecialchars(serialize($reponse))."\">";
+	$tool_content .= "<input type=\"hidden\" name=\"comment\" value=\"".htmlspecialchars(serialize($comment))."\">";
+	$tool_content .= "<input type=\"hidden\" name=\"blanks\" value=\"".htmlspecialchars(serialize($blanks))."\">";
+	$tool_content .= "<input type=\"hidden\" name=\"weighting\" value=\"".htmlspecialchars(serialize($weighting))."\">".
+		"<input type=\"hidden\" name=\"setWeighting\" value=\"1\">";
 
-<?php
 		}
 	} // end submit answers
-?>
 
-	<input type="hidden" name="answerType" value="<?php echo $answerType; ?>">
+	$tool_content .= <<<cData
+		<input type="hidden" name="answerType" value="${answerType}">
 
     <table border="0" cellpadding="3" align="center" width="400" bgcolor="#FFCC00">
     <tr>
-      <td><?php echo $langUsedInSeveralExercises.' :'; ?></td>
+      <td>${langUsedInSeveralExercises} :</td>
     </tr>
     <tr>
-      <td><input type="radio" name="modifyIn" value="allExercises" checked="checked"><?php echo $langModifyInAllExercises; ?></td>
+      <td><input type="radio" name="modifyIn" value="allExercises" checked="checked">
+      	${langModifyInAllExercises}</td>
     </tr>
     <tr>
-      <td><input type="radio" name="modifyIn" value="thisExercise"><?php echo $langModifyInThisExercise; ?></td>
+      <td><input type="radio" name="modifyIn" value="thisExercise">
+      	${langModifyInThisExercise}
+      </td>
     </tr>
     <tr>
-      <input type="submit" name="<?php echo $submitQuestion?'submitQuestion':'submitAnswers'; ?>" value="<?php echo $langOk; ?>"></td>
-      &nbsp;&nbsp;<td align="center"><input type="submit" name="buttonBack" value="<?php echo $langCancel; ?>">
-    </tr>
-    </table>
-  </td>
-</tr>
-</table>
-</form>
+cData;
+    
+      $tool_content .= "<input type=\"submit\" name=\"";
+      if ($submitQuestion)
+      	$tool_content .= "submitQuestion \" ";
+      else	
+      	$tool_content .= "submitAnswers \" ";
+     	
+     	$tool_content .= "value=\"".$langOk."\"></td>&nbsp;&nbsp;<td align=\"center\">".
+     		"<input type=\"submit\" name=\"buttonBack\" value=\"".$langCancel."\">";
+     		
+     	$tool_content .= <<<cData
+				    </tr>
+				    </table>
+				  </td>
+				</tr>
+				</table>
+				</form>
+cData;
 
-<?php
 }
 else
 {
@@ -130,100 +170,81 @@ else
 
 	// is picture set ?
 	$okPicture=file_exists($picturePath.'/quiz-'.$questionId)?true:false;
-?>
 
-<h3>
-  <?php echo $questionName; ?>
-</h3>
+$tool_content .= "<h3>$questionName</h3>";
 
-<?php
 	// show the picture of the question
 	if($okPicture)
 	{
-?>
 
-<center><img src="<?php echo $picturePath.'/quiz-'.$questionId; ?>" border="0"></center>
+$tool_content .= "<center><img src=\"".$picturePath."/quiz-".$questionId."\" border=\"0\"></center>";
 
-<?php
 	}
-?>
 
-<blockquote>
-  <?php echo nl2br($questionDescription); ?>
-</blockquote>
+$tool_content .= "<blockquote>".nl2br($questionDescription)."</blockquote>";
 
-<?php
 	// doesn't show the edit link if we come from the question pool to pick a question for an exercise
 	if(!isset($fromExercise))
 	{
-?>
 
-<a href="<?php echo $PHP_SELF; ?>?modifyQuestion=<?php echo $questionId; ?>"><img src="../../images/edit.gif" border="0" align="absmiddle" alt="<?php echo $langModify; ?>"></a>
+$tool_content .= "<a href=\"".$PHP_SELF."?modifyQuestion=".$questionId.
+	"\"><img src=\"../../images/edit.gif\" border=\"0\" align=\"absmiddle\" alt=\"".$langModify."\"></a>";
 
-<?php
 	}
-?>
 
-<hr size="1" noshade="noshade">
+$tool_content .= "<hr size=\"1\" noshade=\"noshade\">";
 
-<?php
 	// we are in an exercise
 	if(isset($exerciseId))
 	{
-?>
 
-<a href="<?php echo $PHP_SELF; ?>">&lt;&lt; <?php echo $langGoBackToQuestionList; ?></a>
+$tool_content .= <<<cData
+	<a href="${PHP_SELF}">&lt;&lt; ${langGoBackToQuestionList}</a>
+cData;
 
-<?php
 	}
 	// we are not in an exercise, so we come from the question pool
 	else
 	{
-?>
 
-<a href="question_pool.php?fromExercise=<?php echo $fromExercise; ?>">&lt;&lt; <?php echo $langGoBackToQuestionPool; ?></a>
+$tool_content .= <<<cData
+	<a href="question_pool.php?fromExercise=${fromExercise}">&lt;&lt; ${langGoBackToQuestionPool}</a>
+cData;
 
-<?php
 	}
-?>
 
-<br><br>
+$tool_content .= <<<cData
+	<br><br>
+		
+		<b>${langQuestionAnswers}</b>
+		
+		<br><br>
+		
+		<table border="0" align="center" cellpadding="2" cellspacing="2" width="100%">
+	<form>
+cData;
 
-<b><?php echo $langQuestionAnswers; ?></b>
-
-<br><br>
-
-<table border="0" align="center" cellpadding="2" cellspacing="2" width="100%">
-<form>
-
-<?php
 	// shows answers of the question. 'true' means that we don't show the question, only answers
 	if(!showQuestion($questionId,true))
 	{
-?>
 
-<tr>
-  <td><?php echo $langNoAnswer; ?></td>
-</tr>
+$tool_content .= <<<cData
+	<tr>
+	  <td>${langNoAnswer}</td>
+	</tr>
+cData;
 
-<?php
 	}
-?>
 
-</form>
-</table>
+$tool_content .= "</form></table><br>";
 
-<br>
-
-<?php
 	// doesn't show the edit link if we come from the question pool to pick a question for an exercise
 	if(!isset($fromExercise))
 	{
-?>
 
-<a href="<?php echo $PHP_SELF; ?>?modifyAnswers=<?php echo $questionId; ?>"><img src="../../images/edit.gif" border="0" align="absmiddle" alt="<?php echo $langModify; ?>"></a>
+$tool_content .= "<a href=\"".$PHP_SELF."?modifyAnswers=\"".$questionId.
+	"\"><img src=\"../../images/edit.gif\" border=\"0\" align=\"absmiddle\" alt=\"".$langModify."\"></a>";
 
-<?php
 	}
 }
 ?>
