896f71b6a54f722cb942fc1ff14190e97458952a
Vangelis Chaniotakis <haniotak@ucnet.uoc.gr>
2006-06-01 07:15:18 +0000

    *** empty log message ***

--- include/action.php
+++ include/action.php
@@ -1,29 +1,46 @@
 <?php
 
-define ("ACCUEIL_ID_USAGE", 23);
-
 class action {
-    function record($accueil_id, $action_name = "access") {
+    static $module_ids = array();
+    function record($module_name, $action_name = "access") {
         global $uid, $dbname;
-        mysql_select_db($dbname);
         $action_type_id = action_type::get_action_type_id($action_name);
+        $module_id = action::get_module_id($module_name);
         $sql = "INSERT INTO actions SET ".
-                " accueil_id = '$accueil_id', ".
+                " module_id = '$module_id', ".
                 " user_id = '$uid', ".
                 " action_type_id = '$action_type_id', ".
                 " date_time = NOW() ";
-        $result = mysql_query($sql);
+        $result = db_query($sql, $dbname);
     }
+
+    function summarize() {
+
+    }
+
+    function get_module_id($module_name) {
+        global $dbname;
+        if (empty(action::$module_ids)) {
+            $sql = "SELECT id, define_var AS name FROM accueil ";
+            $result = db_query($sql, $dbname);
+            while ($row = mysql_fetch_assoc($result)) {
+                action::$module_ids[$row['name']] = $row['id'];
+            }
+        }
+
+        return action::$module_ids[$module_name];
+
+    }
+
 }
 
 class action_type {
     static $ids = array();
     function get_action_type_id($action_name) {
         global $dbname;
-        mysql_select_db($dbname);
         if (empty(action_type::$ids)) {
             $sql = "SELECT * FROM action_types";
-            $result = mysql_query($sql);
+            $result = db_query($sql, $dbname);
             while ($row = mysql_fetch_assoc($result)) {
                 action_type::$ids[$row['name']] = $row['id'];
             }
