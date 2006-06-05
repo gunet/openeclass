f0e79d0330e2bc7948059a68a2ed87436d79d62d
Vangelis Chaniotakis <haniotak@ucnet.uoc.gr>
2006-06-05 07:48:45 +0000

    Added action::record to record entries to module

--- modules/agenda/agenda.php
+++ modules/agenda/agenda.php
@@ -38,14 +38,17 @@ $helpTopic = 'Agenda';
 //include('../../include/init.php');
 include '../../include/baseTheme.php';
 
-include('../../include/lib/textLib.inc.php'); 
+include('../../include/lib/textLib.inc.php');
+
+include('../../include/action.php');
+action::record(MODULE_ID_AGENDA);
 
 $dateNow = date("d-m-Y / H:i:s",time());
 $nameTools = $langAgenda;
 $local_style1 = '
-	.month { font-weight : bold; color: #FFFFFF; background-color: #000066;
-		padding-left: 15px; padding-right : 15px; }
-	.content {position: relative; left: 25px; }';
+    .month { font-weight : bold; color: #FFFFFF; background-color: #000066;
+        padding-left: 15px; padding-right : 15px; }
+    .content {position: relative; left: 25px; }';
 mysql_select_db($dbname);
 //begin_page();
 
@@ -53,7 +56,7 @@ if ($language == 'greek')
         $lang_editor='gr';
 else
         $lang_editor='en';
-        
+
 $tool_content = "";
 
 
@@ -89,212 +92,212 @@ $body_action = "onload=\"initEditor()\"";
 
 $tool_content .= <<<tContent1
 <div id="tool_operations">
-		<span class="operation">$langDateNow : $dateNow</span>
-	</div>
-	
+        <span class="operation">$langDateNow : $dateNow</span>
+    </div>
+
 tContent1;
 //$is_adminOfCourse=true;
 if ($is_adminOfCourse) {
 
-	if (isset($submit)&&$submit) {	
-		$date_selection = $fyear."-".$fmonth."-".$fday;	
-		$hour = $fhour.":".$fminute.":00";
-		if(isset($tout)&&$tout)
-		{
-			$sql="DELETE FROM agenda";
-		}
-		elseif(isset($id) && $id) {
-			$sql = "UPDATE agenda
-				SET titre='".trim($titre)."',
-				contenu='".trim($contenu)."',
-				day='$date_selection',
-				hour='$hour',
-				lasting='$lasting'
-				WHERE id='$id'";
-			unset($id);
-			unset($contenu);
-			unset($titre);
-		}
-			else
-		{
-	$sql = "INSERT INTO agenda (id, titre,contenu, day, hour, lasting) 
-		VALUES (NULL, '".trim($titre)."','".trim($contenu)."', '$date_selection','$hour', '$lasting')";
-			unset($id);
-			unset($contenu);
-			unset($titre);
-		}	
-		$result = mysql_query($sql);
-	}
-	elseif (isset($delete)&&$delete) {
-		$sql = "DELETE FROM agenda WHERE id=$id";
-		$result = mysql_query($sql);
-	}      
+    if (isset($submit)&&$submit) {
+        $date_selection = $fyear."-".$fmonth."-".$fday;
+        $hour = $fhour.":".$fminute.":00";
+        if(isset($tout)&&$tout)
+        {
+            $sql="DELETE FROM agenda";
+        }
+        elseif(isset($id) && $id) {
+            $sql = "UPDATE agenda
+                SET titre='".trim($titre)."',
+                contenu='".trim($contenu)."',
+                day='$date_selection',
+                hour='$hour',
+                lasting='$lasting'
+                WHERE id='$id'";
+            unset($id);
+            unset($contenu);
+            unset($titre);
+        }
+            else
+        {
+    $sql = "INSERT INTO agenda (id, titre,contenu, day, hour, lasting)
+        VALUES (NULL, '".trim($titre)."','".trim($contenu)."', '$date_selection','$hour', '$lasting')";
+            unset($id);
+            unset($contenu);
+            unset($titre);
+        }
+        $result = mysql_query($sql);
+    }
+    elseif (isset($delete)&&$delete) {
+        $sql = "DELETE FROM agenda WHERE id=$id";
+        $result = mysql_query($sql);
+    }
 
-	if (isset($id) && $id) {
-		$sql = "SELECT id, titre, contenu, day, hour, lasting FROM agenda WHERE id=$id";
-		$result= mysql_query($sql);
-		$myrow = mysql_fetch_array($result);
-		$id = $myrow["id"];
-		$titre = $myrow["titre"];
-		$contenu= $myrow["contenu"];
-		$hourAncient=$myrow["hour"];
-		$dayAncient=$myrow["day"];
-		$lastingAncient=$myrow["lasting"];
-	}
-	
-	//TODO: add this if logic as it was in the old system
+    if (isset($id) && $id) {
+        $sql = "SELECT id, titre, contenu, day, hour, lasting FROM agenda WHERE id=$id";
+        $result= mysql_query($sql);
+        $myrow = mysql_fetch_array($result);
+        $id = $myrow["id"];
+        $titre = $myrow["titre"];
+        $contenu= $myrow["contenu"];
+        $hourAncient=$myrow["hour"];
+        $dayAncient=$myrow["day"];
+        $lastingAncient=$myrow["lasting"];
+    }
+
+    //TODO: add this if logic as it was in the old system
  if (!isset($id)) {
- 	$id="";
+     $id="";
  }
- 
+
 $tool_content .= <<<tContentForm
 
 
-	<p>$langAddEvent</p>
+    <p>$langAddEvent</p>
 <form method="post" action="".$_SERVER[PHP_SELF]."">
-	<input type="hidden" name="id" value="$id">
-	<table width = "99%">
-	<thead>
-	<tr>
-		<th>$langDay</th>
-		<th>$langMonth</th>
-		<th>$langYear</th>
-		<th>$langHour</th>
-		<th>$langMinute</th>
-		<th>$langLasting</th>
-	</thead>
-	</tr>
+    <input type="hidden" name="id" value="$id">
+    <table width = "99%">
+    <thead>
+    <tr>
+        <th>$langDay</th>
+        <th>$langMonth</th>
+        <th>$langYear</th>
+        <th>$langHour</th>
+        <th>$langMinute</th>
+        <th>$langLasting</th>
+    </thead>
+    </tr>
 tContentForm;
 
-	$day	= date("d");
-	$month	= date("m");
-	$year	= date("Y");
-	$hours	= date("H");
-	$minutes= date("i");
+    $day	= date("d");
+    $month	= date("m");
+    $year	= date("Y");
+    $hours	= date("H");
+    $minutes= date("i");
 
-	if (isset($hourAncient) && $hourAncient) {
-		$hourAncient = split(":", $hourAncient);
-		$hours=$hourAncient[0];
-		$minutes=$hourAncient[1];
-	}
-	if (isset($dayAncient) && $dayAncient) {
-		$dayAncient= split("-",  $dayAncient);
-		$year=$dayAncient[0];
-		$month=$dayAncient[1];
-		$day=$dayAncient[2];
-	}
+    if (isset($hourAncient) && $hourAncient) {
+        $hourAncient = split(":", $hourAncient);
+        $hours=$hourAncient[0];
+        $minutes=$hourAncient[1];
+    }
+    if (isset($dayAncient) && $dayAncient) {
+        $dayAncient= split("-",  $dayAncient);
+        $year=$dayAncient[0];
+        $month=$dayAncient[1];
+        $day=$dayAncient[2];
+    }
 $tool_content .= "
 <tbody>
-	<tr>
-		<td>
-			<select name=\"fday\">
-				<option value=\"$day\">[$day]</option>";
-	
-	for ($d=1; $d<=31; $d++) $tool_content .= "<option value='$d'>$d</option>";
-	
-	$tool_content .= "
-	</select>
-	</td>
-	<td>
-	<select name=\"fmonth\">
-	<option value=\"$month\">[".$langMonthNames['long'][($month-1)]."]</option>";
-	
-	for ($i=1; $i<=12; $i++)
-		$tool_content .= "<option value='$i'>".$langMonthNames['long'][$i-1]."</option>";
-	$tool_content .= "
-	</select>
-	</td>
-	<td>
-	<select name=\"fyear\">
-	<option value=\"$year\">[$year]</option>";
-	
-	$currentyear = date("Y");
-	for ($y = $currentyear - 1; $y <= $currentyear + 2; $y++) {
-		$tool_content .= "<option value=\"$y\">$y</option>\n";
-	} 
-	
-	$tool_content .= "
-	</select>
-	</td>
-	<td>
-	<select name=\"fhour\">
-	<option value=\"$hours\">[$hours]</option>
-	<option value=\"--\">--</option>";
-	 
-	for ($h=0; $h<=24; $h++) 
-		$tool_content .= "<option value='$h'>$h</option>";
-	
-		$tool_content .= "
-	</select>
-	</td>
-	<td>
-	<select name=\"fminute\">
-	<option value=\"$minutes\">[$minutes]</option>
-	<option value=\"--\">--</option>";
-	
-	for ($m=0; $m<=55; $m=$m+5) $tool_content .=  "<option value='$m'>$m</option>";
-	
-	$tool_content .= "
-	</select>
-	</td>
-	<td><input type=\"text\" name=\"lasting\" value=\"".@$myrow['lasting']."\" size=\"8\"></td>
-	</tr>
-	</tbody>
-	</table><br>";
-	
-	$tool_content .="
-	<table width = \"99%\">
-	<thead>
-		<tr>
-			<th>$langTitle :</th>
-			<td colspan=\"5\"><input type=\"text\" size=\"60\" name=\"titre\" value=\"".@$titre."\"></td>
-		</tr>
-	</thead>	
-	</table><br>";
-	
-	if (!isset($contenu)){
-		$contenu="";
-	}
-	
-	$tool_content .= "
-	<table width = \"99%\">
-	<thead>
-		<tr>
-			<th colspan=6>$langDetail</th>
-		</tr>
-	</thead>
-	<tbody>
-		<tr>
-			<td colspan=\"6\"> 
-				<textarea id='ta' name='contenu' value='$contenu' rows='20' cols='78'>".$contenu."</textarea>
-			</td></tr>
-	</tbody>
-</table>		
-	<br><div align=\"right\"><input type=\"Submit\" name=\"submit\" value=\"$langOk\"> </div>
+    <tr>
+        <td>
+            <select name=\"fday\">
+                <option value=\"$day\">[$day]</option>";
+
+    for ($d=1; $d<=31; $d++) $tool_content .= "<option value='$d'>$d</option>";
+
+    $tool_content .= "
+    </select>
+    </td>
+    <td>
+    <select name=\"fmonth\">
+    <option value=\"$month\">[".$langMonthNames['long'][($month-1)]."]</option>";
+
+    for ($i=1; $i<=12; $i++)
+        $tool_content .= "<option value='$i'>".$langMonthNames['long'][$i-1]."</option>";
+    $tool_content .= "
+    </select>
+    </td>
+    <td>
+    <select name=\"fyear\">
+    <option value=\"$year\">[$year]</option>";
+
+    $currentyear = date("Y");
+    for ($y = $currentyear - 1; $y <= $currentyear + 2; $y++) {
+        $tool_content .= "<option value=\"$y\">$y</option>\n";
+    }
+
+    $tool_content .= "
+    </select>
+    </td>
+    <td>
+    <select name=\"fhour\">
+    <option value=\"$hours\">[$hours]</option>
+    <option value=\"--\">--</option>";
+
+    for ($h=0; $h<=24; $h++)
+        $tool_content .= "<option value='$h'>$h</option>";
+
+        $tool_content .= "
+    </select>
+    </td>
+    <td>
+    <select name=\"fminute\">
+    <option value=\"$minutes\">[$minutes]</option>
+    <option value=\"--\">--</option>";
+
+    for ($m=0; $m<=55; $m=$m+5) $tool_content .=  "<option value='$m'>$m</option>";
+
+    $tool_content .= "
+    </select>
+    </td>
+    <td><input type=\"text\" name=\"lasting\" value=\"".@$myrow['lasting']."\" size=\"8\"></td>
+    </tr>
+    </tbody>
+    </table><br>";
+
+    $tool_content .="
+    <table width = \"99%\">
+    <thead>
+        <tr>
+            <th>$langTitle :</th>
+            <td colspan=\"5\"><input type=\"text\" size=\"60\" name=\"titre\" value=\"".@$titre."\"></td>
+        </tr>
+    </thead>
+    </table><br>";
+
+    if (!isset($contenu)){
+        $contenu="";
+    }
+
+    $tool_content .= "
+    <table width = \"99%\">
+    <thead>
+        <tr>
+            <th colspan=6>$langDetail</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td colspan=\"6\">
+                <textarea id='ta' name='contenu' value='$contenu' rows='20' cols='78'>".$contenu."</textarea>
+            </td></tr>
+    </tbody>
+</table>
+    <br><div align=\"right\"><input type=\"Submit\" name=\"submit\" value=\"$langOk\"> </div>
 
 </form>
 <br><br><br><br>";
 
 
-	}
+    }
 
 /*---------------------------------------------
- *  End  of  prof only                         
+ *  End  of  prof only
  *-------------------------------------------*/
 $sens =" ASC";
-if (isset($_GET["sens"]) && $_GET["sens"]=="d") 
-{ 
-	$tool_content .=  "<a href=\"".$_SERVER['PHP_SELF']."?sens=\" >$langOldToNew</a>";
-	$sens=" DESC ";
+if (isset($_GET["sens"]) && $_GET["sens"]=="d")
+{
+    $tool_content .=  "<a href=\"".$_SERVER['PHP_SELF']."?sens=\" >$langOldToNew</a>";
+    $sens=" DESC ";
 }
 else
 {
-	$tool_content .=  "<p> <a href=\"".$_SERVER['PHP_SELF']."?sens=d\" >$langNewToOld</a></p>";
+    $tool_content .=  "<p> <a href=\"".$_SERVER['PHP_SELF']."?sens=d\" >$langNewToOld</a></p>";
 }
 $tool_content .=  "<table width=\"99%\" cellpadding=\"2\" cellspacing=\"0\" border=\"0\">";
 $tool_content .=  "<thead><tr><th>$langEvents</th>";
 if ($is_adminOfCourse) {
-	$tool_content .=  "<th>$langActions</th>";
+    $tool_content .=  "<th>$langActions</th>";
 }
 $tool_content .= "</tr></thead>";
 $tool_content .= "<tbody>";
@@ -302,87 +305,87 @@ $numLine=0;
 $result = mysql_query("SELECT id, titre, contenu, day, hour, lasting FROM agenda ORDER BY day ".$sens.", hour ".$sens,$db);
 $barreMois ="";
 $nowBarShowed = FALSE;
-while ($myrow = mysql_fetch_array($result)) 
-{	
-	$contenu = $myrow["contenu"];		
-	$contenu = nl2br($contenu);
-	$contenu = make_clickable($contenu);
-	if (!$nowBarShowed)
-	{
+while ($myrow = mysql_fetch_array($result))
+{
+    $contenu = $myrow["contenu"];
+    $contenu = nl2br($contenu);
+    $contenu = make_clickable($contenu);
+    if (!$nowBarShowed)
+    {
 // Following order
-		if (((strtotime($myrow["day"]." ".$myrow["hour"]) > time()) && ($sens==" ASC")) || 
-			((strtotime($myrow["day"]." ".$myrow["hour"]) < time()) && ($sens==" DESC "))) {
-			if ($barreMois!=date("m",time())) {
-				$barreMois=date("m",time());
-				$tool_content .=  "
-				<tr>
-					<td class=\"month\" colspan=\"2\" valign=\"top\">
-						".$langCalendar."&nbsp;".ucfirst(claro_format_locale_date("%B %Y",time()))."
-					</td>
-				</tr>";
-			}
-			$nowBarShowed = TRUE;
-			$tool_content .=  "<tr> 
-					<td colspan=2 class=\"today\">
-						
-							<b>$langNow : $dateNow</b>
-						
-					</td>
-					
-				</tr>";
-		}
-	}
-	if ($barreMois!=date("m",strtotime($myrow["day"])))
-	{
-		$barreMois=date("m",strtotime($myrow["day"]));
-		$tool_content .=  "<tr><td class=\"month\" colspan=\"2\" valign=\"top\">
-			".$langCalendar."&nbsp;".ucfirst(claro_format_locale_date("%B %Y",strtotime($myrow["day"])))."
-			</td></tr>";
-	}
-	$tool_content .=  "
+        if (((strtotime($myrow["day"]." ".$myrow["hour"]) > time()) && ($sens==" ASC")) ||
+            ((strtotime($myrow["day"]." ".$myrow["hour"]) < time()) && ($sens==" DESC "))) {
+            if ($barreMois!=date("m",time())) {
+                $barreMois=date("m",time());
+                $tool_content .=  "
+                <tr>
+                    <td class=\"month\" colspan=\"2\" valign=\"top\">
+                        ".$langCalendar."&nbsp;".ucfirst(claro_format_locale_date("%B %Y",time()))."
+                    </td>
+                </tr>";
+            }
+            $nowBarShowed = TRUE;
+            $tool_content .=  "<tr>
+                    <td colspan=2 class=\"today\">
+
+                            <b>$langNow : $dateNow</b>
+
+                    </td>
+
+                </tr>";
+        }
+    }
+    if ($barreMois!=date("m",strtotime($myrow["day"])))
+    {
+        $barreMois=date("m",strtotime($myrow["day"]));
+        $tool_content .=  "<tr><td class=\"month\" colspan=\"2\" valign=\"top\">
+            ".$langCalendar."&nbsp;".ucfirst(claro_format_locale_date("%B %Y",strtotime($myrow["day"])))."
+            </td></tr>";
+    }
+    $tool_content .=  "
 <!-- Date -->
-	";
-	if($numLine%2 == 0) 	
-		$tool_content .=  "<tr>";
-	elseif($numLine%2 == 1)     	
-		$tool_content .=  "<tr class=\"odd\">";
-    if ($is_adminOfCourse) 
-		$tool_content .=  "<td valign=\"top\">";
-	else
-		$tool_content .=  "<td valign=\"top\" colspan=\"2\">";
+    ";
+    if($numLine%2 == 0)
+        $tool_content .=  "<tr>";
+    elseif($numLine%2 == 1)
+        $tool_content .=  "<tr class=\"odd\">";
+    if ($is_adminOfCourse)
+        $tool_content .=  "<td valign=\"top\">";
+    else
+        $tool_content .=  "<td valign=\"top\" colspan=\"2\">";
 
-	$tool_content .=  "<p>".ucfirst(claro_format_locale_date($dateFormatLong,strtotime($myrow["day"])))."
-		/ $langHour: 
-		".ucfirst(date("H:i",strtotime($myrow["hour"])))." ";
+    $tool_content .=  "<p>".ucfirst(claro_format_locale_date($dateFormatLong,strtotime($myrow["day"])))."
+        / $langHour:
+        ".ucfirst(date("H:i",strtotime($myrow["hour"])))." ";
 
-	if ($myrow["lasting"] !="") {
-		if ($myrow["lasting"] == 1)
-			$message = $langHour;
-		else 
-			$message = $langHours;
-		$tool_content .=  "<br>".$langLasting.": ".$myrow["lasting"]." ".$message."";
-	}
+    if ($myrow["lasting"] !="") {
+        if ($myrow["lasting"] == 1)
+            $message = $langHour;
+        else
+            $message = $langHours;
+        $tool_content .=  "<br>".$langLasting.": ".$myrow["lasting"]." ".$message."";
+    }
 
-	$tool_content .=  "<br>
-		<b>".$myrow["titre"]."</b>
-		<br>$contenu</p></td>";
-	
-	//agenda event functions
-	//added icons next to each function
-	//(evelthon, 12/05/2006)
+    $tool_content .=  "<br>
+        <b>".$myrow["titre"]."</b>
+        <br>$contenu</p></td>";
+
+    //agenda event functions
+    //added icons next to each function
+    //(evelthon, 12/05/2006)
 //	<span class="help">{HELP_LINK_ICON} {HELP_LINK}</span>
-	if ($is_adminOfCourse) { 
-		$tool_content .=  "<td >
-		<a href=\"$_SERVER[PHP_SELF]?id=".$myrow["id"]."\">
-			<img src=\"../../images/edit.gif\" border=\"0\" alt=\"".$langModify."\"></a>
-				
-		<a href=\"$_SERVER[PHP_SELF]?id=".$myrow[0]."&delete=yes\">
-			<img src=\"../../images/delete.gif\" border=\"0\" alt=\"".$langDelete."\"></a>
-			
-		</td>";
-	}
-	$tool_content .=  "</tr>";
-	$numLine++;
+    if ($is_adminOfCourse) {
+        $tool_content .=  "<td >
+        <a href=\"$_SERVER[PHP_SELF]?id=".$myrow["id"]."\">
+            <img src=\"../../images/edit.gif\" border=\"0\" alt=\"".$langModify."\"></a>
+
+        <a href=\"$_SERVER[PHP_SELF]?id=".$myrow[0]."&delete=yes\">
+            <img src=\"../../images/delete.gif\" border=\"0\" alt=\"".$langDelete."\"></a>
+
+        </td>";
+    }
+    $tool_content .=  "</tr>";
+    $numLine++;
 } 	// while
 $tool_content .= "</tbody>";
 $tool_content .=  "</table>";
