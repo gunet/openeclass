5f92e781c002d8a597fb4c9c98e1b5ed69f70e26
Thanos Kyritsis <atkyritsis@upnet.gr>
2006-06-13 13:43:47 +0000

    adding scorm2004 export template files

--- modules/learnPath/export/APIWrapper.js
+++ modules/learnPath/export/APIWrapper.js
@@ -44,23 +44,20 @@
 **      functions as follows:
 **
 **    javascript:
-**          var result = doLMSInitialize();
+**          var result = doInitialize();
 **          if (result != true) 
 **          {
 **             // handle error
 **          }
 **
-**    authorware
-**          result := ReadURL("javascript:doLMSInitialize()", 100)
+**    authorware:
+**          result := ReadURL("javascript:doInitialize()", 100)
+**
+**    director:
+**          result = externalEvent("javascript:doInitialize()")
+**
 **
 *******************************************************************************/
-
-/*##############################################################################
-##                                                                            ##
-##  The first part contains intermediate-level functions.                     ##
-##  Higher-level and helpers functions are located at the end of this file.   ##
-##                                                                            ##
-##############################################################################*/
 
 var _Debug = false;  // set this to false to turn debugging off
                      // and get rid of those annoying alert boxes.
@@ -68,16 +65,29 @@ var _Debug = false;  // set this to false to turn debugging off
 // Define exception/error codes
 var _NoError = 0;
 var _GeneralException = 101;
-var _ServerBusy = 102;
-var _InvalidArgumentError = 201;
-var _ElementCannotHaveChildren = 202;
-var _ElementIsNotAnArray = 203;
-var _NotInitialized = 301;
-var _NotImplementedError = 401;
-var _InvalidSetValue = 402;
-var _ElementIsReadOnly = 403;
-var _ElementIsWriteOnly = 404;
-var _IncorrectDataType = 405;
+var _GeneralInitializationFailure = 102;
+var _AlreadyInitialized = 103;
+var _ContentInstanceTerminated = 104;
+var _GeneralTerminationFailure = 111;
+var _TerminationBeforeInitialization = 112;
+var _TerminationAfterTermination = 113;
+var _ReceivedDataBeforeInitialization = 122;
+var _ReceivedDataAfterTermination = 123;
+var _StoreDataBeforeInitialization = 132;
+var _StoreDataAfterTermination = 133;
+var _CommitBeforeInitialization = 142;
+var _CommitAfterTermination = 143;
+var _GeneralArgumentError = 201;
+var _GeneralGetFailure = 301;
+var _GeneralSetFailure = 351;
+var _GeneralCommitFailure = 391;
+var _UndefinedDataModelElement = 401;
+var _UnimplementedDataModelElement = 402;
+var _DataModelElementValueNotInitialized = 403;
+var _DataModelElementIsReadOnly = 404;
+var _DataModelElementIsWriteOnly = 405;
+var _DataModelElementTypeMismatch = 406;
+var _DataModelElementValueOutOfRange = 407;
 
 
 // local variable definitions
@@ -88,28 +98,26 @@ var findAPITries = 0;
 
 /*******************************************************************************
 **
-** Function: doLMSInitialize()
+** Function: doInitialize()
 ** Inputs:  None
 ** Return:  CMIBoolean true if the initialization was successful, or
 **          CMIBoolean false if the initialization failed.
 **
 ** Description:
-** Initialize communication with LMS by calling the LMSInitialize
+** Initialize communication with LMS by calling the Initialize
 ** function which will be implemented by the LMS.
 **
 *******************************************************************************/
-function doLMSInitialize()
+function doInitialize()
 {
    var api = getAPIHandle();
    if (api == null)
    {
-	if(_Debug){
-	      alert("Unable to locate the LMS's API Implementation.\nLMSInitialize was not successful.");
-	}
+      alert("Unable to locate the LMS's API Implementation.\nInitialize was not successful.");
       return "false";
    }
 
-   var result = api.LMSInitialize("");
+   var result = api.Initialize("");
 
    if (result.toString() != "true")
    {
@@ -121,31 +129,29 @@ function doLMSInitialize()
 
 /*******************************************************************************
 **
-** Function doLMSFinish()
+** Function doTerminate()
 ** Inputs:  None
 ** Return:  CMIBoolean true if successful
 **          CMIBoolean false if failed.
 **
 ** Description:
-** Close communication with LMS by calling the LMSFinish
+** Close communication with LMS by calling the Terminate
 ** function which will be implemented by the LMS
 **
 *******************************************************************************/
-function doLMSFinish()
-{
+function doTerminate()
+{  
    var api = getAPIHandle();
    if (api == null)
    {
-	if(_Debug){
-	      alert("Unable to locate the LMS's API Implementation.\nLMSFinish was not successful.");
-	}
+      alert("Unable to locate the LMS's API Implementation.\nTerminate was not successful.");
       return "false";
    }
    else
    {
-      // call the LMSFinish function that should be implemented by the API
+      // call the Terminate function that should be implemented by the API
 
-      var result = api.LMSFinish("");
+      var result = api.Terminate("");
       if (result.toString() != "true")
       {
          var err = ErrorHandler();
@@ -158,7 +164,7 @@ function doLMSFinish()
 
 /*******************************************************************************
 **
-** Function doLMSGetValue(name)
+** Function doGetValue(name)
 ** Inputs:  name - string representing the cmi data model defined category or
 **             element (e.g. cmi.core.student_id)
 ** Return:  The value presently assigned by the LMS to the cmi data model
@@ -166,30 +172,26 @@ function doLMSFinish()
 **       input value.
 **
 ** Description:
-** Wraps the call to the LMS LMSGetValue method
+** Wraps the call to the GetValue method
 **
 *******************************************************************************/
-function doLMSGetValue(name)
+function doGetValue(name)
 {
    var api = getAPIHandle();
    if (api == null)
    {
-	if(_Debug){
-	      alert("Unable to locate the LMS's API Implementation.\nLMSGetValue was not successful.");
-	}
+      alert("Unable to locate the LMS's API Implementation.\nGetValue was not successful.");
       return "";
    }
    else
    {
-      var value = api.LMSGetValue(name);
-      var errCode = api.LMSGetLastError().toString();
+      var value = api.GetValue(name);
+      var errCode = api.GetLastError().toString();
       if (errCode != _NoError)
       {
          // an error was encountered so display the error description
-         var errDescription = api.LMSGetErrorString(errCode);
-	if(_Debug){
-         	alert("LMSGetValue("+name+") failed. \n"+ errDescription);
-	}
+         var errDescription = api.GetErrorString(errCode);
+         alert("GetValue("+name+") failed. \n"+ errDescription);
          return "";
       }
       else
@@ -202,30 +204,27 @@ function doLMSGetValue(name)
 
 /*******************************************************************************
 **
-** Function doLMSSetValue(name, value)
+** Function doSetValue(name, value)
 ** Inputs:  name -string representing the data model defined category or element
 **          value -the value that the named element or category will be assigned
 ** Return:  CMIBoolean true if successful
 **          CMIBoolean false if failed.
 **
 ** Description:
-** Wraps the call to the LMS LMSSetValue function
+** Wraps the call to the SetValue function
 **
 *******************************************************************************/
-function doLMSSetValue(name, value)
+function doSetValue(name, value)
 {
    var api = getAPIHandle();
    if (api == null)
    {
-	if(_Debug){
-
-		alert("Unable to locate the LMS's API Implementation.\nLMSSetValue was not successful.");
-	}      
-	return;
+      alert("Unable to locate the LMS's API Implementation.\nSetValue was not successful.");
+      return;
    }
    else
    {
-      var result = api.LMSSetValue(name, value);
+      var result = api.SetValue(name, value);
       if (result.toString() != "true")
       {
          var err = ErrorHandler();
@@ -237,27 +236,25 @@ function doLMSSetValue(name, value)
 
 /*******************************************************************************
 **
-** Function doLMSCommit()
+** Function doCommit()
 ** Inputs:  None
 ** Return:  None
 **
 ** Description:
-** Call the LMSCommit function 
+** Call the Commit function 
 **
 *******************************************************************************/
-function doLMSCommit()
+function doCommit()
 {
    var api = getAPIHandle();
    if (api == null)
    {
-	if(_Debug){
-      		alert("Unable to locate the LMS's API Implementation.\nLMSCommit was not successful.");
-	}
+      alert("Unable to locate the LMS's API Implementation.\nCommit was not successful.");
       return "false";
    }
    else
    {
-      var result = api.LMSCommit("");
+      var result = api.Commit("");
       if (result != "true")
       {
          var err = ErrorHandler();
@@ -269,55 +266,51 @@ function doLMSCommit()
 
 /*******************************************************************************
 **
-** Function doLMSGetLastError()
+** Function doGetLastError()
 ** Inputs:  None
 ** Return:  The error code that was set by the last LMS function call
 **
 ** Description:
-** Call the LMSGetLastError function 
+** Call the GetLastError function 
 **
 *******************************************************************************/
-function doLMSGetLastError()
+function doGetLastError()
 {
    var api = getAPIHandle();
    if (api == null)
    {
-	if(_Debug){
-	      alert("Unable to locate the LMS's API Implementation.\nLMSGetLastError was not successful.");
-	}
+      alert("Unable to locate the LMS's API Implementation.\nGetLastError was not successful.");
       //since we can't get the error code from the LMS, return a general error
       return _GeneralError;
    }
 
-   return api.LMSGetLastError().toString();
+   return api.GetLastError().toString();
 }
 
 /*******************************************************************************
 **
-** Function doLMSGetErrorString(errorCode)
+** Function doGetErrorString(errorCode)
 ** Inputs:  errorCode - Error Code
 ** Return:  The textual description that corresponds to the input error code
 **
 ** Description:
-** Call the LMSGetErrorString function 
+** Call the GetErrorString function 
 **
 ********************************************************************************/
-function doLMSGetErrorString(errorCode)
+function doGetErrorString(errorCode)
 {
    var api = getAPIHandle();
    if (api == null)
    {
-	if(_Debug){
-	      alert("Unable to locate the LMS's API Implementation.\nLMSGetErrorString was not successful.");
-	}
+      alert("Unable to locate the LMS's API Implementation.\nGetErrorString was not successful.");
    }
 
-   return api.LMSGetErrorString(errorCode).toString();
+   return api.GetErrorString(errorCode).toString();
 }
 
 /*******************************************************************************
 **
-** Function doLMSGetDiagnostic(errorCode)
+** Function doGetDiagnostic(errorCode)
 ** Inputs:  errorCode - Error Code(integer format), or null
 ** Return:  The vendor specific textual description that corresponds to the 
 **          input error code
@@ -326,56 +319,15 @@ function doLMSGetErrorString(errorCode)
 ** Call the LMSGetDiagnostic function
 **
 *******************************************************************************/
-function doLMSGetDiagnostic(errorCode)
+function doGetDiagnostic(errorCode)
 {
    var api = getAPIHandle();
    if (api == null)
    {
-	if(_Debug){
-	      alert("Unable to locate the LMS's API Implementation.\nLMSGetDiagnostic was not successful.");
-   	}
-    }
-
-   return api.LMSGetDiagnostic(errorCode).toString();
-}
-
-/*******************************************************************************
-**
-** Function LMSIsInitialized()
-** Inputs:  none
-** Return:  true if the LMS API is currently initialized, otherwise false
-**
-** Description:
-** Determines if the LMS API is currently initialized or not.
-**
-*******************************************************************************/
-function LMSIsInitialized()
-{
-   // there is no direct method for determining if the LMS API is initialized
-   // for example an LMSIsInitialized function defined on the API so we'll try
-   // a simple LMSGetValue and trap for the LMS Not Initialized Error
-
-   var api = getAPIHandle();
-   if (api == null)
-   {
-	if(_Debug){
-	      alert("Unable to locate the LMS's API Implementation.\nLMSIsInitialized() failed.");
-	}
-     	 return false;
+      alert("Unable to locate the LMS's API Implementation.\nGetDiagnostic was not successful.");
    }
-   else
-   {
-      var value = api.LMSGetValue("cmi.core.student_name");
-      var errCode = api.LMSGetLastError().toString();
-      if (errCode == _NotInitialized)
-      {
-         return false;
-      }
-      else
-      {
-         return true;
-      }
-   }
+
+   return api.GetDiagnostic(errorCode).toString();
 }
 
 /*******************************************************************************
@@ -395,30 +347,26 @@ function ErrorHandler()
    var api = getAPIHandle();
    if (api == null)
    {
-	if(_Debug){
-	      alert("Unable to locate the LMS's API Implementation.\nCannot determine LMS error code.");
-	}
+      alert("Unable to locate the LMS's API Implementation.\nCannot determine LMS error code.");
       return;
    }
 
    // check for errors caused by or from the LMS
-   var errCode = api.LMSGetLastError().toString();
-   if (errCode != _NoError)
+   var errCode = api.GetLastError().toString();
+   if (errCode != _NoError && errCode != _AlreadyInitialized )
    {
       // an error was encountered so display the error description
-      var errDescription = api.LMSGetErrorString(errCode);
+      var errDescription = api.GetErrorString(errCode);
 
       if (_Debug == true)
       {
          errDescription += "\n";
-         errDescription += api.LMSGetDiagnostic(null);
-         // by passing null to LMSGetDiagnostic, we get any available diagnostics
+         errDescription += api.GetDiagnostic(null);
+         // by passing null to GetDiagnostic, we get any available diagnostics
          // on the previous error.
       }
 
-	if(_Debug){
-	      alert(errDescription);
-	}
+      alert(errDescription);
    }
 
    return errCode;
@@ -458,22 +406,20 @@ function getAPIHandle()
 *******************************************************************************/
 function findAPI(win)
 {
-   while ((win.API == null) && (win.parent != null) && (win.parent != win))
+   while ((win.API_1484_11 == null) && (win.parent != null) && (win.parent != win))
    {
       findAPITries++;
-      // Note: 7 is an arbitrary number, but should be more than sufficient
-      if (findAPITries > 7) 
+      
+      if (findAPITries > 500) 
       {
-	if(_Debug){
-         	alert("Error finding API -- too deeply nested.");
-	}
+         alert("Error finding API -- too deeply nested.");
          return null;
       }
       
       win = win.parent;
 
    }
-   return win.API;
+   return win.API_1484_11;
 }
 
 
@@ -499,136 +445,9 @@ function getAPI()
    }
    if (theAPI == null)
    {
-	if(_Debug){
-	      alert("Unable to find an API adapter");
-	}
+      alert("Unable to find an API adapter");
    }
    return theAPI
 }
 
-/*##############################################################################
-##                                                                            ##
-##  Higher-level and helpers functions                                        ##
-##                                                                            ##
-##############################################################################*/
-
-var startDate = 0;
-var exitPageStatus;
-
-function loadPage()
-{
-    var result = doLMSInitialize();
-    var status = doLMSGetValue("cmi.core.lesson_status");
-    if (status == "not attempted")
-    {
-        doLMSSetValue("cmi.core.lesson_status", "incomplete");
-    }
-    
-    exitPageStatus = false;
-    startTimer();
-}
-
-function doQuit(status)
-{
-    /* computeTime();*/
-    exitPageStatus = true;
-    
-    var result;
-    result = doLMSSetValue("cmi.core.lesson_status", status);
-    result = doLMSCommit();
-    
-    result = doLMSGetValue("cmi.core.lesson_status");
-    result = doLMSFinish();
-}
-
-/*
-Function used for non-scorm content, like html pages, or pdf, etc.
-Since this kind of document does not embed the javascript calls needed to
-update the SCO's status.
-*/
-function immediateComplete()
-{
-    loadPage();
-    doQuit('completed');
-}
-    
-
-/*
-Time tracking functions.
-*/
-function startTimer()
-{
-    startDate = new Date().getTime();
-}
-
-function computeTime()
-{
-    if (startDate != 0)
-    {
-        var currentDate = new Date().getTime();
-        var elapsedSeconds = ( (currentDate - startDate) / 1000 );
-        var formattedTime = convertTotalSeconds( elapsedSeconds );
-    }
-    else
-    {
-        formattedTime = "00:00:00.0";
-    }
-    
-    doLMSSetValue( "cmi.core.session_time", formattedTime );
-}
-
-/*******************************************************************************
-** this function will convert seconds into hours, minutes, and seconds in
-** CMITimespan type format - HHHH:MM:SS.SS (Hours has a max of 4 digits &
-** Min of 2 digits
-*******************************************************************************/
-function convertTotalSeconds(ts)
-{
-   var sec = (ts % 60);
-
-   ts -= sec;
-   var tmp = (ts % 3600);  //# of seconds in the total # of minutes
-   ts -= tmp;              //# of seconds in the total # of hours
-
-   // convert seconds to conform to CMITimespan type (e.g. SS.00)
-   sec = Math.round(sec*100)/100;
-   
-   var strSec = new String(sec);
-   var strWholeSec = strSec;
-   var strFractionSec = "";
-
-   if (strSec.indexOf(".") != -1)
-   {
-      strWholeSec =  strSec.substring(0, strSec.indexOf("."));
-      strFractionSec = strSec.substring(strSec.indexOf(".")+1, strSec.length);
-   }
-   
-   if (strWholeSec.length < 2)
-   {
-      strWholeSec = "0" + strWholeSec;
-   }
-   strSec = strWholeSec;
-   
-   if (strFractionSec.length)
-   {
-      strSec = strSec+ "." + strFractionSec;
-   }
-
-
-   if ((ts % 3600) != 0 )
-      var hour = 0;
-   else var hour = (ts / 3600);
-   if ( (tmp % 60) != 0 )
-      var min = 0;
-   else var min = (tmp / 60);
-
-   if ((new String(hour)).length < 2)
-      hour = "0"+hour;
-   if ((new String(min)).length < 2)
-      min = "0"+min;
-
-   var rtnVal = hour+":"+min+":"+strSec;
-
-   return rtnVal;
-}
 
