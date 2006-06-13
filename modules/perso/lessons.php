039719fa77cd59275cd6950c40c96e48dee80fc0
Evelthon Prodromou <eprodromou@upnet.gr>
2006-06-13 09:35:58 +0000

    Fixed fetching of user memory

--- modules/perso/lessons.php
+++ modules/perso/lessons.php
@@ -26,16 +26,25 @@
 function  getUserLessonInfo($uid, $type) {
 	//	?$userID=$uid;
 	global $mysqlMainDb;
+	
+//	TODO: add the new fields for memory in the db
+	
 	$user_courses = "SELECT
 								cours.code , cours.fake_code , 
 	                                           cours.intitule , cours.titulaires ,
 	                                           cours.languageCourse ,
-	                                           cours_user.statut 
+	                                           cours_user.statut,
+	                                           user.perso, 
+	                                           user.announce_flag,
+	                                           user.doc_flag,
+	                                           user.forum_flag
 
-	                                   FROM    cours, cours_user
+	                                   FROM    cours, cours_user, user
 	                                  
 	                                  WHERE cours.code = cours_user.code_cours
-	                                  AND   cours_user.user_id = '".$uid."' ";
+	                                  AND   cours_user.user_id = '".$uid."' 
+	                                  AND   user.user_id = '".$uid."'
+	                                  ";
 
 
 	$mysql_query_result = db_query($user_courses, $mysqlMainDb);
@@ -51,9 +60,29 @@ function  getUserLessonInfo($uid, $type) {
 		$lesson_code[$repeat_val]		= $mycourses[0]; //lesson code used in tables
 		$lesson_professor[$repeat_val]	= $mycourses[3]; //lesson professor
 		$lesson_statut[$repeat_val]		= $mycourses[5];//statut (user|prof)
+		/*$lesson_announce_f[$repeat_val]	= eregi_replace("-", " ", $mycourses[7]);//announcements flag
+		$lesson_doc_f[$repeat_val]		= eregi_replace("-", " ", $mycourses[8]);//documents flag
+		$lesson_forum_f[$repeat_val]	= eregi_replace("-", " ", $mycourses[9]);//forum flag*/
 
 		$repeat_val++;
 	}
+	
+	$memory = "SELECT
+				user.announce_flag, user.doc_flag, user.forum_flag
+				FROM user
+				WHERE user.user_id = '".$uid."'
+				";
+	$memory_result = db_query($memory, $mysqlMainDb);
+	
+	while ($my_memory_result = mysql_fetch_row($memory_result)) {
+		dumpArray($my_memory_result);
+		$lesson_announce_f = eregi_replace("-", " ", $my_memory_result[0]);
+		$lesson_doc_f = eregi_replace("-", " ", $my_memory_result[1]);
+		$lesson_forum_f = eregi_replace("-", " ", $my_memory_result[2]);
+	}
+		
+
+	
 	$max_repeat_val = $repeat_val;
 
 	$ret_val[0] = $max_repeat_val;
@@ -61,7 +90,11 @@ function  getUserLessonInfo($uid, $type) {
 	$ret_val[2]	= $lesson_code;
 	$ret_val[3] = $lesson_professor;
 	$ret_val[4] = $lesson_statut;
-
+	$ret_val[5] = $lesson_announce_f;
+	$ret_val[6] = $lesson_doc_f;
+	$ret_val[7] = $lesson_forum_f;
+	
+//dumpArray($ret_val);
 	//check what sort of data should be returned
 	if($type == "html") {
 		return array($ret_val,htmlInterface($ret_val));
@@ -152,12 +185,6 @@ function LessonsView($param = null)
 	return $tree_data;
 
 }
-
-function setDefault($value, $default)
-{
-	return (isset($value)) ? $value : $default;
-}
-
 
 
 ?>
