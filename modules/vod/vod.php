f7ae9ab7d425e8b81f013c500e4fa5efc3227f30
Dimitris Tsachalis <ditsa@ccf.auth.gr>
2006-06-23 14:12:04 +0000

    *** empty log message ***

---
+++ modules/vod/vod.php
@@ -0,0 +1,17 @@
+<?
+//ÌåôáôñïðÞ ôïõ åñãáëåßïõ ãéá íá ÷ñçóéìïðïéåß ôï baseTheme
+$require_current_course = TRUE;
+$langFiles = 'conference';
+$require_help = TRUE;
+$helpTopic = 'User';
+include '../../include/baseTheme.php';
+
+
+$nameTools = "vod";
+
+
+
+
+draw($tool_content, 2, 'user', $head_content,$body_action);
+
+?>
