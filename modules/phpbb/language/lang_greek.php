b0371d92e97860a7e931fe4067f65d5823c6ac75
Artemios Vogiatzis <bogart@upatras.gr>
2006-06-23 14:58:20 +0000

    New file. Translation in Greek language of phpBB interface.

---
+++ modules/phpbb/language/lang_greek.php
@@ -0,0 +1,355 @@
+<?php
+/***************************************************************************
+ *                           lang_greek.php  -  description
+ *                              -------------------
+ *     begin                : Thu Jun 22 2006
+ *	    copyright            : (C) 2006- Artemios G. Voyiatzis
+ *  	 email                : bogart@upatras.gr
+ *
+ *     $Id$
+ *
+ *  ***************************************************************************/
+
+/***************************************************************************
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ ***************************************************************************/
+
+$l_forum 	= "Φόρουμ";
+$l_forums	= "Φόρα";
+$l_topic	= "Θέμα";
+$l_topics 	= "Θέματα";
+$l_replies	= "Απαντήσεις";
+$l_poster	= "Αποστολέας";
+$l_author	= "Συγγραφέας";
+$l_views	= "Θεάσεις";
+$l_post 	= "Δημοσίευση";
+$l_posts 	= "Δημοσιεύσεις";
+$l_message	= "Μήνυμα";
+$l_messages	= "Μηνύματα";
+$l_subject	= "Θέμα";
+$l_body		= "$l_message Σώμα";
+$l_from		= "Από";   // Message from
+$l_moderator 	= "Συντονιστής";
+$l_username 	= "Αναγνωριστικό";
+$l_password 	= "Συνθηματικό";
+$l_email 	= "Email";
+$l_emailaddress	= "Διεύθυνση Email";
+$l_preferences	= "Προτιμήσεις";
+
+$l_anonymous	= "Ανώνυμος";  // Post
+$l_guest	= "Επισκέπτης"; // Whosonline
+$l_noposts	= "Δεν υπάρχουν $l_posts";
+$l_joined	= "Εγγράφηκε";
+$l_gotopage	= "Μετάβαση στη σελίδα";
+$l_nextpage 	= "Επόμενη σελίδα";
+$l_prevpage     = "Προηγούμενη σελίδα";
+$l_go		= "Μετάβαση";
+$l_selectforum	= "Επιλογή $l_forum";
+
+$l_date		= "Ημερομηνία";
+$l_number	= "Αριθμός";
+$l_name		= "Όνομα";
+$l_options 	= "Επιλογές";
+$l_submit	= "Υποβολή";
+$l_confirm 	= "Επιβεβαίωση";
+$l_enter 	= "Είσοδος";
+$l_by		= "από"; // Posted by
+$l_ondate	= "την"; // This message is edited by: $username on $date
+$l_new          = "Νέα";
+
+$l_html		= "HTML";
+$l_bbcode	= "BBcode";
+$l_smilies	= "Smilies";
+$l_on		= "On";
+$l_off		= "Off";
+$l_yes		= "Ναι";
+$l_no		= "Όχι";
+
+$l_click 	= "Πατήστε";
+$l_here 	= "εδώ";
+$l_toreturn	= "για επιστροφή";
+$l_returnindex	= "$l_toreturn στο ευρετήριο του φόρουμ";
+$l_returntopic	= "$l_toreturn στη λίστα θεμάτων του φόρουμ.";
+
+$l_error	= "Σφάλμα";
+$l_tryagain	= "Παρακαλώ επιστρέψτε πίσω και προσπαθήστε ξανά.";
+$l_mismatch 	= "Τα συνθηματικά δεν ταιριάζουν μεταξύ τους.";
+$l_userremoved 	= "Αυτός ο χρήστης αφαιρέθηκε από τη Βάση Χρηστών";
+$l_wrongpass	= "Εισάγατε εσφαλμένο συνθηματικό.";
+$l_userpass	= "Παρακαλώ εισάγετε το αναγνωριστικό και συνθηματικό σας.";
+$l_banned 	= "Έχετε αποκλειστεί από αυτό το φόρουμ. Επικοινωνήστε με το Διαχειριστή Συστήματος αν έχετε κάποια απορία.";
+$l_enterpassword= "Πρέπει να εισάγετε το συνθηματικό σας";
+
+$l_nopost	= "Δεν έχετε πρόσβαση για να δημοσιεύσετε σε αυτό το φόρουμ.";
+$l_noread	= "Δεν έχετε πρόσβαση για να αναγνώσετε αυτό το φόρουμ.";
+
+$l_lastpost 	= "Τελευταία $l_post";
+$l_sincelast	= "από την τελευταία σας επίσκεψη";
+$l_newposts 	= "Νέες $l_posts $l_sincelast";
+$l_nonewposts 	= "Καμία νέα $l_post $l_sincelast";
+
+// Index page
+$l_indextitle	= "Ευρετήριο φόρουμ";
+
+// Members and profile
+$l_profile	= "Προφίλ";
+$l_register	= "Εγγραφή";
+$l_onlyreq 	= "Απαιτείται μόνο αν έχει αλλαχθεί";
+$l_location 	= "Από";
+$l_viewpostuser	= "Θέαση δημοσιεύσεων χρήστη";
+$l_perday       = "$l_messages ανά ημέρα";
+$l_oftotal      = "από σύνολο";
+$l_url 		= "URL";
+$l_icq 		= "ICQ";
+$l_icqnumber	= "Αριθμός ICQ";
+$l_icqadd	= "Προσθήκη";
+$l_icqpager	= "Pager";
+$l_aim 		= "AIM";
+$l_yim 		= "YIM";
+$l_yahoo 	= "Yahoo Messenger";
+$l_msn 		= "MSN";
+$l_messenger 	= "MSN Messenger";
+$l_website 	= "Διεύθυνση Ιστότοπου";
+$l_occupation 	= "Απασχόληση";
+$l_interests 	= "Ενδιαφέροντα";
+$l_signature 	= "Υπογραφή";
+$l_sigexplain 	= "Αυτό είναι ένα μπλοκ κειμένου που μπορεί να προστεθεί στις δημοσιεύσεις που κάνετε.<BR>255 χαρακτήρες το πολύ!";
+$l_usertaken	= "Το $l_username που επιλέξατε χρησιμοποιείται.";
+$l_userdisallowed= "Το $l_username που επιλέξατε έχει απαγορευθεί από το διαχειριστή. $l_tryagain";
+$l_infoupdated	= "Οι πληροφορίες σας ενημερώθηκαν";
+$l_publicmail	= "Επίτρεψη σε άλλους χρήστες να βλέπουν την $l_emailaddress μου";
+$l_itemsreq	= "Τα στοιχεία που σημειώνονται με * απαιτούνται";
+
+// Viewforum
+$l_viewforum	= "Θέαση φόρουμ";
+$l_notopics	= "Δεν υπάρχουν θέματα σε αυτό το φόρουμ. Μπορείτε να δημοσιεύσετε ένα.";
+$l_hotthres	= "Περισσότερες από $hot_threshold $l_posts";
+$l_islocked	= "$l_topic έχει κλειδωθεί (Δεν μπορούν να γίνουν νέες $l_posts σε αυτό)";
+$l_moderatedby	= "Συντονίζεται από";
+
+// Private forums
+$l_privateforum	= "Αυτό είναι ένα <b>ιδιωτικό φόρουμ</b>.";
+$l_private 	= "$l_privateforum<br>Σημείωση: πρέπει να έχετε ενεργοποιημένα τα cookies για να χρησιμοποιήσετε τα ιδιωτικά φόρα.";
+$l_noprivatepost = "$l_privateforum Δεν έχετε δικαίωμα δημοσίευσης σε αυτό.";
+
+// Viewtopic
+$l_topictitle	= "Θέαση $l_topic";
+$l_unregistered	= "Μη εγγεγραμμένος χρήστης";
+$l_posted	= "Δημοσιεύθηκε";
+$l_profileof	= "Θέαση προφίλ του";
+$l_viewsite	= "Μετάβαση στον ιστότοπο του";
+$l_icqstatus	= "$l_icq status";  // ICQ status
+$l_editdelete	= "Επεξεργασία/Διαγραφή $l_post";
+$l_replyquote	= "Απάντηση με παράθεση";
+$l_viewip	= "Θέαση IP συγγραφέα (Μόνο συντονιστές/διαχειριστές)";
+$l_locktopic	= "Κλείδωσε αυτό το $l_topic";
+$l_unlocktopic	= "Ξεκλείδωσε αυτό το $l_topic";
+$l_movetopic	= "Μετακίνησε αυτό το $l_topic";
+$l_deletetopic	= "Διέγραψε αυτό το $l_topic";
+
+// Functions
+$l_loggedinas	= "Σύνδεση ως";
+$l_notloggedin	= "Μη συνδεδεμένος";
+$l_logout	= "Αποσύνδεση";
+$l_login	= "Σύνδεση";
+
+// Page_header
+$l_separator	= "» »";  // Included here because some languages have
+		          // problems with high ASCII (Big-5 and the like).
+$l_editprofile	= "Επεξεργασία προφίλ";
+$l_editprefs	= "Επεξεργασία $l_preferences";
+$l_search	= "Αναζήτηση";
+$l_memberslist	= "Κατάλογος μελών";
+$l_faq		= "FAQ";
+$l_privmsgs	= "Προσωπικά $l_messages";
+$l_sendpmsg	= "Αποστολή προσωπικού μηνύματος";
+$l_statsblock   = '$statsblock = "Οι χρήστες έχουν δημοσιεύσει συνολικά -$total_posts- $l_messages.<br>
+Έχουμε -$total_users- εγγεγραμμένους χρήστες.<br>
+Ο νεότερα εγγεγραμμένος χρήστης είναι ο/η -<a href=\"$profile_url\">$newest_user</a>-.<br>
+-$users_online- ". ($users_online==1?"χρήστης":"χρήστες") ." <a href=\"$online_url\">περιηγούνται</a> στα φόρα.<br>";';
+$l_privnotify   = '$privnotify = "<br>Έχετε $new_message <a href=\"$privmsg_url\">".($new_message>1?"νέα προσωπικά μηνύματα":"νέο προσωπικό μήνυμα")."</a>.";';
+
+// Page_tail
+$l_adminpanel	= "Πίνακας διαχείρισης";
+$l_poweredby	= "Με τη δύναμη του";
+$l_version	= "έκδοση";
+
+// Auth
+
+// Register
+$l_notfilledin	= "Σφάλμα - Δε συμπληρώσατε όλα τα απαιτούμενα πεδία.";
+$l_invalidname	= "Το αναγνωριστικό που επιλέξατε, \"$username\", χρησιμοποιείται ήδη.";
+$l_disallowname	= "Το αναγνωριστικό που επιλέξατε, \"$username\", έχει απαγορευθεί από το διαχειριστή.";
+
+$l_welcomesubj	= "Καλωσήλθατε στα φόρα του $sitename";
+$l_welcomemail	=
+"
+$l_welcomesubj,
+
+Παρακαλούμε κρατήστε αυτό το email για το αρχείο σας.
+
+
+Οι πληροφορίες του λογαριασμού σας είναι ως ακολούθως:
+
+----------------------------
+Αναγνωριστικό: $username
+Συνθηματικό: $password
+----------------------------
+
+Παρακαλούμε μην ξεχάσετε το συνθηματικό σας, καθώς είναι κρυπτογραφημένο στη βάση δεδομένων μας και δεν μπορούμε
+να σάς το ανακτήσουμε. Ωστόσο, αν ξεχάσετε το συνθηματικό σας, σάς παρέχουμε έναν εύκολο τρόπο να σας στείλουμε
+με email ένα νέο, τυχαίο, συνθηματικό.
+
+Σας ευχαριστούμε για την εγγραφή.
+
+$email_sig
+";
+$l_beenadded	= "Προστεθήκατε στη βάση δεδομένων.";
+$l_thankregister= "Σας ευχαριστούμε για την εγγραφή!";
+$l_useruniq	= "Πρέπει να είναι μοναδικό. Δε γίνεται δύο χρήστες να έχουν το ίδιο αναγνωριστικό.";
+$l_storecookie	= "Αποθήκευση του αναγνωριστικού μου σε cookie για ένα έτος";
+
+// Prefs
+$l_prefupdated	= "$l_preferences ενημερώθηκαν. $l_click <a href=\"index.$phpEx\">$l_here</a> $l_returnindex";
+$l_editprefs	= "Επεξεργασία των Προτιμήσεών σας";
+$l_themecookie	= "ΣΗΜΕΙΩΣΗ: Για να χρησιμοποιήσετε τα θέματα ΠΡΕΠΕΙ να έχετε ενεργοποιημένα τα cookies.";
+$l_alwayssig	= "Πάντα προσθήκη της υπογραφής μου";
+$l_alwaysdisable= "Πάντα ανενεργοποιημένα"; // Only used for next three strings
+$l_alwayssmile	= "$l_alwaysdisable $l_smilies";
+$l_alwayshtml	= "$l_alwaysdisable $l_html";
+$l_alwaysbbcode	= "$l_alwaysdisable $l_bbcode";
+$l_boardtheme	= "Θέμα Πίνακα";
+$l_boardlang    = "Γλώσσα Πίνακα";
+$l_nothemes	= "Δεν υπάρχουν θέματα στη βάση";
+$l_saveprefs	= "Αποθήκευση προτιμήσεων";
+
+// Search
+$l_searchterms	= "Λέξεις κλειδιά";
+$l_searchany	= "Αναζήτηση για ΟΠΟΙΟΔΗΠΟΤΕ από τους όρους (Προκαθορισμένο)";
+$l_searchall	= "Αναζήτηση για ΟΛΟΥΣ τους όρους";
+$l_searchallfrm	= "Αναζήτηση σε όλα τα φόρα";
+$l_sortby	= "Ταξινόμηση κατά";
+$l_searchin	= "Αναζήτηση σε";
+$l_titletext	= "Τίτλο και κείμενο";
+$l_search	= "Αναζήτηση";
+$l_nomatches	= "Δε βρέθηκαν εγγραφές που να ταιριάζουν με το ερώτημα. Παρακαλώ διευρύνετε το ερώτημα.";
+
+// Whosonline
+$l_whosonline	= "Ποιός είναι συνδεδεμένος;";
+$l_nousers	= "Δεν υπάρχουν χρήστες που να περιηγούνται στα φόρα";
+
+
+// Editpost
+$l_notedit	= "Δεν μπορείτε να επεξεργαστείτε μία δημοσίευση που δεν έγινε από εσάς.";
+$l_permdeny	= "Δε δώσατε το σωστό $l_password ή δεν έχετε άδεια να επεξεργαστείτε αυτή τη δημοσίευση. $l_tryagain";
+$l_editedby	= "Αυτή η $l_message επεξεργάστηκε από τον/την:";
+$l_stored	= "Η $l_message αποθηκεύθηκε στη βάση.";
+$l_viewmsg	= "για να δείτε τη $l_message σας.";
+$l_deleted	= "Η $l_post διαγράφηκε.";
+$l_nouser	= "Το $l_username δεν υπάρχει.";
+$l_passwdlost	= "Ξέχασα το συνθηματικό μου!";
+$l_delete	= "Διαγραφή της δημοσίευσης";
+
+$l_disable	= "Απενεργοποίηση";
+$l_onthispost	= "σε αυτή τη δημοσίευση";
+
+$l_htmlis	= "$l_html είναι";
+$l_bbcodeis	= "$l_bbcode είναι";
+
+$l_notify	= "Ειδοποίηση με email όταν υπάρξουν απαντήσεις";
+
+// Newtopic
+$l_emptymsg	= Πρέπει να γράψετε ένα μήνυμα $l_message για δημοσίευση. Δεν μπορείτε να δημοσιεύσετε ένα κενό μήνυμα.";
+$l_aboutpost	= "Σχετικά με τη δημοσίευση";
+$l_regusers	= "Όλοι οι <b>εγγεγραμμένοι</b> χρήστες";
+$l_anonusers	= "<b>Ανώνυμοι</b> χρήστες";
+$l_modusers	= "Μόνο οι <B>Συντονιστές και Διαχειριστές</b>";
+$l_anonhint	= "<br>(Για να δημοσιεύσετε ανώνυμα απλά μη δώσετε αναγνωριστικό και συνθηματικό)";
+$l_inthisforum	= "μπορείτε να δημοσιεύσετε ανώνυμα σε αυτό το φόρουμ";
+$l_attachsig	= "Εμφάνιση υπογραφής <font size=-2>(Αυτό μπορεί να αλλαχθεί ή προστεθεί από το προφίλ)</font>";
+$l_cancelpost	= "Ακύρωση δημοσίευσης";
+
+// Reply
+$l_nopostlock	= "Δεν μπορείτε να δημοσιεύσετε μία απάντηση σε αυτό το θέμα, έχει κλειδωθεί.";
+$l_topicreview  = "Ανασκόπηση θέματος";
+$l_notifysubj	= "Μία απάντηση στο θέμα σας έχει δημοσιευθεί.";
+$l_notifybody	= 'Αγαπητέ/ή $m[username]\r\nΛαμβάνετε αυτό το email γιατί ένα μήνυμα
+που δημοσιεύσατε στα φόρα του $sitename έχει απαντηθεί, και
+επιλέξατε να ειδοποιήθητε για αυτό το γεγονός.
+
+Μπορείτε να δείτε το θέμα στο:
+
+http://$SERVER_NAME$url_phpbb/viewtopic.$phpEx?topic=$topic&forum=$forum
+
+ή να δείτε τον κατάλογο των φόρα του $sitename στο:
+
+http://$SERVER_NAME$url_phpbb
+
+Σας ευχαριστούμε που χρησιμοποιείτε τα φόρα του $sitename.
+
+Καλή σας ημέρα.
+
+$email_sig';
+
+
+$l_quotemsg	= '[quote]\nΤην $m[post_time], $m[username] έγραψε:\n$text\n[/quote]';
+
+// Sendpmsg
+$l_norecipient	= "Πρέπει να εισάγετε το αναγνωριστικό στο οποίο θέλετε να στείλετε τη $l_message.";
+$l_sendothermsg	= "Αποστολή ενός προσωπικού μηνύματος";
+$l_cansend	= "μπορείτε να στείλετε $l_privmsgs";  // All registered users can send PM's
+$l_yourname	= "Το $l_username σας";
+$l_recptname	= "Το $l_username του παραλήπτη";
+
+// Replypmsg
+$l_pmposted	= "Η απάντηση δημοσιεύθηκε, μπορείτε να κάνετε κλικ <a href=\"viewpmsg.$phpEx\">εδώ</a> για να δείτε τα $l_privmsgs σας";
+
+// Viewpmsg
+$l_nopmsgs	= "Δεν έχετε κάποιο $l_privmsgs.";
+$l_reply	= "Απάντηση";
+
+// Delpmsg
+$l_deletesucces	= "Επιτυχής διαγραφή.";
+
+// Smilies
+$l_smilesym	= "Τί να πληκτρολογήσετε";
+$l_smileemotion	= "Συναίσθημα";
+$l_smilepict	= "Εικόνα";
+
+// Sendpasswd
+$l_wrongactiv	= "Το κλειδί ενεργοποίησης που δώσατε δεν είναι σωστό. Παρακαλούμε ελέγξτε το $l_message email που λάβατε και βεβαιωθείτε ότι αντιγράψατε ακριβώς το κλειδί ενεργοποίησης.";
+$l_passchange	= "Το συνθηματικό σας αλλάχθηκε επιτυχώς. Μπορείτε να μεταβείτε τώρα στο <a href=\"bb_profile.$phpEx?mode=edit\">προφίλ</a> σας και να αλλάξετε το συνθηματικό σας σε ένα πιο βολικό.";
+$l_wrongmail	= "Η διεύθυνση email που δώσατε δεν ταιριάζει με αυτή που υπάρχει στη βάση.";
+
+$l_passsubj	= "Αλλαγή Συνθηματικού για Φόρα $sitename";
+
+$l_pwdmessage	= 'Αγαπητέ/ή $checkinfo[username],
+Λαμβάνετε αυτό το email γιατί εσείς (ή κάποιος που προσποιείτε ότι είστε εσείς)
+ζητήσατε μία αλλαγή συνθηματικού για τα φόρα του $sitename. Αν πιστεύετε ότι λάβατε
+αυτό το μήνυμα κατά λάθος, απλά διαγράψτε το και το συνθηματικό σας θα παραμείνει το ίδιο.
+
+Το νέο σας συνθηματικό, όπως δημιουργήθηκε από τα φόρα είναι: $newpw
+
+Για να ισχύσει αυτή η αλλαγή πρέπει να επισκεφθείτε αυτή τη σελίδα:
+
+   http://$SERVER_NAME$PHP_SELF?actkey=$key
+
+Αφού επισκεφθείτε αυτή τη σελίδα το συνθηματικό σας θα αλλαχθεί στη βάση μας,
+και θα μπορείτε να συνδεθείτε στην ενότητα του προφίλ σας και να το αλλάξετε σε ότι επιθυμείτε.
+
+Σας ευχαριστούμε που χρησιμοποιείτε τα φόρα του $sitename.
+
+$email_sig';
+
+$l_passsent	= "Το συνθηματικό σας έχει αλλαχθεί σε ένα νέο, τυχαίο συνθηματικό. Παρακαλούμε ελέγξτε τα email σας για να δείτε πώς θα ολοκληρώσετε τη διαδικασία αλλαγής συνθηματικού.";
+$l_emailpass	= "Αποστολή με email του χαμένου συνθηματικού";
+$l_passexplain	= "Παρακαλούμε συμπληρώστε τη φόρμα και ένα νέο συνθηματικό θα σας αποσταλλεί στη διεύθυνση email σας";
+$l_sendpass	= "Αποστολή συνθηματικού";
+
+
+?>
