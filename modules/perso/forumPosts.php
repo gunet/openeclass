c703e6ea9af3e709e613c4f5f6d842f43b4786d9
Evelthon Prodromou <eprodromou@upnet.gr>
2006-06-23 16:46:23 +0000

    revised forum component (alpha 2)

--- modules/perso/forumPosts.php
+++ modules/perso/forumPosts.php
@@ -70,7 +70,7 @@ function getUserForumPosts($param, $type)
 			$forumData = array();
 			$forumSubData = array();
 			$forumContent = array();
-			
+
 			array_push($forumData, $lesson_title[$i]);
 			array_push($forumData, $lesson_code[$i]);
 
@@ -82,7 +82,7 @@ function getUserForumPosts($param, $type)
 			//alliws 8a to kanei se ka8e ma8hma pou exei nees anakoinwseis!! (axreiasto!)
 		}
 
-//		$forum_post_counter = 0;
+		//		$forum_post_counter = 0;
 
 		while ($myForumPosts = mysql_fetch_row($mysql_query_result)) {
 			if ($myForumPosts){
@@ -104,12 +104,12 @@ function getUserForumPosts($param, $type)
 
 			$forum_post_counter++;*/
 		}
-//		dumpArray($forumContent);
+		//		dumpArray($forumContent);
 		//		$forum_posts_per_lesson[$i] = $forum_post_counter;
 		//		$lesson_posts[$i]['lesson_title']						= $lesson_titles[$i];
 		//		$lesson_posts[$i]['number_of_posts']					= $forum_posts_per_lesson[$i];
 		if ($num_rows > 0) {
-			echo "lala";
+//			echo "lala";
 			array_push($forumSubData, $forumContent);
 			array_push($forumData, $forumSubData);
 			array_push($forumPosts, $forumData);
@@ -118,40 +118,82 @@ function getUserForumPosts($param, $type)
 	}
 	//fix apo dw k katw (ta ekana c/p apo announcements)
 	if ($getNewPosts) {
-//		array_push($announceGroup, $announceSubGroup);
+		//		array_push($announceGroup, $announceSubGroup);
 		$sqlNowDate = eregi_replace(" ", "-",$usr_lst_login);
 		$sql = "UPDATE `user` SET `forum_flag` = '$sqlNowDate' WHERE `user_id` = $uid ";
 		db_query($sql, $mysqlMainDb);
-		echo $sql;
+		//		echo $sql;
 		//update announcemenets memory
 		//call announceHtmlInterface("new")
 	} elseif (!$getNewPosts) {
 		//if there are no new announcements, get the last announcements the user had
 		//so that we always have something to display
 		for ($i=0; $i < $max_repeat_val; $i++){
-			$forumData = array();
-			$forumSubData = array();
-			$forumContent = array();
-			
-			array_push($forumData, $lesson_title[$i]);
-			array_push($forumData, $lesson_code[$i]);
-
 			$mysql_query_result = db_query($forum_query_memo, $lesson_code[$i]);
+			if (mysql_num_rows($mysql_query_result) >0) {
+				$forumData = array();
+				$forumSubData = array();
+				$forumContent = array();
 
-			while ($myForumPosts = mysql_fetch_row($mysql_query_result)) {
-				array_push($forumContent, $myForumPosts);
+				array_push($forumData, $lesson_title[$i]);
+				array_push($forumData, $lesson_code[$i]);
+
+				while ($myForumPosts = mysql_fetch_row($mysql_query_result)) {
+					array_push($forumContent, $myForumPosts);
+				}
+
+				array_push($forumSubData, $forumContent);
+				array_push($forumData, $forumSubData);
+				array_push($forumPosts, $forumData);
 			}
-
-			array_push($forumSubData, $forumContent);
-			array_push($forumData, $forumSubData);
-			array_push($forumPosts, $forumData);
 		}
 
 	}
 
-//	dumpArray($forumPosts);
-	print_a($forumPosts);
+//		dumpArray($forumPosts);
+//		print_a($forumPosts);
 
+	if($type == "html") {
+		return forumHtmlInterface($forumPosts);
+	} elseif ($type == "data") {
+		return $forumPosts;
+	}
+
+}
+
+
+function forumHtmlInterface($data) {
+	$content= <<<fCont
+
+	 <div id="datacontainer">
+
+				<ul id="datalist">
+fCont;
+	$numOfLessons = count($data);
+	for ($i=0; $i <$numOfLessons; $i++) {
+		$content .= "
+		<li class=\"category\">".$data[$i][0]."
+		</li>";
+		$iterator =  count($data[$i][2][0]);
+		for ($j=0; $j < $iterator; $j++){
+			$url = $_SERVER['PHP_SELF']."?perso=5&c=".$data[$i][1]."&t=".$data[$i][2][0][$j][2]."&f=".$data[$i][2][0][$j][0]."&s=".$data[$i][2][0][$j][4];
+			$content .= "
+		<li><a class=\"square_bullet\" href=\"$url\"><div class=\"title_pos\">".$data[$i][2][0][$j][3]." (".$data[$i][2][0][$j][5].")</div>
+			<div class=\"content_pos\">".$data[$i][2][0][$j][8]."</div>
+			<div class=\"content_pos\">Apostoleas: ".$data[$i][2][0][$j][6]." ".$data[$i][2][0][$j][7]."</div>
+		</a></li>
+		";
+		}
+
+		if ($i+1 <$numOfLessons) $content .= "<br>";
+	}
+
+	$content .= "</ul>
+			</div> 
+";
+
+
+	return $content;
 }
 
 function createForumQueries($dateVar){
