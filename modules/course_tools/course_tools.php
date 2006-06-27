e7e3b6aa458e196fb7b35b6acd3bd2b28e0b52d8
Evelthon Prodromou <eprodromou@upnet.gr>
2006-06-27 15:32:57 +0000

    Altered logic Added external & import modules to this one

--- modules/course_tools/course_tools.php
+++ modules/course_tools/course_tools.php
@@ -1,7 +1,24 @@
 <?PHP
-//class.courseTools.php
+/*
+*
+*	File : course_tools.php
+*
+*	Various operations regarding a course's tools
+*
+*	Activate/De-activate tools
+*	Upload external html page
+*	Add external links
+*	Delete the last two when inactive
+*
+*	@author Evelthon Prodromou <eprodromou@upnet.gr>
+*
+*	@access public
+*
+*	@version 1.0.1
+*
+*/
 $require_current_course = TRUE;
-$langFiles = array('toolManagement', 'create_course');
+$langFiles = array('toolManagement', 'create_course', 'external_module', 'import');
 $require_help = TRUE;
 $helpTopic = 'User';
 
@@ -50,9 +67,164 @@ if ($is_adminOfCourse){
 	}
 
 	if (isset($delete)) {
+		$sql = "SELECT `lien`, `define_var`
+				FROM accueil
+				WHERE `id` = ". $delete ." ";
+		$result = db_query($sql, $dbname);
+		while ($res = mysql_fetch_row($result)){
+			//			dumpArray($res);
+			if($res[1] == "HTML_PAGE") {
+				$link = explode(" ", $res[0]);
+				$path = substr($link[0], 6);
+				$file2Delete = $webDir . $path;
+				//			echo $file2Delete;
+				@unlink($file2Delete);
+
+			}
+		}
 		$sql = "DELETE FROM `accueil` WHERE `id` = " . $delete ." ";
 		db_query($sql, $dbname);
 		unset($sql);
+
+		$tool_content .= "
+		<table>
+			<tbody>
+				<tr>
+					<td class=\"success\">
+					<p>$deleteSuccess</p>
+					
+					</td>
+				</tr>
+			</tbody>
+		</table>
+		<br/><br/>
+		";
+	}
+
+	//--add external link
+
+	if(isset($submit) &&  @$action ==2){
+		if (($link == "http://") or ($link == "ftp://") or empty($link))  {
+			$tool_content .= "
+		<table>
+			<tbody>
+				<tr>
+					<td class=\"caution\">
+					<p>$langInvalidLink</p>
+					<a href=\"../../courses/$currentCourseID/index.php\">$langHome</a>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+		";
+
+			draw($tool_content, 2);
+			exit();
+		}
+
+		$sql = 'SELECT MAX(`id`) FROM `accueil` ';
+		$res = db_query($sql,$dbname);
+		while ($maxID = mysql_fetch_row($res)) {
+			$mID = $maxID[0];
+		}
+
+		if($mID<101) $mID = 101;
+		else $mID = $mID+1;
+
+
+		mysql_query("INSERT INTO accueil VALUES ($mID,
+					'$name_link',
+					'$link \"target=_blank',
+					'external_link',
+					'1',
+					'0',
+					'$link',
+					''
+					)");
+
+		$tool_content .= "
+		<table>
+			<tbody>
+				<tr>
+					<td class=\"success\">
+					<p>$langAdded</p>
+					
+					</td>
+				</tr>
+			</tbody>
+		</table>
+		<br/><br/>
+		";
+		unset($action);
+	}
+
+	if(isset($submit) &&  @$action ==1){//upload html page
+		// UPLOAD FILE TO "documents" DIRECTORY + INSERT INTO documents TABLE
+		$updir = "$webDir/courses/$currentCourseID/page/"; //path to upload directory
+		$size = "20000000"; //file size ex: 5000000 bytes = 5 megabytes
+		if (($file_name != "") && ($file_size <= "$size" )) {
+
+			$file_name = str_replace(" ", "", $file_name);
+			$file_name = str_replace("é", "e", $file_name);
+			$file_name = str_replace("è", "e", $file_name);
+			$file_name = str_replace("ê", "e", $file_name);
+			$file_name = str_replace("à", "a", $file_name);
+
+			@copy("$file", "$updir/$file_name")
+			or die("
+		
+			<p>
+				$langCouldNot
+			</p>
+	</tr>");
+			$sql = 'SELECT MAX(`id`) FROM `accueil` ';
+			$res = db_query($sql,$dbname);
+			while ($maxID = mysql_fetch_row($res)) {
+				$mID = $maxID[0];
+			}
+
+			if($mID<101) $mID = 101;
+			else $mID = $mID+1;
+
+			db_query("INSERT INTO accueil VALUES (
+					$mID,
+					'$link_name',
+					'../../courses/$currentCourse/page/$file_name \"target=_blank',
+					'external_link',
+					'1',
+					'0',
+					'',
+					'HTML_PAGE'
+					)", $currentCourse);
+
+			$tool_content .=  "
+					<table>
+				<tbody>
+					<tr>
+						<td class=\"success\">
+						$langOkSent
+					</td>
+					</tr>
+				</tbody>
+			</table><br/>";
+		} else {
+			$tool_content .= "
+			<table>
+				<tbody>
+					<tr>
+						<td class=\"caution\">
+					
+						$langTooBig
+					
+						</td>
+					</tr>
+				</tbody>
+			</table>
+			";
+			draw($tool_content, 2);
+		}	// else
+
+		unset($action);
 	}
 
 }
@@ -91,10 +263,92 @@ if ($is_admin){
 	}
 
 }
+//------------------------------------------------------
+if ($is_adminOfCourse && @$action == 1) {//upload html file
+	$tool_content .=  "
+		<p>$langExplanation</p>
+			
+			
+		<form method=\"POST\" action=\"$PHP_SELF?submit=yes&action=1\" enctype=\"multipart/form-data\">
+			<table>
+			<thead>
+				<tr>
+					<th>
+						
+							$langSendPage :
+						
+					</th>
+					<td>
+						<input type=\"file\" name=\"file\" size=\"35\" accept=\"text/html\">
+					</td>
+				</tr>
+				<tr>
+					<th>
+						
+							$langPgTitle :
+						
+					</th>
+					<td>
+						<input type=\"Text\" name=\"link_name\" size=\"50\">
+					</td>
+				</tr>
+				</thead>
+				</table>
+				<br>
+						<input type=\"Submit\" name=\"submit\" value=\"$langAddOk\">
+				
+</form>";
 
+	draw($tool_content, 2);
+}
 
+if ($is_adminOfCourse && @$action == 2) {//add external link
+	$tool_content .=  "
+			<form method=\"post\" action=\"$_SERVER[PHP_SELF]?submit=yes&action=2\">
+			<table>
+				<thead>
+				<tr>
+					<th>
+						
+							$langLink&nbsp;:
+					</th>
+					<td>
+						<input type=\"text\" name=\"link\" size=\"50\" value=\"http://\">
+					</td>
+				</tr>
+				<tr>
+					<th>
+							$langName&nbsp;:
+					</th>
+					<td>
+						<input type=\"Text\" name=\"name_link\" size=\"50\">
+					</td>
+				</tr>
+				</thead></table>
+				<br>
+					<input type=\"Submit\" name=\"submit\" value=\"$langAdd\">
+				
+			</form>
+			";
+	draw($tool_content, 2);
+	//call draw
+}
+//---------------------------------------------------------
 if ($is_adminOfCourse) {
-
+	/*$tool_content .= "
+	<a href=\"" . $urlServer . "modules/import/import.php\">".$langUploadPage."</a>
+	|
+	<a href=\"" . $urlServer . "modules/external_module/external_module.php\">".$langAddExtLink."</a>
+	<br>
+	<br>
+	";*/
+	$tool_content .= "
+<a href=\"".$_SERVER['PHP_SELF']."?action=1\">".$langUploadPage."</a>
+ | 
+<a href=\"".$_SERVER['PHP_SELF']."?action=2\">".$langAddExtLink."</a>
+<br>
+<br>
+";
 	$tool_content .= <<<tForm
 <form action="$_SERVER[PHP_SELF]" method="post" enctype="multipart/form-data">
 
@@ -188,9 +442,9 @@ tForm;
 	   </tbody>
 	</table>
 	
-	  <p>
-	    <input type=\"submit\" name=\"toolStatus\" value=\"Submit\">
-	  </p>
+	 <br/>
+	    <input type=\"submit\" name=\"toolStatus\" value=\"$langSubmit\">
+	  
 	</form>	";
 
 }
