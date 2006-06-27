d008a96cbe53bedc35eccb3bbe1cedf35d394c7e
Evelthon Prodromou <eprodromou@upnet.gr>
2006-06-27 15:31:59 +0000

    Added lang vars

--- modules/lang/english/toolManagement.inc.php
+++ modules/lang/english/toolManagement.inc.php
@@ -4,4 +4,11 @@ $langTool = "Tool";
 $langStatus = "Status";
 $langRename = "Rename";
 $langDelete = "Delete";
+
+$langUploadPage = "Upload html page";
+$langAddExtLink = "Add external link to the left menu";
+
+$langSubmit = "Submit changes";
+
+$deleteSuccess = "The link has been deleted";
 ?>
--- modules/lang/greek/toolManagement.inc.php
+++ modules/lang/greek/toolManagement.inc.php
@@ -5,4 +5,11 @@ $langStatus = "Κατάσταση";
 $langRename = "Αλλαγή Τίτλου";
 $langDelete = "Διαγραφή";
 
+$langUploadPage = "Ανέβασμα ιστοσελίδας";
+$langAddExtLink = "Προσθήκη εξωτερικού σύνδεσμου στο αριστερό μενού";
+
+$langSubmit = "Αποθήκευση αλλαγών";
+
+$deleteSuccess = "Ο σύνδεσμος διαγράφηκε";
+
 ?>
