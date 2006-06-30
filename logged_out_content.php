b57a0ee3d86029062ddfc4f4cbc525be94ce7f20
Evelthon Prodromou <eprodromou@upnet.gr>
2006-06-30 23:43:59 +0000

    Altered logic. When logged out, the user can see the platform's announcemets (if there are any) in both languages

--- logged_out_content.php
+++ logged_out_content.php
@@ -1,16 +1,72 @@
 <?PHP
 //logged_out_content.php
+// @author Evelthon Prodromou <eprodromou@upnet.gr>
+$sql_el ="SELECT `date`, `gr_title` , `gr_body` , `gr_comment`
+		FROM `admin_announcements`
+		WHERE `visible` = \"V\"
+		";
+$sql_en ="SELECT `date`, `en_title` , `en_body` , `en_comment`
+		FROM `admin_announcements`
+		WHERE `visible` = \"V\"
+		";
 
+if(session_is_registered('langswitch')) {
+	$language = $_SESSION['langswitch'];
+}
 
+if ($language == "greek") $sql = $sql_el;
+else $sql = $sql_en;
 
-$tool_content .= <<<lCont
+	$tool_content .= <<<lCont
 <div id="container_login">
-<!--<div id="header"><h1>Header</h1></div>-->
+
 <div id="wrapper">
 <div id="content_login">
 <p>$langInfo</p>
+lCont;
+$result = db_query($sql, $mysqlMainDb);
+	if (mysql_num_rows($result) > 0) {
+		$announceArr = array();
+		while ($eclassAnnounce = mysql_fetch_array($result)) {
+			array_push($announceArr, $eclassAnnounce);
+		}
+
+		$tool_content .= "
+<br/>
+
+<table width=\"99%\">
+	<thead>
+		<tr>
+			<th> $langPlatformAnnounce </th>
+		</tr>
+	</thead>
+	<tbody>";
 
 
+		$numOfAnnouncements = count($announceArr);
+
+		for($i=0; $i < $numOfAnnouncements; $i++) {
+
+			if ($i%2 == 0) $rowClass = "class=\"odd\"";
+			else $rowClass = "";
+
+			$tool_content .= "
+		<tr $rowClass>
+			<td>
+				<p><b>".$announceArr[$i][0].":</b> <u>".$announceArr[$i][1]."</u></p>
+				<p>".$announceArr[$i][2]."</p>
+				<p><i>".$announceArr[$i][3]."</i></p>
+			</td>
+		</tr>
+		";
+
+		}
+
+		$tool_content .= "
+			</tbody>
+		</table>";
+	}
+	$tool_content .= <<<lCont2
 </div>
 </div>
 <div id="navigation">
@@ -43,10 +99,10 @@ $tool_content .= <<<lCont
 <div id="extra">
 <p>{ECLASS_HOME_EXTRAS_RIGHT}</p>
 </div>
-<!--<div id="footer"><p>Here it goes the footer</p></div>-->
+
 </div>
 
-lCont;
+lCont2;
 
 
 
