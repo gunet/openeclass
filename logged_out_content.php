86531481ec44a75a51366c63604c0d802090f136
Evelthon Prodromou <eprodromou@upnet.gr>
2006-07-05 18:25:10 +0000

    Altered logic regarding the language switch

--- logged_out_content.php
+++ logged_out_content.php
@@ -12,6 +12,8 @@ $sql_en ="SELECT `date`, `en_title` , `en_body` , `en_comment`
 
 if(session_is_registered('langswitch')) {
 	$language = $_SESSION['langswitch'];
+} else {
+	$userLanguage == "el";
 }
 
 if ($language == "greek") $sql = $sql_el;
