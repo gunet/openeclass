8b699abec7e5cf93b41e1ec008336e089076b08d
Artemios Vogiatzis <bogart@upatras.gr>
2006-07-08 14:32:01 +0000

    *** empty log message ***

--- modules/phpbb/page_header.php
+++ modules/phpbb/page_header.php
@@ -31,8 +31,6 @@ $mtime = explode(" ",$mtime);
 $mtime = $mtime[1] + $mtime[0];
 $starttime = $mtime;
 
-
-
 /* Who's Online Hack */
 $IP=$REMOTE_ADDR;
 
@@ -76,7 +74,7 @@ if($l_special_meta) {
 	$tool_content .= $l_special_meta . "\n";
 }
 if($forward) {
-	$tool_content .= "<META HTTP-EQUIV=\"refresh\" content=\"3;URL=$url_phpbb/viewtopic.$phpEx?topic=$topic&forum=$forum&$total_topic\">";
+	$tool_content .= "<META HTTP-EQUIV=\"refresh\" content=\"3;URL=$url_phpbb/viewtopic.php?topic=$topic&forum=$forum&$total_topic\">";
 } 
 $meta = showmeta($db);
 
@@ -84,14 +82,14 @@ $tool_content .= $meta;
 $tool_content .="
 </HEAD>
 <BODY>
-<table border="0" align="center" cellpadding="0" cellspacing="0" width="99%">
+<table border=\"0\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\" width=\"99%\">
 		<tr>
-			<td colspan="4">
+			<td colspan=\"4\">
 ";
 
 				$noPHP_SELF = true; //because  phpBB need always param IN URL
 				include('../../include/phpbb_header.php');
-echo "
+$tool_content .= "
 				<br>
 				<a href=\"./search.php?addterms=any&forum=all&sortby=p.post_time%20desc&searchboth=both&submit=Rechercher\">$langLastMsgs</a>
 			</td>
@@ -108,16 +106,13 @@ echo "
 
 // cell one and three in the first TD with rowspan (logo)
 
-echo "<center><table width=600><TR><TD ALIGN=left WIDTH=50% ROWSPAN=2>
+$tool_content .= "<center><table width=600><TR><TD ALIGN=left WIDTH=50% ROWSPAN=2>
 	<font size=3 face=\"arial, helvetica\"><b>$l_forums</b>&nbsp;";
 
 if($status[$dbname]==1 OR $status[$dbname]==2) {
-
-echo "<br><font size=2><a href=../forum_admin/forum_admin.php>$langAdm</a>";
-
-}	// if prof or allowed to admin	
-
-
+	// if prof or allowed to admin	
+	$tool_content .= "<br><font size=2><a href=../forum_admin/forum_admin.php>$langAdm</a>";
+}
 
 ##################################################################
 #######  RELATE TO GROUP DOCUMENT AND SPACE FOR CLAROLINE  #######
@@ -129,7 +124,6 @@ $forumPriv=mysql_query("SELECT private FROM group_properties");
 while ($myForumPriv = mysql_fetch_array($forumPriv)) 
 {
 	$privProp=$myForumPriv['private'];
-	// echo "<br>privProp: $privProp<br>";	// Debugging
 }
 
 // Determine if uid is tutor for this course
@@ -139,7 +133,6 @@ $sqlTutor=mysql_query("SELECT tutor FROM $mysqlMainDb.cours_user
 while ($myTutor = mysql_fetch_array($sqlTutor)) 
 {
 	$tutorCheck=$myTutor[tutor];
-	 // echo "<br>tutorCheck: $tutorCheck<br>uid $uid";	// Debugging
 }
 
 // Determine if forum category is Groups
@@ -147,7 +140,6 @@ $forumCatId=mysql_query("SELECT cat_id FROM forums WHERE forum_id='$forum'");
 while ($myForumCat = mysql_fetch_array($forumCatId)) 
 {
 	$catId=$myForumCat[cat_id];
-	// echo "<br>catId: $catId<br>";	// Debugging
 }
 
 // Check which group and which forum user is a member of
@@ -160,110 +152,68 @@ while ($myTeamUser = mysql_fetch_array($findTeamUser))
 	$myTeam=$myTeamUser[team];
 	$myGroupForum=$myTeamUser[forumId];
 	$myTutor=$myTeamUser[tutor];
-	// echo "<br><br>uid : $uid Team : $myTeam<br> ForumId: $myGroupForum<br>tuteur : $myTutor";	// Debugging
 }
 
 // Show Group Documents and Group Space only if in Category 2 = Group Forums Category
 if (($catId==1) AND ($forum==$myGroupForum))
 {
-
 	// Added by Thomas group space links
-	echo "<BR><BR><a href=\"../group/group_space.php\">$langGroupSpaceLink</a>&nbsp;&nbsp;
+	$tool_content .= "<BR><BR><a href=\"../group/group_space.php\">$langGroupSpaceLink</a>&nbsp;&nbsp;
 	<a href=\"../group/document.php?userGroupId=$userGroupId\">$langGroupDocumentsLink</a><br><br>";
 }
 
 
 ##################################################################
 
-
-
-
-
-echo "</TD>";
+$tool_content .= "</TD>";
 
 // Switch for cell two  (posts buttons)
 switch($pagetype) {
 	// 'index' is covered by default
 	case 'newtopic':
-?>
-	<TD ALIGN="CENTER">
-		<FONT FACE="<?php echo $FontFace?>" SIZE="<?php echo $FontSize2?>" COLOR="<?php echo $textcolor?>"><b>Υποβολή 
-νέου θέματος στο:<BR>
-		<a href="<?php echo $url_phpbb?>/viewforum.<?php echo $phpEx ?>?forum=<?php echo $forum?>"><?php echo $forum_name?></a></b>
-		</font>
-	</TD>
-<?php
-	break;
+		$tool_content .= "
+			<TD ALIGN=\"CENTER\">
+			<FONT FACE=\"$FontFace\" SIZE=\"$FontSize2\" COLOR=\"$textcolor\"><b>Υποβολή νέου θέματος στο:<BR>
+			<a href=\"$url_phpbb/viewforum.php?forum=$forum\">$forum_name</a></b>
+			</FONT>
+			</TD>";
+		break;
 	case 'viewforum':
-?>
-	<TD ALIGN="right"><font face="arial, helvetica" size=2>
-		<a href="newtopic.php?forum=<?php echo $forum?>">
-	
-<?php
-
-		echo "$langNewTopic</a></TD>";
-	break;
+		$tool_content .= "
+			<TD ALIGN=\"right\"><font face=\"arial, helvetica\" size=2>
+			<a href=\"newtopic.php?forum=$forum>$langNewTopic</a></TD>";
+		break;
 	case 'viewtopic':
-?>
-	<TD ALIGN="right"><font size=2 face="arial, helvetica">
-<?php
-	if($lock_state != 1) {
-echo "<a href=\"$url_phpbb/reply.php?topic=$topic&forum=$forum\"><font color=#0000FF>$langAnswer</font color></a></TD>";
-	}
-	else
-			echo "<img src=\"$reply_locked_image\" BORDER=0>\n";
-?>
-	</TD>
-<?php
-	break;
-	// 'Register' is covered by default
+		$tool_content .= "
+			<TD ALIGN=\"right\"><font size=2 face=\"arial, helvetica\">";
+		if($lock_state != 1) {
+			$tool_content .= "<a href=\"$url_phpbb/reply.php?topic=$topic&forum=$forum\"><font color=#0000FF>$langAnswer</font color></a></TD>";
+		} else {
+			$tool_content .= "<img src=\"$reply_locked_image\" BORDER=0>\n";
+		}
+		$tool_content .= "</TD>";
+		break;
 	case '':
-?>
-<?php
         default:
-?>
-	<TD ALIGN=\"left\">
-
-	</TD>
-<?php
-	break;
+		$tool_content .= "<TD ALIGN=\"left\"> </TD>";
+		break;
 }  // End for switch cell two
 // Cell four (block with links)
-?>
-</TR>
-<TR>
-	<TD ALIGN="right" valign=top>
-<?
+$tool_content .= "</TR><TR><TD ALIGN=\"right\" valign=top>";
 if($status[$dbname]==1 OR $status[$dbname]==2) {
-?>
-
-<a href="../help/help.php?topic=For&language=<?= $languageInterface ?>" 
-onClick="window.open('../help/help.php?topic=For&language=<?= $languageInterface ?>','Help','toolbar=no,location=no,directories=no,status=yes,menubar=no,scrollbars=yes,resizable=yes,width=350,height=450,left=300,top=10'); 
-return false;">
-<font size=2 face ="arial, helvetica">
-<?= $langHelp ?>
-</font>
-</a>
-
-<?
-		}	// END IF PROF OR ASSISTANT
-
-		if ($user_logged_in)
-		{
+	$tool_content .= "<a href=\"../help/help.php?topic=For&language=$languageInterface\" onClick=\"window.open('../help/help.php?topic=For&language=$languageInterface','Help','toolbar=no,location=no,directories=no,status=yes,menubar=no,scrollbars=yes,resizable=yes,width=350,height=450,left=300,top=10'); return false;\"><font size=2 face =\"arial, helvetica\">=$langHelp\"</font></a>";
+}	// END IF PROF OR ASSISTANT
+if ($user_logged_in) {
 			// do PM notification.
 			$last_visit_date = date("Y-m-d h:i", $last_visit);
-
 			$username = addslashes($userdata[username]);
-
 			$sql = "SELECT count(*) AS count
 			      FROM priv_msgs p, users u
 			      WHERE p.to_userid = u.user_id and p.msg_status = '0' and u.username = '$username'";
-
 			if(!$result = mysql_query($sql, $db))
 			{
 				error_die("phpBB was unable to check private messages because " .mysql_error($db));
 			}
-
 			$row = @mysql_fetch_array($result);
 			$new_message = $row[count];
 			$word = ($new_message > 1) ? "messages" : "message";
@@ -272,88 +222,51 @@ return false;">
 			if ($new_message != 0)
 			{
 				eval($l_privnotify);
-				print $privnotify;
+				$tool_content .= "$privnotify";
 			}
-		}
-?>		
-		
-		</font>
-	</TD>
-</TR>
-<?php
+}
+$tool_content .= "</font></TD></TR>";
+
 //Third row with cell five and six (misc. information)
 switch($pagetype) {
 	case 'index':
-	$total_posts = get_total_posts("0", $db, "all");
-	$total_users = get_total_posts("0", $db, "users");
-	$sql = "SELECT username, user_id FROM users WHERE user_level != -1 ORDER BY user_id DESC LIMIT 1";
-	$res = mysql_query($sql, $db);
-	$row = mysql_fetch_array($res);
-	$newest_user = $row["username"];
-	$newest_user_id = $row["user_id"];
-	$profile_url = "$url_phpbb/bb_profile.$phpEx?mode=view&user=$newest_user_id";
-	$online_url = "$url_phpbb/whosonline.$phpEx";
-
-?>
-<TR>
-	<TD COLSPAN="2" ALIGN="RIGHT">
-		<FONT FACE="<?php echo $FontFace?>" SIZE="<?php echo $FontSize1?>" COLOR="<?php echo $textcolor?>">
-		<?php 
-			eval($l_statsblock);
-			// print $statsblock;
-			// print_login_status($user_logged_in, $userdata[username], $url_phpbb);   // deactivated by CLAROLINE
-		?>
-		</font>
-	</TD>
-</TR>
-<?php
-	break;
+		$total_posts = get_total_posts("0", $db, "all");
+		$total_users = get_total_posts("0", $db, "users");
+		$sql = "SELECT username, user_id FROM users WHERE user_level != -1 ORDER BY user_id DESC LIMIT 1";
+		$res = mysql_query($sql, $db);
+		$row = mysql_fetch_array($res);
+		$newest_user = $row["username"];
+		$newest_user_id = $row["user_id"];
+		$profile_url = "$url_phpbb/bb_profile.php?mode=view&user=$newest_user_id";
+		$online_url = "$url_phpbb/whosonline.$phpEx";
+		$tool_content .="
+			<TR>
+			<TD COLSPAN=\"2\" ALIGN=\"RIGHT\">
+			<FONT FACE=\"$FontFace\" SIZE=\"$FontSize1\" COLOR=\"$textcolor\">";
+		eval($l_statsblock);
+		$tool_content .= "</font></TD></TR>";
+		break;
 	case 'newforum':
-	// No third row
-	break;
+		// No third row
+		break;
 	case 'viewforum':
-?>
-<TR>
-	<TD COLSPAN="2" ALIGN="LEFT">
-	<FONT FACE="<?php echo $FontFace?>" SIZE="<?php echo $FontSize2?>" COLOR="<?php echo $textcolor?>">
-		<b><?php echo $forum_name?></b>
-		<BR>
-		<FONT FACE="<?php echo $FontFace?>" SIZE="<?php echo $FontSize1?>" COLOR="<?php echo $textcolor?>">
-			<?php 
-		
-?></font></TD>
-</TR>
-
-<?php
+		$tool_content .= "
+			<TR>
+			<TD COLSPAN=\"2\" ALIGN=\"LEFT\">
+			<FONT FACE=\"$FontFace\" SIZE=\"$FontSize2\" COLOR=\"$textcolor\"><b>$forum_name</b><BR></TD></TR>";
 	case 'viewtopic':
-	$total_forum = get_total_posts($forum, $db, 'forum');
-?>
-<TR>
-	<TD COLSPAN="2" ALIGN="LEFT">
-	<FONT FACE="<?php echo $FontFace?>" SIZE="<?php echo $FontSize1?>" COLOR="<?php echo $textcolor?>">
-		<a href="<?php echo $url_phpbb?>/index.<?php echo $phpEx ?>"><?php echo $sitename?> Forum Index</a>
-		<b><?php echo $l_separator?></b>
-		<a href="<?php echo "$url_phpbb/viewforum.$phpEx?forum=$forum&$total_forum"?>"><?php echo stripslashes($forum_name)?></a> 
-<?php
-        if($pagetype != "viewforum")
-		echo "<b>$l_separator</b>";
-?>
-		<?php echo $topic_subject?>
-	</TD>
-</TR>
-<?php
-	break;
+		$total_forum = get_total_posts($forum, $db, 'forum');
+		$tool_content .= "
+			<TR>
+			<TD COLSPAN=\"2\" ALIGN=\"LEFT\">
+			<FONT FACE=\"$FontFace\" SIZE=\"$FontSize1\" COLOR=\"$textcolor\"><a href=\"$url_phpbb/index.php>$sitename Forum Index</a><b>$l_separator</b><a href=\"$url_phpbb/viewforum.php?forum=$forum&$total_forum\">" . stripslashes($forum_name) . "</a> ";
+		if($pagetype != "viewforum")
+			$tool_content = "<b>$l_separator</b>";
+		$tool_content .= "$topic_subject";
+		$tool_content .= "</TD></TR>";
+		break;
 	case 'privmsgs':
-?>
-<TR>
-        <TD COLSPAN="2" ALIGN="CENTER">
-	<FONT FACE="<?php echo $FontFace?>" SIZE="<?php echo $FontSize2?>" COLOR="<?php echo $textcolor?>">
-		[<a href="<?php echo $url_phpbb?>/sendpmsg.<?php echo $phpEx ?>"><?php echo $l_sendpmsg?></a>]
-	<br>
-        </TD>
-</TR>
-<?php
-	// break;
+		$tool_content .= "<TR><TD COLSPAN=\"2\" ALIGN=\"CENTER\"><FONT FACE=\"$FontFace\" SIZE=\"$FontSize2\" COLOR=\"$textcolor\"> [<a href=\"$url_phpbb/sendpmsg.php\">$l_sendpmsg</a>] <br> </TD> </TR>";
+		break;
 }
-?>
-</TABLE> </center>
+$tool_content .= "</TABLE> </center>";
