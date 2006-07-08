0543051a7bfb233cacc343a0cf0d0604d66bb99d
Artemios Vogiatzis <bogart@upatras.gr>
2006-07-08 13:35:27 +0000

    *** empty log message ***

--- include/phpbb_header.php
+++ include/phpbb_header.php
@@ -1,23 +1,23 @@
-<?
+<?php
 $colorLight     ="#F5F5F5";
 $colorMedium    ="#004571";
 $colorDark	="#000066";
 
-echo "<table cellpadding=\"3\" border=0 width=\"100%\">
+$tool_content .= "<table cellpadding=\"3\" border=0 width=\"99%\">
 <tr><td colspan=\"5\" style='padding:0px;' align=\"center\" bgcolor=\"$colorMedium\">
 <font face=\"Arial, Helvetica, sans-serif\" color=\"#FFFFFF\" size=\"2\">
 <img src='$urlServer/images/gunet/banner.jpg'></td></tr>
 <tr><td colspan=\"5\" align=\"left\" bgcolor=\"$colorMedium\">
 	<font face=\"Arial, Helvetica, sans-serif\" color=\"#FFFFFF\" size=\"2\">";
 if (isset($uid)) {
-	echo "<span style='float: left'>$langUser : $prenom $nom</span>";
-	echo "<a href='".$urlServer."index.php?logout=yes' style='float: right; color: white;'>$langLogout</a></td>";
+	$tool_content .= "<span style='float: left'>$langUser : $prenom $nom</span>";
+	$tool_content .= "<a href='".$urlServer."index.php?logout=yes' style='float: right; color: white;'>$langLogout</a></td>";
+} else {
+	$tool_conent .= "<br></font></td></tr>";
 }
-else
-	echo "<br></font></td></tr>";
 
 if (isset($currentCourseID))
-echo "<tr bgcolor=\"$colorLight\">
+$tool_content .= "<tr bgcolor=\"$colorLight\">
 	<td colspan=\"5\"><b>
 	<font face=\" Arial, Helvetica, sans-serif\" size=\"3\" color=\"$colorDark\">$intitule</font>
 	<font color=\"#000066\" face=\"Arial, Helvetica, sans-serif\" size=\"2\">
@@ -32,20 +32,19 @@ while ($info= @mysql_fetch_array($lesson_info)) {
 	$lesson=$info['intitule'];
 }
 			
-echo "<tr><td colspan=\"4\"><font face=\"Arial, Helvetica, sans-serif\" size=\"1\">
-<a href=\"".$urlServer."index.php\" target=\"_top\">$siteName</a>";
+$tool_content .= "<tr><td colspan=\"4\"><font face=\"Arial, Helvetica, sans-serif\" size=\"1\"><a href=\"".$urlServer."index.php\" target=\"_top\">$siteName</a>";
 
 if (isset($currentCourseID))
-	echo "&nbsp;&gt;&nbsp;<a href=\"".$urlServer."courses/$currentCourseID/index.php\" target=\"_top\" >$intitule</a>";
+	$tool_content .= "&nbsp;&gt;&nbsp;<a href=\"".$urlServer."courses/$currentCourseID/index.php\" target=\"_top\" >$intitule</a>";
 if (isset($interbredcrump) && is_array($interbredcrump)) 
 while (list(,$step) = each($interbredcrump) )
 {
-	echo "&nbsp;&gt;&nbsp;<a target=\"_top\" href=\"".$step["url"]."\" >".$step["name"]."</a>";
+	$tool_content .= "&nbsp;&gt;&nbsp;<a target=\"_top\" href=\"".$step["url"]."\" >".$step["name"]."</a>";
 };
 
-if (isset($nameTools)) echo "&nbsp;&gt;&nbsp;<b>$nameTools</b>";
+if (isset($nameTools)) $tool_content .= "&nbsp;&gt;&nbsp;<b>$nameTools</b>";
 
-echo "<br></font></td></tr></table>";                   // closed table
+$tool_content .= "<br></font></td></tr></table>";                   // closed table
 
 if (isset($db)) {
 	if (isset($currentCourseID))	
@@ -53,7 +52,5 @@ if (isset($db)) {
 	else
 		mysql_select_db("$mysqlMainDb",$db);
 }
-
-
 
 ?>
