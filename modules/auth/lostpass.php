3ee791034ae3146427b60bbf587034faa39bb83a
Stratos Karatzidis <kstratos>
2006-07-12 07:49:46 +0000

    apply new template

--- modules/auth/lostpass.php
+++ modules/auth/lostpass.php
@@ -1,52 +1,51 @@
 <?php
-/*
-      +----------------------------------------------------------------------+
-      | e-class version 1.0                                                  |
-      | based on CLAROLINE version 1.3.0 $Revision$		     |
-      +----------------------------------------------------------------------+
-      |   $Id$
-      +----------------------------------------------------------------------+
-      | Copyright (c) 2001, 2002 Universite catholique de Louvain (UCL)      |
-      | Copyright (c) 2003 GUNet                                             |
-      +----------------------------------------------------------------------+
-      |   This program is free software; you can redistribute it and/or      |
-      |   modify it under the terms of the GNU General Public License        |
-      |   as published by the Free Software Foundation; either version 2     |
-      |   of the License, or (at your option) any later version.             |
-      |                                                                      |
-      |   This program is distributed in the hope that it will be useful,    |
-      |   but WITHOUT ANY WARRANTY; without even the implied warranty of     |
-      |   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the      |
-      |   GNU General Public License for more details.                       |
-      |                                                                      |
-      |   You should have received a copy of the GNU General Public License  |
-      |   along with this program; if not, write to the Free Software        |
-      |   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA          |
-      |   02111-1307, USA. The GNU GPL license is also available through     |
-      |   the world-wide-web at http://www.gnu.org/copyleft/gpl.html         |
-      +----------------------------------------------------------------------+
-      | Authors: Thomas Depraetere <depraetere@ipm.ucl.ac.be>                |
-      |          Hugues Peeters    <peeters@ipm.ucl.ac.be>                   |
-      |          Christophe Gesche <gesche@ipm.ucl.ac.be>                    |
-      |                                                                      |
-      | e-class changes by: Costas Tsibanis <costas@noc.uoa.gr>              |
-      |                     Yannis Exidaridis <jexi@noc.uoa.gr>              |
-      |                     Alexandros Diamantidis <adia@noc.uoa.gr>         |
-      +----------------------------------------------------------------------+
+/**=============================================================================
+       	GUnet e-Class 2.0 
+        E-learning and Course Management Program  
+================================================================================
+       	Copyright(c) 2003-2006  Greek Universities Network - GUnet
+        Á full copyright notice can be read in "/info/copyright.txt".
+        
+       	Authors:    Costas Tsibanis <k.tsibanis@noc.uoa.gr>
+        	    Yannis Exidaridis <jexi@noc.uoa.gr> 
+      		    Alexandros Diamantidis <adia@noc.uoa.gr> 
 
- */
-/*
- * Send a user's password via e-mail
- */
+        For a full list of contributors, see "credits.txt".  
+     
+        This program is a free software under the terms of the GNU 
+        (General Public License) as published by the Free Software 
+        Foundation. See the GNU License for more details. 
+        The full license can be read in "license.txt".
+     
+       	Contact address: GUnet Asynchronous Teleteaching Group, 
+        Network Operations Center, University of Athens, 
+        Panepistimiopolis Ilissia, 15784, Athens, Greece
+        eMail: eclassadmin@gunet.gr
+==============================================================================*/
 
-$langFiles = 'lostpass';
+/**===========================================================================
+	lostpass.php
+	@last update: 12-07-2006 by Karatzidis Stratos
+	@authors list: Karatzidis Stratos <kstratos@uom.gr>
+		       Vagelis Pitsioygas <vagpits@uom.gr>
+==============================================================================        
+  @Description: Send a user's password via e-mail
 
-include '../../include/init.php';
+ 	
+==============================================================================
+*/
+
+//LANGFILES, BASETHEME, OTHER INCLUDES AND NAMETOOLS
+$langFiles = array('lostpass');
+include '../../include/baseTheme.php';
+include 'auth.inc.php';
+include('../../include/sendMail.inc.php');
+
+// Initialise $tool_content
+$tool_content = ""; 
+
 
 $nameTools = $lang_remind_pass;
-begin_page();
-
-include('../../include/sendMail.inc.php');
 
 function valid_email($e) {
 	$elements = explode('@', $e);
@@ -56,59 +55,59 @@ function valid_email($e) {
 	return TRUE;
 }
 
-?>
-<html>
-<body bgcolor="white">
-	<tr><td>
-	<table cellpadding="3" cellspacing="0" border="0" width="100%">
-	<tr valign="top" bgcolor="<?= $color2 ?>">
-	<td><font size="2" face="arial, helvetica">
-	<p align="justify" style="padding-left: 10px; padding-right: 10px;">
-		
-<?
 if (!isset($femail)) {
 	/***** Email address entry form *****/
 
-echo $lang_pass_intro;
-?>
-	<form method="post" action="<?= $REQUEST_URI ?>">
-	<em style="padding-left:5px; font-size:10pt;"><?= $lang_email ?>: </em>
-	<input type="text" name="femail" size="40"><br><br>
-	<input type="submit" name="doit" value="<?= $lang_pass_submit ?>">
-	</form>
-<?
-} else {
-	if (!valid_email($femail)) {
-		echo $lang_pass_invalid_mail1;
-		echo "<code> $femail </code>";
-		echo $lang_pass_invalid_mail2;
-		echo " <a href='mailto: $emailhelpdesk'>$emailhelpdesk</a>, "; 
-		echo $lang_pass_invalid_mail3;
-?>
-		<form method="post" action="<?= $REQUEST_URI ?>">
-		<input type="text" name="femail" size="50"><br>
-		<input type="submit" name="doit" value="<?= $lang_pass_submit ?>">
-		</form>
-<?
-	} else {
-/***** If valid e-mail address was entered, find user and send email *****/
+$tool_content .= $lang_pass_intro;
+
+$tool_content .= "<form method=\"post\" action=\"".$REQUEST_URI."\">
+	<em style=\"padding-left:5px; font-size:10pt;\">".$lang_email.": </em>
+	<input type=\"text\" name=\"femail\" size=\"40\"><br><br>
+	<input type=\"submit\" name=\"doit\" value=\"".$lang_pass_submit."\">
+	</form>";
+
+} 
+else 
+{
+	if (!valid_email($femail)) 
+	{
+		$tool_content .=  $lang_pass_invalid_mail1
+		."<code> ".$femail." </code>"
+		.$lang_pass_invalid_mail2
+		." <a href='mailto: $emailhelpdesk'>".$emailhelpdesk."</a>, "
+		.$lang_pass_invalid_mail3;
+
+		$tool_content .= "<form method=\"post\" action=\"".$REQUEST_URI."\">
+		<input type=\"text\" name=\"femail\" size=\"50\"><br>
+		<input type=\"submit\" name=\"doit\" value=\"".$lang_pass_submit."\">
+		</form>";
+
+	} 
+	else 
+	{
+		/***** If valid e-mail address was entered, find user and send email *****/
 		$res = mysql_query("SELECT nom, prenom, username, password, statut, inst_id FROM user
 			WHERE email = '" . mysql_escape_string($femail) . "'");
-		if (mysql_num_rows($res) > 0) {
-		$text = $lang_pass_email_intro. $emailhelpdesk;
-			if (mysql_num_rows($res) == 1) {
+		if (mysql_num_rows($res) > 0) 
+		{
+			$text = $lang_pass_email_intro. $emailhelpdesk;
+			if (mysql_num_rows($res) == 1) 
+			{
 				$text .= "\n$lang_pass_email_account\n";
-			} else {
+			} 
+			else 
+			{
 				$text .= "\n$lang_pass_email_many_accounts\n";
 			}
-			while ($s = mysql_fetch_array($res, MYSQL_ASSOC)) {
+			while ($s = mysql_fetch_array($res, MYSQL_ASSOC)) 
+			{
 				$text .= "
-$lang_pass_email_name " . htmlspecialchars($s['prenom']." ".$s['nom']) . "
-$lang_pass_email_status " . (($s['statut'] == 1)? "$lang_prof": (
-		($s['statut'] == 5)? "$lang_student": "$lang_other")) . "
-$lang_pass_email_username " . htmlspecialchars($s['username']) .
-($s['inst_id']? " $lang_pass_email_ldap": "
-$lang_pass_email_password " . htmlspecialchars($s['password']) . "\n");
+					$lang_pass_email_name " . htmlspecialchars($s['prenom']." ".$s['nom']) . "
+					$lang_pass_email_status " . (($s['statut'] == 1)? "$lang_prof": (
+						($s['statut'] == 5)? "$lang_student": "$lang_other")) . "
+						$lang_pass_email_username " . htmlspecialchars($s['username']) .
+						($s['inst_id']? " $lang_pass_email_ldap": "
+						$lang_pass_email_password " . htmlspecialchars($s['password']) . "\n");
 			}
 			/***** Account details found, now send e-mail *****/
 			$emailheaders = "From: $siteName <$emailAdministrator>\n".
@@ -117,27 +116,32 @@ $lang_pass_email_password " . htmlspecialchars($s['password']) . "\n");
 					"Content-Transfer-Encoding: 8bit";
 			$emailsubject = "Account information";
 			if (!send_mail($siteName, $emailAdministrator, '', $femail,
-				   $emailsubject, $text, $charset)) {
-					echo $lang_pass_email_error1;
-					echo "<code> $femail </code>.";
-					echo $lang_pass_email_error2;
-					echo "<a href='mailto:$emailhelpdesk'>$emailhelpdesk</a>.";
-			} else {
-				echo $lang_pass_email_ok;
-				echo "<code> $femail </code>.";
+				   $emailsubject, $text, $charset)) 
+			{
+				$tool_content .= $lang_pass_email_error1
+					."<code> ".$femail." </code>"
+					.$lang_pass_email_error2
+					."<a href='mailto:$emailhelpdesk'>$emailhelpdesk</a>.";
+			} 
+			else 
+			{
+				$tool_content .= $lang_pass_email_ok
+				."<code> ".$femail." </code>.";
 			}
-		} else {
-			echo $lang_pass_not_found1;	
-			echo "<code> $femail </code>."; 
-			echo $lang_pass_not_found2;
-			echo "<a href='mailto: $emailhelpdesk'>$emailhelpdesk</a>"; 
-			echo $lang_pass_not_found3;
+		} 
+		else 
+		{
+			$tool_content .= $lang_pass_not_found1
+			."<code> ".$femail." </code>."
+			.$lang_pass_not_found2
+			."<a href='mailto: $emailhelpdesk'>$emailhelpdesk</a>"
+			.$lang_pass_not_found3;
 		}
 	}
 }
-?>		
-			</p></font></td></tr></table>
-	</td></tr>
-</table>
-</body>
-</html>
+
+$tool_content .= "<br />";
+
+draw($tool_content,0);
+
+?>
