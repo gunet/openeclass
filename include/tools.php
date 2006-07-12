a90e6b37eeb4cf0da073bfee13e324a37b9cc3f5
Yannis Exidaridis <jexi@noc.uoa.gr>
2006-07-12 09:03:54 +0000

    προσθήκη link για τις "ανακοινώσεις διαχειριστή"

--- include/tools.php
+++ include/tools.php
@@ -405,6 +405,10 @@ function adminMenu(){
 
 	array_push($sideMenuSubGroup, $langGenAdmin);
 
+	array_push($sideMenuText, $langAdminAn);
+	array_push($sideMenuLink, "../admin/adminannouncements.php");
+	array_push($sideMenuImg, "adm_announce.gif");
+
 	//array_push($sideMenuText, $langVersion);
 	//array_push($sideMenuLink, "../admin/about.php");
 	//array_push($sideMenuImg, "eclass_version.gif");
