13cb41c47a4227664ea6e504c903c6677ba35dfc
Yannis Exidaridis <jexi@noc.uoa.gr>
2006-09-29 13:41:19 +0000

    διορθώσεις μηνυμάτων

--- modules/lang/greek/registration.inc.php
+++ modules/lang/greek/registration.inc.php
@@ -1,39 +1,4 @@
 <?php
-/*
-      +----------------------------------------------------------------------+
-      | CLAROLINE version 1.3.0 $Revision$                            |
-      +----------------------------------------------------------------------+
-      | Copyright (c) 2001, 2002 Universite catholique de Louvain (UCL)      |
-      +----------------------------------------------------------------------+
-      |   $Id$      |
-      |   English Translation                                                |
-      +----------------------------------------------------------------------+
-      |   This program is free software; you can redistribute it and/or      |
-      |   modify it under the terms of the GNU General Public License        |
-      |   as published by the Free Software Foundation; either version 2     |
-      |   of the License, or (at your option) any later version.             |
-      |                                                                      |
-      |   This program is distributed in the hope that it will be useful,    |
-      |   but WITHOUT ANY WARRANTY; without even the implied warranty of     |
-      |   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the      |
-      |   GNU General Public License for more details.                       |
-      |                                                                      |
-      |   You should have received a copy of the GNU General Public License  |
-      |   along with this program; if not, write to the Free Software        |
-      |   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA          |
-      |   02111-1307, USA. The GNU GPL license is also available through     |
-      |   the world-wide-web at http://www.gnu.org/copyleft/gpl.html         |
-      +----------------------------------------------------------------------+
-      | Authors: Thomas Depraetere <depraetere@ipm.ucl.ac.be>                |
-      |          Hugues Peeters    <peeters@ipm.ucl.ac.be>                   |
-      |          Christophe Geschι <gesche@ipm.ucl.ac.be>                    |
-      +----------------------------------------------------------------------+
-      | Translator :                                                         |
-      |          Thomas Depraetere <depraetere@ipm.ucl.ac.be>                |
-      |          Andrew Lynn       <Andrew.Lynn@strath.ac.uk>                |
-      +----------------------------------------------------------------------+
- */
-// REGISTRATION - AUTH
 
 
 $langSee = "Προεπισκόπηση";
@@ -331,7 +296,7 @@ $langAcceptRequest = "Εγγραφή";
 $langActions = "Ενέργειες";
 $langListRequest = "Λίστα Αιτήσεων";
 $langComments = "Σχόλια";
-$langTeacherRequestHasDeleted = "Η αίτηση του καθηγητή διαγράφτηκε!";
+$langTeacherRequestHasDeleted = "Η αίτηση του καθηγητή διαγράφηκε!";
 $langRejectRequestSubject = "Απόρριψη αίτησης εγγραφής στην Πλατφόρμα Ασύγχρονης Τηλεκπαίδευσης";
 $langGoingRejectRequest = "Πρόκειται να απορρίψετε την αίτηση καθηγητή με στοιχεία:";
 $langRequestSendMessage = "Αποστολή μηνύματος στο χρήστη - e-mail διεύθυνση:";
