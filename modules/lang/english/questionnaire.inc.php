8d3010790b790fb8634d6f1c374537e3e5cc13eb
Dionysios G. Synodinos <dsin@noc.ntua.gr>
2006-10-23 13:34:00 +0000

    minor addition

--- modules/lang/english/questionnaire.inc.php
+++ modules/lang/english/questionnaire.inc.php
@@ -130,4 +130,7 @@ $langSurveyDeleteMsg = "Are you sure you would like to delete this survey?";
 $langPollDeleteMsg = "Are you sure you would like to delete this poll?";
 $langSurveyDeleteYes = "Yes";
 $langSurveyDeleteNo = "No";
+
+$langSurveyInfo1 ="Please ass the name of the Poll and how logn you wish it to be available to the users (by default 365 days)";
+$langSurveyInfo2 ="Please confirm that the name of the Poll is correct and that the time period is th edesired. Then you may choose from a the list of COLLES/ATTL questions or add your own question in the blank fields.";
 ?>
