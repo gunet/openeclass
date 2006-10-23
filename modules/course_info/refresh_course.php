468d0462b7b5377ace88215fff149ad201402cd4
Pitsiougas Vagelis <vagpits@uom.gr>
2006-10-23 08:43:41 +0000

    Εφαρμογή νέου template στην ανανέωση μαθήματος

--- modules/course_info/refresh_course.php
+++ modules/course_info/refresh_course.php
@@ -1,99 +1,105 @@
 <?php
-/*
-      +----------------------------------------------------------------------+
-      | CLAROLINE version 1.3.0  $Revision$                            |
-      +----------------------------------------------------------------------+
-      | Copyright (c) 2001, 2002 Universite catholique de Louvain (UCL)      |
-      +----------------------------------------------------------------------+
-      |   $Id$
-      +----------------------------------------------------------------------+
-      |   This program is free software; you can redistribute it and/or      |
-      |   modify it under the terms of the GNU General Public License        |
-      |   as published by the Free Software Foundation; either version 2     |
-      |   of the License, or (at your option) any later version.             |
-      |                                                                      |
-      |   This program is distributed in the hope that it will be useful,    |
-      |   but WITHOUT ANY WARRANTY; without even the implied warranty of     |
-      |   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the      |
-      |   GNU General Public License for more details.                       |
-      |                                                                      |
-      |   You should have received a copy of the GNU General Public License  |
-      |   along with this program; if not, write to the Free Software        |
-      |   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA          |
-      |   02111-1307, USA. The GNU GPL license is also available through     |
-      |   the world-wide-web at http://www.gnu.org/copyleft/gpl.html         |
-      +----------------------------------------------------------------------+
-      | Authors: Thomas Depraetere <depraetere@ipm.ucl.ac.be>                |
-      |          Hugues Peeters    <peeters@ipm.ucl.ac.be>                   |
-      |          Christophe Geschι <gesche@ipm.ucl.ac.be>                    |
-      +----------------------------------------------------------------------+
- */
+/**=============================================================================
+           GUnet e-Class 2.0
+        E-learning and Course Management Program
+================================================================================
+           Copyright(c) 2003-2006  Greek Universities Network - GUnet
+        Α full copyright notice can be read in "/info/copyright.txt".
+
+           Authors:    Costas Tsibanis <k.tsibanis@noc.uoa.gr>
+                Yannis Exidaridis <jexi@noc.uoa.gr>
+                  Alexandros Diamantidis <adia@noc.uoa.gr>
+
+        For a full list of contributors, see "credits.txt".
+
+        This program is a free software under the terms of the GNU
+        (General Public License) as published by the Free Software
+        Foundation. See the GNU License for more details.
+        The full license can be read in "license.txt".
+
+           Contact address: GUnet Asynchronous Teleteaching Group,
+        Network Operations Center, University of Athens,
+        Panepistimiopolis Ilissia, 15784, Athens, Greece
+        eMail: eclassadmin@gunet.gr
+==============================================================================*/
+
+/**===========================================================================
+    refresh_course.php
+    @last update: 23-10-2006 by Pitsiougas Vagelis
+    @authors list: Karatzidis Stratos <kstratos@uom.gr>
+               Pitsiougas Vagelis <vagpits@uom.gr>
+==============================================================================
+        @Description: Refresh page for a course
+
+==============================================================================*/
 
 $require_current_course=TRUE;
 $require_login=TRUE;
 $langFiles="course_info";
 
-include '../../include/init.php';
+//include '../../include/init.php';
+
+include '../../include/baseTheme.php';
 
 $local_style="table#info { background: $color2 }";
 $nameTools = $langRefreshCourse;
-begin_page();
+
+$tool_content = "";
 
 if (!$is_adminOfCourse) 
 {
-	echo "Error! access by non-admin.";
-	end_page();
+	$tool_content .= "Error! access by non-admin.";
 	exit();
 }
 
 if(isset($submit)) {
 	mysql_select_db($mysqlMainDb);
 	if (isset($delusers)) 	
-		delete_users();
+		$tool_content .= delete_users();
 	if (isset($delannounces))
-		delete_announcements();
+		$tool_content .= delete_announcements();
 
 	mysql_select_db($currentCourseID);
         if (isset($delagenda))
-		delete_agenda();
+		$tool_content .= delete_agenda();
 	if (isset($hideworks))
-		hide_work();
-	echo "<p><center><a href='infocours.php'>$langBack</a></center></p>";
+		$tool_content .= hide_work();
+	$tool_content .=  "<p><center><a href='infocours.php'>$langBack</a></center></p>";
 				
 } else {
-	echo "<blockquote>$langRefresh</blockquote>";
-	echo "<form action='refresh_course.php' method='post'>
-		<table id='info'>
-			<tr><td>&nbsp;</td></tr>
-			<tr><td><input type='checkbox' name='delusers'></td>
+	$tool_content .= "<blockquote>$langRefresh</blockquote>";
+	$tool_content .= "<form action='refresh_course.php' method='post'>
+		<table width=\"99%\"><caption>Επιλογή Ενεργειών</caption<tbody>
+			<tr><td align='center'><input type='checkbox' name='delusers'></td>
 				<td>$langUserDelCourse</td></tr>
-			<tr><td><input type='checkbox' name='delannounces'></td>
+			<tr><td align='center'><input type='checkbox' name='delannounces'></td>
 				<td>$langAnnouncesDel</td></tr>
-			<tr><td><input type='checkbox' name='delagenda'></td>
+			<tr><td align='center'><input type='checkbox' name='delagenda'></td>
 				<td>$langAgendaDel</td></tr>
-			<tr><td><input type='checkbox' name='hideworks'></td>
-				<td>$langHideWork</td></tr>
-			<tr><td>&nbsp;</td></tr>
-			<tr><td colspan='2'><input type='submit' value='$langSubmit' name='submit'></td></tr>
+			<tr><td align='center'><input type='checkbox' name='hideworks'></td>
+				<td>$langHideWork</td></tr></tbody>
 		</table>
+		<br>
+		<input type='submit' value='$langSubmit' name='submit'>
 		</form>";
 	
 }
-end_page();
+
+draw($tool_content, 2, 'course_info');
 
 	  
 function delete_users() {
 	global $currentCourseID,$langUsersDeleted;
 
 	db_query("DELETE FROM cours_user WHERE code_cours='$currentCourseID' and statut <> '1'");
-	echo "<p>$langUsersDeleted</p>";
+	return "<p>$langUsersDeleted</p>";
 }
 
 function delete_announcements() {
 	global $currentCourseID,$langAnnDeleted;
 
 	db_query("DELETE FROM annonces WHERE code_cours='$currentCourseID'");
-	echo "<p>$langAnnDeleted</p>";
+	return "<p>$langAnnDeleted</p>";
 }
 
 function delete_agenda() {
@@ -104,21 +110,21 @@ function delete_agenda() {
 	##[BEGIN personalisation modification]############
 	db_query("DELETE FROM ".$mysqlMainDb.".agenda WHERE lesson_code='$currentCourseID'");
 	##[END personalisation modification]############
-	echo "<p>$langAgendaDeleted</p>";
+	return "<p>$langAgendaDeleted</p>";
 }
 
 function hide_doc()  {
 	global $langDocsDeleted;
 	
 	db_query("UPDATE document SET visibility='i'");
-	echo "<p>$langDocsDeleted</p>";
+	return "<p>$langDocsDeleted</p>";
 }
 
 function hide_work()  {
 	global $langWorksDeleted;
 
 	db_query("UPDATE assignments SET active=0");
-	echo "<p>$langWorksDeleted</p>";
+	return "<p>$langWorksDeleted</p>";
 }
 
 
