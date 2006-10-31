945bd9e269de7b28985fdced2205a6b92d2ec318
Evelthon Prodromou <eprodromou@upnet.gr>
2006-10-31 14:50:02 +0000

    - Fixed  url issues - Updated UI

--- manuals/manual.php
+++ manuals/manual.php
@@ -1,67 +1,61 @@
 <? 
-/*
-      +----------------------------------------------------------------------+
-      | e-Class version 1.2                                                  |
-      | based on CLAROLINE version 1.3.0 $Revision$                   |
-      +----------------------------------------------------------------------+
-      | Copyright (c) 2001, 2002 Universite catholique de Louvain (UCL)      |
-      | Copyright (c) 2003 GUNet                                             |
-      +----------------------------------------------------------------------+
-      |   This program is free software; you can redistribute it and/or      |
-      |   modify it under the terms of the GNU General Public License        |
-      |   as published by the Free Software Foundation; either version 2     |
-      |   of the License, or (at your option) any later version.             |
-      |                                                                      |
-      |   This program is distributed in the hope that it will be useful,    |
-      |   but WITHOUT ANY WARRANTY; without even the implied warranty of     |
-      |   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the      |
-      |   GNU General Public License for more details.                       |
-      |                                                                      |
-      |   You should have received a copy of the GNU General Public License  |
-      |   along with this program; if not, write to the Free Software        |
-      |   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA          |
-      |   02111-1307, USA. The GNU GPL license is also available through     |
-      |   the world-wide-web at http://www.gnu.org/copyleft/gpl.html         |
-      +----------------------------------------------------------------------+
-      | e-Class Authors:    Costas Tsibanis <costas@noc.uoa.gr>              |
-      |                     Yannis Exidaridis <jexi@noc.uoa.gr>              |
-      |                     Alexandros Diamantidis <adia@noc.uoa.gr>         |
-      |                                                                      |
-      | Claroline Authors:  Thomas Depraetere <depraetere@ipm.ucl.ac.be>     |
-      |                     Hugues Peeters    <peeters@ipm.ucl.ac.be>        |
-      |                     Christophe Geschι <gesche@ipm.ucl.ac.be>         |
-      |                                                                      |
-      +----------------------------------------------------------------------+
-*/
+/**===========================================================================
+*              GUnet e-Class 2.0
+*       E-learning and Course Management Program
+* ===========================================================================
+*	Copyright(c) 2003-2006  Greek Universities Network - GUnet
+*	Α full copyright notice can be read in "/info/copyright.txt".
+*
+*  Authors:	Costas Tsibanis <k.tsibanis@noc.uoa.gr>
+*				Yannis Exidaridis <jexi@noc.uoa.gr>
+*				Alexandros Diamantidis <adia@noc.uoa.gr>
+*
+*	For a full list of contributors, see "credits.txt".
+*
+*	This program is a free software under the terms of the GNU
+*	(General Public License) as published by the Free Software
+*	Foundation. See the GNU License for more details.
+*	The full license can be read in "license.txt".
+*
+*	Contact address: 	GUnet Asynchronous Teleteaching Group,
+*						Network Operations Center, University of Athens,
+*						Panepistimiopolis Ilissia, 15784, Athens, Greece
+*						eMail: eclassadmin@gunet.gr
+============================================================================*/
 
+/**
+ * e-Class manuals Component
+ * 
+ * @author Evelthon Prodromou <eprodromou@upnet.gr>
+ * @version $Id$
+ * 
+ * @abstract This component offers  the platform's manuals.
+ *
+ */
 
-//include ('../include/init.php');
 $path2add=2;
 include '../include/baseTheme.php';
 $nameTools = "Διαθέσιμα Εγχειρίδια";
-//begin_page();
 
-//$um="$urlServer/modules/manuals/";
 $tool_content = "";
+$urlServer = strrev(substr(strrev($urlServer),1));
+
 $tool_content .= <<<tCont
 <p>Στη σελίδα αυτή θα βρείτε χρήσιμα εγχειρίδια που αφορούν την πλατφόρμα e-Class.</p>
-<ul>
+<ul class="listBullet">
 
-<li><a href="$urlServer/modules/manuals/e-Class.pdf" target=_blank>Αναλυτική Περιγραφή e-Class</a></li>
-<li><a href="$urlServer/modules/manuals/e-Class_short.pdf" target=_blank>Σύντομη Περιγραφή e-Class</a></li>
+<li><a href="$urlServer/manuals/e-Class.pdf" target=_blank>Αναλυτική Περιγραφή e-Class</a></li>
+<li><a href="$urlServer/manuals/e-Class_short.pdf" target=_blank>Σύντομη Περιγραφή e-Class</a></li>
 
-</ul>
-<ul>
 
-<li>Εγχειρίδιο Χρήστη (Μαθητή/Φοιτητή): σε μορφή <a href="$urlServer/modules/manuals/manS/ManS.pdf" target=_blank>PDF</a>
-ή <a href="$urlServer/modules/manuals/manS/ManS.htm" target=_blank>HTML</a></li>
-<li>Εγχειρίδιο Καθηγητή: σε μορφή <a href="$urlServer/modules/manuals/manT/ManT.pdf" target=_blank>PDF</a>
-ή <a href="$urlServer/modules/manuals/manT/ManT.htm" target=_blank>HTML</a></li>
-</ul>
 
-<ul>
+<li>Εγχειρίδιο Χρήστη (Μαθητή/Φοιτητή): σε μορφή <a href="$urlServer/manuals/manS/ManS.pdf" target=_blank>PDF</a>
+ή <a href="$urlServer/manuals/manS/ManS.htm" target=_blank>HTML</a></li>
+<li>Εγχειρίδιο Καθηγητή: σε μορφή <a href="$urlServer/manuals/manT/ManT.pdf" target=_blank>PDF</a>
+ή <a href="$urlServer/manuals/manT/ManT.htm" target=_blank>HTML</a></li>
 
-<li><a href="$urlServer/modules/manuals/Teleteaching_Std.pdf" target=_blank>Πρότυπα Μαθησιακών Τεχνολογιών</a></li>
+
+<li><a href="$urlServer/manuals/Teleteaching_Std.pdf" target=_blank>Πρότυπα Μαθησιακών Τεχνολογιών</a></li>
 
 </ul>
 
