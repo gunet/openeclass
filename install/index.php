9737f7f29df4c962a844a944e436ab8b096c7d56
Yannis Exidaridis <jexi@noc.uoa.gr>
2006-11-01 09:28:43 +0000

    bug στην δημιουργία του πινακα pma_bookmark

--- install/index.php
+++ install/index.php
@@ -897,7 +897,7 @@ elseif(isset($install7))
 
 		mysql_query("
     CREATE TABLE `pma_bookmark` (
-       id int(11) DEFAULT '0' NOT NULL auto_increment,
+       id int(11) NOT NULL auto_increment,
        dbase varchar(255) NOT NULL,
        user varchar(255) NOT NULL,
        label varchar(255) NOT NULL,
