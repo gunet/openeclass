c1b2ed171015f8b1e91c9e305b09a7adfe98f0a4
Evelthon Prodromou <eprodromou@upnet.gr>
2006-12-13 02:14:26 +0000

    UI improvements for eclass 2.0 compatibility

--- modules/auth/newuser.php
+++ modules/auth/newuser.php
@@ -25,7 +25,7 @@
 
 /**===========================================================================
 	newuser.php
-	@last update: 07-06-2006 by Stratos Karatzidis
+* @version $Id$
 	@authors list: Karatzidis Stratos <kstratos@uom.gr>
 		       Vagelis Pitsioygas <vagpits@uom.gr>
 ==============================================================================        
@@ -45,54 +45,56 @@ $nameTools = $langUserDetails;
 $tool_content = "";		// Initialise $tool_content
 
 // Main body
-$tool_content .= "<table width=\"99%\"><tr>
-<td width=\"600\">
+$tool_content .= "
 <form action=\"newuser_second.php\" method=\"post\" name=\"newusersecond\">
-<table cellpadding=\"3\" cellspacing=\"0\" border=\"0\" width=\"100%\" bgcolor=\"".$color2."\">
-<tr valign=\"top\">
-<td>".$langName."</td>
-<td><input type=\"text\" name=\"prenom_form\"><font size=\"1\">&nbsp;(*)</font></td>
+
+<table width=\"99%\">
+<thead>
+<t>
+<th>$langName</th>
+<td><input type=\"text\" name=\"prenom_form\">(*)</td>
 </tr>
-<tr><td>".$langSurname."</td>
-<td><input type=\"text\" name=\"nom_form\"><font size=\"1\">&nbsp;(*)</font></td>
+<tr><th>$langSurname</th>
+<td><input type=\"text\" name=\"nom_form\">(*)</td>
 </tr>
-<tr><td>".$langUsername."</td>
-<td><input type=\"text\" name=\"uname\" size=\"20\" maxlength=\"20\"><font size=\"1\">&nbsp;(*)</font><font size=\"1\">&nbsp;(**)</font></td>
+<tr><th>$langUsername</th>
+<td><input type=\"text\" name=\"uname\" size=\"20\" maxlength=\"20\">(*) (**) $langUserNotice hhh</td>
 </tr>
-<tr><td>&nbsp;</td><td><font size=\"1\">".$langUserNotice."</font></td></tr>
-<tr><td>".$langPass."</td>
-<td><input type=\"password\" name=\"password1\" size=\"20\" maxlength=\"20\"><font size=\"1\">&nbsp;(*)</font><font size=\"1\">&nbsp;(**)</font></td>
+
+<tr><th>$langPass</th>
+<td><input type=\"password\" name=\"password1\" size=\"20\" maxlength=\"20\">(*) (**)</td>
 </tr>
 <tr>
-<td>".$langConfirmation."<br /></td>
-<td valign=\"top\"><input type=\"password\" name=\"password\" size=\"20\" maxlength=\"20\"><font size=\"1\">&nbsp;(*)</font><font size=\"1\">&nbsp;(**)</font></td>
+<th>$langConfirmation</th>
+<td valign=\"top\"><input type=\"password\" name=\"password\" size=\"20\" maxlength=\"20\">(*) (**) $langUserNotice</td>
 </tr>
-<tr><td>&nbsp;</td><td><font size=\"1\">".$langUserNotice."</font></td></tr>
+
 <tr>
-<td>".$langEmail."</td>
-<td><input type=\"text\" name=\"email\"></td>
+<th>$langEmail</th>
+<td><input type=\"text\" name=\"email\"> $langEmailNotice</td>
 </tr>
-<tr><td>&nbsp;</td><td><font size=\"1\">".$langEmailNotice."</font></td></tr>
-<tr><td>".$langAm."</td>
+
+<tr><th>$langAm</th>
 <td><input type=\"text\" name=\"am\"></td>
 </tr>
-<tr><td>".$langDepartment."</td>
+<tr><th>$langDepartment</th>
 <td><select name=\"department\">";
 $deps=mysql_query("SELECT name, id FROM faculte ORDER BY id");
 while ($dep = mysql_fetch_array($deps)) 
 	$tool_content .= "\n<option value=\"".$dep[1]."\">".$dep[0]."</option>";
 $tool_content .= "</select></td>
 </tr>
-<tr><td>&nbsp;</td><td>
+</thead>
+</table>
+<br/>
 <input type=\"hidden\" name=\"auth\" value=\"1\">
 <input type=\"submit\" name=\"submit\" value=\"".$langRegistration."\"></td></tr>
-</table>
+
 </form>
-</td>
-</tr>
-<tr><td align=\"right\"><font size=\"1\">".$langRequiredFields."</font><br />
-<font size=\"1\">".$star2 . $langCharactersNotAllowed."</font>
-</td></tr></table>";
+<br/>
+<p>$langRequiredFields</p>
+<p>$star2 $langCharactersNotAllowed</p>
+";
 
 draw($tool_content,0);
 
