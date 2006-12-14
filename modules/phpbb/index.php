38daf636c13672ca06d42120abfe19f37ef06ac9
Evelthon Prodromou <eprodromou@upnet.gr>
2006-12-14 05:51:32 +0000

    UI improvements for eclass 2.0 compatibility

--- modules/phpbb/index.php
+++ modules/phpbb/index.php
@@ -1,61 +1,61 @@
 <?php
 /**=============================================================================
-       	GUnet e-Class 2.0 
-        E-learning and Course Management Program  
+GUnet e-Class 2.0
+E-learning and Course Management Program
 ================================================================================
-       	Copyright(c) 2003-2006  Greek Universities Network - GUnet
-        A full copyright notice can be read in "/info/copyright.txt".
-        
-       	Authors:    Costas Tsibanis <k.tsibanis@noc.uoa.gr>
-        	    Yannis Exidaridis <jexi@noc.uoa.gr> 
-      		    Alexandros Diamantidis <adia@noc.uoa.gr> 
+Copyright(c) 2003-2006  Greek Universities Network - GUnet
+A full copyright notice can be read in "/info/copyright.txt".
 
-        For a full list of contributors, see "credits.txt".  
-     
-        This program is a free software under the terms of the GNU 
-        (General Public License) as published by the Free Software 
-        Foundation. See the GNU License for more details. 
-        The full license can be read in "license.txt".
-     
-       	Contact address: GUnet Asynchronous Teleteaching Group, 
-        Network Operations Center, University of Athens, 
-        Panepistimiopolis Ilissia, 15784, Athens, Greece
-        eMail: eclassadmin@gunet.gr
+Authors:    Costas Tsibanis <k.tsibanis@noc.uoa.gr>
+Yannis Exidaridis <jexi@noc.uoa.gr>
+Alexandros Diamantidis <adia@noc.uoa.gr>
+
+For a full list of contributors, see "credits.txt".
+
+This program is a free software under the terms of the GNU
+(General Public License) as published by the Free Software
+Foundation. See the GNU License for more details.
+The full license can be read in "license.txt".
+
+Contact address: GUnet Asynchronous Teleteaching Group,
+Network Operations Center, University of Athens,
+Panepistimiopolis Ilissia, 15784, Athens, Greece
+eMail: eclassadmin@gunet.gr
 ==============================================================================*/
 
 /**===========================================================================
-        phpbb/index.php
-        @last update: 2006-07-23 by Artemios G. Voyiatzis
-        @authors list: Artemios G. Voyiatzis <bogart@upnet.gr>
+phpbb/index.php
+@last update: 2006-07-23 by Artemios G. Voyiatzis
+@authors list: Artemios G. Voyiatzis <bogart@upnet.gr>
 
-        based on Claroline version 1.7 licensed under GPL
-              copyright (c) 2001, 2006 Universite catholique de Louvain (UCL)
+based on Claroline version 1.7 licensed under GPL
+copyright (c) 2001, 2006 Universite catholique de Louvain (UCL)
 
-        Claroline authors: Piraux Sebastien <pir@cerdecam.be>
-                      Lederer Guillaume <led@cerdecam.be>
+Claroline authors: Piraux Sebastien <pir@cerdecam.be>
+Lederer Guillaume <led@cerdecam.be>
 
-	based on phpBB version 1.4.1 licensed under GPL
-		copyright (c) 2001, The phpBB Group
+based on phpBB version 1.4.1 licensed under GPL
+copyright (c) 2001, The phpBB Group
 ==============================================================================
-    @Description: This module implements a per course forum for supporting
-	discussions between teachers and students or group of students.
-	It is a heavily modified adaptation of phpBB for (initially) Claroline
-	and (later) eclass. In the future, a new forum should be developed.
-	Currently we use only a fraction of phpBB tables and functionality
-	(viewforum, viewtopic, post_reply, newtopic); the time cost is
-	enormous for both core phpBB code upgrades and migration from an
-	existing (phpBB-based) to a new eclass forum :-(
+@Description: This module implements a per course forum for supporting
+discussions between teachers and students or group of students.
+It is a heavily modified adaptation of phpBB for (initially) Claroline
+and (later) eclass. In the future, a new forum should be developed.
+Currently we use only a fraction of phpBB tables and functionality
+(viewforum, viewtopic, post_reply, newtopic); the time cost is
+enormous for both core phpBB code upgrades and migration from an
+existing (phpBB-based) to a new eclass forum :-(
 
-    @Comments:
+@Comments:
 
-    @todo:
+@todo:
 ==============================================================================
 */
 
 error_reporting(E_ALL);
 /*
- * GUNET eclass 2.0 standard stuff
- */
+* GUNET eclass 2.0 standard stuff
+*/
 $require_current_course = TRUE;
 $langFiles = 'phpbb';
 $require_help = FALSE;
@@ -71,37 +71,38 @@ $action->record('MODULE_ID_FORUM');
 
 
 /*
- * Tool-specific includes
- */
+* Tool-specific includes
+*/
 include_once("./config.php");
 
 /******************************************************************************
- * Actual code starts here
- *****************************************************************************/
+* Actual code starts here
+*****************************************************************************/
 
 /*
- * First, some decoration
- */
+* First, some decoration
+*/
 
 if ( $is_adminOfCourse || $is_admin ) {
 	$tool_content .= "<a href=\"../forum_admin/forum_admin.php\">$l_adminpanel</a><P>&nbsp;";
 }
 $tool_content .= <<< cData
 <TABLE WIDTH="99%" CELLPADDING="1" CELLSPACING="0" ALIGN="CENTER" VALIGN="TOP">
-<TR><TD>
-	<TABLE BORDER="0" CELLPADDING="1" CELLSPACING="1" WIDTH="100%">
-	<TR>
-		<TD>&nbsp;</TD>
-		<TD><B>$l_forum </B></TD>
-		<TD><B>$l_topics</B></TD>
-		<TD><B>$l_posts</B></TD>
-		<TD><B>$l_lastpost</B></TD>
+<thead>
+<TR>
+	
+		<th> </th>
+		<th>$l_forum</th>
+		<th>$l_topics</th>
+		<th>$l_posts</th>
+		<th>$l_lastpost</th>
 	</TR>
+	</thead>
 cData;
 
 /*
- * Populate data with forum categories
- */
+* Populate data with forum categories
+*/
 $sql = "SELECT c.* FROM catagories c, forums f
 	 WHERE f.cat_id=c.cat_id
 	 GROUP BY c.cat_id, c.cat_title, c.cat_order
@@ -109,8 +110,7 @@ $sql = "SELECT c.* FROM catagories c, forums f
 
 if ( !$result = db_query($sql, $currentCourseID) ) {
 	$tool_content .= <<<cData
-			</TABLE>
-		</TD></TR>
+			
 		</TABLE>
 cData;
 	$tool_content .= "Unable to get categories from database<br>$sql";
@@ -142,11 +142,11 @@ if ( $total_categories ) {
 		ORDER BY f.cat_id, f.forum_id";
 	if ( !$f_res = db_query($sql, $currentCourseID) ) {
 		$tool_content .= <<<cData
-			</TABLE>
-			</TD></TR>
+			
 			</TABLE>
 cData;
-		$tool_content .= "Error getting forum data<br>$sql";
+		//		$tool_content .= "Error getting forum data<br>$sql";
+		$tool_content .= "Error getting forum data.";
 		draw($tool_content, 2);
 		exit();
 	}
@@ -157,14 +157,14 @@ cData;
 		if ( $viewcat != -1 ) {
 			if ( $categories[$i][cat_id] != $viewcat ) {
 				$title = stripslashes($categories[$i][cat_title]);
-				$tool_content .= "<TR><TD COLSPAN=5><B>$title</B></TD></TR>";
+				$tool_content .= "<TR><TD COLSPAN=5><p><b>$title</b></p></TD></TR>";
 				continue;
 			}
 		}
 		$title = stripslashes($categories[$i]["cat_title"]);
 		/// Added by Thomas for Claroline : distinguish group forums from others
 		$catNum = $categories[$i]["cat_id"];
-		$tool_content .= "<TR><TD COLSPAN=5><B>$title</B></TD></TR>";
+		$tool_content .= "<TR><TD COLSPAN=5><p><b>$title</b></p></TD></TR>";
 		@reset($forum_row);
 		for ( $x=0; $x < count($forum_row); $x++) {
 			unset($last_post);
@@ -195,7 +195,7 @@ cData;
 				$total_topics = $forum_row[$x]["forum_topics"];
 				$desc = stripslashes($forum_row[$x]["forum_desc"]);
 
-				$tool_content .= "<TD>"; 
+				$tool_content .= "<TD>";
 				$forum=$forum_row[$x]["forum_id"];
 				// Claroline function added by Thomas July 2002
 				// Visit only my group forum if not admin or tutor
@@ -204,7 +204,7 @@ cData;
 				if ( $is_adminOfCourse==1 ) { // TUTOR VIEW
 					$sqlTutor = db_query("SELECT id FROM student_group
 								WHERE forumId='$forum' AND tutor='$uid'", $currentCourseID );
-					$countTutor = mysql_num_rows($sqlTutor); 
+					$countTutor = mysql_num_rows($sqlTutor);
 					if ( $countTutor == 0 ) {
 						$tool_content .= "<a href=\"viewforum.php?forum=" . $forum_row[$x]["forum_id"] . "&$total_posts\">$name</a>";
 					} else {
@@ -212,7 +212,7 @@ cData;
 					}
 				} elseif ( $status[$dbname] == 1 OR $status[$dbname] == 2 ) { // ADMIN VIEW
 					$tool_content .= "<a href=\"viewforum.php?forum=".$forum_row[$x]["forum_id"]."&$total_posts\">$name</a>";
-				} elseif ( $catNum == 1 ) { // STUDENT VIEW 
+				} elseif ( $catNum == 1 ) { // STUDENT VIEW
 					if ( $forum == $myGroupForum ) {
 						$tool_content .= "<a href=\"viewforum.php?forum=".$forum_row[$x]["forum_id"]."&$total_posts\">$name</a>&nbsp;&nbsp;($langMyGroup)";
 					} else {
@@ -231,18 +231,17 @@ cData;
 				$tool_content .= "<TD WIDTH=5%>$total_posts</TD>";
 				$tool_content .= "<TD WIDTH=15%>$last_post</TD>";
 				$tool_content .= "</TR>";
-		      }
+			}
 		}
 	}
 }
 
 /*
- * Closing decoration and actual drawing
- */
+* Closing decoration and actual drawing
+*/
 $tool_content .= <<<cData
 	</TABLE>
-</TD></TR>
-</TABLE>
+
 cData;
 draw($tool_content, 2);
 ?>
--- modules/phpbb/viewforum.php
+++ modules/phpbb/viewforum.php
@@ -1,91 +1,93 @@
 <?php
 /**=============================================================================
-       	GUnet e-Class 2.0 
-        E-learning and Course Management Program  
+GUnet e-Class 2.0
+E-learning and Course Management Program
 ================================================================================
-       	Copyright(c) 2003-2006  Greek Universities Network - GUnet
-        A full copyright notice can be read in "/info/copyright.txt".
-        
-       	Authors:    Costas Tsibanis <k.tsibanis@noc.uoa.gr>
-        	    Yannis Exidaridis <jexi@noc.uoa.gr> 
-      		    Alexandros Diamantidis <adia@noc.uoa.gr> 
+Copyright(c) 2003-2006  Greek Universities Network - GUnet
+A full copyright notice can be read in "/info/copyright.txt".
 
-        For a full list of contributors, see "credits.txt".  
-     
-        This program is a free software under the terms of the GNU 
-        (General Public License) as published by the Free Software 
-        Foundation. See the GNU License for more details. 
-        The full license can be read in "license.txt".
-     
-       	Contact address: GUnet Asynchronous Teleteaching Group, 
-        Network Operations Center, University of Athens, 
-        Panepistimiopolis Ilissia, 15784, Athens, Greece
-        eMail: eclassadmin@gunet.gr
+Authors:    Costas Tsibanis <k.tsibanis@noc.uoa.gr>
+Yannis Exidaridis <jexi@noc.uoa.gr>
+Alexandros Diamantidis <adia@noc.uoa.gr>
+
+For a full list of contributors, see "credits.txt".
+
+This program is a free software under the terms of the GNU
+(General Public License) as published by the Free Software
+Foundation. See the GNU License for more details.
+The full license can be read in "license.txt".
+
+Contact address: GUnet Asynchronous Teleteaching Group,
+Network Operations Center, University of Athens,
+Panepistimiopolis Ilissia, 15784, Athens, Greece
+eMail: eclassadmin@gunet.gr
 ==============================================================================*/
 
 /**===========================================================================
-        phpbb/viewforum.php
-        @last update: 2006-07-23 by Artemios G. Voyiatzis
-        @authors list: Artemios G. Voyiatzis <bogart@upnet.gr>
+phpbb/viewforum.php
+* @version $Id$
+@last update: 2006-07-23 by Artemios G. Voyiatzis
+@authors list: Artemios G. Voyiatzis <bogart@upnet.gr>
 
-        based on Claroline version 1.7 licensed under GPL
-              copyright (c) 2001, 2006 Universite catholique de Louvain (UCL)
+based on Claroline version 1.7 licensed under GPL
+copyright (c) 2001, 2006 Universite catholique de Louvain (UCL)
 
-        Claroline authors: Piraux Sebastien <pir@cerdecam.be>
-                      Lederer Guillaume <led@cerdecam.be>
+Claroline authors: Piraux Sebastien <pir@cerdecam.be>
+Lederer Guillaume <led@cerdecam.be>
 
-	based on phpBB version 1.4.1 licensed under GPL
-		copyright (c) 2001, The phpBB Group
+based on phpBB version 1.4.1 licensed under GPL
+copyright (c) 2001, The phpBB Group
 ==============================================================================
-    @Description: This module implements a per course forum for supporting
-	discussions between teachers and students or group of students.
-	It is a heavily modified adaptation of phpBB for (initially) Claroline
-	and (later) eclass. In the future, a new forum should be developed.
-	Currently we use only a fraction of phpBB tables and functionality
-	(viewforum, viewtopic, post_reply, newtopic); the time cost is
-	enormous for both core phpBB code upgrades and migration from an
-	existing (phpBB-based) to a new eclass forum :-(
+@Description: This module implements a per course forum for supporting
+discussions between teachers and students or group of students.
+It is a heavily modified adaptation of phpBB for (initially) Claroline
+and (later) eclass. In the future, a new forum should be developed.
+Currently we use only a fraction of phpBB tables and functionality
+(viewforum, viewtopic, post_reply, newtopic); the time cost is
+enormous for both core phpBB code upgrades and migration from an
+existing (phpBB-based) to a new eclass forum :-(
 
-    @Comments:
+@Comments:
 
-    @todo:
+@todo:
 ==============================================================================
 */
 
 error_reporting(E_ALL);
 /*
- * GUNET eclass 2.0 standard stuff
- */
+* GUNET eclass 2.0 standard stuff
+*/
 $require_current_course = TRUE;
 $require_login = TRUE;
 $langFiles = 'phpbb';
 $require_help = FALSE;
 include '../../include/baseTheme.php';
-$nameTools = $l_forums;
+$navigation[]= array ("url"=>"index.php", "name"=> $l_forums);
+
 $tool_content = "";
 
 /*
- * Tool-specific includes
- */
+* Tool-specific includes
+*/
 include_once("./config.php");
 include("functions.php"); // application logic for phpBB
 
 /******************************************************************************
- * Actual code starts here
- *****************************************************************************/
+* Actual code starts here
+*****************************************************************************/
 
 /*
- * First, some decoration
- */
+* First, some decoration
+*/
 if ( $is_adminOfCourse || $is_admin ) {
-        $tool_content .= "<a href=\"../forum_admin/forum_admin.php\">$l_adminpanel</a><P>&nbsp;";
+	$tool_content .= "<a href=\"../forum_admin/forum_admin.php\">$l_adminpanel</a></P>&nbsp;";
 }
 
-$tool_content .= "<P><a href=\"newtopic.php?forum=$forum\">$langNewTopic</a><P>";
+$tool_content .= "<P><a href=\"newtopic.php?forum=$forum\">$langNewTopic</a></P>";
 
 /*
- * Retrieve and present data from course's forum
- */
+* Retrieve and present data from course's forum
+*/
 
 $sql = "SELECT f.forum_type, f.forum_name
 	FROM forums f
@@ -103,19 +105,22 @@ if (!$myrow = mysql_fetch_array($result)) {
 	exit();
 }
 $forum_name = own_stripslashes($myrow["forum_name"]);
+$nameTools = $forum_name;//set name tools here cause we need $forum_name
 
 $tool_content .= <<<cData
-<TABLE BORDER="0" CELLPADDING="1" CELLSPACING="0" ALIGN="CENTER" VALIGN="TOP" WIDTH="99%">
-<TR><TD>
-	<TABLE BORDER="0" CELLPADDING="1" CELLSPACING="1" WIDTH="99%">
+
+	<TABLE WIDTH="99%">
+	<thead>
 	<TR>
-		<TD WIDTH="2%">&nbsp;</TD>
-		<TD>&nbsp;$l_topic</TD>
-		<TD WIDTH="9%">$l_replies</TD>
-		<TD WIDTH="20%">&nbsp;$l_poster</TD>
-		<TD WIDTH="8%">$langSeen</TD>
-		<TD WIDTH="15%"$langLastMsg</TD>
+		<th></th>
+		<th>&nbsp;$l_topic</th>
+		<th>$l_replies</th>
+		<th>$l_poster</th>
+		<th>$langSeen</th>
+		<th>$langLastMsg</th>
 	</TR>
+	</thead>
+	<tbody>
 cData;
 
 if ( isset($start) ) {
@@ -125,104 +130,104 @@ if ( isset($start) ) {
 } else {
 	$start = 0;
 }
-  
+
 if ( !isset($topics_per_page) ) {
 	$topics_per_page = 50;
 }
- 
+
 $sql = "SELECT t.*, u.username, u2.username as last_poster, p.post_time FROM topics t
         LEFT JOIN users u ON t.topic_poster = u.user_id 
         LEFT JOIN posts p ON t.topic_last_post_id = p.post_id
         LEFT JOIN users u2 ON p.poster_id = u2.user_id
         WHERE t.forum_id = '$forum' 
         ORDER BY topic_time DESC LIMIT $start, $topics_per_page";
-        
+
 if(!$result = db_query($sql, $currentCourseID)) {
 	$tool_content .= <<<cData
-	</TABLE>
-</TD></TR>
+
 </TABLE>
 cData;
-	$tool_content .= "An Error Occured. Could not query the topics database.<br>$sql";
+	//	$tool_content .= "An Error Occured. Could not query the topics database.<br>$sql";
+	$tool_content .= "<p>An Error Occured. Could not query the topics database.</p>";
 	draw($tool_content, 2);
 	exit();
 }
 
 $topics_start = $start;
-   
+
 if ($myrow = mysql_fetch_array($result)) {
-   do {
-      $tool_content .= "<TR>\n";
-      $replys = $myrow["topic_replies"];
-      $last_post = $myrow["post_time"];
-      $last_post_datetime = $myrow["post_time"];
-      
-      list($last_post_date, $last_post_time) = split(" ", $last_post_datetime);
-      list($year, $month, $day) = explode("-", $last_post_date);
-      list($hour, $min) = explode(":", $last_post_time);
-      $last_post_time = mktime($hour, $min, 0, $month, $day, $year);
-      // XXX: Find last_visit
-      if ( !isset($last_visit) ) {
-		$last_visit = 0;
-      }
-      if($replys >= $hot_threshold) {
-		if ( $last_post_time < $last_visit ) 
+	do {
+		$tool_content .= "<TR>\n";
+		$replys = $myrow["topic_replies"];
+		$last_post = $myrow["post_time"];
+		$last_post_datetime = $myrow["post_time"];
+
+		list($last_post_date, $last_post_time) = split(" ", $last_post_datetime);
+		list($year, $month, $day) = explode("-", $last_post_date);
+		list($hour, $min) = explode(":", $last_post_time);
+		$last_post_time = mktime($hour, $min, 0, $month, $day, $year);
+		// XXX: Find last_visit
+		if ( !isset($last_visit) ) {
+			$last_visit = 0;
+		}
+		if($replys >= $hot_threshold) {
+			if ( $last_post_time < $last_visit )
 			$image = $hot_folder_image;
-		else 
+			else
 			$image = $hot_newposts_image;
-      } else {
-		if ( $last_post_time < $last_visit ) {
-			 $image = $folder_image;
 		} else {
-			 $image = $newposts_image;
-		}
-		if ( $myrow["topic_status"] == 1 ) {
-			 $image = $locked_image;
-		}
-      }
-      $tool_content .= "<TD><IMG SRC=\"$image\"></TD>\n";
-      $topic_title = own_stripslashes($myrow["topic_title"]);
-      $pagination = '';
-      $start = '';
-      $topiclink = "viewtopic.php?topic=" . $myrow["topic_id"] . "&forum=$forum";
-      if($replys+1 > $posts_per_page) {
-	$pagination .= '&nbsp;&nbsp;&nbsp;(<img src=\"' . $posticon . '\">' . $l_gotopage;
-	$pagenr = 1;
-	$skippages = 0;
-	for($x = 0; $x < $replys + 1; $x += $posts_per_page) {
-		$lastpage = (($x + $posts_per_page) >= $replys + 1);
-		if ($lastpage) {
-			$start = "&start=$x&$replys";
-		} else {
-			if ($x != 0) {
-				$start = "&start=$x";
+			if ( $last_post_time < $last_visit ) {
+				$image = $folder_image;
+			} else {
+				$image = $newposts_image;
 			}
-			$start .= "&" . ($x + $posts_per_page - 1);
-		}
-		if($pagenr > 3 && $skippages != 1) {
-			$pagination .= ", ... ";
-			$skippages = 1;
-		} 
-		if ($skippages != 1 || $lastpage) {
-			if ($x != 0) {
-				 $pagination .= ", ";
-				$pagination .= "<a href=\"$topiclink$start\">$pagenr</a>";
+			if ( $myrow["topic_status"] == 1 ) {
+				$image = $locked_image;
 			}
-			$pagenr++;
 		}
-		$pagination .= ")";
-	  }
-      }
-      $topiclink .= "&$replys";
-      $tool_content .= "<TD>&nbsp;<a href=\"$topiclink\">$topic_title</a>$pagination</TD>\n";
-      $tool_content .= "<TD>$replys</TD>\n";
-      $tool_content .= "<TD>" . $myrow["prenom"] . " " . $myrow["nom"] . "</TD>\n";
-      $tool_content .= "<TD>" . $myrow["topic_views"] . "</TD>\n";
-      $tool_content .= "<TD>$last_post</TD></TR>\n";
-  } while($myrow = mysql_fetch_array($result));
+		$tool_content .= "<TD><IMG SRC=\"$image\"></TD>\n";
+		$topic_title = own_stripslashes($myrow["topic_title"]);
+		$pagination = '';
+		$start = '';
+		$topiclink = "viewtopic.php?topic=" . $myrow["topic_id"] . "&forum=$forum";
+		if($replys+1 > $posts_per_page) {
+			$pagination .= '&nbsp;&nbsp;&nbsp;(<img src=\"' . $posticon . '\">' . $l_gotopage;
+			$pagenr = 1;
+			$skippages = 0;
+			for($x = 0; $x < $replys + 1; $x += $posts_per_page) {
+				$lastpage = (($x + $posts_per_page) >= $replys + 1);
+				if ($lastpage) {
+					$start = "&start=$x&$replys";
+				} else {
+					if ($x != 0) {
+						$start = "&start=$x";
+					}
+					$start .= "&" . ($x + $posts_per_page - 1);
+				}
+				if($pagenr > 3 && $skippages != 1) {
+					$pagination .= ", ... ";
+					$skippages = 1;
+				}
+				if ($skippages != 1 || $lastpage) {
+					if ($x != 0) {
+						$pagination .= ", ";
+						$pagination .= "<a href=\"$topiclink$start\">$pagenr</a>";
+					}
+					$pagenr++;
+				}
+				$pagination .= ")";
+			}
+		}
+		$topiclink .= "&$replys";
+		$tool_content .= "<TD><a href=\"$topiclink\">$topic_title</a>$pagination</TD>\n";
+		$tool_content .= "<TD>$replys</TD>\n";
+		$tool_content .= "<TD>" . $myrow["prenom"] . " " . $myrow["nom"] . "</TD>\n";
+		$tool_content .= "<TD>" . $myrow["topic_views"] . "</TD>\n";
+		$tool_content .= "<TD>$last_post</TD></TR>\n";
+	} while($myrow = mysql_fetch_array($result));
 } else {
 	$tool_content .= "<TD COLSPAN=6>$l_notopics</TD></TR>\n";
 }
-$tool_content .= "</TABLE></TD></TR></TABLE>";
+$tool_content .= "</tbody></TABLE>";
 draw($tool_content, 2);
 ?>
