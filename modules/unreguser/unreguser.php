592f0eaaeee0af7325eead6009d868ed932a048d
Evelthon Prodromou <eprodromou@upnet.gr>
2006-12-16 16:43:56 +0000

    UI improvements for eclass 2.0 compatibility

--- modules/unreguser/unreguser.php
+++ modules/unreguser/unreguser.php
@@ -1,27 +1,27 @@
 <?
 /*
 =============================================================================
-           GUnet e-Class 2.0
-        E-learning and Course Management Program
+GUnet e-Class 2.0
+E-learning and Course Management Program
 ================================================================================
-        Copyright(c) 2003-2006  Greek Universities Network - GUnet
-        A full copyright notice can be read in "/info/copyright.txt".
+Copyright(c) 2003-2006  Greek Universities Network - GUnet
+A full copyright notice can be read in "/info/copyright.txt".
 
-           Authors:     Costas Tsibanis <k.tsibanis@noc.uoa.gr>
-                    Yannis Exidaridis <jexi@noc.uoa.gr>
-                       Alexandros Diamantidis <adia@noc.uoa.gr>
+Authors:     Costas Tsibanis <k.tsibanis@noc.uoa.gr>
+Yannis Exidaridis <jexi@noc.uoa.gr>
+Alexandros Diamantidis <adia@noc.uoa.gr>
 
-        For a full list of contributors, see "credits.txt".
+For a full list of contributors, see "credits.txt".
 
-        This program is a free software under the terms of the GNU
-        (General Public License) as published by the Free Software
-        Foundation. See the GNU License for more details.
-        The full license can be read in "license.txt".
+This program is a free software under the terms of the GNU
+(General Public License) as published by the Free Software
+Foundation. See the GNU License for more details.
+The full license can be read in "license.txt".
 
-        Contact address: GUnet Asynchronous Teleteaching Group,
-        Network Operations Center, University of Athens,
-        Panepistimiopolis Ilissia, 15784, Athens, Greece
-        eMail: eclassadmin@gunet.gr
+Contact address: GUnet Asynchronous Teleteaching Group,
+Network Operations Center, University of Athens,
+Panepistimiopolis Ilissia, 15784, Athens, Greece
+eMail: eclassadmin@gunet.gr
 ==============================================================================
 */
 
@@ -41,18 +41,19 @@ if (!isset($doit) or $doit != "yes") {
 
 	// admin cannot be deleted
 	if ($is_admin) {
-		$tool_content .=  $langAdminNo;
+		$tool_content .=  "<p><b>$langAdminNo</b></p>";
 		$tool_content .=  "<p><a href='../profile/profile.php'>$langBack</a></p>";
+		$tool_content .= "</td></tr></tbody></table>";
+		draw($tool_content,1);
 		exit;
 	} else {
 		$q = db_query ("SELECT code_cours FROM cours_user WHERE user_id = '$uid'") ;
 		if (mysql_num_rows($q) == 0) {
-			$tool_content .=  "<h3>$langConfirm</h3>";
-			$tool_content .=  "<ul>";
+			$tool_content .=  "<p><b>$langConfirm</b></p>";
+			$tool_content .=  "<ul class=\"listBullet\">";
 			$tool_content .=  "<li>$langYes: ";
 			$tool_content .=  "<a href='$_SERVER[PHP_SELF]?u=$uid&doit=yes'>$langDelete</a>";
 			$tool_content .=  "</li>";
-			$tool_content .=  "<br>";
 			$tool_content .=  "<li>$langNo: <a href='../profile/profile.php'>$langBack</a>";
 			$tool_content .=  "</li></ul>";
 			$tool_content .= "</td></tr></tbody></table>";
@@ -70,19 +71,23 @@ if (!isset($doit) or $doit != "yes") {
 		$tool_content .=  "<td class=\"success\">";
 		db_query("DELETE from user WHERE user_id = '$uid'");
 		if (mysql_affected_rows() > 0) {
-			$tool_content .=  "<p>$langDelSuccess</p>";
+			$tool_content .=  "<p><b>$langDelSuccess</b></p>";
 			$tool_content .=  "<p>$langThanks</p>";
-			$tool_content .=  "<br><a href='../../index.php?logout=yes'>$langLogout</a><br>";
+			$tool_content .=  "<br><a href='../../index.php?logout=yes'>$langLogout</a>";
 			unset($_SESSION['uid']);
 		} else {
 			$tool_content .=  "<p>$langError</p>";
 			$tool_content .=  "<p><a href='../profile/profile.php'>$langBack</a></p><br>";
-//			exit;
+			//			exit;
 		}
 	}
-	
+
 	$tool_content .= "</td></tr></tbody></table>";
 }
-draw($tool_content, 1);
+if (isset($_SESSION['uid'])) {
+	draw($tool_content, 1);
+} else {
+	draw($tool_content, 0);
+}
 ?>
 
