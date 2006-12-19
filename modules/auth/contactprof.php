f5e077ac4032233f90b5b2721bc756e77818b61b
Evelthon Prodromou <eprodromou@upnet.gr>
2006-12-19 14:03:51 +0000

    Ported to eclass 2.0 api

--- modules/auth/contactprof.php
+++ modules/auth/contactprof.php
@@ -11,12 +11,12 @@ check_guest();
 $tool_content = "";
 
 if (isset($_POST["submit"])) {
-	$tool_content .= "<table width=\"99%\"><tbody><tr><td>";
+//	$tool_content .= "<table width=\"99%\"><tbody><tr><td>";
 
 	$sql=db_query("SELECT * FROM cours_user WHERE code_cours='".mysql_real_escape_string($_POST['cc'])."'");
 
 	while ($m = mysql_fetch_array($sql)) {
-		
+
 		$sql1 = db_query("SELECT email FROM user WHERE user_id='".$m["user_id"]."'");
 		$m1 = mysql_fetch_array($sql1);
 		$to = $m1["email"];
@@ -29,41 +29,63 @@ Email: $email
 Σχόλια: $body
 
 ";
-
+$errorExists = false;
 		if (!send_mail($siteName, $emailAdministrator, '', $to,
-			$emailsubject, $emailbody, $charset)) {
-				$tool_content .= "<h4>Σφάλμα κατά την αποστολή e-mail στη διεύθυνση '$to'!</h4>";
+		$emailsubject, $emailbody, $charset)) {
+			$tool_content .= "
+				<table width=\"99%\">
+	<tbody>
+	<tr>
+	<td class=\"caution\">Σφάλμα κατά την αποστολή e-mail στη διεύθυνση '$to'!
+	</td>
+	</tr>
+	</tbody>";
+			$errorExists = true;
 		}
 	}
-	$tool_content .= "<h4>$emailsuccess</h4></td></tr><tbody></table><br>";
+	if (!$errorExists) {
+		$tool_content .= "
+	<table width=\"99%\">
+	<tbody>
+	<tr>
+	<td class=\"success\">$emailsuccess
+	</td>
+	</tr>
+	</tbody>
+	</table>";
+	}
 }
 else
 {
 	$sql = "SELECT * FROM user WHERE user_id='".$uid."'";
 	$res = mysql_query($sql);
 	$row = mysql_fetch_array($res);
-	
+
 	if (!isset($_GET['fc'])) {
 		$_GET['fc'] = "";
 	}
 	$_GET['fc'] = htmlspecialchars($_GET['fc']);
-	
+
 	if (!isset($_GET['cc'])) {
 		$_GET['cc'] = "";
 	}
 	$_GET['cc'] = htmlspecialchars($_GET['cc']);
-	
+	$tool_content .= "<p><a href=\"courses.php?fc=".$_GET['fc']."\">Επιστροφή</a></p>";
 	$tool_content .= "<form action=\"".$_SERVER['PHP_SELF']."?fc=".$_GET['fc']."\" method=\"post\">
-<table width=\"99%\"><caption>Συμπλήρωση Φόρμας</caption><tbody>";
-	$tool_content .= "<tr><td width=\"3%\" nowrap valign=\"top\"><b>Όνομα:</b></td><td><input type=\"text\" name=\"firstname\" value=\"".$row["nom"]."\" readonly></td></tr>";	
-	$tool_content .= "<tr><td width=\"3%\" nowrap valign=\"top\"><b>Επίθετο:</b></td><td><input type=\"text\" name=\"lastname\" value=\"".$row["prenom"]."\" readonly></td></tr>";	
-	$tool_content .= "<tr><td width=\"3%\" nowrap valign=\"top\"><b>Email:</b></td><td><input type=\"text\" name=\"email\" value=\"".$row["email"]."\" readonly></td></tr>";
-	$tool_content .= "<tr><td width=\"3%\" nowrap valign=\"top\"><b>Σχόλια:</b></td><td><textarea rows=\"6\" cols=\"40\" name=\"body\"></textarea></td></tr>";
-	$tool_content .= "<tr><td width=\"3%\" nowrap valign=\"top\">&nbsp;</td><td><input type=\"submit\" name=\"submit\" value=\"Αποστολή\"><input type=\"hidden\" name=\"cc\" value=\"".$_GET['cc']."\"</td></tr>";
-	$tool_content .= "</tbody></table></form>";
+<table width=\"99%\"><thead>";
+	$tool_content .= "<tr><th>Όνομα:</th><td><input type=\"text\" name=\"firstname\" value=\"".$row["nom"]."\" readonly></td></tr>";
+	$tool_content .= "<tr><th>Επίθετο:</th><td><input type=\"text\" name=\"lastname\" value=\"".$row["prenom"]."\" readonly></td></tr>";
+	$tool_content .= "<tr><th>Email:</th><td><input type=\"text\" name=\"email\" value=\"".$row["email"]."\" readonly></td></tr>";
+	$tool_content .= "<th>Σχόλια:</th><td><textarea rows=\"6\" cols=\"40\" name=\"body\"></textarea></td></tr>";
+	//	$tool_content .= "<tr><td width=\"3%\" nowrap valign=\"top\">&nbsp;</td><td><input type=\"submit\" name=\"submit\" value=\"Αποστολή\"><input type=\"hidden\" name=\"cc\" value=\"".$_GET['cc']."\"></td></tr>";
+	$tool_content .= "</thead></table>
+	<br/>
+	<input type=\"submit\" name=\"submit\" value=\"Αποστολή\"><input type=\"hidden\" name=\"cc\" value=\"".$_GET['cc']."\">
+	</form>";
+
 }
 
-$tool_content .= "<center><p><a href=\"courses.php?fc=".$_GET['fc']."\">Επιστροφή</a></p></center>";
+//$tool_content .= "<p><a href=\"courses.php?fc=".$_GET['fc']."\">Επιστροφή</a></p>";
 
 draw($tool_content,1,'admin');
 
