9460bb2ba77748ec15f399250e0826ebe0fc2c62
Thanos Kyritsis <atkyritsis@upnet.gr>
2007-04-25 15:44:47 +0000

    added translations for wiki module

---
+++ modules/lang/english/wiki.inc.php
@@ -0,0 +1,239 @@
+<?php
+$langAction = "Action";
+$langAddImage = "Include picture";
+$langAddress = "The address of ";
+$langAdministrator = "Administrator";
+$langAgenda = "Agenda";
+$langAnnouncement = "Announcements";
+$langCancel = "Cancel";
+$langChangePwdexp = "Enter new password twice to change, leave empty to keep it";
+$langChat = "Chat";
+$langChooseYourPassword = "Choose now a username and a password for the user account.";
+$langCloseWindow = "Close window";
+$langCodeUsed = "This official code is already used by another user.";
+$langConfirmation = "Confirmation";
+$langContinue = "Continue";
+$langCourseHome = "Course Home";
+$langCourseManager = "Course manager";
+$langCreate = "Create";
+$langDay_of_weekNames = "Array";
+$langDear = "Dear";
+$langDelImage = "Remove picture";
+$langDelete = "Delete";
+$langDescriptionCours = "Course description";
+$langDocument = "Documents and Links";
+$langEdit = "Edit";
+$langEmail = "Email";
+$langEmailWrong = "The email address is not valid";
+$langEmptyFields = "You left some required fields empty";
+$langExercises = "Exercises";
+$langFirstname = "First name";
+$langFormula = "Yours sincerely";
+$langForums = "Forums";
+$langGroupTutor = "Group Tutor";
+$langGroups = "Groups";
+$langHelp = "Help";
+$langIs = "is";
+$langLastname = "Last name";
+$langLearningPath = "Learning Path";
+$langLegendRequiredFields = "<span class=\"required\">*</span> denotes required field";
+$langLogin = "Login";
+$langLogout = "Logout";
+$langManager = "Manager";
+$langMemorizeYourPassord = "Memorize them, you will use them the next time you will enter to this site.";
+$langMinuteShort = "min.";
+$langModifyProfile = "My User Account";
+$langMonthNames = "Array";
+$langMyAgenda = "My calendar";
+$langMyCourses = "My course list";
+$langNameOfLang = "Array";
+$langNo = "No";
+$langNotAllowed = "Not allowed";
+$langOfficialCode = "Administrative code";
+$langOk = "Ok";
+$langOneResp = "One of the course administrators";
+$langOtherCourses = "Course list";
+$langPassTooEasy = "this password is too simple. Use a password like this ";
+$langPassTwice = "You typed two different passwords";
+$langPassword = "Password";
+$langPeriodDayShort = "d.";
+$langPeriodHourShort = "h.";
+$langPersonalCourseList = "Personal course list";
+$langPhone = "Phone";
+$langPlatformAdministration = "Platform Administration";
+$langPoweredBy = "Powered by";
+$langPreview = "Preview";
+$langProblem = "In case of problems, contact us.";
+$langRegAdmin = "Create course websites";
+$langRegStudent = "Follow courses";
+$langRegYou = "has registered you on this course";
+$langSave = "Save";
+$langSaveChanges = "Save changes";
+$langSearch = "Search";
+$langSecondShort = "sec.";
+$langSettings = "with the following settings:
+User name:";
+$langStudent = "Student";
+$langSwitchEditorToTextConfirm = "This command is going to remove the current text layout. Do you want to continue ?";
+$langTextEditorDisable = "Disable text editor";
+$langTextEditorEnable = "Enable text editor";
+$langTheSystemIsCaseSensitive = "The system is case sensitive.";
+$langTitle = "Title";
+$langUpdateImage = "Change picture";
+$langUserIsPlaformAdmin = "Is platform admin";
+$langUserName = "User name";
+$langUserTaken = "This user name is already taken";
+$langUserid = "User Id";
+$langUsers = "Users";
+$langViewMode = "View mode";
+$langWarning = "Warning.";
+$langWiki = "Wiki";
+$langWikiAccessControl = "Access control management";
+$langWikiAccessControlText = "You can set access rights for users using the following grid : ";
+$langWikiAllPages = "All pages";
+$langWikiBackToPage = "Back to page";
+$langWikiConflictHowTo = "<p><strong>Edit conflict</strong> : The page you try to save seems to have been changed since you modify it.<br /><br />
+What to do now ?<ul>
+<li>You can copy/paste your modification to a text editor (like notepad) and click on 'edit last version' to try to add your modification to the new version of the page.</li>
+<li>You can also click on cancel to cancel your changes.</li>
+</ul></p>";
+$langWikiContentEmpty = "This page is empty, click on 'Edit this page' to add a content";
+$langWikiCourseMembers = "Course members";
+$langWikiCreateNewWiki = "Create a new Wiki";
+$langWikiCreatePrivilege = "Create Pages";
+$langWikiCreationSucceed = "Wiki creation succeed";
+$langWikiDefaultDescription = "Enter the description of your wiki here";
+$langWikiDefaultTitle = "New Wiki";
+$langWikiDeleteWiki = "Delete Wiki";
+$langWikiDeleteWikiWarning = "WARNING : you are going to delete this wiki and all its pages. Are you sure to want to continue ?";
+$langWikiDeletionSucceed = "Wiki deletion succeed";
+$langWikiDescription = "Description of the Wiki";
+$langWikiDescriptionForm = "Wiki description";
+$langWikiDescriptionFormText = "You can choose a title an a description for the wiki : ";
+$langWikiDiffAddedLine = "Added line";
+$langWikiDiffDeletedLine = "Deleted line";
+$langWikiDiffMovedLine = "Moved line";
+$langWikiDiffUnchangedLine = "Unchanged line";
+$langWikiDifferenceKeys = "Keys :";
+$langWikiDifferencePattern = "differences between version of %1\$s modified by %2\$s and version of %3\$s modified by %4\$s";
+$langWikiDifferenceTitle = "Differences :";
+$langWikiEditConflict = "Edit conflict";
+$langWikiEditLastVersion = "Edit last version";
+$langWikiEditPage = "Edit this page";
+$langWikiEditPrivilege = "Edit Pages";
+$langWikiEditProperties = "Edit properties";
+$langWikiEditionSucceed = "Wiki edition succeed";
+$langWikiGroupMembers = "Group members";
+$langWikiHelpAdminContent = "<h3>Wiki administration help</h3>
+<dl class=\"wikiHelp\">
+<dt>How to create a new Wiki ?</dt>
+<dd>Click on the link 'Create a new Wiki'. Then enter the properties of the Wiki :
+<ul>
+<li><b>Title of the Wiki</b> : choose a title for the Wiki</li>
+<li><b>Description of the Wiki</b> : choose a description for the Wiki</li>
+<li><b>Access control management</b> : set the access ontrol of the Wiki by checking/uncheking the box (see below)</li>
+</ul>
+</dd>
+<dt>How to enter a Wiki ?</dt>
+<dd>Click on the title of the Wiki in the list.</dd>
+<dt>How to change the properties of a Wiki ?</dt>
+<dd>Click on the icon in the 'Properties' column in the Wiki list to go to the Wiki properties form.</dd>
+<dt>How to use the Access control management options ?</dt>
+<dd>You can set the access rights for users by checking/unchecking the box in the 'Access control management' section of Wiki properties.
+ You can grant/ungrant access to three types of users :<ul>
+<li><b>Course members</b> : users enroll to the course (except the course managers)</li>
+<li><b>Group members</b> (only available whithin a group) : users which are members of the group (expect the group's tutors)</li>
+<li><b>Other users</b> : anonymous users or users which are not course members</li></ul>
+For each user type, you can grant three type of privileges to the Wiki(*) :<ul>
+<li><b>Read pages</b> : the user of the given type can read the pages of the Wiki</li>
+<li><b>Edit pages</b> : the user of the given type can modify the content of th pages of the Wiki</li>
+<li><b>Create pages</b> : the user of the given type can create new pages in the Wiki</li>
+</ul><small><em>(*) Note that if a user cannot read the pages of a Wiki, he cannot edit or modify them. Note that if a user cannot edit the pages of a Wiki, he cannot create new pages.</em></small></dd>
+<dt>How to delete a Wiki ?</dt>
+<dd>Click on the icon in the 'Delete' column to delete a Wiki and all its pages.</dd>
+<dt>How to get the list of the pages in a Wiki ?</dt>
+<dd>Click on the number of pages of this Wiki in the Wiki list.</dd>
+<dt>How to get the list of the last modified pages in a Wiki ?</dt>
+<dd>Click on the icon in the 'Recent changes' column in the Wiki list.</dd>
+</dl>";
+$langWikiHelpSyntax = "Wiki syntax";
+$langWikiHelpSyntaxContent = "<h1>Wiki Syntax</h1>
+<h2>1. Basic syntax</h2>
+<dl class=\"wikiHelp\">
+<dt>Creating wiki pages and links between them</dt>
+<dd><strong>Wiki words</strong> : Wiki words are words written like <em>WikiWord</em>. Wiki2xhtml automaticaly recognise them as Wiki page links. To create a wiki page or to create a link to a wiki page, edit an existing one and add the title of the page in WikiWord syntax, for example <em>MyPage</em>, then save the page. Wiki2xhtml will automaticaly replace the Wiki Word <em>MyPage</em> with a link to the Wiki page <em>MyPage</em>&nbsp;;</dd>
+<dd><strong>Wiki links</strong> : Wiki links are like Hyperlinks (see below) expect that they do not contain any protocol scheme (like <em>http://</em> or <em>ftp://</em>) and thatthey are automaticaly recognise as links to Wiki pages. To create a new page or create a link to an existing one using Wiki links, edit a page and add <code>[page title]</code> or <code>[name of link|title of page]</code> to its content. You can also use this syntax to change the text of a WikiWord link : <code>[name of link|WikiWord]</code>.</dd>
+<dt>Hyperlinks</dt>
+<dd><code>[url]</code>, <code>[name|url]</code>, <code>[name|url|language]</code> or <code>[name|url|language|title]</code>.&nbsp;;</dd>
+<dt>Image inclusion</dt>
+<dd><code>((url|alternate text))</code>, <code>((url|alternate text|position))</code> ou <code>((url|alternate text|position|long description))</code>. <br />The position argument can take the following values : L (left), R (right) or C (centeré).&nbsp;;</dd>
+<dd>You can use the syntax as Hyperlinks. For example <code>[title|image.gif]</code>. This syntax is deprecated, consider to use the preceding one&nbsp;;</dd>
+<dt>Link to an image</dt>
+<dd>same as Hyperlinks but put a 0 in fourth argument to avoid image recognition and get an hyperlink to an image. For example <code>[image|image.gif||0]</code> will display a link to image.gif instead of display the imge itself</dd>
+<dt>Layout</dt>
+<dd><strong>Italic</strong> : surround your text with two single quotes <code>''text''</code>&nbsp;;</dd>
+<dd><strong>Bold</strong> : surround your text with three single quotes underscores <code>'''text'''</code>&nbsp;;</dd>
+<dd><strong>Underline</strong> : surround your text with two underscores <code>__text__</code>&nbsp;;</dd>
+<dd><strong>Stroke</strong> : surround your text with two minus symbols <code>--text--</code>&nbsp;;</dd>
+<dd><strong>Title</strong> : <code>!!!</code>, <code>!!</code>, <code>!</code> respectively for titles, sub-titles and sub-sub-titles&nbsp;;</dd>
+<dt>List</dt>
+<dd>line starting with <code>*</code> (unordered list) or <code>#</code> (ordered list). You can mix lists (<code>*#*</code>) to create multi-level lists.&nbsp;;</dd>
+<dt>Paragraph</dt>
+<dd>Separate paragraphs with one or more new lines&nbsp;;</dd>
+</dl>
+<h2>2. Advanced syntax</h2>
+<dl class=\"wikiHelp\">
+<dt>Footnote</dt>
+<dd><code>\$\$footnote text\$\$</code>&nbsp;;</dd>
+<dt>Preformated text</dt>
+<dd>begin each line of preformated text with a blank space&nbsp;;</dd>
+<dt>Cite block</dt>
+<dd><code>&gt;</code> or <code>;:</code> before each line&nbsp;;</dd>
+<dt>Horizontal line</dt>
+<dd><code>----</code>&nbsp;;</dd>
+<dt>Forced line break</dt>
+<dd><code>%%%</code>&nbsp;;</dd>
+<dt>Acronym</dt>
+<dd><code>??acronym??</code> or <code>??acronym|definition??</code>&nbsp;;</dd>
+<dt>Inline cite</dt>
+<dd><code>{{cite}}</code>, <code>{{cite|language}}</code> or <code>{{cite|language|url}}</code>&nbsp;;</dd>
+<dt>Code</dt>
+<dd><code>@@your code here@@</code>&nbsp;;</dd>
+<dt>Named anchor</dt>
+<dd><code>~anchor~</code>&nbsp;;</dd>
+</dl>";
+$langWikiIdenticalContent = "Identical content<br />no modification saved";
+$langWikiInvalidWikiId = "Invalid Wiki Id";
+$langWikiList = "List of Wiki";
+$langWikiMainPage = "Main page";
+$langWikiMainPageContent = "This is the main page of the Wiki %s. Click on '''Edit''' to modify the content.";
+$langWikiNoWiki = "No Wiki";
+$langWikiNotAllowedToCreate = "You are not allowed to create pages";
+$langWikiNotAllowedToEdit = "You are not allowed to edit this page";
+$langWikiNotAllowedToRead = "You are not allowed to read this page";
+$langWikiNumberOfPages = "Number of pages";
+$langWikiOtherUsers = "Others (*)";
+$langWikiOtherUsersText = "(*) anonymous users, users who are not members of this course...";
+$langWikiPageHistory = "Page history";
+$langWikiPageSaved = "Page saved";
+$langWikiPreviewTitle = "Preview : ";
+$langWikiPreviewWarning = "WARNING: this page is a preview. Your modifications to the wiki has not been saved yet ! To save them do not forget to click on the 'save' button at the bottom of the page.";
+$langWikiProperties = "Properties";
+$langWikiReadPrivilege = "Read Pages";
+$langWikiRecentChanges = "Recent changes";
+$langWikiRecentChangesPattern = "%1\$s modified on %2\$s by %3\$s";
+$langWikiShowDifferences = "Show differences";
+$langWikiTitle = "Title of the wiki";
+$langWikiTitleEdit = "Wiki : Edit properties";
+$langWikiTitleNew = "Wiki : Create new Wiki";
+$langWikiTitlePattern = "Wiki : %s";
+$langWikiVersionInfoPattern = "(version of %1\$s modified by %2\$s)";
+$langWikiVersionPattern = "%1\$s by %2\$s";
+$langWork = "Assignments";
+$langYes = "Yes";
+$langYouAreReg = "You are registered on";
+$langYourReg = "Your registration on";
+$lang_enroll = "Enrol";
+$lang_footer_p_CourseManager = "Manager(s) for %s";
+$lang_p_platformManager = "Administrator for %s";
+?>
