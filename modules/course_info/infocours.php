7d7cb60c850ddca4358bab5d825b5e265651642d
Evelthon Prodromou <eprodromou@upnet.gr>
2007-04-25 22:08:42 +0000

    Bug Fix : The language setting for a lesson would not overwrite the user's preferred language

--- modules/course_info/infocours.php
+++ modules/course_info/infocours.php
@@ -3,7 +3,19 @@
 $require_current_course = TRUE;
 $require_prof = true;
 $langFiles = array('course_info', 'create_course', 'opencours');
+if (isset($localize)) {
+	if ($localize == 'el')
+	$newlang = 'greek';
+	else
+	$newlang = 'english';
+	$language = $newlang;
+}
+
 include '../../include/baseTheme.php';
+if(isset($newlang)) {
+			$inclPath =  $webDir."modules/lang/$newlang/course_info.inc.php";
+			include($inclPath);
+		}
 $nameTools = $langModifInfo;
 $tool_content = "";
 
@@ -11,18 +23,19 @@ $tool_content = "";
 
 if($is_adminOfCourse) {
 	if (isset($submit)) {
+		if(isset($newlang)) {
+			$inclPath =  $webDir."modules/lang/$newlang/create_course.inc.php";
+			include($inclPath);
+		}
 		// update course settings
 		if (isset($checkpassword) && $checkpassword=="on" && $formvisible=="1") {
 			$password = $password;
 		} else {
 			$password = "";
 		}
-		if ($localize == 'el') 
-					$newlang = 'greek';
-		else 
-				$newlang = 'english';
+
 		list($facid, $facname) = split("--", $facu);
-		$sql = "UPDATE $mysqlMainDb.cours 
+		$sql = "UPDATE $mysqlMainDb.cours
 			SET intitule='$int', 
 				faculte='$facname', 
 				description='$description',
@@ -49,8 +62,8 @@ if($is_adminOfCourse) {
 		mysql_query("UPDATE `$currentCourseID`.accueil SET rubrique='$langForums' WHERE id='9'");
 		mysql_query("UPDATE `$currentCourseID`.accueil SET rubrique='$langExercices' WHERE id='10'");
 		mysql_query("UPDATE `$currentCourseID`.accueil SET rubrique='$langStatistics' WHERE id='11'");
-//		mysql_query("UPDATE `$currentCourseID`.accueil SET rubrique='$langAddPageHome' WHERE id='12'");	
-//		mysql_query("UPDATE `$currentCourseID`.accueil SET rubrique='$langLinkSite' WHERE id='13'");
+		//		mysql_query("UPDATE `$currentCourseID`.accueil SET rubrique='$langAddPageHome' WHERE id='12'");
+		//		mysql_query("UPDATE `$currentCourseID`.accueil SET rubrique='$langLinkSite' WHERE id='13'");
 		mysql_query("UPDATE `$currentCourseID`.accueil SET rubrique='$langModifyInfo' WHERE id='14'");
 		mysql_query("UPDATE `$currentCourseID`.accueil SET rubrique='$langGroups' WHERE id='15'");
 		mysql_query("UPDATE `$currentCourseID`.accueil SET rubrique='$langDropBox' WHERE id='16'");
@@ -60,34 +73,35 @@ if($is_adminOfCourse) {
 		mysql_query("UPDATE `$currentCourseID`.accueil SET rubrique='$langLearnPath' WHERE id='23'");
 		mysql_query("UPDATE `$currentCourseID`.accueil SET rubrique='$langUsage' WHERE id='24'");
 		mysql_query("UPDATE `$currentCourseID`.accueil SET rubrique='$langWiki' WHERE id='26'");
+		
 		$tool_content .= "<p>$langModifDone.</p>
 		<center><p><a href=\"".$_SERVER['PHP_SELF']."\">$langBack</a></p></center><br>
 		<center><p><a href=\"../../courses/".$currentCourseID."/index.php\">".$langHome."</a></p></center>";
 	} else {
-	  $sql = "SELECT cours_faculte.faculte, 
+		$sql = "SELECT cours_faculte.faculte,
 			cours.intitule, cours.description, course_keywords, course_addon,
 			cours.visible, cours.fake_code, cours.titulaires, cours.languageCourse, 
 			cours.departmentUrlName, cours.departmentUrl, cours.type, cours.password
 			FROM `$mysqlMainDb`.cours, `$mysqlMainDb`.cours_faculte 
 			WHERE cours.code='$currentCourseID' 
 			AND cours_faculte.code='$currentCourseID'";
-	  $result = mysql_query($sql);
-	  $leCours = mysql_fetch_array($result);
-	  $int = $leCours['intitule'];
-	  $facu = $leCours['faculte'];
-	  $type = $leCours['type'];
-	  $visible = $leCours['visible'];
-	  $visibleChecked[$visible]="checked";
-	  $fake_code = $leCours['fake_code'];
-	  $titulary = $leCours['titulaires'];
-	  $languageCourse	= $leCours['languageCourse'];
+		$result = mysql_query($sql);
+		$leCours = mysql_fetch_array($result);
+		$int = $leCours['intitule'];
+		$facu = $leCours['faculte'];
+		$type = $leCours['type'];
+		$visible = $leCours['visible'];
+		$visibleChecked[$visible]="checked";
+		$fake_code = $leCours['fake_code'];
+		$titulary = $leCours['titulaires'];
+		$languageCourse	= $leCours['languageCourse'];
 		$description = $leCours['description'];
-	  $course_keywords = $leCours['course_keywords'];
-	  $course_addon = $leCours['course_addon'];
-	  $password = $leCours['password'];
-	  if ($password!="") $checkpasssel = "checked"; else $checkpasssel="";
-	    
-	  @$tool_content .="
+		$course_keywords = $leCours['course_keywords'];
+		$course_addon = $leCours['course_addon'];
+		$password = $leCours['password'];
+		if ($password!="") $checkpasssel = "checked"; else $checkpasssel="";
+
+		@$tool_content .="
 	  
 	  <form method='post' action='$_SERVER[PHP_SELF]'>
 		<table width=\"99%\"><caption>$langGeneralInfo</caption><thead>
@@ -102,21 +116,21 @@ if($is_adminOfCourse) {
 		<tr><th>$langFaculty:</th>
 		<td>
 		<select name=\"facu\">";
-	    
+
 		$resultFac=mysql_query("SELECT id,name FROM `$mysqlMainDb`.faculte ORDER BY number");
-		while ($myfac = mysql_fetch_array($resultFac)) {	
+		while ($myfac = mysql_fetch_array($resultFac)) {
 			if($myfac['name']==$facu)
-				$tool_content .= "<option value=\"".$myfac['id']."--".$myfac['name']."\" selected>$myfac[name]</option>";
+			$tool_content .= "<option value=\"".$myfac['id']."--".$myfac['name']."\" selected>$myfac[name]</option>";
 			else
-				$tool_content .= "<option value=\"".$myfac['id']."--".$myfac['name']."\">$myfac[name]</option>";
+			$tool_content .= "<option value=\"".$myfac['id']."--".$myfac['name']."\">$myfac[name]</option>";
 		}
 		$tool_content .= "</select></td></tr>
 		<tr><th>$m[type]:</th><td>";
 
 		$tool_content .= selection(array('pre' => $m['pre'], 'post' => $m['post'], 'other' => $m['other']),'type', $type);
-		
+
 		$tool_content .= "</td></tr>";
-		
+
 		@$tool_content .= "<tr>
 			<th>$langcourse_references:</th>
 			<td><textarea name=\"course_addon\" value=\"$leCours[course_addon]\" cols=\"40\" rows=\"4\">$leCours[course_addon]</textarea></td>
@@ -147,13 +161,13 @@ if($is_adminOfCourse) {
 		<tr >
 		<th width=\"200\">$langLanguage</th>
 		<td>";		
-		if ($leCours['languageCourse'] == 'english') 
-				$curLang = 'en';
-		else 
-				$curLang = 'el';
+		if ($leCours['languageCourse'] == 'english')
+		$curLang = 'en';
+		else
+		$curLang = 'el';
 		$tool_content .= selection(array('el' => $langNameOfLang['greek'],
-								'en' => $langNameOfLang['english']),'localize', $curLang);
-		
+		'en' => $langNameOfLang['english']),'localize', $curLang);
+
 		$tool_content .= "</td></tr>";
 		$tool_content .= "</thead></table>
 		
@@ -175,7 +189,7 @@ if($is_adminOfCourse) {
 // student view
 else {
 	$tool_content .= "<p>$langForbidden</p>";
-}  
+}
 
 draw($tool_content,2,'admin');
 ?>
