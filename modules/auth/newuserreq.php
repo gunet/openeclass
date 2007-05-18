1eb7f961462076249712e8a85d91c897403fd01a
Yannis Exidaridis <jexi@noc.uoa.gr>
2007-05-18 10:43:11 +0000

    ..porting...

---
+++ modules/auth/newuserreq.php
@@ -0,0 +1,80 @@
+<?
+$langFiles = array('registration', 'admin');
+
+$require_admin = TRUE;
+include '../../include/baseTheme.php';
+include('../../include/sendMail.inc.php');
+
+$nameTools = $langNewUser;
+$navigation[]= array ("url"=>"../admin/", "name"=> $langAdmin);
+
+// Initialise $tool_content
+$tool_content = "";
+
+$tool_content .= "<table width=100% border='0' height=316 cellspacing='0' align=center cellpadding='0'>\n";
+$tool_content .= "<tr>\n";
+$tool_content .= "<td valign=top>\n";
+
+$tool_content .= "
+   <table border=0 width=60% align=center>
+   <tr>
+    <td>
+    <form action='newuserreq_second.php' method='post'>
+    <table  border=0 cellpadding='1' cellspacing='2' border='0' width='100%' align=center>
+    <tr valign='top'>
+    <td class=color1 style='border : 1px solid $table_border;' width=50%>$langSurname</td>
+	  <td><input type='text' class=auth_input_admin name='nom_form' value='".@$ps."' >
+		<small>&nbsp;(*)</small></td>
+	  </tr>
+	  <tr>
+	  <td class=color1 style='border : 1px solid $table_border;' width=50%>$langName</td>
+	  <td><input type='text' class=auth_input_admin name='prenom_form' value='".@$pn."' >
+		<small>&nbsp;(*)</small></td>
+	  </tr>
+	  <tr>
+	  <td class=color1 style='border : 1px solid $table_border;' width=50%>$langUsername</td>
+	  <td><input type='text' class=auth_input_admin name='uname' value='".@$pu."'>
+		<small>&nbsp;(*)</small></td>
+	  </tr>
+	  <tr>
+	  <td class=color1 style='border : 1px solid $table_border;' width=50%>$langPass&nbsp;:</td>
+	  <td><input type='text' class=auth_input_admin name='password' value=".create_pass(5)."></td>
+	  </tr>
+	  <tr>
+    <td class=color1 style='border : 1px solid $table_border;' width=50%>$langEmail</td>
+	  <td><input type='text' class=auth_input_admin name='email_form' value='".@$pe."'>
+		<small>&nbsp;(*)</small></td>
+	  </tr>
+	  <tr>
+	  <td class=color1 style='border : 1px solid $table_border;' width=50%>$langDepartment &nbsp;
+		</span></td>
+	  <td>";
+
+		$dep = array();
+        $deps=db_query("SELECT name FROM faculte order by id");
+			while ($n = mysql_fetch_array($deps))
+					$dep[$n[0]] = $n['name'];  
+
+		if (isset($pt))
+			$tool_content .= selection ($dep, 'department', $pt);
+		else 
+			$tool_content .= selection ($dep, 'department');
+ 
+	   	$tool_content .= "</td></tr>
+	  		<tr><td>&nbsp;</td>
+			 	<td><input type='submit' name='submit' value='$langOk' ></td>
+		    </tr></table>
+				<input type='hidden' name='rid' value='$id'>
+    		</form>
+				</td></tr>
+    <tr><td align='right'><span class='explanationtext'>$langRequiredFields</span></td></tr>
+    </table></td></tr>
+    <tr><td>&nbsp;</td></tr>
+    <tr><td align=right>";
+		        
+   	$tool_content .= "<a href=\"../admin/index.php\" class=mainpage>$langBackAdmin&nbsp;</a>";
+		$tool_content .= "<tr><td>&nbsp;</td></tr>";
+		$tool_content .= "</table>\n";
+
+		draw($tool_content,3);
+?>
---
+++ modules/auth/newuserreq_second.php
@@ -0,0 +1,148 @@
+<?
+$langFiles = array('registration', 'admin');
+
+$require_admin = TRUE;
+include '../../include/baseTheme.php';
+include '../../include/sendMail.inc.php';
+
+$nameTools = $reguser;
+$navigation[]= array ("url"=>"../admin/", "name"=> $langAdmin);
+
+$tool_content = "";
+$tool_content .= "<table width=100% border='0' height=316 cellspacing='0' align=center cellpadding='0'>";
+$tool_content .= "<tr>";
+$tool_content .= "<td valign=top>";
+
+$tool_content = "<table height=300 width='96%' align='center' class='admin'>
+								<tr><td valign=top><br>";
+
+if($submit) {
+	$regexp = "^[0-9a-z_\.-\+]+@(([0-9]{1,3}\.){3}[0-9]{1,3}|([0-9a-z][0-9a-z-]*[0-9a-z]\.)+[a-z]{2,4})$";
+
+// check if user name exists
+	$username_check=mysql_query("SELECT username FROM `$mysqlMainDb`.user WHERE username='$uname'");
+	while ($myusername = mysql_fetch_array($username_check)) {
+		$user_exist=$myusername[0];
+	}
+
+// check if passwd is too easy
+
+	if ((strtoupper($password) == strtoupper($uname)) 
+			or (strtoupper($password) == strtoupper($nom_form))
+			or (strtoupper($password) == strtoupper($prenom_form))
+			or (strtoupper($password) == strtoupper($email))) {
+			$tool_content .= error_screen($langPassTooEasy);
+      $tool_content .= end_tables();
+	}
+
+// check if there are empty fields
+	elseif (empty($nom_form) or empty($prenom_form) or empty($password)
+				or empty($uname) or empty($email_form)) {
+			$tool_content .= error_screen($langEmptyFields);	
+      $tool_content .= end_tables();
+	}
+
+	elseif(isset($user_exist) and $uname==$user_exist) {
+			$tool_content .= error_screen($langUserFree);	
+  	  $tool_content .= end_tables();
+    }
+
+// check if email syntax is valid
+   
+ elseif(!eregi($regexp,$email_form)) {
+		$tool_content .= error_screen($langEmailWrong);
+    $tool_content .= end_tables();
+	}
+
+
+// registration accepted
+
+	else {
+		$emailsubject = "$langYourReg $siteName $langAsUser";
+
+		if (isset($institut) and ($institut > 0)) {
+			$emailbody = "
+$langDestination $prenom_form $nom_form
+
+$langYouAreReg$siteName $langAsUser, $langSettings $uname
+$langPassSameLDAP
+$langAddress $siteName $langIs: $urlServer
+$langProblem
+
+$administratorName $administratorSurname
+$langManager $siteName
+$langTel $telephone
+$langEmail : $emailAdministrator
+";
+		} else {
+			$emailbody = "
+$langDestination $prenom_form $nom_form
+
+$langYouAreReg$siteName $langAsUser, $langSettings $uname
+$langPass : $password
+$langAddress $siteName $langIs: $urlServer
+$langProblem
+
+$administratorName $administratorSurname
+$langManager $siteName
+$langTel $telephone
+$langEmail : $emailAdministrator
+";
+			}
+
+send_mail($siteName, $emailAdministrator, '', $email_form, $emailsubject, $emailbody, $charset);
+
+// register user 
+
+		if (!isset($institut)) {
+			$institut = "NULL";
+		}
+		$s = mysql_query("SELECT id FROM faculte WHERE name='$department'");
+		$dep = mysql_fetch_array($s);
+		$inscr_user=mysql_query("INSERT INTO `$mysqlMainDb`.user
+			(user_id, nom, prenom, username, password, email, statut, department, inst_id)
+			VALUES ('NULL', '$nom_form', '$prenom_form', '$uname', '$password', '$email_form', '5', '$dep[id]', '$institut')");
+		
+		// close request
+        $rid = intval($_POST['rid']);
+        db_query("UPDATE prof_request set status = '2',
+         date_closed = NOW() WHERE rid = '$rid'");
+
+    $tool_content .= "<tr><td>&nbsp;</td></tr>
+		<tr><td valign='top' align='center' class=alert1>$usersuccess
+		<br><br>
+		<a href='../admin/listrequsers.php' class=mainpage>$langBackReqUsers</a>
+		<br>&nbsp;<br>&nbsp;<br>&nbsp;<br>";
+    $tool_content .= end_tables();
+	}
+}
+
+draw($tool_content,3);
+
+
+// -----------------
+// functions
+// -----------------
+function error_screen($message) {
+
+global $langTryAgain;
+
+return "<tr height='80'><td colspan='3' valign='top' align='center' class=alert1>$message</td></tr>
+      <tr height='30' valign='top' align='center'><td align=center>
+      <a href='newuserreq.php' class=mainpage>$langTryAgain</a></span></td></tr>";
+}
+
+function end_tables() {
+global $langBackAdmin;
+
+$retstring = "</td></tr><tr><td align=right valign=bottom height='180'>";
+$retstring .= "<a href=\"../admin/index.php\" class=mainpage>$langBackAdmin&nbsp;</a>";
+$retstring .= "</td></tr></table>
+						  </td></tr>
+							<tr><td>&nbsp;</td></tr>
+						  </table>";
+
+return $retstring;
+}
+
+?>
