fc4fb6fccfbf1f50a266e56459203214eea93749
Yannis Exidaridis <jexi@noc.uoa.gr>
2007-05-29 14:20:51 +0000

    ξεκαθαρίσματα

--- upgrade/document_upgrade.php
+++ upgrade/document_upgrade.php
@@ -36,15 +36,12 @@
  	The script does not return any feedback
 ==============================================================================*/
 
-
 include '../include/lib/fileUploadLib.inc.php';
 include '../modules/document/forcedownload.php';
 
 //anadromikos algorithmos scannarismatos arxeiwn kai fakelwmn - einai h kyria function gia to scannarisma olwn twn arxeiwn kai twn fakelwn 
 function RecurseDir($directory, $baseFolder)
 {
-	//echo "<b>".getcwd()."</b>";
-	
 	$thisdir = array("name", "struct");
 	$thisdir['name'] = $directory;
 	if ($dir = @opendir($directory))
@@ -90,11 +87,8 @@ function RecurseDir($directory, $baseFolder)
 		$thisdir['struct'] = -1;
 	}
 	
-	//echo "<hr>";
-	//print_r ($thisdir);
 	return $thisdir;
 }//telos anadromikou algorithmou scannarismatos arxeiwn kai fakelwn
-
 
 
 //leitourgeia pou pairnei san orismata $uploadPath = sxetikos fakelos pou vrisketai to arxeio (p.x. "/" h' "/folder/") kai $file = pragmatiko_onoma_arxeiou
@@ -102,16 +96,13 @@ function RecurseDir($directory, $baseFolder)
 function upgrade_document ($uploadPath, $file)
 {
 	
-	$fileName = trim ($file);
-
-        	
+	$fileName = trim ($file);     	
         	
 	//elegxos ean to "path" tou arxeiou pros upload vrisketai hdh se eggrafh ston pinaka documents
     //(aftos einai ousiastika o elegxos if_exists dedomenou tou oti to onoma tou arxeiou sto filesystem
     //einai monadiko)
     
     $result = mysql_query ("SELECT filename FROM document WHERE path LIKE '%".$uploadPath.$fileName."%'");
-    //echo "<br>SELECT filename FROM document WHERE path LIKE '%".$uploadPath.$fileName."%'<br>";
     
 	$row = mysql_fetch_array($result);
 	
@@ -130,14 +121,10 @@ function upgrade_document ($uploadPath, $file)
             
             
             
-            
             //ypologismos onomatos arxeiou me date + time + 3 tyxaia alfarithmitika psifia.
             //to onoma afto tha xrhsimopoiei sto filesystem & tha apothikevetai ston pinaka documents
             $safe_fileName = date("YmdGis")."_".randomkeys('3').".".get_file_extention($fileName);
             
-            
-            
-                   
             
             //prosthiki eggrafhs kai metadedomenwn gia to eggrafo sth vash
             if ($uploadPath == ".")
@@ -158,8 +145,6 @@ function upgrade_document ($uploadPath, $file)
             //san date you arxeiou xrhsimopoihse thn shmerinh hm/nia
             $file_date = date("Y\-m\-d G\:i\:s");
             
-            
-           
             //arxikopoihsh timwn twn metavlhtwn gia to upgrade twn eggrafwn
             $file_comment = "";
             $file_category = "";
@@ -189,36 +174,13 @@ function upgrade_document ($uploadPath, $file)
             	format			=		'$file_format',
             	language		=		'$file_language',
             	copyrighted		=		'$file_copyrighted'";
-            
-            
-            
-            
-            //debuging commands
-            /*$tool_content .=  $query;
-            $tool_content .=  "<br><br>";
-            $tool_content .=  "Copy: $userFile to $baseWorkDir$uploadPath/$safe_fileName<br><br>";
-            exit;*/
-            
-            
-            
-            
-            /*** Allagh onomatos arxeiou me safe_filename ***/
             rename (getcwd().$existinguploadPath, getcwd().$uploadPath2);
-            //echo "<br>rename ".getcwd().$existinguploadPath." ---TO--- ".getcwd().$uploadPath2."<br>";
             
-            
-            //echo "<br>query: $query<br><hr>";
             mysql_query($query);
-            
-            
-            
-            
                   
 	} //telos if(empty($row['filename']))
-        	
 	
 }
-
 
 //function pou epistrefei tyxaious xarakthres. to orisma $length kathorizei to megethos tou apistrefomenou xarakthra
 function randomkeys($length)
