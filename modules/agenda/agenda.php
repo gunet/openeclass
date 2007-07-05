8f9df43ebd79bab3c437f517e798c8d2bfa15621
Yannis Exidaridis <jexi@noc.uoa.gr>
2007-07-05 09:00:11 +0000

    ενσωμάτωση του jscalendar. Ξεκαθάρισμα κώδικα και βελτίωση format ημερομηνιών

--- modules/agenda/agenda.php
+++ modules/agenda/agenda.php
@@ -40,25 +40,29 @@ $helpTopic = 'Agenda';
 $guest_allowed = true;
 
 include '../../include/baseTheme.php';
-
-include('../../include/lib/textLib.inc.php');
-
-include('../../include/action.php');
+include '../../include/lib/textLib.inc.php';
+include '../../include/action.php';
+include '../../include/jscalendar/calendar.php';
 $action = new action();
 $action->record('MODULE_ID_AGENDA');
 
-$dateNow = date("d-m-Y / H:i:s",time());
+$dateNow = date("j-n-Y / H:i",time());
 $nameTools = $langAgenda;
 $tool_content = $head_content = "";
 
 mysql_select_db($dbname);
-if ((@$addEvent == 1 || (isset($id) && $id)) &&$is_adminOfCourse) {
-	if ($language == 'greek')
-	$lang_editor='gr';
-	else
-	$lang_editor='en';
-	//--end if add event
+if ((@$addEvent == 1 || (isset($id) && $id)) && $is_adminOfCourse) {
 
+if ($language == 'greek') {
+		$lang_editor='gr';
+		$lang_jscalendar = 'el';
+}
+	else {
+		$lang_editor='en';
+		$lang_jscalendar = $lang_editor;
+}
+
+	//--end if add event
 
 	//--if add event
 	$head_content = <<<hContent
@@ -69,10 +73,20 @@ if ((@$addEvent == 1 || (isset($id) && $id)) &&$is_adminOfCourse) {
 </script>
 <script type="text/javascript" src="$urlAppend/include/xinha/XinhaCore.js"></script>
 <script type="text/javascript" src="$urlAppend/include/xinha/my_config.js"></script>
+
 hContent;
 
-}
+$jscalendar = new DHTML_Calendar($urlServer.'include/jscalendar/', $lang_jscalendar, 'calendar-blue2', false);
+$head_content .= $jscalendar->get_load_files_code();
 
+$start_cal = $jscalendar->make_input_field(
+					array('showOthers' => true,
+								'align' => 'Tl',
+                 'ifFormat' => '%Y-%m-%d'),
+					array('style' => 'width: 15em; color: #840; background-color: #ff8; border: 1px solid #000; text-align: center',
+                 'name' => 'date',
+                 'value' => ' '));
+}
 //Make top tool links
 if ($is_adminOfCourse) {
 	
@@ -100,13 +114,10 @@ function confirmation (name)
 	$result = db_query("SELECT id FROM agenda", $currentCourseID);
 	if (mysql_num_rows($result) > 1) {
 
-		if (isset($_GET["sens"]) && $_GET["sens"]=="d")
-		{
+		if (isset($_GET["sens"]) && $_GET["sens"]=="d") {
 			$tool_content .=  "<li><a href=\"".$_SERVER['PHP_SELF']."?sens=\" >$langOldToNew</a></li>";
 			$sens=" DESC ";
-		}
-		else
-		{
+		} else {
 			$tool_content .=  "<li><a href=\"".$_SERVER['PHP_SELF']."?sens=d\" >$langNewToOld</a></li>";
 		}
 	}
@@ -114,17 +125,11 @@ function confirmation (name)
 }
 
 if ($is_adminOfCourse) {
-
-
-
 	if (isset($_POST['submit'])) {
-		$date_selection = $fyear."-".$fmonth."-".$fday;
-		$hour = $fhour.":".$fminute.":00";
-		if(isset($tout)&&$tout)
-		{
-			$sql="DELETE FROM agenda";
-		}
-		elseif(isset($id) && $id) {
+		$date_selection = $date;
+		$hour = $fhour.":".$fminute;
+
+		if(isset($id) && $id) {
 			$sql = "UPDATE agenda
                 SET titre='".mysql_real_escape_string(trim($titre))."',
                 contenu='".mysql_real_escape_string(trim($contenu))."',
@@ -135,24 +140,21 @@ if ($is_adminOfCourse) {
 
 			##[BEGIN personalisation modification]############
 			$perso_sql = "UPDATE $mysqlMainDb.agenda
-                                                        SET
-                                                                titre=        '".mysql_real_escape_string(trim($titre))."',
-                                                                contenu='".mysql_real_escape_string(trim($contenu))."',
-                                                                day=        '".mysql_real_escape_string($date_selection)."',
-                                                                hour=        '".mysql_real_escape_string($hour)."',
-                                                                lasting='".mysql_real_escape_string($lasting)."'
-                                                        WHERE
-                                                                lesson_code= '$currentCourseID'
-                                                        AND
-                                                                lesson_event_id='".mysql_real_escape_string($id)."' ";
+                  SET titre='".mysql_real_escape_string(trim($titre))."',
+                      contenu='".mysql_real_escape_string(trim($contenu))."',
+                      day = '".mysql_real_escape_string($date_selection)."',
+                      hour= '".mysql_real_escape_string($hour)."',
+                      lasting='".mysql_real_escape_string($lasting)."'
+                WHERE lesson_code= '$currentCourseID'
+               AND lesson_event_id='".mysql_real_escape_string($id)."' ";
 			##[END personalisation modification]############
 
 			unset($id);
 			unset($contenu);
 			unset($titre);
-		}
-		else
-		{
+
+		} else {
+
 			$sql = "INSERT INTO agenda (id, titre,contenu, day, hour, lasting)
         VALUES (NULL, '".mysql_real_escape_string(trim($titre))."','".mysql_real_escape_string(trim($contenu))."', '".mysql_real_escape_string($date_selection)."','".mysql_real_escape_string($hour)."', '".mysql_real_escape_string($lasting)."')";
 			unset($id);
@@ -162,33 +164,26 @@ if ($is_adminOfCourse) {
 		$result = db_query($sql, $currentCourseID);
 
 		##[BEGIN personalisation modification]############
-		if (substr_count($sql,"INSERT") == 1)
-		{
-			$perso_sql = "SELECT id, titre, contenu,
-                                        DAY , HOUR , lasting
-                                        FROM agenda
-                                        ORDER BY id DESC 
-                                        LIMIT 1 ";
+		if (substr_count($sql,"INSERT") == 1) {
+			$perso_sql = "SELECT id, titre, contenu, DAY, HOUR , lasting
+           FROM agenda ORDER BY id DESC LIMIT 1 ";
 
 			$perso_result = db_query($perso_sql, $currentCourseID);
 			$perso_query_result = mysql_fetch_row($perso_result);
 
-			$perso_matrix['titre']                         = $perso_query_result[0];
-			$perso_matrix['titre']                         = $perso_query_result[1];
-			$perso_matrix['contenu']                 = $perso_query_result[2];
+			$perso_matrix['titre'] = $perso_query_result[0];
+			$perso_matrix['titre'] = $perso_query_result[1];
+			$perso_matrix['contenu'] = $perso_query_result[2];
 			$perso_matrix['date_selection'] = $perso_query_result[3];
-			$perso_matrix['hour']                         = $perso_query_result[4];
-			$perso_matrix['lasting']                 = $perso_query_result[5];
+			$perso_matrix['hour'] = $perso_query_result[4];
+			$perso_matrix['lasting'] = $perso_query_result[5];
 
-			//                Add all data to the main table.
-			$perso_sql = "INSERT INTO $mysqlMainDb.agenda
-                                (lesson_event_id, titre, contenu, day, hour, lasting, lesson_code)
-                                VALUES
-                                (        '".$perso_query_result[0]."','".$perso_query_result[1]."',
-                                        '".$perso_query_result[2]."','".$perso_query_result[3]."',
-                                        '".$perso_query_result[4]."','".$perso_query_result[5]."',
-                                        '".$currentCourseID."'
-                                )";
+			// Add all data to the main table.
+			$perso_sql = "INSERT INTO $mysqlMainDb.agenda (lesson_event_id, titre, contenu, day, hour, lasting, lesson_code)
+               VALUES('".$perso_query_result[0]."','".$perso_query_result[1]."',
+                      '".$perso_query_result[2]."','".$perso_query_result[3]."',
+                      '".$perso_query_result[4]."','".$perso_query_result[5]."',
+                      '".$currentCourseID."')";
 
 		}
 
@@ -236,6 +231,13 @@ if ($is_adminOfCourse) {
 		$dayAncient=$myrow["day"];
 		$lastingAncient=$myrow["lasting"];
 
+		$start_cal = $jscalendar->make_input_field(
+          array('showOthers' => true,
+                'align' => 'Tl',
+                 'ifFormat' => '%Y-%m-%d'),
+          array('style' => 'width: 15em; color: #840; background-color: #ff8; border: 1px solid #000; text-align: center',
+                 'name' => 'date',
+                 'value' => $dayAncient));
 	}
 
 	if (!isset($id)) {
@@ -246,26 +248,21 @@ if ($is_adminOfCourse) {
 	if (@$addEvent == 1 || (isset($id) && $id)) {
 		$tool_content .= <<<tContentForm
 
-
     <p>$langAddEvent</p>
-<form method="post" action="".$_SERVER[PHP_SELF]."">
+	<form method="post" action="".$_SERVER[PHP_SELF]."">
     <input type="hidden" name="id" value="$id">
     <table width = "99%">
     <thead>
     <tr>
-        <th>$langDay</th>
-        <th>$langMonth</th>
-        <th>$langYear</th>
+				<th>$langChooseDate</th>
         <th>$langHour</th>
         <th>$langMinute</th>
         <th>$langLasting</th>
-    </thead>
     </tr>
+    </thead>
 tContentForm;
 
 		$day	= date("d");
-		$month	= date("m");
-		$year	= date("Y");
 		$hours	= date("H");
 		$minutes= date("i");
 
@@ -274,58 +271,39 @@ tContentForm;
 			$hours=$hourAncient[0];
 			$minutes=$hourAncient[1];
 		}
+/*
 		if (isset($dayAncient) && $dayAncient) {
 			$dayAncient= split("-",  $dayAncient);
 			$year=$dayAncient[0];
 			$month=$dayAncient[1];
 			$day=$dayAncient[2];
-		}
-		$tool_content .= "<tbody><tr><td align='center'>
-     <select name=\"fday\">
-     <option value=\"$day\">[$day]</option>";
+		}  */
+		
+		$tool_content .= "<tbody><tr><td align='center'>";
+		
+		// display jscalendar
+		$tool_content .= " ".$start_cal."</td>";
 
-		for ($d=1; $d<=31; $d++) $tool_content .= "<option value='$d'>$d</option>";
+		$tool_content .= "<td align='center'><select name='fhour'>
+		    <option value='$hours'>$hours</option>
+    		<option value='--'>--</option>";
 
-		$tool_content .= "
-    </select>
-    </td>
-    <td align='center'>
-    <select name=\"fmonth\">
-    <option value=\"$month\">[".$langMonthNames['long'][($month-1)]."]</option>";
+    for ($h=0; $h<=24; $h++)
+          $tool_content .= "<option value='$h'>$h</option>";
 
-		for ($i=1; $i<=12; $i++)
-		$tool_content .= "<option value='$i'>".$langMonthNames['long'][$i-1]."</option>";
-		$tool_content .= "</select></td>
-    <td align='center'>
-    <select name=\"fyear\">
-    <option value=\"$year\">[$year]</option>";
+    $tool_content .= "</select></td>";
 
-		$currentyear = date("Y");
-		for ($y = $currentyear - 1; $y <= $currentyear + 2; $y++) {
-			$tool_content .= "<option value=\"$y\">$y</option>\n";
-		}
-
-		$tool_content .= "</select></td>
-    <td align='center'>
-    <select name=\"fhour\">
-    <option value=\"$hours\">[$hours]</option>
-    <option value=\"--\">--</option>";
-
-		for ($h=0; $h<=24; $h++)
-		$tool_content .= "<option value='$h'>$h</option>";
-
-		$tool_content .= "</select></td>
-    <td align='center'>
+		$tool_content .= "<td align='center'>
     <select name=\"fminute\">
     <option value=\"$minutes\">[$minutes]</option>
     <option value=\"--\">--</option>";
 
-		for ($m=0; $m<=55; $m=$m+5) $tool_content .=  "<option value='$m'>$m</option>";
+		for ($m=0; $m<=55; $m=$m+5) 
+					$tool_content .=  "<option value='$m'>$m</option>";
 
 		$tool_content .= "</select></td>
-    <td align='center'><input type=\"text\" name=\"lasting\" value=\"".@$myrow['lasting']."\" size=\"8\"></td>
-    </tr>
-    </tbody>
+    <td align='center'><input type=\"text\" name=\"lasting\" value=\"".@$myrow['lasting']."\" size=\"2\" maxlength=\"2\"></td>
+    </tr></tbody>
     </table><br>";
 
 		$tool_content .="
@@ -334,7 +312,7 @@ tContentForm;
     <td colspan=\"5\"><input type=\"text\" size=\"60\" name=\"titre\" value=\"".@$titre."\"></td>
     </tr></thead></table><br>";
 
-		if (!isset($contenu)){
+		if (!isset($contenu)) {
 			$contenu="";
 		}
 
@@ -350,15 +328,8 @@ tContentForm;
     <input type=\"Submit\" name=\"submit\" value=\"$langOk\">
 </form>
 <br><br>";
+
 	}
-
-	//	if (@$addEvent != 1) {
-	//		$tool_content .= "
-	//			<a href=\"".$_SERVER['PHP_SELF']."?addEvent=1\">".$langAddEvent."</a> |
-	//
-	//";
-
-	//	}
 }
 
 /*---------------------------------------------
@@ -376,7 +347,6 @@ if (mysql_num_rows($result) > 0) {
 	$tool_content .= "<tbody>";
 
 	$numLine=0;
-	//$result = db_query("SELECT id, titre, contenu, day, hour, lasting FROM agenda ORDER BY day ".$sens.", hour ".$sens,$currentCourseID);
 	$barreMois ="";
 	$nowBarShowed = FALSE;
 
@@ -384,19 +354,16 @@ if (mysql_num_rows($result) > 0) {
 		$contenu = $myrow["contenu"];
 		$contenu = nl2br($contenu);
 		$contenu = make_clickable($contenu);
-		if (!$nowBarShowed)
-		{
+		if (!$nowBarShowed) {
 			// Following order
 			if (((strtotime($myrow["day"]." ".$myrow["hour"]) > time()) && ($sens==" ASC")) ||
 			((strtotime($myrow["day"]." ".$myrow["hour"]) < time()) && ($sens==" DESC "))) {
 				if ($barreMois!=date("m",time())) {
 					$barreMois=date("m",time());
-					$tool_content .=  "
-                <tr>
-                    <td class=\"month\" colspan=\"2\" valign=\"top\">
-                        ".$langCalendar."&nbsp;".ucfirst(claro_format_locale_date("%B %Y",time()))."
-                    </td>
-                </tr>";
+					$tool_content .= "<tr>
+               <td class=\"month\" colspan=\"2\" valign=\"top\">
+                ".$langCalendar."&nbsp;".ucfirst(claro_format_locale_date("%B %Y",time()))."
+                </td></tr>";
 				}
 				$nowBarShowed = TRUE;
 				$tool_content .=  "<tr>
@@ -409,17 +376,15 @@ if (mysql_num_rows($result) > 0) {
 	   ".$langCalendar."&nbsp;".ucfirst(claro_format_locale_date("%B %Y",strtotime($myrow["day"])))."
      </td></tr>";
 		}
-		$tool_content .=  "
-<!-- Date -->
-    ";
+		
 		if($numLine%2 == 0)
-		$tool_content .=  "<tr>";
+			$tool_content .=  "<tr>";
 		elseif($numLine%2 == 1)
-		$tool_content .=  "<tr class=\"odd\">";
+			$tool_content .=  "<tr class=\"odd\">";
 		if ($is_adminOfCourse)
-		$tool_content .=  "<td valign=\"top\">";
+			$tool_content .=  "<td valign=\"top\">";
 		else
-		$tool_content .=  "<td valign=\"top\" colspan=\"2\">";
+			$tool_content .=  "<td valign=\"top\" colspan=\"2\">";
 
 		$tool_content .=  "<p>".ucfirst(claro_format_locale_date($dateFormatLong,strtotime($myrow["day"])))."
         / $langHour:
@@ -427,9 +392,9 @@ if (mysql_num_rows($result) > 0) {
 
 		if ($myrow["lasting"] !="") {
 			if ($myrow["lasting"] == 1)
-			$message = $langHour;
+				$message = $langHour;
 			else
-			$message = $langHours;
+				$message = $langHours;
 			$tool_content .=  "<br>".$langLasting.": ".$myrow["lasting"]." ".$message."";
 		}
 
