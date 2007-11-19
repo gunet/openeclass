da416dcf03b40eaf3f0ce04622697112f92efa3b
Yannis Exidaridis <jexi@noc.uoa.gr>
2007-11-19 13:39:51 +0000

    added

---
+++ modules/lang/english/common.inc.php
@@ -0,0 +1,15 @@
+<?
+
+/*******************************************************
+* General Messages. 
+*******************************************************/
+
+$langYes = "Yes";
+$langNo = "No";
+
+$langTeachers = "Teachers";
+$langTeacher = "Teacher";
+$langStudent = "Student";
+$langStudents = "Students";
+
+?>
