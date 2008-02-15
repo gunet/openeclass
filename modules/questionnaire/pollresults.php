8bc55b304508e793dfa91b23e1de411bd7dbb465
Yannis Exidaridis <jexi@noc.uoa.gr>
2008-02-15 13:02:18 +0000

    small changes

--- modules/questionnaire/pollresults.php
+++ modules/questionnaire/pollresults.php
@@ -23,17 +23,8 @@
         eMail: eclassadmin@gunet.gr
 ==============================================================================*/
 
-/*===========================================================================
-	pollresults.php
-	@last update: 26-5-2006 by Dionysios Synodinos
-	@authors list: Dionysios G. Synodinos <synodinos@gmail.com>
-==============================================================================        
-
-*/
 
 $require_current_course = TRUE;
-$langFiles = 'questionnaire';
-
 $require_help = TRUE;
 $helpTopic = 'Questionnaire';
 
@@ -59,6 +50,7 @@ if(!isset($_GET['pid']) || !is_numeric($_GET['pid'])) die();
 
 	$questions = db_query("SELECT * FROM poll_question WHERE pid=$pid");
 	while ($theQuestion = mysql_fetch_array($questions)) {
+		$tool_content .= "<h3>$theQuestion[question_text]</h3>";
 		if ($theQuestion['qtype'] == 'multiple') {
 			$answers = db_query("SELECT COUNT(aid) AS count, poll_question_answer.answer_text AS answer 
 					FROM poll_answer_record LEFT JOIN poll_question_answer 
@@ -72,9 +64,9 @@ if(!isset($_GET['pid']) || !is_numeric($_GET['pid'])) die();
 				$answer_total += $theAnswer['count'];
 				$answer_text[] = $theAnswer['answer'];
 			}
-	
 			$chart = new PieChart(600, 300);
-			$chart->setTitle($theQuestion['question_text']	);
+			$chart->setMargin(5);
+			$chart->setTitle('');
 			foreach ($answer_counts as $i => $count) {
 				$percentage = 100 * ($count / $answer_total);
 				$label = sprintf("$answer_text[$i] (%2.1f%%)", $percentage); 
@@ -85,48 +77,17 @@ if(!isset($_GET['pid']) || !is_numeric($_GET['pid'])) die();
 			$chart->render($webDir.$chart_path);
 			$tool_content .= '<img src="'.$urlServer.$chart_path.'" /><br>';
 		} else {
-			$tool_content .= "<p><b>$theQuestion[question_text]</b></p><dl>";
 			$answers = db_query("SELECT answer_text, user_id FROM poll_answer_record 
 					WHERE qid = $theQuestion[pqid]", $currentCourseID);
+			$tool_content .= '<dl>';
 			while ($theAnswer = mysql_fetch_array($answers)) {
-				$tool_content .= "<dt>" . uid_to_name($theAnswer['user_id']) . "</dt><dd>$theAnswer[answer_text]</dd>"; 
-			}		
+				$tool_content .= "<dt>" . uid_to_name($theAnswer['user_id']) . ":</dt> <dd>$theAnswer[answer_text]</dd>"; 
+			}
+			$tool_content .= '</dl>';
 		}
-
 	}
-
 	$tool_content .= "$langPollTotalAnswers : <b>$answer_total</b><br>";
 
-/*
-// display individual results 
-	$tool_content .= "<p><b>" . $langIndividuals . "</b><br>";
-	
-	$answers = db_query("
-		select * from poll_answer 
-		where pid=".mysql_real_escape_string($_GET['pid'])." "
-		."ORDER BY pid", $currentCourse);
-	
-	while ($theAnswer = mysql_fetch_array($answers)) {
-		$creator_id = $theAnswer["creator_id"];
-		$aid = $theAnswer["aid"];
-		$answer_creator = db_query("
-			select nom,prenom from user 
-			where user_id='$creator_id'", $mysqlMainDb);
-		$theCreator = mysql_fetch_array($answer_creator);
-		$tool_content .= "<table width=\"99%\"><tr><td>";
-		$tool_content .= "<br><b>" . $theCreator["nom"]. " " . $theCreator["prenom"] . "</b><br>";
-		$qas = db_query("
-			select * from poll_answer_record 
-			where aid=$aid 
-			ORDER BY aid", $currentCourse);
-			while ($theQAs = mysql_fetch_array($qas)) {	
-				$tool_content .= "<br><b>" . $theQAs["question_text"]. "</b>: <br>" . $theQAs["question_answer"] . "<br>";
-			}
-			$tool_content .= "<br>";
-			$tool_content .= "</td></tr></table><br><br>";
-		}
-}
-*/
 // display page
 draw($tool_content, 2); 
 ?>
