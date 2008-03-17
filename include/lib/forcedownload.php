db72758e065b8c11264ea0f36deb6f6b0ddae5dd
Alexandros Diamantidis <adia@noc.uoa.gr>
2008-03-17 11:09:37 +0000

    Add a third parameter to send_file_to_client, to indicate if files should be send inline or as attachments

--- include/lib/forcedownload.php
+++ include/lib/forcedownload.php
@@ -45,201 +45,202 @@
     
 ==============================================================================*/
 
-function send_file_to_client($real_filename,$filename)
+function send_file_to_client($real_filename, $filename, $send_inline = false)
 {
- if(!file_exists($real_filename))
-  {
-  return false;
-  }
- 
- if(isset($content_type))
- {
-  header( "Content-type: $content_type\n");
-  header( "Content-Disposition: inline; filename=\"$filename\"");
-  }
-  else
-    {
-     header( "Content-type: application/octet-stream\n");
-  if(preg_match("/MSIE 5.5/",$_SERVER['HTTP_USER_AGENT']))
-         header("Content-Disposition: filename=\"$filename\"\n");
-     else header("Content-Disposition: attachment; filename=\"$filename\"\n");
-   }
- 
- if(strpos($_SERVER['HTTP_USER_AGENT'],'MSIE'))
- {
-     header( "Pragma: \n");
-     header( "Cache-Control: \n");
-     header( "Cache-Control: public\n"); // IE cannot download from sessions without a cache
- }
- 
- header("Content-Description: \"".trim(htmlentities($filename))."\"\n");
- header("Content-transfer-encoding: binary\n");
- header("Content-length: ".filesize($real_filename)."\n" );
- 
-    $fp=fopen("$real_filename","r");
-    fpassthru($fp);
-    return true;
+        if(!file_exists($real_filename))
+        {
+                return false;
+        }
+
+        $content_type = get_mime_type($filename);
+        if ($send_inline) {
+                $disposition = 'inline';
+        } else {
+                $disposition = 'attachment';
+        }
+        header("Content-type: $content_type");
+        header("Content-Disposition: $disposition; filename=\"$filename\"");
+
+        // IE cannot download from sessions without a cache
+        if (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE'))
+        {
+                header('Pragma: public');
+                header('Cache-Control: no-store, no-cache, no-transform, must-revalidate, private');
+                header('Expires: 0');
+        }
+
+        header('Content-length: ' . filesize($real_filename));
+
+        $fp = fopen($real_filename, 'r');
+        fpassthru($fp);
+        return true;
 }
-//-----------------------------------------------------------------------------
+
+
 function get_file_extention($filename)
 {
- $pos=strpos(strrev($filename),".");
- if($pos)
-  $ext=substr($filename,strlen($filename)-$pos,$pos);
- else $ext="empty";
- return $ext;
+        $pos=strpos(strrev($filename),".");
+        if($pos) {
+                $ext = substr($filename,strlen($filename)-$pos,$pos);
+        } else {
+                $ext="";
+        }
+        return $ext;
 }
-//-----------------------------------------------------------------------------
+
+
 function get_mime_type($filename)
 {
-$f=array(
-         "ez" => "application/andrew-inset",
-         "hqx" => "application/mac-binhex40",
-         "cpt" => "application/mac-compactpro",
-         "doc" => "application/msword",
-         "bin" => "application/octet-stream",
-         "dms" => "application/octet-stream",
-         "lha" => "application/octet-stream",
-         "lzh" => "application/octet-stream",
-         "exe" => "application/octet-stream",
-         "class" => "application/octet-stream",
-         "so" => "application/octet-stream",
-         "dll" => "application/octet-stream",
-         "oda" => "application/oda",
-         "pdf" => "application/pdf",
-         "ai" => "application/postscript",
-         "eps" => "application/postscript",
-         "ps" => "application/postscript",
-         "smi" => "application/smil",
-         "smil" => "application/smil",
-         "wbxml" => "application/vnd.wap.wbxml",
-         "wmlc" => "application/vnd.wap.wmlc",
-         "wmlsc" => "application/vnd.wap.wmlscriptc",
-         "bcpio" => "application/x-bcpio",
-         "vcd" => "application/x-cdlink",
-         "pgn" => "application/x-chess-pgn",
-         "cpio" => "application/x-cpio",
-         "csh" => "application/x-csh",
-         "dcr" => "application/x-director",
-         "dir" => "application/x-director",
-         "dxr" => "application/x-director",
-         "dvi" => "application/x-dvi",
-         "spl" => "application/x-futuresplash",
-         "gtar" => "application/x-gtar",
-         "hdf" => "application/x-hdf",
-         "js" => "application/x-javascript",
-         "skp" => "application/x-koan",
-         "skd" => "application/x-koan",
-         "skt" => "application/x-koan",
-         "skm" => "application/x-koan",
-         "latex" => "application/x-latex",
-         "nc" => "application/x-netcdf",
-         "cdf" => "application/x-netcdf",
-         "sh" => "application/x-sh",
-         "shar" => "application/x-shar",
-         "swf" => "application/x-shockwave-flash",
-         "sit" => "application/x-stuffit",
-         "sv4cpio" => "application/x-sv4cpio",
-         "sv4crc" => "application/x-sv4crc",
-         "tar" => "application/x-tar",
-         "tcl" => "application/x-tcl",
-         "tex" => "application/x-tex",
-         "texinfo" => "application/x-texinfo",
-         "texi" => "application/x-texinfo",
-         "t" => "application/x-troff",
-         "tr" => "application/x-troff",
-         "roff" => "application/x-troff",
-         "man" => "application/x-troff-man",
-         "me" => "application/x-troff-me",
-         "ms" => "application/x-troff-ms",
-         "ustar" => "application/x-ustar",
-         "src" => "application/x-wais-source",
-         "xhtml" => "application/xhtml+xml",
-         "xht" => "application/xhtml+xml",
-         "zip" => "application/zip",
-         "au" => "audio/basic",
-         "snd" => "audio/basic",
-         "mid" => "audio/midi",
-         "midi" => "audio/midi",
-         "kar" => "audio/midi",
-         "mpga" => "audio/mpeg",
-         "mp2" => "audio/mpeg",
-         "mp3" => "audio/mpeg",
-         "aif" => "audio/x-aiff",
-         "aiff" => "audio/x-aiff",
-         "aifc" => "audio/x-aiff",
-         "m3u" => "audio/x-mpegurl",
-         "ram" => "audio/x-pn-realaudio",
-         "rm" => "audio/x-pn-realaudio",
-         "rpm" => "audio/x-pn-realaudio-plugin",
-         "ra" => "audio/x-realaudio",
-         "wav" => "audio/x-wav",
-         "pdb" => "chemical/x-pdb",
-         "xyz" => "chemical/x-xyz",
-         "bmp" => "image/bmp",
-         "gif" => "image/gif",
-         "ief" => "image/ief",
-         "jpeg" => "image/jpeg",
-         "jpg" => "image/jpeg",
-         "jpe" => "image/jpeg",
-         "png" => "image/png",
-         "tiff" => "image/tiff",
-         "tif" => "image/tif",
-         "djvu" => "image/vnd.djvu",
-         "djv" => "image/vnd.djvu",
-         "wbmp" => "image/vnd.wap.wbmp",
-         "ras" => "image/x-cmu-raster",
-         "pnm" => "image/x-portable-anymap",
-         "pbm" => "image/x-portable-bitmap",
-         "pgm" => "image/x-portable-graymap",
-         "ppm" => "image/x-portable-pixmap",
-         "rgb" => "image/x-rgb",
-         "xbm" => "image/x-xbitmap",
-         "xpm" => "image/x-xpixmap",
-         "xwd" => "image/x-windowdump",
-         "igs" => "model/iges",
-         "iges" => "model/iges",
-         "msh" => "model/mesh",
-         "mesh" => "model/mesh",
-         "silo" => "model/mesh",
-         "wrl" => "model/vrml",
-         "vrml" => "model/vrml",
-         "css" => "text/css",
-         "htm" => "text/html",
-         "html" => "application/octet-stream",
-         "asc" => "text/plain",
-         "txt" => "text/plain",
-         "rtx" => "text/richtext",
-         "rtf" => "text/rtf",
-         "sgml" => "text/sgml",
-         "sgm" => "text/sgml",
-         "tsv" => "text/tab-seperated-values",
-         "wml" => "text/vnd.wap.wml",
-         "wmls" => "text/vnd.wap.wmlscript",
-         "etx" => "text/x-setext",
-         "xml" => "text/xml",
-         "xsl" => "text/xml",
-         "mpeg" => "video/mpeg",
-         "mpg" => "video/mpeg",
-         "mpe" => "video/mpeg",
-         "qt" => "video/quicktime",
-         "mov" => "video/quicktime",
-         "mxu" => "video/vnd.mpegurl",
-         "avi" => "video/x-msvideo",
-         "movie" => "video/x-sgi-movie",
-         "ice" => "x-conference-xcooltalk",
-	 "asx" => "video/x-ms-asf",
-	 "wma" => "audio/x-ms-wma",
-	 "wax" => "audio/x-ms-wax",
-	 "wmv" => "video/x-ms-wmv",
-	 "wvx" => "video/x-ms-wvx",
-	 "wm" => "video/x-ms-wm",
-	"wmx" => "video/x-ms-wmx",
-	"wmz" => "application/x-ms-wmz",
-	"wmd" => "application/x-ms-wmd"
-      );
-      $ext=get_file_extention($filename);
-      if(isset($f[$ext])) return $f[$ext];
-      return "application/octet-stream";
+        $f=array(
+                "ez" => "application/andrew-inset",
+                "hqx" => "application/mac-binhex40",
+                "cpt" => "application/mac-compactpro",
+                "doc" => "application/msword",
+                "bin" => "application/octet-stream",
+                "dms" => "application/octet-stream",
+                "lha" => "application/octet-stream",
+                "lzh" => "application/octet-stream",
+                "exe" => "application/octet-stream",
+                "class" => "application/octet-stream",
+                "so" => "application/octet-stream",
+                "dll" => "application/octet-stream",
+                "oda" => "application/oda",
+                "pdf" => "application/pdf",
+                "ai" => "application/postscript",
+                "eps" => "application/postscript",
+                "ps" => "application/postscript",
+                "smi" => "application/smil",
+                "smil" => "application/smil",
+                "wbxml" => "application/vnd.wap.wbxml",
+                "wmlc" => "application/vnd.wap.wmlc",
+                "wmlsc" => "application/vnd.wap.wmlscriptc",
+                "bcpio" => "application/x-bcpio",
+                "vcd" => "application/x-cdlink",
+                "pgn" => "application/x-chess-pgn",
+                "cpio" => "application/x-cpio",
+                "csh" => "application/x-csh",
+                "dcr" => "application/x-director",
+                "dir" => "application/x-director",
+                "dxr" => "application/x-director",
+                "dvi" => "application/x-dvi",
+                "spl" => "application/x-futuresplash",
+                "gtar" => "application/x-gtar",
+                "hdf" => "application/x-hdf",
+                "js" => "application/x-javascript",
+                "skp" => "application/x-koan",
+                "skd" => "application/x-koan",
+                "skt" => "application/x-koan",
+                "skm" => "application/x-koan",
+                "latex" => "application/x-latex",
+                "nc" => "application/x-netcdf",
+                "cdf" => "application/x-netcdf",
+                "sh" => "application/x-sh",
+                "shar" => "application/x-shar",
+                "swf" => "application/x-shockwave-flash",
+                "sit" => "application/x-stuffit",
+                "sv4cpio" => "application/x-sv4cpio",
+                "sv4crc" => "application/x-sv4crc",
+                "tar" => "application/x-tar",
+                "tcl" => "application/x-tcl",
+                "tex" => "application/x-tex",
+                "texinfo" => "application/x-texinfo",
+                "texi" => "application/x-texinfo",
+                "t" => "application/x-troff",
+                "tr" => "application/x-troff",
+                "roff" => "application/x-troff",
+                "man" => "application/x-troff-man",
+                "me" => "application/x-troff-me",
+                "ms" => "application/x-troff-ms",
+                "ustar" => "application/x-ustar",
+                "src" => "application/x-wais-source",
+                "xhtml" => "application/xhtml+xml",
+                "xht" => "application/xhtml+xml",
+                "zip" => "application/zip",
+                "au" => "audio/basic",
+                "snd" => "audio/basic",
+                "mid" => "audio/midi",
+                "midi" => "audio/midi",
+                "kar" => "audio/midi",
+                "mpga" => "audio/mpeg",
+                "mp2" => "audio/mpeg",
+                "mp3" => "audio/mpeg",
+                "aif" => "audio/x-aiff",
+                "aiff" => "audio/x-aiff",
+                "aifc" => "audio/x-aiff",
+                "m3u" => "audio/x-mpegurl",
+                "ram" => "audio/x-pn-realaudio",
+                "rm" => "audio/x-pn-realaudio",
+                "rpm" => "audio/x-pn-realaudio-plugin",
+                "ra" => "audio/x-realaudio",
+                "wav" => "audio/x-wav",
+                "pdb" => "chemical/x-pdb",
+                "xyz" => "chemical/x-xyz",
+                "bmp" => "image/bmp",
+                "gif" => "image/gif",
+                "ief" => "image/ief",
+                "jpeg" => "image/jpeg",
+                "jpg" => "image/jpeg",
+                "jpe" => "image/jpeg",
+                "png" => "image/png",
+                "tiff" => "image/tiff",
+                "tif" => "image/tif",
+                "djvu" => "image/vnd.djvu",
+                "djv" => "image/vnd.djvu",
+                "wbmp" => "image/vnd.wap.wbmp",
+                "ras" => "image/x-cmu-raster",
+                "pnm" => "image/x-portable-anymap",
+                "pbm" => "image/x-portable-bitmap",
+                "pgm" => "image/x-portable-graymap",
+                "ppm" => "image/x-portable-pixmap",
+                "rgb" => "image/x-rgb",
+                "xbm" => "image/x-xbitmap",
+                "xpm" => "image/x-xpixmap",
+                "xwd" => "image/x-windowdump",
+                "igs" => "model/iges",
+                "iges" => "model/iges",
+                "msh" => "model/mesh",
+                "mesh" => "model/mesh",
+                "silo" => "model/mesh",
+                "wrl" => "model/vrml",
+                "vrml" => "model/vrml",
+                "css" => "text/css",
+                "htm" => "text/html",
+                "html" => "application/octet-stream",
+                "asc" => "text/plain",
+                "txt" => "text/plain",
+                "rtx" => "text/richtext",
+                "rtf" => "text/rtf",
+                "sgml" => "text/sgml",
+                "sgm" => "text/sgml",
+                "tsv" => "text/tab-seperated-values",
+                "wml" => "text/vnd.wap.wml",
+                "wmls" => "text/vnd.wap.wmlscript",
+                "etx" => "text/x-setext",
+                "xml" => "text/xml",
+                "xsl" => "text/xml",
+                "mpeg" => "video/mpeg",
+                "mpg" => "video/mpeg",
+                "mpe" => "video/mpeg",
+                "qt" => "video/quicktime",
+                "mov" => "video/quicktime",
+                "mxu" => "video/vnd.mpegurl",
+                "avi" => "video/x-msvideo",
+                "movie" => "video/x-sgi-movie",
+                "ice" => "x-conference-xcooltalk",
+                "asx" => "video/x-ms-asf",
+                "wma" => "audio/x-ms-wma",
+                "wax" => "audio/x-ms-wax",
+                "wmv" => "video/x-ms-wmv",
+                "wvx" => "video/x-ms-wvx",
+                "wm" => "video/x-ms-wm",
+                "wmx" => "video/x-ms-wmx",
+                "wmz" => "application/x-ms-wmz",
+                "wmd" => "application/x-ms-wmd");
+        $ext = get_file_extention($filename);
+        if (isset($f[$ext])) {
+                return $f[$ext];
+        } else {
+                return "application/octet-stream";
+        }
 }
 ?>
