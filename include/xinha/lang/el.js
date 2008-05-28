62555b7115b1923bf2daf6fcedcc6365bf9c7083
Yannis Exidaridis <jexi@noc.uoa.gr>
2008-05-28 12:52:11 +0000

    convert to utf

--- include/xinha/lang/el.js
+++ include/xinha/lang/el.js
@@ -4,52 +4,52 @@
 // Author: Dimitris Glezos, dimitris@glezos.com
 
 {
-  "Bold": "ΞΞ½ΟΞΏΞ½Ξ±",
-  "Italic": "Ξ Ξ»Ξ¬Ξ³ΞΉΞ±",
-  "Underline": "Ξ�ΟΞΏΞ³ΟΞ±ΞΌΞΌΞΉΟΞΌΞ­Ξ½Ξ±",
-  "Strikethrough": "ΞΞΉΞ±Ξ³ΟΞ±ΞΌΞΌΞ­Ξ½Ξ±",
-  "Subscript": "ΞΞ΅Ξ―ΞΊΟΞ·Ο",
-  "Superscript": "ΞΞ΅Ξ―ΞΊΟΞ·Ο",
-  "Justify Left": "Ξ£ΟΞΏΞ―ΟΞΉΟΞ· ΞΟΞΉΟΟΞ΅ΟΞ¬",
-  "Justify Center": "Ξ£ΟΞΏΞ―ΟΞΉΟΞ· ΞΞ­Ξ½ΟΟΞΏ",
-  "Justify Right": "Ξ£ΟΞΏΞ―ΟΞΉΟΞ· ΞΞ΅ΞΎΞΉΞ¬",
-  "Justify Full": "Ξ Ξ»Ξ�ΟΞ·Ο Ξ£ΟΞΏΞ―ΟΞΉΟΞ·",
-  "Ordered List": "ΞΟΞ―ΞΈΞΌΞ·ΟΞ·",
-  "Bulleted List": "ΞΞΏΟΞΊΞΊΞ―Ξ΄Ξ΅Ο",
-  "Decrease Indent": "ΞΞ΅Ξ―ΟΟΞ· ΞΟΞΏΟΞ�Ο",
-  "Increase Indent": "ΞΟΞΎΞ·ΟΞ· ΞΟΞΏΟΞ�Ο",
-  "Font Color": "Ξ§ΟΟΞΌΞ± ΞΟΞ±ΞΌΞΌΞ±ΟΞΏΟΞ΅ΞΉΟΞ¬Ο",
-  "Background Color": "Ξ§ΟΟΞΌΞ± Ξ¦ΟΞ½ΟΞΏΟ",
-  "Horizontal Rule": "ΞΟΞΉΞΆΟΞ½ΟΞΉΞ± ΞΟΞ±ΞΌΞΌΞ�",
-  "Insert Web Link": "ΞΞΉΟΞ±Ξ³ΟΞ³Ξ� Ξ£ΟΞ½Ξ΄Ξ­ΟΞΌΞΏΟ",
-  "Insert/Modify Image": "ΞΞΉΟΞ±Ξ³ΟΞ³Ξ�/Ξ�ΟΞΏΟΞΏΟΞΏΞ―Ξ·ΟΞ· ΞΞΉΞΊΟΞ½Ξ±Ο",
-  "Insert Table": "ΞΞΉΟΞ±Ξ³ΟΞ³Ξ� Ξ Ξ―Ξ½Ξ±ΞΊΞ±",
-  "Toggle HTML Source": "ΞΞ½Ξ±Ξ»Ξ»Ξ±Ξ³Ξ� ΟΞ΅/Ξ±ΟΟ HTML",
-  "Enlarge Editor": "ΞΞ΅Ξ³Ξ­Ξ½ΞΈΟΞ½ΟΞ· Ξ΅ΟΞ΅ΞΎΞ΅ΟΞ³Ξ±ΟΟΞ�",
-  "About this editor": "Ξ Ξ»Ξ·ΟΞΏΟΞΏΟΞ―Ξ΅Ο",
-  "Help using editor": "ΞΞΏΞ�ΞΈΞ΅ΞΉΞ±",
-  "Current style": "Ξ Ξ±ΟΟΞ½ ΟΟΟΞ»",
-  "Undoes your last action": "ΞΞ½Ξ±Ξ―ΟΞ΅ΟΞ· ΟΞ΅Ξ»Ξ΅ΟΟΞ±Ξ―Ξ±Ο Ξ΅Ξ½Ξ­ΟΞ³Ξ΅ΞΉΞ±Ο",
-  "Redoes your last action": "ΞΟΞ±Ξ½Ξ±ΟΞΏΟΞ¬ Ξ±ΟΟ Ξ±Ξ½Ξ±Ξ―ΟΞ΅ΟΞ·",
-  "Cut selection": "ΞΟΞΏΞΊΞΏΟΞ�",
-  "Copy selection": "ΞΞ½ΟΞΉΞ³ΟΞ±ΟΞ�",
-  "Paste from clipboard": "ΞΟΞΉΞΊΟΞ»Ξ»Ξ·ΟΞ·",
-  "Direction left to right": "ΞΞ±ΟΞ΅ΟΞΈΟΞ½ΟΞ· Ξ±ΟΞΉΟΟΞ΅ΟΞ¬ ΟΟΞΏΟ Ξ΄Ξ΅ΞΎΞΉΞ¬",
-  "Direction right to left": "ΞΞ±ΟΞ΅ΟΞΈΟΞ½ΟΞ· Ξ±ΟΟ Ξ΄Ξ΅ΞΎΞΉΞ¬ ΟΟΞΏΟ ΟΞ± Ξ±ΟΞΉΟΟΞ΅ΟΞ¬",
-  "OK": "OK",
-  "Cancel": "ΞΞΊΟΟΟΟΞ·",
-  "Path": "ΞΞΉΞ±Ξ΄ΟΞΏΞΌΞ�",
-  "You are in TEXT MODE.  Use the [<>] button to switch back to WYSIWYG.": "ΞΞ―ΟΟΞ΅ ΟΞ΅ TEXT MODE.  Ξ§ΟΞ·ΟΞΉΞΌΞΏΟΞΏΞΉΞ�ΟΟΞ΅ ΟΞΏ ΞΊΞΏΟΞΌΟΞ― [<>] Ξ³ΞΉΞ± Ξ½Ξ± Ξ΅ΟΞ±Ξ½Ξ­ΟΞΈΞ΅ΟΞ΅ ΟΟΞΏ WYSIWIG.",
-  "The full screen mode is known to cause problems with Internet Explorer, due to browser bugs that we weren": "Ξ ΞΊΞ±ΟΞ¬ΟΟΞ±ΟΞ· ΟΞ»Ξ�ΟΞ·Ο ΞΏΞΈΟΞ½Ξ·Ο Ξ­ΟΞ΅ΞΉ ΟΟΞΏΞ²Ξ»Ξ�ΞΌΞ±ΟΞ± ΞΌΞ΅ ΟΞΏΞ½ Internet Explorer, Ξ»ΟΞ³Ο ΟΟΞ±Ξ»ΞΌΞ¬ΟΟΞ½ ΟΟΞΏΞ½ Ξ―Ξ΄ΞΉΞΏ ΟΞΏΞ½ browser.  ΞΞ½ ΟΞΏ ΟΟΟΟΞ·ΞΌΞ± ΟΞ±Ο Ξ΅Ξ―Ξ½Ξ±ΞΉ Windows 9x ΞΌΟΞΏΟΞ΅Ξ― ΞΊΞ±ΞΉ Ξ½Ξ± ΟΟΞ΅ΞΉΞ±ΟΟΞ΅Ξ―ΟΞ΅ reboot. ΞΞ½ Ξ΅Ξ―ΟΟΞ΅ ΟΞ―Ξ³ΞΏΟΟΞΏΞΉ, ΟΞ±ΟΞ�ΟΟΞ΅ ΞΞ.",
-  "Cancel": "ΞΞΊΟΟΟΟΞ·",
-  "Insert/Modify Link": "ΞΞΉΟΞ±Ξ³ΟΞ³Ξ�/Ξ�ΟΞΏΟΞΏΟΞΏΞ―Ξ·ΟΞ· ΟΟΞ½Ξ΄Ξ΅ΟΞΌΞΏΟ",
-  "New window (_blank)": "ΞΞ­ΞΏ ΟΞ±ΟΞ¬ΞΈΟΟΞΏ (_blank)",
-  "None (use implicit)": "ΞΞ±Ξ½Ξ­Ξ½Ξ± (ΟΟΞ�ΟΞ· Ξ±ΟΟΞ»ΟΟΞΏΟ)",
-  "Other": "ΞΞ»Ξ»ΞΏ",
-  "Same frame (_self)": "ΞΞ΄ΞΉΞΏ frame (_self)",
-  "Target:": "Target:",
-  "Title (tooltip):": "Ξ�Ξ―ΟΞ»ΞΏΟ (tooltip):",
-  "Top frame (_top)": "Ξ Ξ¬Ξ½Ο frame (_top)",
+  "Bold": "Έντονα",
+  "Italic": "Πλαγιαστά",
+  "Underline": "Υπογραμμισμένα",
+  "Strikethrough": "Διαγραμμισμένα",
+  "Subscript": "Δείκτης",
+  "Superscript": "Εκθέτης",
+  "Justify Left": "Στοίχιση στα Αριστερά",
+  "Justify Center": "Στοίχιση στο Κέντρο",
+  "Justify Right": "Στοίχιση στα Δεξιά",
+  "Justify Full": "Ομοιόμορφη Στοίχιση",
+  "Ordered List": "Διατεταγμένη Λίστα",
+  "Bulleted List": "Λίστα με εικονίδια",
+  "Decrease Indent": "Μείωση Εσοχής",
+  "Increase Indent": "Αύξηση Εσοχής",
+  "Font Color": "Χρώμα Γραμματοσειράς",
+  "Background Color": "Χρώμα Φόντου",
+  "Horizontal Rule": "Οριζόντια Γραμμή",
+  "Insert Web Link": "Εισαγωγή δικτυακού συνδέσμου",
+  "Insert/Modify Image": "Εισαγωγή / Αλλαγή Εικόνας",
+  "Insert Table": "Εισαγωγή Πίνακα",
+  "Toggle HTML Source": "Μετάβαση σε κώδικα HTML",
+  "Enlarge Editor": "Μεγέθυνση του συντάκτη κειμένου",
+  "About this editor": "Πληροφορίες για τον συντάκτη κειμένου",
+  "Help using editor": "Βοήθεια",
+  "Current style": "Τρέχον στύλ",
+  "Undoes your last action": "Ακύρωση τελευταίας ενέργειας",
+  "Redoes your last action": "Επανάληψη τελευταίας ενέργειας",
+  "Cut selection": "Αποκοπή Επιλογής",
+  "Copy selection": "Αντιγραφή Επιλογής",
+  "Paste from clipboard": "Επικόλληση",
+  "Direction left to right": "Με κατεύθυνση αριστερά προς τα δεξιά",
+  "Direction right to left": "Με κατεύθυνση δεξιά προς τα αριστερά",
+  "OK": "Εντάξει",
+  "Cancel": "Ακύρωση",
+  "Path": "Διαδρομή",
+  "You are in TEXT MODE.  Use the [<>] button to switch back to WYSIWYG.": "Είστε σε κατάσταση κειμένου. Χρησιμοποιήστε το κουμπί [<>] για να επανέλθετε.",
+  "The full screen mode is known to cause problems with Internet Explorer, due to browser bugs": "Η κατάσταση πλήρης οθόνης μπορεί να μην λειτουργεί σωστά αν χρησιμοποιείται Internet Explorer",
+  "Cancel": "Ακύρωση",
+  "Insert/Modify Link": "Εισαγωγή / Τροποποίηση Συνδέσμου",
+  "New window (_blank)": "Σε καινούριο παράθυρο",
+  "None (use implicit)": "Τίποτα",
+  "Other": "Άλλο",
+  "Same frame (_self)": "στο ίδιο frame (_self)",
+  "Target:": "target:",
+  "Title (tooltip):": "Τίτλος:",
+  "Top frame (_top)": "στο πάνω frame (_top)",
   "URL:": "URL:",
-  "You must enter the URL where this link points to": "Ξ ΟΞ­ΟΞ΅ΞΉ Ξ½Ξ± Ξ΅ΞΉΟΞ¬Ξ³Ξ΅ΟΞ΅ ΟΞΏ URL ΟΞΏΟ ΞΏΞ΄Ξ·Ξ³Ξ΅Ξ― Ξ±ΟΟΟΟ ΞΏ ΟΟΞ½Ξ΄Ξ΅ΟΞΌΞΏΟ"
+  "You must enter the URL where this link points to": "Βάλτε το url στο οποίο δείχνει ο σύνδεσμος"
 }
