d4d5a8e288c603e4b7bd42a5a6ff793755e2141b
Yannis Exidaridis <jexi@noc.uoa.gr>
2008-06-05 12:01:50 +0000

    some cleanups

--- include/lib/main.lib.php
+++ include/lib/main.lib.php
@@ -218,11 +218,11 @@ function uid_to_name($uid)
 	global $mysqlMainDb;
 
 	if ($r = mysql_fetch_row(db_query("SELECT CONCAT(nom, ' ', prenom)
-						FROM user WHERE user_id = '".mysql_real_escape_string($uid)."'", $mysqlMainDb))) {
-	return $r[0];
-						} else {
-							return FALSE;
-						}
+		FROM user WHERE user_id = '".mysql_real_escape_string($uid)."'", $mysqlMainDb))) {
+		return $r[0];
+	} else {
+		return FALSE;
+	}
 }
 
 // Translate uid to AM (student number)
