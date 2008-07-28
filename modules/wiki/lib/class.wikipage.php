c56225ec0532763362a154f48be523e90cb90195
Antonis Maurommatis <antonism@noc.uoa.gr>
2008-07-28 14:09:40 +0000

    *** empty log message ***

--- modules/wiki/lib/class.wikipage.php
+++ modules/wiki/lib/class.wikipage.php
@@ -1,28 +1,27 @@
 <?php
-
-/**=============================================================================
-       	GUnet eClass 2.0 
-        E-learning and Course Management Program  
-================================================================================
-       	Copyright(c) 2003-2007  Greek Universities Network - GUnet
-        A full copyright notice can be read in "/info/copyright.txt".
-        
-       	Authors:    Costas Tsibanis <k.tsibanis@noc.uoa.gr>
-                     Yannis Exidaridis <jexi@noc.uoa.gr> 
-                     Alexandros Diamantidis <adia@noc.uoa.gr> 
-
-        For a full list of contributors, see "credits.txt".  
-     
-        This program is a free software under the terms of the GNU 
-        (General Public License) as published by the Free Software 
-        Foundation. See the GNU License for more details. 
-        The full license can be read in "license.txt".
-     
-       	Contact address: GUnet Asynchronous Teleteaching Group, 
-        Network Operations Center, University of Athens, 
-        Panepistimiopolis Ilissia, 15784, Athens, Greece
-        eMail: eclassadmin@gunet.gr
-==============================================================================*/
+/*===========================================================================
+*   Open eClass 2.1
+*   E-learning and Course Management System
+* ===========================================================================
+*	Copyright(c) 2003-2008  Greek Universities Network - GUnet
+*	A full copyright notice can be read in "/info/copyright.txt".
+*
+*  	Authors:	Costas Tsibanis <k.tsibanis@noc.uoa.gr>
+*				Yannis Exidaridis <jexi@noc.uoa.gr>
+*				Alexandros Diamantidis <adia@noc.uoa.gr>
+*
+*	For a full list of contributors, see "credits.txt".
+*
+*	This program is a free software under the terms of the GNU
+*	(General Public License) as published by the Free Software
+*	Foundation. See the GNU License for more details.
+*	The full license can be read in "license.txt".
+*
+*	Contact address: 	GUnet Asynchronous Teleteaching Group,
+*						Network Operations Center, University of Athens,
+*						Panepistimiopolis Ilissia, 15784, Athens, Greece
+*						eMail: eclassadmin@gunet.gr
+============================================================================*/
 
 /**===========================================================================
 	class.wikipage.php
