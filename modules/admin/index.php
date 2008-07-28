f5a6faf6ef8e123187ab166c202a96d76c354050
Antonis Maurommatis <antonism@noc.uoa.gr>
2008-07-28 12:42:10 +0000

    popyrights note

--- modules/admin/index.php
+++ modules/admin/index.php
@@ -4,6 +4,33 @@
         DEAL WITH  BASETHEME, OTHER INCLUDES AND NAMETOOLS
 ******************************************************************************/
 // Check if user is administrator and if yes continue
+
+/*===========================================================================
+*   Open eClass 2.1
+*   E-learning and Course Management System
+* ===========================================================================
+*	Copyright(c) 2003-2008  Greek Universities Network - GUnet
+*	A full copyright notice can be read in "/info/copyright.txt".
+*
+*  	Authors:	Costas Tsibanis <k.tsibanis@noc.uoa.gr>
+*				Yannis Exidaridis <jexi@noc.uoa.gr>
+*				Alexandros Diamantidis <adia@noc.uoa.gr>
+*
+*	For a full list of contributors, see "credits.txt".
+*
+*	This program is a free software under the terms of the GNU
+*	(General Public License) as published by the Free Software
+*	Foundation. See the GNU License for more details.
+*	The full license can be read in "license.txt".
+*
+*	Contact address: 	GUnet Asynchronous Teleteaching Group,
+*						Network Operations Center, University of Athens,
+*						Panepistimiopolis Ilissia, 15784, Athens, Greece
+*						eMail: eclassadmin@gunet.gr
+============================================================================*/
+
+
+
 // Othewise exit with appropriate message
 $require_admin = true;
 // Include baseTheme
--- modules/admin/listrequsers.php
+++ modules/admin/listrequsers.php
@@ -1,4 +1,30 @@
+
 <?
+
+/*===========================================================================
+*   Open eClass 2.1
+*   E-learning and Course Management System
+* ===========================================================================
+*	Copyright(c) 2003-2008  Greek Universities Network - GUnet
+*	A full copyright notice can be read in "/info/copyright.txt".
+*
+*  	Authors:	Costas Tsibanis <k.tsibanis@noc.uoa.gr>
+*				Yannis Exidaridis <jexi@noc.uoa.gr>
+*				Alexandros Diamantidis <adia@noc.uoa.gr>
+*
+*	For a full list of contributors, see "credits.txt".
+*
+*	This program is a free software under the terms of the GNU
+*	(General Public License) as published by the Free Software
+*	Foundation. See the GNU License for more details.
+*	The full license can be read in "license.txt".
+*
+*	Contact address: 	GUnet Asynchronous Teleteaching Group,
+*						Network Operations Center, University of Athens,
+*						Panepistimiopolis Ilissia, 15784, Athens, Greece
+*						eMail: eclassadmin@gunet.gr
+============================================================================*/
+
 $require_admin = TRUE;
 include '../../include/baseTheme.php';
 include '../../include/sendMail.inc.php';
