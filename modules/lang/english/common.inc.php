0cd8945a2d2604b1bed45915418996113393fbd2
Alexandros Diamantidis <adia@noc.uoa.gr>
2008-10-07 13:53:52 +0000

    Updating english messages from Babel.

--- modules/lang/english/common.inc.php
+++ modules/lang/english/common.inc.php
@@ -1,58 +1,28 @@
 <?php
-/*========================================================================
-*   Open eClass 2.1
-*   E-learning and Course Management System
-* ========================================================================
-*  Copyright(c) 2003-2008  Greek Universities Network - GUnet
-*  A full copyright notice can be read in "/info/copyright.txt".
-*
-*  Developers Group:	Costas Tsibanis <k.tsibanis@noc.uoa.gr>
-*			Yannis Exidaridis <jexi@noc.uoa.gr>
-*			Alexandros Diamantidis <adia@noc.uoa.gr>
-*			Tilemachos Raptis <traptis@noc.uoa.gr>
-*
-*  For a full list of contributors, see "credits.txt".
-*
-*  Open eClass is an open platform distributed in the hope that it will
-*  be useful (without any warranty), under the terms of the GNU (General
-*  Public License) as published by the Free Software Foundation.
-*  The full license can be read in "/info/license/license_gpl.txt".
-*
-*  Contact address: 	GUnet Asynchronous eLearning Group,
-*  			Network Operations Center, University of Athens,
-*  			Panepistimiopolis Ilissia, 15784, Athens, Greece
-*  			eMail: info@openeclass.org
-* =========================================================================*/
 
+// Message file for language en
+// Generated 2008-10-07 16:51:31
 
-/*******************************************************
-* General Messages - Feel free to suit them in your needs
-*******************************************************/
-
-$langEclass = "Open eClass - Course Management System";
-$langYes = "Yes";
-$langNo = "No";
-
-$langTeacher = "Instructor";
-$langTeachers = "Instructors";
-$langOfTeacher = "Instructor";
-$langOfTeachers = "Instructors";
-
-$langsTeacher = "instructor";
-$langsTeachers = "instructors";
-$langsOfTeacher = "instructor";
-$langsOfTeachers = "instructors";
-
-$langStudent = "Student";
-$langStudents = "Students";
-$langOfStudent = "Student";
-$langOfStudents = "Students";
-
-$langsStudent = "student";
-$langsstudent_acc = $langsStudent;
-$langsStudents = "students";
-$langsOfStudent = "student";
-$langsOfStudents = "students";
-
-$langGuest = "Guest User";
-$langGuests = "Guest Users";
+$langEclass = "Open eClass - Asynchronous eLearning Platform"; // 
+$langYes = "Yes"; // 
+$langNo = "No"; // 
+$langTeacher = "Teacher"; // 
+$langOfTeacher = "Teacher"; // 
+$langTeachers = "Teachers"; // 
+$langOfTeachers = "Teachers'"; // 
+$langsTeacher = "teacher"; // 
+$langsOfTeacher = "teacher"; // 
+$langsTeachers = "teachers"; // 
+$langsOfTeachers = "teachers'"; // 
+$langStudent = "Student"; // 
+$langOfStudent = "Student"; // 
+$langStudents = "Students"; // 
+$langOfStudents = "Students"; // 
+$langsStudent = "student"; // 
+$langsOfStudent = "student"; // 
+$langsStudents = "students"; // 
+$langsOfStudents = "students"; // 
+$langsOfStudentss = "students"; // 
+$langsstudent_acc = $langsStudent; // 
+$langGuest = "Guest User"; // 
+$langGuests = "Guest Users"; // 
