9fba99fe3cdee4d6e583030b25f3dcc53c316a9f
Alexandros Diamantidis <adia@noc.uoa.gr>
2008-10-23 10:38:37 +0000

    New admin tool to login as a different user

--- include/tools.php
+++ include/tools.php
@@ -333,6 +333,9 @@ function adminMenu(){
 	array_push($sideMenuText, $GLOBALS['langUserAuthentication']);
 	array_push($sideMenuLink, "../admin/auth.php");
 	array_push($sideMenuImg, "black-arrow1.gif");
+	array_push($sideMenuText, $GLOBALS['langChangeUser']);
+	array_push($sideMenuLink, "../admin/change_user.php");
+	array_push($sideMenuImg, "black-arrow1.gif");
 
 	array_push($sideMenuSubGroup, $sideMenuText);
 	array_push($sideMenuSubGroup, $sideMenuLink);
