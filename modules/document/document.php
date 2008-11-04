3f9604477536655cd890dc2bdce305614e7d6e5b
Alexandros Diamantidis <adia@noc.uoa.gr>
2008-11-04 13:29:44 +0000

    Replace arrow characters with images

--- modules/document/document.php
+++ modules/document/document.php
@@ -664,7 +664,8 @@ if (mysql_num_rows($sql) == 0) {
                 }
                 if ($sort == $this_sort) {
                         $this_reverse = !$reverse;
-                        $indicator = $reverse? ' &#8593;': ' &#8595;';
+                        $indicator = ' <img src="../../template/classic/img/arrow_' . 
+                                ($reverse? 'up': 'down') . '.gif" border="0"/>';
                 } else {
                         $this_reverse = $reverse;
                         $indicator = '';
@@ -683,7 +684,7 @@ if (mysql_num_rows($sql) == 0) {
         $tool_content .= "</div></th>";
         $tool_content .= "\n  </tr>";
         $tool_content .= "\n  <tr>";
-        $tool_content .= "\n    <td width='15%' class='DocHead'><div align='center'><b>" .
+        $tool_content .= "\n    <td width='10%' class='DocHead'><div align='center'><b>" .
                          headlink($m['type'], 'type') . '</b></div></td>';
         $tool_content .= "\n    <td class='DocHead'><div align='left'><b>" .
                          headlink($langName, 'name') . '</b></div></td>';
