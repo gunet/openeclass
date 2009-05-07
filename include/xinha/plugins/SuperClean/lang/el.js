1b3a7e3c1f40e5de4976caef8bb2de495a05397e
Yannis Exidaridis <jexi@noc.uoa.gr>
2009-05-07 12:32:12 +0000

    προσθήκη ελληνικής μετάφρασης στο supercleanup plugin του xinha

---
+++ include/xinha/plugins/SuperClean/lang/el.js
@@ -0,0 +1,18 @@
+// I18N constants
+// LANG: "el", ENCODING: UTF-8
+// Author: AlexSoft Software (ASCOOS CMS), http://www.alexsoft.gr, http://ascoos.com, webmaster@alexsoft.gr
+{
+  "Clean up HTML": "Ξεκαθάρισμα κώδικα HTML",
+  "Please select from the following cleaning options...": "Παρακαλώ επιλέξτε από τις παρακάτω επιλογές ξεκαθαρίσματος...",
+  "General tidy up and correction of some problems.": "Γενικό ξεκαθάρισμα και διόρθωση μερικών προβλημάτων.",
+  "Clean bad HTML from Microsoft Word": "Ξεκαθάρισμα από κακό κώδικα HTML που παράγεται από το Microsoft Word",
+  "Remove custom typefaces (font \"styles\").": "Αφαίρεση των στύλ μορφοποίησης (font face).",
+  "Remove custom font sizes.": "Αφαίρεση των μεγεθών της γραμματοσειράς (font size).",
+  "Remove custom text colors.": "Αφαίρεση των χρωμάτων του κειμένου (font color).",
+  "Remove lang attributes.": "Αφαίρεση ιδιοτήτων γλώσσας.",
+  "Replace directional quote marks with non-directional quote marks.": "Αντικατάσταση άμεσων εισαγωγικών με έμμεσα",
+  "Go": "Πήγαινε",
+  "Cancel": "Ακύρωση",
+  "Tidy failed.  Check your HTML for syntax errors.": "Αποτυχία ξεκαθαρίσματος. Ελέγξτε για συντακτικά λάθη στην HTML σας.",
+  "You don't have anything to tidy!": "Δεν υπάρχει τίποτα προς ξεκαθάρισμα!"
+};
