71948861ad6115903cd109e346e6232c18e86f91
Yannis Exidaridis <jexi@noc.uoa.gr>
2009-06-30 09:38:30 +0000

    spanish messages added

---
+++ modules/lang/spanish/common.inc.php
@@ -0,0 +1,28 @@
+<?php
+
+// Message file for language es
+// Generated 2009-06-30 12:29:03
+
+$langEclass = "eClass Libre - Sistema de Administración del Curso";
+$langYes = "Si";
+$langNo = "No";
+$langTeacher = "Formador";
+$langOfTeacher = "Formador";
+$langTeachers = "Formadores";
+$langOfTeachers = "Formadores";
+$langsTeacher = "Formador";
+$langsOfTeacher = "formador";
+$langsTeachers = "formadores";
+$langsOfTeachers = "formadores";
+$langStudent = "Aprendiz";
+$langOfStudent = "Aprendiz";
+$langStudents = "Aprendices";
+$langOfStudents = "Aprendices";
+$langsStudent = "aprendiz";
+$langsOfStudent = "aprendiz";
+$langsStudents = "aprendices";
+$langsOfStudents = "aprendices";
+$langsOfStudentss = "Estudiantes";
+$langsstudent_acc = $langsStudent;
+$langGuest = "Usuario invitado";
+$langGuests = "Usuarios invitados";
