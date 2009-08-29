a269bc66dd291a95657e5757ecdeafca2c3b7867
Thanos Kyritsis <atkyritsis@upnet.gr>
2009-08-29 13:11:35 +0000

    katallhlos xeirismos kai twn SCORM_ASSET types

--- modules/learnPath/modules_pool.php
+++ modules/learnPath/modules_pool.php
@@ -26,7 +26,7 @@
 
 /*===========================================================================
 	modules_pool.php
-	@last update: 29-08-2009 by Thanos Kyritsis
+	@last update: 30-06-2006 by Thanos Kyritsis
 	@authors list: Thanos Kyritsis <atkyritsis@upnet.gr>
 
 	based on Claroline version 1.7 licensed under GPL
