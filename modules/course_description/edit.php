6de12c1edced23797e62299f02c76338e23e0a51
Alexandros Diamantidis <adia@noc.uoa.gr>
2010-03-12 15:16:33 +0000

    Fixes for multiple "additional info" sections". Also fix HTML warnings.

--- modules/course_description/edit.php
+++ modules/course_description/edit.php
@@ -48,9 +48,8 @@ include '../../include/phpmathpublisher/mathpublisher.php';
 
 $tool_content = $head_content = "";
 $nameTools = $langEditCourseProgram ;
-$navigation[]= array ("url"=>"index.php", "name"=> $langCourseProgram);
+$navigation[] = array ('url' => 'index.php', 'name' => $langCourseProgram);
 
-$db = $_SESSION['dbname'];
 $lang_editor = langname_to_code($language);
 
 $head_content .= <<<hCont
@@ -76,108 +75,83 @@ hCont;
 
 $body_action = 'onload="initEditor()"';
 
+mysql_select_db($_SESSION['dbname']);
+
 if ($is_adminOfCourse) {
-
-//Save  actions
-	if (isset($save)) {
-		if($_POST['edIdBloc'] == 'add') {
-		    $sql="SELECT MAX(id) as idMax from course_description";
-			$res = db_query($sql, $db);
-			$idMax = mysql_fetch_array($res);
-			$idMax = max(sizeof($titreBloc),$idMax["idMax"]);
-			$sql ="INSERT IGNORE INTO `course_description`
-                               SET `id` = " . ($idMax + 1);
-			$_POST['edIdBloc'] = $idMax + 1;
-		} else {
-			$sql ="INSERT IGNORE INTO `course_description`
-                               SET `id` = " . intval($_POST['edIdBloc']);
-		}
-		db_query($sql, $db);
-		if (empty($edTitleBloc)) {
-			$edTitleBloc = $titreBloc[$edIdBloc];
-		}
-		$sql ="UPDATE course_description SET title= ".autoquote(trim($edTitleBloc)).",
-			content = ".autoquote(trim($edContentBloc)).",
-			`upDate` = NOW()
-			WHERE id = ".intval($_POST['edIdBloc']);
-		db_query($sql, $db);
-	}
-
-//Delete action
-	if (isset($delete) and $delete == 'yes') {
-		$sql ="DELETE FROM `course_description` WHERE id = '$_GET[numBloc]'";
-		$res = db_query($sql,$db);
+        if (isset($_POST['save'])) {
+                if ($_POST['edIdBloc'] == 'add') {
+                        $res = db_query("SELECT MAX(id) FROM course_description");
+                        list($max_id) = mysql_fetch_row($res);
+                        $new_id = max(sizeof($titreBloc), $max_id) + 1;
+                } else {
+                        $new_id = intval($_POST['edIdBloc']);
+                }
+                if (empty($edTitleBloc)) {
+                        $edTitleBloc = $titreBloc[$edIdBloc];
+                }
+                db_query("INSERT IGNORE INTO course_description SET id = $new_id");
+                db_query("UPDATE course_description
+                                SET title = " . autoquote(trim($edTitleBloc)) . ",
+                                    content = " . autoquote(trim($edContentBloc)) . ",
+                                    `upDate` = NOW()
+                                WHERE id = $new_id");
+                header('Location: ' . $urlServer . 'modules/course_description/edit.php');
+                exit;
+        } elseif (isset($_GET['delete'])) {
+                $del_id = intval($_GET['numBloc']);
+		$res = db_query("DELETE FROM course_description WHERE id = $del_id");
 		$tool_content .= "<p class='success'>$langBlockDeleted<br /><br /><a href='$_SERVER[PHP_SELF]'>$langBack</a></p>";
-	}
-//Edit action
-	elseif(isset($numBloc)) {
-		if (is_numeric($numBloc)) {
-			$sql = "SELECT * FROM `course_description`
-				WHERE id = '".mysql_real_escape_string($numBloc)."'";
-			$res = db_query($sql,$db);
-			$blocs = mysql_fetch_array($res);
-			if (is_array($blocs)) {
-				$titreBloc[$numBloc]=$blocs["title"];
-				$contentBloc = $blocs["content"];
-			}
-		}
 
-    $tool_content .= "
-    <form method='post' action='$_SERVER[PHP_SELF]'>
+        } elseif (isset($_REQUEST['numBloc'])) {
+                // Edit action
+                $edit_id = intval($_REQUEST['numBloc']);
+                $numBlock = $edit_id;
+                $res = db_query("SELECT * FROM course_description WHERE id = $edit_id");
+                $title = '';
+                if ($res and mysql_num_rows($res) > 0) {
+                        $blocs = mysql_fetch_array($res);
+                        $title = q($blocs['title']);
+                        $contentBloc = $blocs["content"];
+                } else {
+                        if (isset($titreBloc[$edit_id])) {
+                                $title = q($titreBloc[$edit_id]);
+                        }
+                        if (!isset($titreBlocNotEditable[$edit_id]) or !$titreBlocNotEditable[$numBloc]) {
+                                $numBloc = 'add';
+                        }
+                }
 
-    <table width='99%' class='FormData' align='left'>
-    <tbody>
-    <tr>
-      <th class='left' width='220'>".$langTitle.":</th>
-      <td><b>".@$titreBloc[$numBloc]."</b></td>
-    </tr>";
-	if (($numBloc =="add") || @(!$titreBlocNotEditable[$numBloc])) {
-		$tool_content .= "
-    <tr>
-      <th class='left'>&nbsp;</th>
-      <td><input type='text' name='edTitleBloc' rows='20' cols='90' value='".@$titreBloc[$numBloc]."' /></td>
-    </tr>";
-	} else {
-		$tool_content .= "<input type='hidden' name='edTitleBloc' value='".$titreBloc[$numBloc]."' />";
-	}
-		if ($numBloc =="add") {
-			$tool_content .= "<input type='hidden' name='edIdBloc' value='add' />";
-		} else {
-			$tool_content .= "<input type='hidden' name='edIdBloc' value='$numBloc' />";
-		}
-		$tool_content .= "
-    <tr>
-      <th class='left'>&nbsp;</th>
-      <td>
-	    <table class='xinha_editor'>
-        <tr>
-          <td><textarea id='xinha' width='100%' name='edContentBloc'>".@$contentBloc."</textarea></td>
-        </tr>
-        </table>
-      </td>
-    </tr>";
+                $tool_content .= "<form method='post' action='$_SERVER[PHP_SELF]'>
+                        <input type='hidden' name='edIdBloc' value='$numBloc' />
+                        <table width='99%' class='FormData' align='left'><tbody>
+                           <tr><th class='left' width='220'>$langTitle:</th>
+                               <td><b>$title</b>";
+                if (!isset($titreBlocNotEditable[$edit_id]) or !$titreBlocNotEditable[$numBloc]) {
+                        $tool_content .= "</td></tr><tr><th class='left'>&nbsp;</th>
+                            <td><input type='text' name='edTitleBloc' value='$title' />
+                                </td></tr>";
+                } else {
+                        $tool_content .= "<input type='hidden' name='edTitleBloc' value='$title' /></td></tr>";
+                }
 
-	$tool_content .= "
-    <tr>
-      <th class='left'>&nbsp;</th>
-      <td><input type='submit' name='save' value='$langAdd' />&nbsp;&nbsp;
-          <input type='submit' name='ignore' value='$langBackAndForget' />
-      </td>
-    </tr>";
-	$tool_content .= "
-    <tbody>
-    </table>
-
-    </form>";
-} else {
-		$sql = "SELECT * FROM `course_description` order by id";
-		$res = db_query($sql,$db);
-		while($bloc = mysql_fetch_array($res)) {
-			$blocState[$bloc["id"]] = "used";
-			$titreBloc[$bloc["id"]]	= $bloc["title"];
-			$contentBloc[$bloc["id"]] = $bloc["content"];
-		}
-		$tool_content .= "
+                $tool_content .= "
+                        <tr><th class='left'>&nbsp;</th>
+                            <td><table class='xinha_editor'>
+                            <tr><td><textarea id='xinha' name='edContentBloc'>" . 
+                                q(@$contentBloc) . "</textarea></td></tr></table></td></tr>
+                        <tr><th class='left'>&nbsp;</th>
+                            <td><input type='submit' name='save' value='$langAdd' />&nbsp;&nbsp;
+                                <input type='submit' name='ignore' value='$langBackAndForget' /></td></tr>
+                    </tbody></table></form>\n";
+        } else {
+                $sql = "SELECT * FROM `course_description` order by id";
+                $res = db_query($sql,$db);
+                while($bloc = mysql_fetch_array($res)) {
+                        $blocState[$bloc["id"]] = "used";
+                        $titreBloc[$bloc["id"]]	= $bloc["title"];
+                        $contentBloc[$bloc["id"]] = $bloc["content"];
+                }
+                $tool_content .= "
     <form method='post' action='$_SERVER[PHP_SELF]'>
 
     <table width='99%' align='left' class='FormData'>
@@ -222,7 +196,6 @@ if ($is_adminOfCourse) {
 	exit();
 }
 
-// End of page
 if(isset($numBloc)) {
 	draw($tool_content, 2, 'course_description', $head_content, $body_action);
 } else {
