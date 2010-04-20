4c8c34aa31e35514d2981f2034df51062e85014a
Yannis Exidaridis <jexi@noc.uoa.gr>
2010-04-20 12:54:12 +0000

    function for rich_text_editor added

--- include/lib/main.lib.php
+++ include/lib/main.lib.php
@@ -1232,6 +1232,19 @@ function course_code_to_id($code)
 	}
 }
 
+// Find the title of a course from its id
+function course_id_to_title($cid)
+{
+	global $mysqlMainDb;
+        $r = db_query("SELECT intitule FROM cours WHERE cours_id='$cid'", $mysqlMainDb);
+        if ($r and mysql_num_rows($r) > 0) {
+                $row = mysql_fetch_row($r);
+                return $row[0];
+	} else {
+                return false;
+	}
+}
+
 function csv_escape($string, $force = false)
 {
         global $charset;
@@ -1284,4 +1297,24 @@ function register_posted_variables($var_array)
                 }
         }
         return $all_set;
+}
+
+// Display a textarea with name $name using the rich text editor
+// Apply automatically arious fixes for the text to be edited
+function rich_text_editor($name, $rows, $cols, $text, $extra = '')
+{
+	static $id = 0;
+	
+	$text = str_ireplace(array('<m>', '</m>'),
+			     array('[m]', '[/m]'),
+			     $text);
+	if ($id == 0) {
+		$id1 = '';
+	} else {
+		$id1 = "_$id";
+	}
+	$id++;
+	return "<textarea id='xinha$id1' name='$name' rows='$rows' cols='$cols' $extra>" .
+	       str_replace('{','&#123;',htmlspecialchars($text)) .
+	       "</textarea>\n";
 }
