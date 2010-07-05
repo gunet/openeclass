cd9c74ff68a4fe35d878b75042aa36accf98ff7c
Yannis Exidaridis <jexi@noc.uoa.gr>
2010-07-05 13:36:18 +0000

    working with register globals = off

--- modules/auth/newuser.php
+++ modules/auth/newuser.php
@@ -40,24 +40,20 @@
 
 include '../../include/baseTheme.php';
 include '../../include/sendMail.inc.php';
-include 'auth.inc.php';
+$tool_content = "";	
 $nameTools = $langUserDetails;
-// Main body
 $navigation[] = array("url"=>"registration.php", "name"=> $langNewUser);
-
-$tool_content = "";	// Initialise $tool_content
 
 if (isset($close_user_registration) and $close_user_registration == TRUE) {
 	$tool_content .= "<div class='td_main'>$langForbidden</div>";
         draw($tool_content,0);
 	exit;
- }
+}
  
 $lang = langname_to_code($language);
 
 // display form
-if (!isset($submit)) {
-	// Main body
+if (!isset($_POST['submit'])) {
 	@$tool_content .= "<form action='$_SERVER[PHP_SELF]' method='post'>
 	<table width='99%' style='border: 1px solid #edecdf;'>
 	<thead>
@@ -67,15 +63,15 @@ if (!isset($submit)) {
 	<thead>
 	<tr>
 	<th class='left' width='220'>$langName</th>
-	<td colspan='2'><input type='text' name='prenom_form' value='$prenom_form' class='FormData_InputText' />&nbsp;&nbsp;<small>(*)</small></td>
+	<td colspan='2'><input type='text' name='prenom_form' value='".$_GET['prenom_form']."' class='FormData_InputText' />&nbsp;&nbsp;<small>(*)</small></td>
 	</tr>
 	<tr>
 	<th class='left'>$langSurname</th>
-	<td colspan='2'><input type='text' name='nom_form' value='$nom_form' class='FormData_InputText' />&nbsp;&nbsp;<small>(*)</small></td>
+	<td colspan='2'><input type='text' name='nom_form' value='".$_GET['nom_form']."' class='FormData_InputText' />&nbsp;&nbsp;<small>(*)</small></td>
 	</tr>
 	<tr>
 	<th class='left'>$langUsername</th>
-	<td colspan='2'><input type='text' name='uname' value='$uname' size='20' maxlength='20' class='FormData_InputText' />&nbsp;&nbsp;<small>(*) $langUserNotice</small></td>
+	<td colspan='2'><input type='text' name='uname' value='".$_GET['uname']."' size='20' maxlength='20' class='FormData_InputText' />&nbsp;&nbsp;<small>(*) $langUserNotice</small></td>
 	</tr>
 	<tr>
 	<th class='left'>$langPass</th>
@@ -87,23 +83,22 @@ if (!isset($submit)) {
 	</tr>
 	<tr>
 	<th class='left'>$langEmail</th>
-	<td valign='top'><input type='text' name='email' value='$email' class='FormData_InputText' /></td>
+	<td valign='top'><input type='text' name='email' value='".$_GET['email']."' class='FormData_InputText' /></td>
 	<td><small>$langEmailNotice</small></td>
 	</tr>
 	<tr>
 	<th class='left'>$langAm</th>
-	<td colspan='2' valign='top'><input type='text' name='am' value='$am' class='FormData_InputText' /></td>
+	<td colspan='2' valign='top'><input type='text' name='am' value='".$_GET['am']."' class='FormData_InputText' /></td>
 	</tr>
 	<tr>
 	<th class='left'>$langFaculty</th>
 		<td colspan='2'><select name='department'>";
-	$deps=mysql_query("SELECT name, id FROM faculte ORDER BY id");
+	$deps = mysql_query("SELECT name, id FROM faculte ORDER BY id");
 	while ($dep = mysql_fetch_array($deps)) {
 		$tool_content .= "\n<option value='".$dep[1]."'>".$dep[0]."</option>";
 	}
 	$tool_content .= "\n</select>
-	</td>
-	</tr>
+	</td></tr>
 	<tr>
 	<th class='left'>$langLanguage</th>
 	<td width='1'>";
@@ -114,29 +109,31 @@ if (!isset($submit)) {
 	<tr>
 	<th class='left'>&nbsp;</th>
 	<td colspan='2'>
-	<input type='hidden' name='auth' value='1' />
 	<input type='submit' name='submit' value='".$langRegistration."' />
 	</td>
 	</tr>
 	</thead>
 	</table>
 	<div align='right'><small>$langRequiredFields</small></div>
-	</td>
-	</tr>
-	</thead>
-	</table>
+	</td></tr></thead></table>
 	</form>";
 } else {
 
 	// trim white spaces in the end and in the beginning of the word
 	$uname = preg_replace('/\ +/', ' ', trim(isset($_POST['uname'])?$_POST['uname']:''));
-	// registration
+	$nom_form = isset($_POST['nom_form'])?$_POST['nom_form']:'';
+	$prenom_form = isset($_POST['prenom_form'])?$_POST['prenom_form']:'';
+	$password = isset($_POST['password'])?$_POST['password']:'';
+	$email = isset($_POST['email'])?$_POST['email']:'';
+	$department = isset($_POST['department'])?$_POST['department']:'';
+	$am = isset($_POST['am'])?$_POST['am']:'';
+	
 	$registration_errors = array();
 	// check if there are empty fields
 	if (empty($nom_form) or empty($prenom_form) or empty($password) or empty($uname)) {
 		$registration_errors[] = $langEmptyFields;
 	} else {
-	// check if the username is already in use
+		// check if the username is already in use
 		$q2 = "SELECT username FROM `$mysqlMainDb`.user WHERE username='".escapeSimple($uname)."'";
 		$username_check = mysql_query($q2);
 		if ($myusername = mysql_fetch_array($username_check)) {
@@ -146,77 +143,47 @@ if (!isset($submit)) {
 	if (!empty($email) and !email_seems_valid($email)) {
 		$registration_errors[] = $langEmailWrong;
 	}
-	$auth_method_settings = get_auth_settings($auth);
-	if (!empty($auth_method_settings) and $auth != 1) {
-		$password = $auth_method_settings['auth_name'];
-	} else {
-		// check if the two passwords match
-		if ($password != $_POST['password1']) {
-			$registration_errors[] = $langPassTwice;
-		} elseif (strtoupper($password) == strtoupper($uname)
-			or strtoupper($password) == strtoupper($nom_form)
-			or strtoupper($password) == strtoupper($prenom_form)
-			or strtoupper($password) == strtoupper($email)) {
-				// if the passwd is too easy offer a password sugestion
-				$registration_errors[] = $langPassTooEasy . ': <strong>' .
-					substr(md5(date("Bis").$_SERVER['REMOTE_ADDR']),0,8) . '</strong>';
-			}
-	}
+	if ($password != $_POST['password1']) { // check if the two passwords match
+		$registration_errors[] = $langPassTwice;
+	} 
 	if (count($registration_errors) == 0) {
-	$emailsubject = "$langYourReg $siteName";
-			$uname = unescapeSimple($uname); // un-escape the characters: simple and double quote
-			$password = unescapeSimple($password);
-			if((!empty($auth_method_settings)) && ($auth!=1)) {
-				$emailbody = "$langDestination $prenom_form $nom_form\n" .
-					"$langYouAreReg $siteName $langSettings $uname\n" .
-					"$langPassSameAuth\n$langAddress $siteName: " .
-					"$urlServer\n$langProblem\n$langFormula" .
-					"$administratorName $administratorSurname" .
-					"$langManager $siteName \n$langTel $telephone \n" .
-					"$langEmail: $emailhelpdesk";
-		} else {
-			$emailbody = "$langDestination $prenom_form $nom_form\n" .
-				"$langYouAreReg $siteName $langSettings $uname\n" .
-				"$langPass: $password\n$langAddress $siteName: " .
-				"$urlServer\n$langProblem\n$langFormula" .
-				"$administratorName $administratorSurname" .
-				"$langManager $siteName \n$langTel $telephone \n" .
-				"$langEmail: $emailhelpdesk";
-		}
-	
+		$emailsubject = "$langYourReg $siteName";
+		$uname = unescapeSimple($uname); 
+		$password = unescapeSimple($password);
+		$emailbody = "$langDestination $prenom_form $nom_form\n" .
+			"$langYouAreReg $siteName $langSettings $uname\n" .
+			"$langPass: $password\n$langAddress $siteName: " .
+			"$urlServer\n$langProblem\n$langFormula" .
+			"$administratorName $administratorSurname" .
+			"$langManager $siteName \n$langTel $telephone \n" .
+			"$langEmail: $emailhelpdesk";
 	send_mail('', '', '', $email, $emailsubject, $emailbody, $charset);
 	$registered_at = time();
-	$expires_at = time() + $durationAccount;  //$expires_at = time() + 31536000;
-	
+	$expires_at = time() + $durationAccount;  
 	// manage the store/encrypt process of password into database
-	$authmethods = array("2","3","4","5");
-	$uname = escapeSimple($uname);  // escape the characters: simple and double quote
-	$password = escapeSimpleSelect($password);  // escape the characters: simple and double quote
-	if(!in_array($auth,$authmethods)) {
-		$password_encrypted = md5($password);
-	} else {
-		$password_encrypted = $password;
-	}
+	$uname = escapeSimple($uname);  
+	$password = escapeSimpleSelect($password); 
+	$password_encrypted = md5($password);
+	
 	$q1 = "INSERT INTO `$mysqlMainDb`.user
-	(user_id, nom, prenom, username, password, email, statut, department, am, registered_at, expires_at, lang)
-	VALUES ('NULL', '$nom_form', '$prenom_form', '$uname', '$password_encrypted', '$email','5',
-		'$department','$am',".$registered_at.",".$expires_at.",'$lang')";
+		(user_id, nom, prenom, username, password, email, statut, department, am, registered_at, expires_at, lang)
+		VALUES ('NULL', '$nom_form', '$prenom_form', '$uname', '$password_encrypted', '$email','5',
+			'$department','$am',".$registered_at.",".$expires_at.",'$lang')";
 	$inscr_user = mysql_query($q1);
 	$last_id = mysql_insert_id();
-	$result=mysql_query("SELECT user_id, nom, prenom FROM `$mysqlMainDb`.user WHERE user_id='$last_id'");
+	$result = mysql_query("SELECT user_id, nom, prenom FROM `$mysqlMainDb`.user WHERE user_id='$last_id'");
 	while ($myrow = mysql_fetch_array($result)) {
-		$uid=$myrow[0];
-		$nom=$myrow[1];
-		$prenom=$myrow[2];
+		$uid = $myrow[0];
+		$nom = $myrow[1];
+		$prenom = $myrow[2];
 	}
 	mysql_query("INSERT INTO `$mysqlMainDb`.loginout (loginout.idLog, loginout.id_user, loginout.ip, loginout.when, loginout.action)
-	VALUES ('', '".$uid."', '".$REMOTE_ADDR."', NOW(), 'LOGIN')");
+		VALUES ('', '".$uid."', '".$_SERVER['REMOTE_ADDR']."', NOW(), 'LOGIN')");
 	$_SESSION['uid'] = $uid;
 	$_SESSION['statut'] = 5;
 	$_SESSION['prenom'] = $prenom;
 	$_SESSION['nom'] = $nom;
 	$_SESSION['uname'] = $uname;
-	
 	// registration form
 	$tool_content .= "<table width='99%'><tbody><tr>" .
 			"<td class='well-done' height='60'>" .
@@ -227,14 +194,13 @@ if (!isset($submit)) {
 	} else {
 		// errors exist - registration failed
 		$tool_content .= "<table width='99%'><tbody><tr>" .
-				"<td class='caution' height='60'>";
+			"<td class='caution' height='60'>";
 		foreach ($registration_errors as $error) {
 			$tool_content .= "<p>$error</p>";
 		}
 		$tool_content .= "<p><a href='$_SERVER[PHP_SELF]?prenom_form=$_POST[prenom_form]&nom_form=$_POST[nom_form]&uname=$_POST[uname]&email=$_POST[email]&am=$_POST[am]'>$langAgain</a></p>" .
-					"</td></tr></tbody></table><br /><br />";
+				"</td></tr></tbody></table><br /><br />";
 	}
-
 } // end of registration
 
 draw($tool_content,0);
