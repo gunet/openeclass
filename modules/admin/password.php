540a11babfe05e4e2301b9b38393252979e6025d
Alexandros Diamantidis <adia@noc.uoa.gr>
2010-07-20 09:54:50 +0000

    Fix HTML warnings

--- modules/admin/password.php
+++ modules/admin/password.php
@@ -41,8 +41,8 @@ $require_valid_uid = TRUE;
 include '../../include/baseTheme.php';
 
 $nameTools = $langChangePass;
-$navigation[] = array("url" => "index.php", "name" => $langAdmin);
-$navigation[]= array ("url"=>"./edituser.php", "name"=> $langEditUser);
+$navigation[] = array('url' => 'index.php', 'name' => $langAdmin);
+$navigation[] = array ('url' => 'edituser.php', 'name'=> $langEditUser);
 
 check_uid();
 $tool_content = "";
@@ -53,51 +53,50 @@ if (!isset($urlSecure)) {
 	$passurl = $urlSecure.'modules/admin/password.php';
 }
 
-if (!isset($changePass)) {
+if (!isset($_POST['changePass'])) {
+        if (!isset($_GET['userid'])) {
+                header("Location: {$urlServer}modules/admin/");
+                exit;
+        }
 	$tool_content .= "
-<form method=\"post\" action=\"$passurl?submit=yes&changePass=do&userid=$userid\">
-  <table class=\"FormData\" width=\"99%\" align=\"left\">
+<form method='post' action='$passurl'>
+  <input type='hidden' name='userid' value='$_GET[userid]' />
+  <table class='FormData' width='99%' align='left'>
   <tbody>
   <tr>
-    <th width=\"220\">&nbsp;</th>
+    <th width='220'>&nbsp;</th>
     <td><b>$lang_remind_pass</b></td>
   </tr>
   <tr>
-    <th class=\"left\">$langNewPass1</th>
-    <td><input type=\"password\" size=\"40\" name=\"password_form\" value=\"\"></td>
+    <th class='left'>$langNewPass1</th>
+    <td><input type='password' size='40' name='password_form' value='' /></td>
   </tr>
   <tr>
-    <th class=\"left\">$langNewPass2</th>
-    <td><input type=\"password\" size=\"40\" name=\"password_form1\" value=\"\"></td>
+    <th class='left'>$langNewPass2</th>
+    <td><input type='password' size='40' name='password_form1' value='' /></td>
   </tr>
   <tr>
-    <th class=\"left\">&nbsp;</th>
-    <td><input type=\"submit\" name=\"submit\" value=\"$langModify\"></td>
+    <th class='left'>&nbsp;</th>
+    <td><input type='submit' name='changePass' value='$langModify' /></td>
   </tr>
   </tbody>
   </table>
 </form>";
-}
-
-elseif (isset($submit) && isset($changePass) && ($changePass == "do")) {
-	$userid = $_REQUEST['userid'];
-	if (empty($_REQUEST['password_form']) || empty($_REQUEST['password_form1'])) {
+} else {
+	$userid = intval($_POST['userid']);
+	if (empty($_POST['password_form']) || empty($_POST['password_form1'])) {
 		$tool_content .= mes($langFields, "", 'caution');
 		draw($tool_content, 3);
 		exit();
 	}
-	if ($_REQUEST['password_form1'] !== $_REQUEST['password_form']) {
-		$tool_content .= mes($langPassTwo, "", 'caution_small');
+	if ($_POST['password_form1'] !== $_POST['password_form']) {
+		$tool_content .= mes($langPassTwo, '', 'caution_small');
 		draw($tool_content, 3);
 		exit();
 	}
-	//all checks ok. Change password!
-	$sql = "SELECT `password` FROM `user` WHERE `user_id`='$userid'";
-	$result = db_query($sql, $mysqlMainDb);
-	$myrow = mysql_fetch_array($result);
-	$old_pass_db = $myrow['password'];
-	$new_pass = md5($_REQUEST['password_form']);
-	$sql = "UPDATE `user` SET `password` = '$new_pass' WHERE `user_id` = '$userid'";
+	// All checks ok. Change password!
+	$new_pass = md5($_POST['password_form']);
+	$sql = "UPDATE `user` SET `password` = '$new_pass' WHERE `user_id` = $userid";
 	db_query($sql, $mysqlMainDb);
 	$tool_content .= mes($langPassChanged, $langHome, 'success_small');
 	draw($tool_content, 3);
@@ -112,5 +111,3 @@ function mes($message, $urlText, $type) {
  	$str = "<p class='$type'>$message<br /><a href='$urlServer'>$urlText</a><br /><a href='$_SERVER[PHP_SELF]?userid=$userid'>$langBack</a></p><br />";
 	return $str;
 }
-
-?>
