abb048c2390f15aa4d70f23102169811b5c8787c
Tilemachos Raptis <traptis@noc.uoa.gr>
2010-07-21 15:13:37 +0000

    style changes

--- modules/usage/group.php
+++ modules/usage/group.php
@@ -44,38 +44,37 @@ $head_content = '<script type="text/javascript" src="../auth/sorttable.js"></scr
 $i = 0;
 $q = db_query("SELECT id, name, tutor, maxStudent FROM student_group", $currentCourse);
 if (mysql_num_rows($q) > 0) {
-        $tool_content .= "<table class='GroupList sortable' width='100%' id='b'>
-		<tbody>
+        $tool_content .= "
+                <table class='sortable' width='99%' id='b'>
 		<tr>
-		<th class='GroupHead'><div align='left'>$langGroupName</div></th>
-		<th width='35%' class='GroupHead'>$langGroupTutor</th>
-		<th class='GroupHead'>$langRegistered</th>
-		<th class='GroupHead'>$langMax</th>
-		</tr>";
+		  <th class='left'>$langGroupName</th>
+		  <th>$langGroupTutor</th>
+		  <th class='center'>$langRegistered</th>
+		  <th class='center'>$langMax</th>
+		</tr>\n";
 	while ($group = mysql_fetch_array($q)) {
 		// Count students registered in each group
 		$resultRegistered = db_query("SELECT id FROM user_group WHERE team = $group[id]", $currentCourseID);
 		$countRegistered = mysql_num_rows($resultRegistered);
 		if ($i % 2 == 0) {
-			$tool_content .= "<tr>";
+			$tool_content .= "                  <tr class='even'>\n";
 		} else {
-			$tool_content .= "<tr class='odd'>";
+			$tool_content .= "                  <tr class='odd'>\n";
 		}
-		$tool_content .= "<td class='arrow'><a href='../group/group_usage.php?module=usage&amp;userGroupId=".$group["id"]."'>".$group["name"]."</a></td>";
-		$tool_content .= "<td width='35%'>".uid_to_name($group['tutor'])."</td>";
-      		$tool_content .= "<td align='center'>$countRegistered</td>";
+		$tool_content .= "                    <td class='arrow'><a href='../group/group_usage.php?module=usage&amp;userGroupId=".$group["id"]."'>".$group["name"]."</a></td>\n";
+		$tool_content .= "                    <td>".uid_to_name($group['tutor'])."</td>\n";
+      		$tool_content .= "                    <td class='center'>$countRegistered</td>\n";
 		if ($group['maxStudent'] == 0) {
-			$tool_content .= "<td align='center'>-</td>";
+			$tool_content .= "                    <td class='center'>-</td>\n";
 		} else {
-			$tool_content .= "
-      			<td  align='center'>$group[maxStudent]</td>";
+			$tool_content .= "                    <td class='center'>$group[maxStudent]</td>\n";
 		}
-    		$tool_content .= '</tr>';
+    		$tool_content .= "                  </tr>\n";
 		$i++;
         }
-        $tool_content .= "</tbody></table>";
+        $tool_content .= "                  </table>\n";
 } else {
-	$tool_content .= "<p>&nbsp;</p><p class='caution_small'>$langNoGroup</p>";
+	$tool_content .= "    <p class='caution_small'>$langNoGroup</p>";
 }
 
 draw($tool_content, 2, 'usage', $head_content);
