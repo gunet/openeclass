9c185e7cc50a60de7bfa8aea534a3ba7a7c78ad0
Yannis Exidaridis <jexi@noc.uoa.gr>
2010-11-10 12:44:35 +0000

    fix database query

--- modules/group/group_space.php
+++ modules/group/group_space.php
@@ -86,7 +86,8 @@ $tool_content .=  "<br /><table width='99%' class='FormData'>
 
 $tutors = array();
 $members = array();
-$q = db_query("SELECT user.user_id, nom, prenom, email, am, is_tutor, description
+$q = db_query("SELECT user.user_id, nom, prenom, email, am, is_tutor,
+		      group_members.description
                       FROM group_members, user
                       WHERE group_id = $group_id AND
                             group_members.user_id = user.user_id
