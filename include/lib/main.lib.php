d89bbe1d01302588b9a82289d6f387655fa957bd
Alexandros Diamantidis <adia@noc.uoa.gr>
2011-02-03 16:24:13 +0000

    Proper quoting of glossary terms in substitution regexp.

--- include/lib/main.lib.php
+++ include/lib/main.lib.php
@@ -1624,19 +1624,17 @@ function glossary_expand($text)
 	} elseif (!isset($_SESSION['glossary']) or
             $_SESSION['glossary_course_id'] != $cours_id) {
                 get_glossary_terms($cours_id);
-                //$_SESSION['glossary_terms_regexp'] = '/\b(';
-                $_SESSION['glossary_terms_regexp'] = '/(';
+                $_SESSION['glossary_terms_regexp'] = chr(1) . '(';
                 $begin = true;
                 foreach (array_keys($_SESSION['glossary']) as $term) {
                         $_SESSION['glossary_terms_regexp'] .= ($begin? '': '|') .
-                                                              $term;
+                                                              preg_quote($term);
                         if ($begin) {
                                 $begin = false;
                         }
                 }
-//                $_SESSION['glossary_terms_regexp'] .= ')\b/ui';
-                $_SESSION['glossary_terms_regexp'] .= ')/ui';
-		if ($begin) {
+                $_SESSION['glossary_terms_regexp'] .= ')' . chr(1) . 'ui';
+                if ($begin) {
 			unset($_SESSION['glossary_terms_regexp']);
 		}
         }
