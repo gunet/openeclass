4d77a1a16275bde68482e2e494e8813fcaeff059
Stevy Georgiadi <stevy@gunet.gr>
2011-02-21 14:07:05 +0000

    missing '

--- include/logged_out_content.php
+++ include/logged_out_content.php
@@ -109,7 +109,7 @@ if (!get_config('dont_display_login_form')) {
 	   <input class='Login' name='uname' size='17' /><br />
 	   $langPass <br />
 	   <input class='Login' name='pass' type = 'password' size = '17' /><br /><br />
-	   <input class='Login' name='submit' type = 'submit' size = 17' value = '$langEnter' /><br />
+	   <input class='Login' name='submit' type = 'submit' size = '17' value = '$langEnter' /><br />
 	   $warning<br />$shibboleth_link
 	   <a href='modules/auth/lostpass.php'>$lang_forgot_pass</a>
 	   </form>
