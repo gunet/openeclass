78c8b2e924461ba2660a071d13a53a93b196611e
Stevy Georgiadi <stevy@gunet.gr>
2011-02-24 12:56:30 +0000

    replace gif -> png

--- modules/document/document.php
+++ modules/document/document.php
@@ -867,7 +867,7 @@ if ($doc_count == 0) {
 						 "title='$langMove' alt='$langMove' /></a>&nbsp;";
                                 /*** rename command ***/
                                 $tool_content .=  "<a href='{$base_url}rename=$cmdDirName'>";
-                                $tool_content .=  "<img src='../../template/classic/img/edit.png' " .
+                                $tool_content .=  "<img src='../../template/classic/img/rename.png' " .
 					          "title='$langRename' alt='$langRename' /></a>&nbsp;";
                                 /*** comment command ***/
                                 $tool_content .= "<a href='{$base_url}comment=$cmdDirName'>";
