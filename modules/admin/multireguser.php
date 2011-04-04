0ed8e6ddc9c5382a0de5d692fd36d6c39439254e
Stevy Georgiadi <stevy@gunet.gr>
2011-04-04 12:41:34 +0000

    css changes

--- modules/admin/multireguser.php
+++ modules/admin/multireguser.php
@@ -116,38 +116,41 @@ if (isset($_POST['submit'])) {
                 $facs[$n['id']] = $n['name'];
         }
         $tool_content .= "$langMultiRegUserInfo
-<form method='post' action='$_SERVER[PHP_SELF]'>
-<table class='FormData'>
-<tr><th>$langMultiRegFields</th>
+            <form method='post' action='$_SERVER[PHP_SELF]'>
+            <fieldset>
+  <legend>...</legend>
+<table class='tbl' width='100%'>
+<tr><th>$langMultiRegFields:</th>
     <td><input type='text' name='fields' size='50' value='first last id email phone' /></td>
-<tr><th>$langUsersData</th>
+<tr><th>$langUsersData:</th>
     <td><textarea class='auth_input' name='user_info' rows='10' cols='60'></textarea></td>
 </tr>
-<tr><th>$langMultiRegType</th>
+<tr><th>$langMultiRegType:</th>
     <td><select name='type'>
         <option value='stud'>$langsOfStudents</option>
         <option value='prof'>$langOfTeachers</option></select></td>
 </tr>
-<tr><th>$langMultiRegPrefix</th>
+<tr><th>$langMultiRegPrefix:</th>
     <td><input type='text' name='prefix' size='10' value='user' /></td>
 </tr>
-<tr><th>$langFaculty</th>
+<tr><th>$langFaculty:</th>
     <td>" . selection($facs, 'facid') . "</td>
 </tr>
-<tr><th>$langAm</th>
+<tr><th>$langAm:</th>
     <td><input type='text' name='am' size='10' /></td>
 </tr>
-<tr><th>$langLanguage</th>
+<tr><th>$langLanguage:</th>
     <td>" . lang_select_options('lang') . "</td>
 </tr>
-<tr><th>$langInfoMail</th>
+<tr><th>$langInfoMail:</th>
     <td><input name='send_mail' type='checkbox' />
         $langMultiRegSendMail</td>
 </tr>
 <tr><th>&nbsp;</th>
-    <td><input type='submit' name='submit' value='$langSubmit' /></td>
+    <td class='right'><input type='submit' name='submit' value='$langSubmit' /></td>
 </tr>
 </table>
+</fieldset>
 </form>";
 }
 
