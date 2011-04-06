c9c0b00fa12ec9af55a3ac7b84f053045468b0bf
Stevy Georgiadi <stevy@gunet.gr>
2011-04-06 15:10:03 +0000

    css changes

--- modules/user/muladduser.php
+++ modules/user/muladduser.php
@@ -42,38 +42,38 @@ if($is_adminOfCourse) {
     $tool_content .= "
     <fieldset>
     <legend>$langUsersData</legend>
-    <table width='99%' class='tbl'>
+    <table width='100%' class='tbl'>
     <tr>
-      <th width='180' class='left'>$langAskUserFile</th>
+      <th width='120' class='left'>$langAskUserFile:</th>
       <td><input type='file' name='users_file'></td>
-      <td align='justify'><small>$langAskManyUsers</small></td>
+      <td class='smaller'>$langAskManyUsers</td>
     </tr>
     <tr>
-      <th class='left'>&nbsp;</th>
-      <td><input type='submit' value='$langAdd'></td>
-      <td align='right'>&nbsp;</td>
-    </tr>
+      <th colspan='3' class='right'><input type='submit' value='$langAdd'></th>
+      </tr>
+
     </table>
+
     </fieldset>
     </form>
-    <p>$langAskManyUsers1<br />$langAskManyUsers2</small></p>";
+    <p>$langAskManyUsers1<br />$langAskManyUsers2</p>";
 
     mysql_select_db($mysqlMainDb);
     
     if (isset($_FILES['users_file']) && is_uploaded_file($_FILES['users_file']['tmp_name'])) {
 	$tmpusers = trim($_FILES['users_file']['tmp_name']);
-	$tool_content .= "<table width=99%><thead>
-	    <tr><th>$langUsers</th><th>$langResult</th></thead><tbody>";
+	$tool_content .= "<table width=100% class='tbl_alt'>
+	    <tr><th>$langUsers</th><th>$langResult</th>";
 	$f = fopen($tmpusers,"r");
 	while (!feof($f)) {
 	    $uname = trim(fgets($f,1024));
 	    if (!$uname) continue;
 	    if (!check_uname_line($uname)) {
-		    $tool_content .= "<tr><td colspan=\"2\">$langFileNotAllowed</td></tr>\n";
+		    $tool_content .= "<tr><td colspan=\"2\"><div class='alert1'>$langFileNotAllowed</div></td></tr>\n";
 		    break;
 	    }
 	    $result = adduser($uname, $cours_id);
-	    $tool_content .= "<tr><td align=center>$uname</td><td>";
+	    $tool_content .= "<tr><td>$uname</td><td>";
 	    if ($result == -1) {
 		    $tool_content .= $langUserNoExist;
 	    } elseif ($result == -2) {
@@ -83,7 +83,7 @@ if($is_adminOfCourse) {
 	    }
 	    $tool_content .= "</td></tr>\n";
 	}
-	$tool_content .= "</tbody></table>\n";
+	$tool_content .= "</table>\n";
 	fclose($f);
     }
 } // end
