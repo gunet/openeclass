9cc97ad48c8f394f319f0b20ef50de0fa872780f
Yannis Exidaridis <jexi@noc.uoa.gr>
2011-10-14 13:03:07 +0000

    mail verification. Kapetanakis patch

---
+++ modules/auth/mail_verify.php
@@ -0,0 +1,172 @@
+<?php
+/* ========================================================================
+ * Open eClass 2.4
+ * E-learning and Course Management System
+ * ========================================================================
+ * Copyright 2003-2011  Greek Universities Network - GUnet
+ * A full copyright notice can be read in "/info/copyright.txt".
+ * For a full list of contributors, see "credits.txt".
+ *
+ * Open eClass is an open platform distributed in the hope that it will
+ * be useful (without any warranty), under the terms of the GNU (General
+ * Public License) as published by the Free Software Foundation.
+ * The full license can be read in "/info/license/license_gpl.txt".
+ *
+ * Contact address: GUnet Asynchronous eLearning Group,
+ *                  Network Operations Center, University of Athens,
+ *                  Panepistimiopolis Ilissia, 15784, Athens, Greece
+ *                  e-mail: info@openeclass.org
+ * ======================================================================== */
+
+/*
+ * User mail verification
+ * 
+ * @author Kapetanakis Giannis <bilias@edu.physics.uoc.gr>
+ * 
+ * @abstract This component verifies user's email address according to the verification code
+ *
+ */
+
+$mail_ver_excluded = true;
+include '../../include/baseTheme.php';
+include('../../include/sendMail.inc.php');
+$nameTools = $langMailVerify;
+
+$code = isset($_GET['ver'])? $_GET['ver']: NULL;
+$req_id = isset($_GET['rid'])? escapeSimple($_GET['rid']): NULL;
+$u_id = isset($_GET['id'])? escapeSimple($_GET['id']): NULL;
+
+if ( !empty($code) and (!empty($u_id) or !empty($req_id)) ) {
+	// user has applied for account
+	if(!empty($req_id)) {
+		$qry = "SELECT id, uname, email, verified_mail, name, surname, faculty_id, phone, am, status, statut, comment, lang from `user_request` WHERE id=".autoquote($req_id);
+		$id = $req_id;
+	}
+	// no user application. user account has been created with pending mail verification
+	elseif(!empty($u_id)) {
+		$qry = "SELECT user_id, username, email, verified_mail from `user` WHERE user_id=".autoquote($u_id);
+		$id = $u_id;
+	}
+	// no id given
+	else {
+		$user_error_msg = $langMailVerifyNoId;
+		continue;
+	}
+
+	$res = mysql_query($qry);
+	if (!$res) {
+		$user_error_msg = $langMailVerifyNoDB;
+		continue;
+	}
+	else {
+		$ar = mysql_fetch_array($res);
+		if (!empty($ar)) {
+			$username = $ar[1];
+			$email = $ar[2];
+
+			$code_key = get_config('code_key');
+			$hmac = hash_hmac('sha256', $username.$email.$id, base64_decode($code_key));
+
+			// success
+			if ($hmac == $code) {
+				$verified_mail = intval($ar['verified_mail']);
+				// update user's application
+				if (!empty($req_id) and ($verified_mail!==1) ) {
+					$qry = "UPDATE `user_request` SET verified_mail=1 WHERE id=$req_id";
+					mysql_query($qry);
+
+					$department = find_faculty_by_id($ar['faculty_id']);
+					$prof = isset($ar['statut']) && intval($ar['statut'])===1? 1: NULL;
+					$name = $ar['name'];
+					$am = $ar['am'];
+					$surname = $ar['surname'];
+					$usercomment = $ar['comment'];
+					$usermail = $ar['email'];
+					$userphone = $ar['phone'];
+
+					$subject = $prof? $mailsubject: $mailsubject2;
+					$MailMessage = $mailbody1 . $mailbody2 . "$name $surname\n\n" .
+						$mailbody3 . $mailbody4 . $mailbody5 .
+						($prof? $mailbody6: $mailbody8) .
+						"\n\n$langFaculty: $department\n$langComments: $usercomment\n" .
+						"$langAm: $am\n" .
+						"$langProfUname: $username\n$langProfEmail : $usermail\n" .
+						"$contactphone: $userphone\n\n\n$logo\n\n";
+
+					if (!send_mail('', $usermail, '', $emailhelpdesk, $subject, $MailMessage, $charset))  {
+						$user_msg = $langMailErrorMessage;
+					}
+					else {
+						$user_msg = $infoprof;
+					}
+				}
+				// update user's account
+				elseif (!empty($u_id) and ($verified_mail!==1) ) {
+					$qry = "UPDATE `user` SET verified_mail=1 WHERE user_id=$u_id";
+					mysql_query($qry);
+					$user_msg = $langMailVerifySuccessU;
+					if (isset($_SESSION['mail_verification_required'])) {
+						unset($_SESSION['mail_verification_required']);
+					}
+				}
+				// don't update twice (application)
+				elseif (($verified_mail==1) && !empty($req_id)) {
+					$user_msg = $infoprof;
+					if (isset($_SESSION['mail_verification_required'])) {
+						unset($_SESSION['mail_verification_required']);
+					}
+					$tool_content = "<div class='info'>$langMailVerifySuccess2 </div>
+					<p>$user_msg<br /><br />$click <a href='$urlServer' class='mainpage'>$langHere</a>
+						$langBackPage</p>";
+					draw($tool_content,0);
+					exit;
+				}
+				// don't update twice (no application)
+				elseif (($verified_mail==1) && !empty($u_id)) {
+					$user_msg = $langMailVerifySuccessU;
+					if (isset($_SESSION['mail_verification_required'])) {
+						unset($_SESSION['mail_verification_required']);
+					}
+					$tool_content = "<div class='info'>$langMailVerifySuccess2 </div>
+					<p>$user_msg<br /><br />$click <a href='$urlServer' class='mainpage'>$langHere</a>
+						$langBackPage</p>";
+					draw($tool_content,0);
+					exit;
+				}
+
+				$tool_content = "<div class='success'>$langMailVerifySuccess </div>
+					<p>$user_msg<br /><br />$click <a href='$urlServer' class='mainpage'>$langHere</a>
+					$langBackPage</p>";
+			}
+			// code and id given but they are wrong!
+			else {
+				$user_error_msg = $langMailVerifyCodeError;
+				$tool_content = "<div class='caution'>$user_error_msg </div>
+					<p>$click <a href='$urlServer' class='mainpage'>$langHere</a>
+					$langBackPage</p>";
+			}
+		}
+		else {
+			if (!empty($req_id)) {
+				$user_error_msg = $langMailVerifyNoApplication;
+			}
+			else {
+				$user_error_msg = $langMailVerifyNoAccount;
+			}
+			$tool_content = "<div class='caution'>$user_error_msg </div>
+				<p>$click <a href='$urlServer' class='mainpage'>$langHere</a>
+				$langBackPage</p>";
+		}
+	}
+}
+// no code given and no id given
+else {
+	$user_error_msg = $langMailVerifyNoCode;
+	$tool_content = "<div class='caution'>$user_error_msg </div>
+		<p>$click <a href='$urlServer' class='mainpage'>$langHere</a>
+		$langBackPage</p>";
+}
+
+draw($tool_content,0);
+exit;
+?>
---
+++ modules/auth/mail_verify_change.php
@@ -0,0 +1,107 @@
+<?php
+/* ========================================================================
+ * Open eClass 2.4
+ * E-learning and Course Management System
+ * ========================================================================
+ * Copyright 2003-2011  Greek Universities Network - GUnet
+ * A full copyright notice can be read in "/info/copyright.txt".
+ * For a full list of contributors, see "credits.txt".
+ *
+ * Open eClass is an open platform distributed in the hope that it will
+ * be useful (without any warranty), under the terms of the GNU (General
+ * Public License) as published by the Free Software Foundation.
+ * The full license can be read in "/info/license/license_gpl.txt".
+ *
+ * Contact address: GUnet Asynchronous eLearning Group,
+ *                  Network Operations Center, University of Athens,
+ *                  Panepistimiopolis Ilissia, 15784, Athens, Greece
+ *                  e-mail: info@openeclass.org
+ * ======================================================================== */
+
+/*
+ * User mail verification
+ * 
+ * @author Kapetanakis Giannis <bilias@edu.physics.uoc.gr>
+ * 
+ * @abstract This component sends email verification code and can change user's email address
+ *
+ */
+
+$require_login = true;
+$require_valid_uid = true;
+$mail_ver_excluded = true;
+include '../../include/baseTheme.php';
+include('../../include/sendMail.inc.php');
+$nameTools = $langMailVerify;
+
+$uid = (isset($_SESSION['uid']) && !empty($_SESSION['uid']))? $_SESSION['uid']: NULL;
+
+if (empty($uid)) {
+	$tool_content .= "<div class='caution'>$langMailVerificationError2</div> ";
+	draw($tool_content,0);
+	exit;
+}
+
+// user might allready verified mail account or verification is no more needed
+if (!get_config('email_verification_required') or !check_mail_ver_required($uid)) {
+	if (isset($_SESSION['mail_verification_required'])) {
+		unset($_SESSION['mail_verification_required']);
+	}
+	header("Location:" . $urlServer);
+}
+
+if (!empty($_POST['submit'])) {
+	if (!empty($_POST['email']) && email_seems_valid($_POST['email'])) {
+		$email = $_POST['email'];
+		// user put a new email address update db and session
+		if ($email != $_SESSION['email']) {
+			$_SESSION['email'] = $email;
+			$qry = "UPDATE `user` set email=".autoquote($email). " WHERE user_id=$uid";
+			db_query($qry);
+		}
+		//send new code
+		$code_key = get_config('code_key');
+		$hmac = hash_hmac('sha256', $_SESSION['uname'].$email.$uid, base64_decode($code_key));
+		
+		$subject = $langMailChangeVerificationSubject;
+		$MailMessage = sprintf($mailbody1.$langMailVerificationChangeBody, $urlServer.'modules/auth/mail_verify.php?ver='.$hmac.'&id='.$uid);
+		if (!send_mail('', $emailhelpdesk, '', $email, $subject, $MailMessage, $charset)) {
+			$mail_ver_error = sprintf("<p class='alert1'>".$langMailVerificationError,$email,$urlServer."auth/registration.php",
+				"<a href='mailto:$emailhelpdesk' class='mainpage'>$emailhelpdesk</a>.</p>");
+			$tool_content .= $mail_ver_error;
+		}
+		else {
+			$tool_content .= "<div class='success'>$langMailVerificationSuccess4</div> ";
+		}
+	}
+	// email wrong or empty
+	else {
+		$tool_content .= "<div class='caution'>$langMailVerificationWrong</div> ";
+	}
+}
+elseif (!empty($_SESSION['mail_verification_required']) && ($_SESSION['mail_verification_required']===1) ) {
+	$tool_content .= "<div class='info'>$langMailVerificationReq</div> ";
+}
+	
+	$tool_content .= "<br /><br /><form method='post' action='$_SERVER[PHP_SELF]'>
+		<fieldset>
+			<legend>$langUserData</legend>
+			<table class='tbl' with='100%'>
+			<br />
+			<tr>
+				<th class='left'>$lang_email:</th>
+				<td><input type='text' name='email' size='30' maxlength='40' value='{$_SESSION['email']}' /></td>
+				<td><small>($langMailVerificationAddrChange)</small></td>
+			</tr>
+			<tr>
+				<th class='left'>&nbsp;</th>
+				<td colspan='2'><input type='submit' name='submit' value='$langMailVerificationNewCode' /></td>
+			</tr>
+			</table>
+			<br />
+		</fieldset>
+	</form>";
+
+draw($tool_content,0);
+exit;
+?>
