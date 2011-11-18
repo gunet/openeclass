53ab0d544c855631c9faf843c69855ee137692a3
Alexandros Diamantidis <adia@noc.uoa.gr>
2011-11-18 17:33:25 +0200

    Adding German messages (incomplete)
    --HG--
    branch : 2.5

---
+++ modules/lang/german/common.inc.php
@@ -0,0 +1,58 @@
+<?php
+
+// Message file for language de
+// Generated 2011-07-26 13:47:32
+
+$langEclass = "Open eClass - die asynchrone E-Learning Plattform";
+$langYes = "Ja";
+$langNo = "Nein";
+$langTeacher = "Lehrer";
+$langOfTeacher = "Lehrers";
+$langTeachers = "Lehrer";
+$langOfTeachers = "Lehrer";
+$langsTeacher = "Lehrer";
+$langsOfTeacher = "Lehrers";
+$langsTeachers = "Lehrer";
+$langsOfTeachers = "Lehrer";
+$langStudent = "Student";
+$langOfStudent = "Studenten";
+$langStudents = "Studenten";
+$langOfStudents = "Studenten";
+$langsStudent = "Student";
+$langsOfStudent = "Studenten";
+$langsStudents = "Studenten";
+$langsOfStudents = "Studenten";
+$langsOfStudentss = "Studenten";
+$langsstudent_acc = "Student";
+$langGuest = "Gast Benutzer";
+$langGuests = "Gast Benutzer";
+$langFaculty = "Fakultät";
+$langOfFaculty = "Fakultät";
+$langOfFaculties = "Fakultäten";
+$langFaculties = "Fakultäten";
+$langsFaculty = "Fakultät";
+$langsFaculties = "Fakultäten";
+$langpres = "Studium vor Bachelorabschluss (undergraduate)";
+$langposts = "Postgraduiert";
+$langothers = "Andere";
+$langpre = "Studium vor Bachelorabschluss (undergraduate)";
+$langpost = "Postgraduiert";
+$langother = "Postgraduiert";
+$langInfoAbout = "Die Plattform eClass realisiert ein integriertes System für die Verwaltung von elektronischen 
+Unterrichtsfächern (Kursen).
+Der Entwurf orientiert sich an der Unterstützung des konventionellen Unterrichts unter Zuhilfenahme 
+von Informatiktechnologien, welche im Bereich der Erziehungswissenschaften bereits Einzug gehalten 
+haben.
+Sie verfolgt die Philosophie des offenen Quellkodes und unterstützt den Dienst des asynchronen 
+E-Learnings ohne Beschränkungen und Verpflichtungen.
+Der Zugang zum Dienst erfolgt mit Hilfe eines einfachen Browsers ohne Zuhilfenahme von 
+speziellen technischen Fertigkeiten
+Die Zielsetzung besteht in der Förderung des Unterrichtsprozesses, indem den Teilnehmern eine 
+dynamische, interaktive Arbeitsumgebung sowie eine stetige Kommunikation zwischen Ausbildern und 
+Lernenden geboten wird.
+Im einzelnen unterstützt die Plattform den Ausbilder bei der elektronischen Verwaltung, der 
+Speicherung und Präsentation des Unterrichtsmaterials. Außerdem stellt sie dem Lernenden einen 
+alternativen Kanal zur individuellen Bildung zur Verfügung, welcher unabhängig von zeitlichen 
+und räumlichen Zwängen ist.";
+$langExtrasLeft = '';
+$langExtrasRight = '&nbsp;';
