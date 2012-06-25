03aaf187a0c0779e543835613180d40817ce24eb
Yannis Exidaridis <jexi@noc.uoa.gr>
2012-06-25 14:34:07 +0300

    small fix
    --HG--
    branch : 2.5

--- modules/course_tools/course_tools.php
+++ modules/course_tools/course_tools.php
@@ -234,7 +234,7 @@ if (isset($_POST['submit'])) {
         $helpTopic = 'Import';
 
         $tool_content .= "\n 
-            <form method='post' action='$_SERVER[SCRIPT_NAME]?course=$code_cours&amp;submit=yes&action=1' enctype='multipart/form-data'>
+            <form method='post' action='$_SERVER[SCRIPT_NAME]?course=$code_cours&amp;submit=yes&action=1' enctype='multipart/form-data' onsubmit=\"return checkrequired(this, 'link_name');\>
               <div class='info'><p>$langExplanation_0</p>
               <p>$langExplanation_3</p></div>
 
