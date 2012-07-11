8d8fb16852e254e80721a8b9db6703165da6f5f9
Alexandros Diamantidis <adia@noc.uoa.gr>
2012-07-11 18:38:28 +0300

    Code formatting

--- modules/work/grade_edit.php
+++ modules/work/grade_edit.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 /* ========================================================================
  * Open eClass 3.0
@@ -50,49 +50,40 @@ function get_assignment_details($id)
 // $assign contains an array with the assignment's details
 function show_edit_form($id, $sid, $assign)
 {
-	global $m, $langGradeOk, $tool_content, $course_code;
+        global $m, $langGradeOk, $tool_content, $course_code;
 
-	if ($sub = mysql_fetch_array(db_query("SELECT * FROM assignment_submit WHERE id = '$sid'"))) {		
-		$uid_2_name = display_user($sub['uid']);
-		if (!empty($sub['group_id'])) {
-				$group_submission = "($m[groupsubmit] ".
-					"<a href='../group/group_space.php?course=$course_code&amp;group_id=$sub[group_id]'>".
-					"$m[ofgroup] ".gid_to_name($sub['group_id'])."</a>)";
-		} else $group_submission = "";
-			$tool_content .= "
-			<form method='post' action='index.php?course=$course_code'>
-			<input type='hidden' name='assignment' value='$id'>
-			<input type='hidden' name='submission' value='$sid'>
-                        <fieldset>
-                        <legend>$m[addgradecomments]</legend>
-			<table width='99%' class='tbl'>
-			<tr>
-			  <th class='left' width='180'>${m['username']}:</th>
-			  <td>${uid_2_name} $group_submission</td></tr>
-			<tr>
-			  <th class='left'>${m['sub_date']}:</th>
-			  <td>${sub['submission_date']}</td></tr>
-			<tr>
-			  <th class='left'>${m['filename']}:</th>
-			  <td><a href='index.php?course=$course_code&amp;get=${sub['id']}'>${sub['file_name']}</a></td>
-			</tr>";
-		        $tool_content .= "<tr>
-			  <th class='left'>$m[grade]:</th>
-			  <td><input type='text' name='grade' maxlength='3' size='3' value='$sub[grade]'></td>
-			</tr>
-			<tr>
-			  <th class='left'>$m[gradecomments]:</th>
-			  <td><textarea cols='60' rows='3' name='comments'>$sub[grade_comments]</textarea></td>
-			</tr>
-			<tr>
-			  <th class='left'>&nbsp;</th>
-			  <td><input type='submit' name='grade_comments' value='$langGradeOk'></td>
-			</tr>
-			</table>
-                        </fieldset>
-			</form>
-			<br/>";
-	} else {
-		$tool_content .= "<p class='caution'>$langSubmissionError</p>";
-	}
+        if ($sub = mysql_fetch_array(db_query("SELECT * FROM assignment_submit WHERE id = '$sid'"))) {
+                $uid_2_name = display_user($sub['uid']);
+                if (!empty($sub['group_id'])) {
+                        $group_submission = "($m[groupsubmit] ".
+                                "<a href='../group/group_space.php?course=$course_code&amp;group_id=$sub[group_id]'>".
+                                "$m[ofgroup] ".gid_to_name($sub['group_id'])."</a>)";
+                } else {
+                        $group_submission = '';
+                }
+                $tool_content .= "
+        <form method='post' action='index.php?course=$course_code'>
+          <input type='hidden' name='assignment' value='$id'>
+          <input type='hidden' name='submission' value='$sid'>
+          <fieldset>
+            <legend>$m[addgradecomments]</legend>
+            <table width='99%' class='tbl'>
+              <tr><th class='left' width='180'>${m['username']}:</th>
+                  <td>${uid_2_name} $group_submission</td></tr>
+              <tr><th class='left'>${m['sub_date']}:</th>
+                  <td>${sub['submission_date']}</td></tr>
+              <tr><th class='left'>${m['filename']}:</th>
+                  <td><a href='index.php?course=$course_code&amp;get=${sub['id']}'>${sub['file_name']}</a></td></tr>
+              <tr><th class='left'>$m[grade]:</th>
+                  <td><input type='text' name='grade' maxlength='3' size='3' value='$sub[grade]'></td></tr>
+              <tr><th class='left'>$m[gradecomments]:</th>
+                  <td><textarea cols='60' rows='3' name='comments'>$sub[grade_comments]</textarea></td></tr>
+              <tr><th class='left'>&nbsp;</th>
+                  <td><input type='submit' name='grade_comments' value='$langGradeOk'></td></tr>
+            </table>
+          </fieldset>
+        </form><br>";
+        } else {
+                $tool_content .= "<p class='caution'>$langSubmissionError</p>";
+        }
 }
