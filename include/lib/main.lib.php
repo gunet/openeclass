952a4fc5b6264fa15901ad1ebab680345fc091cd
Yannis Exidaridis <jexi@noc.uoa.gr>
2012-09-10 13:01:24 +0300

    bug fixed in course description
    --HG--
    branch : 2.5

--- include/lib/main.lib.php
+++ include/lib/main.lib.php
@@ -1693,7 +1693,7 @@ function description_unit_id($cours_id)
                 db_query('INSERT INTO course_units SET `order` = -1,
                                 `title` = ' . quote($langCourseDescription) . ',
                                 `visibility` = "i",
-                                `description` = "",
+                                `comments` = "",
                                 `course_id` = ' . $cours_id);
                 return mysql_insert_id();
         }
