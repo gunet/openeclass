75c8b021d2da8b3809988c2423245ed09ef59ab2
Yannis Exidaridis <jexi@noc.uoa.gr>
2012-09-21 12:27:43 +0300

    allow unlimited time for creating archive
    --HG--
    branch : 2.5

--- modules/document/document.php
+++ modules/document/document.php
@@ -126,7 +126,9 @@ if (isset($_GET['download'])) {
                 }
                 list($real_filename, $format) = mysql_fetch_row($q);
         }
-
+        // Allow unlimited time for creating the archive
+        set_time_limit(0);
+        
         if ($format == '.dir') {
                 $real_filename = $real_filename.'.zip';
                 $dload_filename = $webDir . 'courses/temp/'.safe_filename('zip');
