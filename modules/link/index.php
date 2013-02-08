e440b695fe7ab49c8d677fc51de6cadb61982e0b
Thanos Kyritsis <atkyritsis@upnet.gr>
2013-02-08 14:55:43 +0200

    Refactor common JS code when encapsulating a module under TinyMCE Resource Browser

--- modules/link/index.php
+++ modules/link/index.php
@@ -66,36 +66,7 @@ if ($is_in_tinymce) {
 
     load_js('jquery');
     load_js('tinymce/jscripts/tiny_mce/tiny_mce_popup.js');
-
-    $head_content .= <<<EOF
-<script type='text/javascript'>
-$(document).ready(function() {
-
-    $("a.fileURL").click(function() {
-        var URL = $(this).attr('href');
-        var win = tinyMCEPopup.getWindowArg("window");
-
-        // insert information now
-        win.document.getElementById(tinyMCEPopup.getWindowArg("input")).value = URL;
-
-        // are we an image browser
-        if (typeof(win.ImageDialog) != "undefined") {
-            // we are, so update image dimensions...
-            if (win.ImageDialog.getImageData)
-                win.ImageDialog.getImageData();
-
-            // ... and preview if necessary
-            if (win.ImageDialog.showPreviewImage)
-                win.ImageDialog.showPreviewImage(URL);
-        }
-
-        // close popup window
-        tinyMCEPopup.close();
-        return false;
-    });
-});
-</script>
-EOF;
+    load_js('tinymce.popup.urlgrabber.min.js');
 }
 
 $head_content .= <<<hContent
