c0d8f3324273c3cd2abfc44d54422ab630dbb191
Thanos Kyritsis <atkyritsis@upnet.gr>
2013-02-14 17:17:50 +0200

    Implement pseudo-cron as a webbug

--- cron.php
+++ cron.php
@@ -1,33 +1,65 @@
 <?php
+/* ========================================================================
+ * Open eClass 3.0
+ * E-learning and Course Management System
+ * ========================================================================
+ * Copyright 2003-2012  Greek Universities Network - GUnet
+ * A full copyright notice can be read in "/info/copyright.txt".
+ * For a full list of contributors, see "credits.txt".
+ *
+ * Open eClass is an open platform distributed in the hope that it will
+ * be useful (without any warranty), under the terms of the GNU (General
+ * Public License) as published by the Free Software Foundation.
+ * The full license can be read in "/info/license/license_gpl.txt".
+ *
+ * Contact address: GUnet Asynchronous eLearning Group,
+ *                  Network Operations Center, University of Athens,
+ *                  Panepistimiopolis Ilissia, 15784, Athens, Greece
+ *                  e-mail: info@openeclass.org
+ * ======================================================================== */
+
 require_once 'include/baseTheme.php';
+define('DLOCK', $webDir . '/courses/cron.lock');
+session_write_close();
 
+lock();
 ignore_user_abort(true);
-date_default_timezone_set('Europe/Athens');
-
-$response = "response"; 
-header("Connection: close");
-header("Content-Length: " . mb_strlen($response));
-header('Location: '. $urlServer);
-echo $response;
+imgOut();
 custom_flush();
-
-register_shutdown_function('cronjob');
+cronjob();
+unlock();
 
 
 function cronjob() {
-  //$file = '/tmp/koko.txt';
-  //file_put_contents($file, "run1: ". date('G:i:s') ."\n", FILE_APPEND);
+    $file = '/tmp/koko.txt';
+    file_put_contents($file, "run1: ". date('G:i:s') ."\n", FILE_APPEND);
+    sleep(7);
+    file_put_contents($file, "run2: ". date('G:i:s') ."\n", FILE_APPEND);
+}
 
-  sleep(7);
-  //for ($i=0; $i <= 100000000; $i++)
-  //  $k=5+5;
+function imgOut() {
+    $img = base64_decode('R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAEALAAAAAABAAEAAAIBTAA7');
+    header('Content-Type: image/gif');
+    header('Content-Length: ' . strlen($img));
+    header('Connection: Close');
+    echo $img;
+}
 
-  //file_put_contents($file, "run2: ". date('G:i:s') ."\n", FILE_APPEND);
+function lock() {
+    if (file_exists(DLOCK)) {
+        imgOut();
+        exit();
+    }
+    mkdir(DLOCK);
+}
 
+function unlock() {
+    if (file_exists(DLOCK))
+        rmdir (DLOCK);
 }
 
 function custom_flush() {
-    echo(str_repeat(' ',256));
+    echo(str_repeat(' ', 256));
     // check that buffer is actually set before flushing
     if (ob_get_length()){           
         @ob_flush();
