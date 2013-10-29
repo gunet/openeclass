ff180cf2d928b1c5bcb24669c4420bb468c27b4b
Yannis Exidaridis <jexi@noc.uoa.gr>
2013-10-29 12:59:54 +0200

    fix default value in course license (to avoid notices during course restoration)
    --HG--
    branch : 2.7

--- install/install_db.php
+++ install/install_db.php
@@ -128,7 +128,7 @@ db_query("CREATE TABLE `cours` (
   `description` TEXT,
   `course_keywords` TEXT,
   `course_addon` TEXT,
-  `course_license` TINYINT(4),
+  `course_license` TINYINT(4) NOT NULL DEFAULT 20,
   `visible` tinyint(4) NOT NULL,
   `titulaires` varchar(200) NOT NULL DEFAULT '',
   `fake_code` varchar(20) NOT NULL DEFAULT '',
