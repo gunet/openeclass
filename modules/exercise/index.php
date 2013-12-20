916bf986cd9fe129b1d21bc6cab7cdefada18f4f
Nikos Vavoulas <none@none>
2013-12-20 13:11:39 +0200

    Changing queries in order to use the Database Object
    --HG--
    branch : nikosv_exercises

--- modules/exercise/index.php
+++ modules/exercise/index.php
@@ -22,9 +22,9 @@
  * @file index.php
  * @brief main exercise module script
  */
-$TBL_EXERCISE_QUESTION = 'exercise_with_questions';
-$TBL_EXERCISE = 'exercise';
-$TBL_QUESTION = 'exercise_question';
+ $TBL_EXERCISE_QUESTION = 'exercise_with_questions';
+ $TBL_EXERCISE = 'exercise';
+ $TBL_QUESTION = 'exercise_question';
 
 require_once 'exercise.class.php';
 require_once 'question.class.php';
@@ -124,20 +124,16 @@ if ($is_editor) {
         // destruction of Exercise
         unset($objExerciseTmp);
     }
-    $sql = "SELECT id, title, description, type, active, public FROM `$TBL_EXERCISE` 
-                        WHERE course_id = $course_id ORDER BY id LIMIT $from, $limitExPage";
-    $result = db_query($sql);
-    $qnum = db_query("SELECT COUNT(*) FROM `$TBL_EXERCISE` WHERE course_id = $course_id");
+    $result = Database::get()->queryArray("SELECT id, title, description, type, active, public FROM exercise WHERE course_id = ? ORDER BY id LIMIT ?, ?", $course_id, $from, $limitExPage);
+	$qnum = Database::get()->querySingle("SELECT COUNT(*) as count FROM exercise WHERE course_id = ?", $course_id)->count;
 } else {
-    // only for students
-    $sql = "SELECT id, title, description, type, active, public, start_date, end_date, time_constraint, attempts_allowed " .
-            "FROM `$TBL_EXERCISE` WHERE course_id = $course_id ORDER BY id LIMIT $from, $limitExPage";
-    $result = db_query($sql);
-    $qnum = db_query("SELECT COUNT(*) FROM `$TBL_EXERCISE` WHERE course_id = $course_id AND active = 1");
+	$result = Database::get()->queryArray("SELECT id, title, description, type, active, public, start_date, end_date, time_constraint, attempts_allowed " .
+            "FROM exercise WHERE course_id = ? ORDER BY id LIMIT ?, ?", $course_id, $from, $limitExPage);
+	$qnum = Database::get()->querySingle("SELECT COUNT(*) as count FROM exercise WHERE course_id = ? AND active = 1", $course_id)->count;
 }
 
-list($num_of_ex) = mysql_fetch_array($qnum);
-$nbrExercises = mysql_num_rows($result);
+$num_of_ex = $qnum; //Getting number of all active exercises of the course
+$nbrExercises = count($result); //Getting number of limited (offset and limit) exercises of the course (active and inactive)
 
 if ($is_editor) {
     $tool_content .= "<div align='left' id='operations_container'>
@@ -184,9 +180,9 @@ if (!$nbrExercises) {
     }
     // display exercise list
     $k = 0;
-    while ($row = mysql_fetch_array($result)) {
+    foreach ($result as $row) {
         if ($is_editor) {
-            if (!$row['active']) {
+            if (!$row->active) {
                 $tool_content .= "<tr class='invisible'>";
             } else {
                 if ($k % 2 == 0) {
@@ -203,26 +199,24 @@ if (!$nbrExercises) {
             }
         }
 
-        $row['description'] = standard_text_escape($row['description']);
+        $row->description = standard_text_escape($row->description);
 
         // prof only
         if ($is_editor) {
-            if (!empty($row['description'])) {
-                $descr = "<br/>$row[description]";
+            if (!empty($row->description)) {
+                $descr = "<br/>$row->description";
             } else {
                 $descr = '';
             }
             $tool_content .= "<td width='16'>
 				<img src='$themeimg/arrow.png' alt='' /></td>
-				<td><a href='exercise_submit.php?course=$course_code&amp;exerciseId=${row['id']}'>" . q($row['title']) . "</a>$descr</td>";
-            $eid = $row['id'];
-            $NumOfResults = mysql_fetch_array(db_query("SELECT COUNT(*) FROM exercise_user_record
-                                                WHERE eid = '$eid'", $mysqlMainDb));
+				<td><a href='exercise_submit.php?course=$course_code&amp;exerciseId={$row->id}'>" . q($row->title) . "</a>$descr</td>";
+            $eid = $row->id;
+			$NumOfResults = Database::get()->querySingle("SELECT COUNT(*) as count FROM exercise_user_record WHERE eid = ?", $eid)->count;
 
-            if ($NumOfResults[0]) {
-                $tool_content .= "<td align='center'><a href='results.php?course=$course_code&amp;exerciseId=" . $row['id'] . "'>" .
-                        $langExerciseScores1 . "</a> |
-				<a href='csv.php?course=$course_code&amp;exerciseId=" . $row['id'] . "' target=_blank>" . $langExerciseScores3 . "</a></td>";
+            if ($NumOfResults) {
+                $tool_content .= "<td align='center'><a href='results.php?course=$course_code&amp;exerciseId={$row->id}'>$langExerciseScores1</a> |
+				<a href='csv.php?course=$course_code&amp;exerciseId=" . $row->id . "' target=_blank>" . $langExerciseScores3 . "</a></td>";
             } else {
                 $tool_content .= "<td align='center'>	-&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;- </td>";
             }
@@ -230,39 +224,39 @@ if (!$nbrExercises) {
             $langConfirmYourChoice_temp = addslashes(htmlspecialchars($langConfirmYourChoice));
             $langDelete_temp = htmlspecialchars($langDelete);
             $tool_content .= "<td align = 'right'>
-                                  <a href='admin.php?course=$course_code&amp;exerciseId=$row[id]'>
-                                        <img src='$themeimg/edit.png' alt='$langModify_temp' title='$langModify_temp' />
+                                   <a href='admin.php?course=$course_code&amp;exerciseId=$row->id'>
+                                         <img src='$themeimg/edit.png' alt='$langModify_temp' title='$langModify_temp' />
+                                   </a>
+                                   <a href='$_SERVER[SCRIPT_NAME]?course=$course_code&amp;choice=delete&amp;exerciseId=$row->id' onClick=\"return confirmation('$langConfirmDelete');\">
+                                         <img src='$themeimg/delete.png' alt='$langDelete_temp' title='$langDelete_temp' />
                                   </a>
-                                  <a href='$_SERVER[SCRIPT_NAME]?course=$course_code&amp;choice=delete&amp;exerciseId=$row[id]' onClick=\"return confirmation('$langConfirmDelete');\">
-                                        <img src='$themeimg/delete.png' alt='$langDelete_temp' title='$langDelete_temp' />
-                                 </a>
-                                  <a href='$_SERVER[SCRIPT_NAME]?course=$course_code&amp;choice=purge&amp;exerciseId=$row[id]' onClick=\"return confirmation('$langConfirmPurgeExercises');\">
-                                        <img src='$themeimg/clear.png' alt='" . q($langPurgeExercises) . "' title='" . q($langPurgeExercises) . "' />
-                                  </a>";
+                                   <a href='$_SERVER[SCRIPT_NAME]?course=$course_code&amp;choice=purge&amp;exerciseId=$row->id' onClick=\"return confirmation('$langConfirmPurgeExercises');\">
+                                         <img src='$themeimg/clear.png' alt='" . q($langPurgeExercises) . "' title='" . q($langPurgeExercises) . "' />
+                                   </a>";
 
             // if active
-            if ($row['active']) {
+            if ($row->active) {
                 if (isset($page)) {
-                    $tool_content .= "<a href=\"$_SERVER[SCRIPT_NAME]?course=$course_code&amp;choice=disable&amp;page=${page}&amp;exerciseId=" . $row['id'] . "\">
+                    $tool_content .= "<a href=\"$_SERVER[SCRIPT_NAME]?course=$course_code&amp;choice=disable&amp;page=${page}&amp;exerciseId=" . $row->id . "\">
 					<img src='$themeimg/visible.png' alt='$langVisible' title='$langVisible' /></a>&nbsp;";
                 } else {
-                    $tool_content .= "<a href='$_SERVER[SCRIPT_NAME]?course=$course_code&amp;choice=disable&amp;exerciseId=" . $row['id'] . "'>
+                    $tool_content .= "<a href='$_SERVER[SCRIPT_NAME]?course=$course_code&amp;choice=disable&amp;exerciseId=" . $row->id . "'>
 					<img src='$themeimg/visible.png' alt='$langVisible' title='$langVisible' /></a>&nbsp;";
                 }
             } else { // else if not active
                 if (isset($page)) {
-                    $tool_content .= "<a href='$_SERVER[SCRIPT_NAME]?course=$course_code&amp;choice=enable&amp;page=${page}&amp;exerciseId=" . $row['id'] . "'>
+                    $tool_content .= "<a href='$_SERVER[SCRIPT_NAME]?course=$course_code&amp;choice=enable&amp;page=${page}&amp;exerciseId=" . $row->id . "'>
 					<img src='$themeimg/invisible.png' alt='$langVisible' title='$langVisible' /></a>&nbsp;";
                 } else {
-                    $tool_content .= "<a href='$_SERVER[SCRIPT_NAME]?course=$course_code&amp;choice=enable&amp;exerciseId=" . $row['id'] . "'>
+                    $tool_content .= "<a href='$_SERVER[SCRIPT_NAME]?course=$course_code&amp;choice=enable&amp;exerciseId=" . $row->id . "'>
 					<img src='$themeimg/invisible.png' alt='$langVisible' title='$langVisible' /></a>&nbsp;";
                 }
             }
             if (course_status($course_id) == COURSE_OPEN) {
                 if ($row['public']) {
-                    $tool_content .= icon('access_public', $langResourceAccess, "$_SERVER[SCRIPT_NAME]?course=$course_code&amp;choice=limited&amp;exerciseId=" . $row['id'] . "");
+                    $tool_content .= icon('access_public', $langResourceAccess, "$_SERVER[SCRIPT_NAME]?course=$course_code&amp;choice=limited&amp;exerciseId=" . $row->id . "");
                 } else {
-                    $tool_content .= icon('access_limited', $langResourceAccess, "$_SERVER[SCRIPT_NAME]?course=$course_code&amp;choice=public&amp;exerciseId=" . $row['id'] . "");
+                    $tool_content .= icon('access_limited', $langResourceAccess, "$_SERVER[SCRIPT_NAME]?course=$course_code&amp;choice=public&amp;exerciseId=" . $row->id . "");
                 }
                 $tool_content .= "&nbsp;";
             }
@@ -270,50 +264,52 @@ if (!$nbrExercises) {
         }
         // student only
         else {
-            if (!resource_access($row['active'], $row['public'])) {
+            if (!resource_access($row->active, $row->public)) {
                 continue;
             }
             $currentDate = date("Y-m-d H:i");
-            $temp_StartDate = mktime(substr($row['start_date'], 11, 2), substr($row['start_date'], 14, 2), 0, substr($row['start_date'], 5, 2), substr($row['start_date'], 8, 2), substr($row['start_date'], 0, 4));
-            $temp_EndDate = mktime(substr($row['end_date'], 11, 2), substr($row['end_date'], 14, 2), 0, substr($row['end_date'], 5, 2), substr($row['end_date'], 8, 2), substr($row['end_date'], 0, 4));
+			//These convertions do not seem to be necessary
+            $temp_StartDate = mktime(substr($row->start_date, 11, 2), substr($row->start_date, 14, 2), 0, substr($row->start_date, 5, 2), substr($row->start_date, 8, 2), substr($row->start_date, 0, 4));
+            $temp_EndDate = mktime(substr($row->end_date, 11, 2), substr($row->end_date, 14, 2), 0, substr($row->end_date, 5, 2), substr($row->end_date, 8, 2), substr($row->end_date, 0, 4));
             $currentDate = mktime(substr($currentDate, 11, 2), substr($currentDate, 14, 2), 0, substr($currentDate, 5, 2), substr($currentDate, 8, 2), substr($currentDate, 0, 4));
+        
             if (($currentDate >= $temp_StartDate) && ($currentDate <= $temp_EndDate)) {
                 $tool_content .= "<td width='16'><img src='$themeimg/arrow.png' alt='' /></td>
-                                        <td><a href='exercise_submit.php?course=$course_code&amp;exerciseId=" . $row['id'] . "'>" . q($row['title']) . "</a>";
-            } elseif ($currentDate <= $temp_StartDate) { // exercise has not yet started
+                                       <td><a href='exercise_submit.php?course=$course_code&amp;exerciseId=$row->id'>" . q($row->title) . "</a>";
+             } 
+            elseif ($currentDate <= $temp_StartDate) { // exercise has not yet started
                 $tool_content .= "<td width='16'><img src='$themeimg/arrow.png' alt='' /></td>
-                                        <td class='invisible'>" . q($row['title']) . "&nbsp;&nbsp;";
+                                         <td class='invisible'>" . q($row['title']) . "&nbsp;&nbsp;";
             } else { // exercise has expired
                 $tool_content .= "<td width='16'>
-                                <img src='$themeimg/arrow.png' alt='' />
-                                </td><td>" . q($row['title']) . "&nbsp;&nbsp;(<font color='red'>$m[expired]</font>)";
+                                 <img src='$themeimg/arrow.png' alt='' />
+                                 </td><td>" . q($row->title) . "&nbsp;&nbsp;(<font color='red'>$m[expired]</font>)";
             }
-            $tool_content .= "<br />$row[description]</td><td class='smaller' align='center'>
-                                " . nice_format(date("Y-m-d H:i", strtotime($row['start_date'])), true) . " /
-                                " . nice_format(date("Y-m-d H:i", strtotime($row['end_date'])), true) . "</td>";
+            $tool_content .= "<br />$row->description</td><td class='smaller' align='center'>
+                                " . nice_format(date("Y-m-d H:i", strtotime($row->start_date)), true) . " /
+                                " . nice_format(date("Y-m-d H:i", strtotime($row->end_date)), true) . "</td>";
             // how many attempts we have.
-            $currentAttempt = mysql_fetch_array(db_query("SELECT COUNT(*) FROM exercise_user_record
-                                                                      WHERE eid = '$row[id]' AND uid = '$uid'", $mysqlMainDb));
-            if ($row['time_constraint'] > 0) {
+			$currentAttempt = Database::get()->querySingle("SELECT COUNT(*) FROM exercise_user_record WHERE eid = ? AND uid = ?", $row->id, $uid)->count;														  
+            if ($row->time_constraint > 0) {
                 $tool_content .= "<td align='center'>
-                                $row[time_constraint] $langExerciseConstrainUnit</td>";
+                                $row->time_constraint $langExerciseConstrainUnit</td>";
             } else {
                 $tool_content .= "<td align='center'> - </td>";
             }
-            if ($row['attempts_allowed'] > 0) {
-                $tool_content .= "<td align='center'>$currentAttempt[0]/$row[attempts_allowed]</td>";
+            if ($row->attempts_allowed > 0) {
+                $tool_content .= "<td align='center'>$currentAttempt/$row->attempts_allowed</td>";
             } else {
                 $tool_content .= "<td align='center'> - </td>";
             }
             // user last exercise score
-            $r = mysql_fetch_array(db_query("SELECT total_score, total_weighting
-                                        FROM exercise_user_record WHERE uid=$uid
-                                        AND eid=$row[id]
-                                        ORDER BY eurid DESC LIMIT 1", $mysqlMainDb));
-            if (empty($r['total_score'])) {
+            $r = Database::get()->querySingle("SELECT total_score, total_weighting
+                                        FROM exercise_user_record WHERE uid = ?
+                                        AND eid = ?
+                                        ORDER BY eurid DESC LIMIT 1", $uid, $row->id);
+            if (empty($r->total_score)) {
                 $tool_content .= "<td align='center'>&dash;</td>";
             } else {
-                $tool_content .= "<td align='center'>$r[total_score]/$r[total_weighting]</td>";
+                $tool_content .= "<td align='center'>$r->total_score/$r->total_weighting</td>";
             }
             $tool_content .= "</tr>";
         }
