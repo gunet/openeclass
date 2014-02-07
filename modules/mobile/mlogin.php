6c7d4b7df75f4f2d72a7b8a3e8bdb1f86f204837
Thanos Kyritsis <atkyritsis@upnet.gr>
2014-02-07 19:15:00 +0200

    add support for alternative logins in mobile login (port from 2.8 changeset 6607 e1ab3ded676a)

--- modules/mobile/mlogin.php
+++ modules/mobile/mlogin.php
@@ -87,8 +87,13 @@ if (isset($_POST['uname']) && isset($_POST['pass'])) {
     }
     $result = db_query($sqlLogin);
 
-    while ($myrow = mysql_fetch_assoc($result))
-        $ok = login($myrow, $uname, $pass);
+    while ($myrow = mysql_fetch_assoc($result)) {
+        if (in_array($myrow['password'], $auth_ids)) {
+            $ok = alt_login($myrow, $uname, $pass);
+        } else {
+            $ok = login($myrow, $uname, $pass);
+        }
+    }
 
     if (isset($_SESSION['uid']) && $ok == 1) {
         Database::get()->query("INSERT INTO loginout (loginout.id_user, loginout.ip, loginout.when, loginout.action)
