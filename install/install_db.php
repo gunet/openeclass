589abe5fe474467f22b478ebe66b1baf7165bac1
plessas <plessas@westgate.gr>
2014-03-26 12:22:50 +0200

    add tables indices
    --HG--
    branch : messaging

--- install/install_db.php
+++ install/install_db.php
@@ -457,14 +457,17 @@ db_query("CREATE TABLE IF NOT EXISTS dropbox_attachment (
                 `msg_id` INT(11) UNSIGNED NOT NULL,
                 `filename` VARCHAR(250) NOT NULL,
                  real_filename varchar(255) NOT NULL,
-                `filesize` INT(11) UNSIGNED NOT NULL) $charset_spec");
+                `filesize` INT(11) UNSIGNED NOT NULL,
+                KEY `msg` (`msg_id`)) $charset_spec");
 
 db_query("CREATE TABLE IF NOT EXISTS dropbox_index (
                 `msg_id` INT(11) UNSIGNED NOT NULL,
                 `recipient_id` INT(11) UNSIGNED NOT NULL,
                 `thread_id` INT(11) UNSIGNED NOT NULL,
                 `is_read` BOOLEAN NOT NULL DEFAULT 0,
-                PRIMARY KEY (msg_id, recipient_id)) $charset_spec");
+                PRIMARY KEY (`msg_id`, `recipient_id`),
+                KEY `list` (`recipient_id`,`is_read`),
+                KEY `participants` (`thread_id`,`recipient_id`)) $charset_spec");
 
 db_query("CREATE TABLE IF NOT EXISTS `lp_module` (
                 `module_id` INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
