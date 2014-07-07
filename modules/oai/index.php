8d01631485224d9fce86c194509789df03c9b457
Nikos Chatzimpontozis <none@none>
2014-07-07 19:29:09 +0300

    OAI-PMH-2 PHP Library as eClass Module (port from changeset 7066 daff14f66898)

---
+++ modules/oai/index.php
@@ -0,0 +1,113 @@
+<?php
+/* ! \mainpage notitle
+ *
+ * \section intro_sec Introduction
+ *
+ * This is an implementation for an OAI-PMH 2.0 Data Provider (sometimes, repository is used exchangeablly) written in PHP.
+ *
+ * This implementation completely complies to OAI-PMH 2.0, including
+ * the support of on-the-fly output compression which may significantly
+ * reduce the amount of data being transfered.
+ *
+ * This package has been inspired by PHP OAI Data Provider developed by Heinrich Stamerjohanns at University of Oldenburg.  
+ * Some of the functions and algorithms used in this code were transplanted from his  implementation at http://physnet.uni-oldenburg.de/oai/.
+ *
+ * Database support is supported through PDO (PHP Data Objects included in 
+ * the PHP distribution), so almost any popular SQL-database can be 
+ * used without any change in the code. Only thing need to do is to configure
+ * database connection and define a suitable data structure.
+ *
+ * The repository can be quite easily configured by just editing 
+ * oaidp-config.php, most possible values and options are explained.
+ *
+ * \section req_sec Requirements
+ * - A running web server + PHP version 5.0 or above.
+ * - A databse can be connected by PDO. 
+ *
+ * \section install_sec Installation
+ *
+ * - Copy the the files in source package to a location under your
+ * 	 document root of your web server. The directory structure should be
+ * 	 preserved. 
+ * - Change to that directory (e.g. cd /var/www/html/oai).
+ * - Allow your webserver to write to the token directory.
+ * 	 The default token directory is /tmp which does not need any attention.
+ * - Edit oaidp-config.php. Almost all possible options are 
+ * 	 explained. It is assumed that basic elements of a record are stored in 
+ * 	 one simple table. You can find sql examples of table definition in doc folder.
+ * 	 If your data is organized differently, you have to adjust the <i>Query</i> functions 
+ *  to reflect it and even develop your own code.
+ * - Check your oai site through a web browser. e.g. : \code http://localhost/oai/ \endcode
+ * - SELinux needs special treatments for database connection and other permission.
+ *
+ * \section struct_sec Structure
+  The system includes files for individual functionality and utility classes and functions to get it work.
+  - Controller
+  - oai2.php
+  - Individual functionalities:
+  - identify.php: identifies the data provider. Responses to <B>Identify</B>.
+  - listmetadataformats.php: lists supported metadata formats, e.g. dc or rif-cs. Responses to <B>ListMetadataFormats</B>.
+  - listsets.php: lists supported sets, e.g. Activity, Collection or Party. Responses to <B>ListSets</B>.
+  - listrecords.php: lists a group of records without details. Responses to <B>ListRecords</B>. It also serves to <B>ListIdentifiers</B> which only returns identifiers.
+  - getrecord.php: gets an individual record. Responses to <B>GetRecord</B>.
+  - Utility classes
+  - xml_creater.php which includes classess ANDS_XML, ANDS_Error_XML, ANDS_Response_XML
+  - Utility functions
+  - oaidp-util.php
+  - Support to different metadataformats in your own systems. Two examples provided with the package are: record_dc.php and record_rif.php. They are helpers and need information from the real records. They need to be devloped for your particular system.
+  - Configurations
+  - oaidp-config.php
+
+ *
+ * \author Jianfeng Li
+ * \version 1.1
+ * \date 2010-2011
+ */
+
+/**
+ * \file
+ * \brief
+ * Default starting point of OAI Data Provider for a human to check.
+ *
+ * OAI Data Provider is not designed for human to retrieve data but it is possible to use this page to test and check the functionality of current implementation.
+ * This page provides a summary of the OAI-PMH and the implementation.
+ *
+ */
+$MY_URI = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['SCRIPT_NAME'];
+$pos = strrpos($MY_URI, '/');
+$MY_URI = substr($MY_URI, 0, $pos) . '/oai2.php';
+?>
+<html>
+    <head>
+        <title>php-oai2 Data Provider</title>
+    </head>
+    <body>
+
+        <h3>php-oai2 Data Provider</h3>
+        <p>This is an implementation of an <a href="http://www.openarchives.org/OAI/openarchivesprotocol.html" target="_blank">OAI-PMH 2.0 Data Provider</a>, written in <a href="http://www.php.net" title="PHP's website" target="_blank">PHP</a>.</p>
+
+        <p>This implementation completely complies to <a href="http://www.openarchives.org/OAI/openarchivesprotocol.html" target="_blank">OAI-PMH 2.0</a>, including the support of on-the-fly output compression which may significantly
+            reduce the amount of data being transfered.</p>
+
+        <p> This package has been inspired by <a href='http://code.google.com/p/oai-pmh-2/' target="_blank">PHP OAI-PMH 2.0 Data Provider</a>.  
+            Some of the functions and algorithms used in this code were transplanted from that implementation.<p>
+
+        <p>For requirements and instructions to install and configure, please reference <a href="doc/index.html">the documentation</a>.</p>
+
+        <p>Once you have setup your Data Provider, you can the easiliy check the generated answers (it will be XML) of your Data Provider
+            by clicking on the <a href="#tests">test links below</a>. </p>
+
+        <p>
+        <dl>
+            <dt>Query and check your Data-Provider</dt>
+            <dd><a href="<?php echo $MY_URI; ?>?verb=Identify">Identify</a></dd>
+            <dd><a href="<?php echo $MY_URI; ?>?verb=ListMetadataFormats">ListMetadataFormats</a></dd>
+            <dd><a href="<?php echo $MY_URI; ?>?verb=ListSets">ListSets</a></dd>
+            <dd><a href="<?php echo $MY_URI; ?>?verb=ListIdentifiers&amp;metadataPrefix=oai_dc">ListIdentifiers</a></dd>
+            <dd><a href="<?php echo $MY_URI; ?>?verb=ListRecords&amp;metadataPrefix=oai_dc">ListRecords</a></dd>
+            </dt>
+        </dl>
+    </p>
+
+</body>
+</html>
---
+++ modules/oai/oai2.php
@@ -0,0 +1,177 @@
+<?php
+/**
+ * \file oai2.php
+ * \brief
+ * OAI Data Provider command processor
+ *
+ * OAI Data Provider is not designed for human to retrieve data.
+ *
+ * This is an implementation of OAI Data Provider version 2.0.
+ * @see http://www.openarchives.org/OAI/2.0/openarchivesprotocol.htm
+ * 
+ * It needs other files:
+ * - oaidp-config.php : Configuration of provider
+ * - oaidp-util.php : Utility functions
+ * - xml_creater.php : XML generating functions
+ * - Actions:
+ * 	- identify.php : About the provider
+ * 	- listmetadataformats.php : List supported metadata formats
+ * 	- listrecords.php : List identifiers and records
+ * 	- listsets.php : List sets
+ * 	- getrecord.php : Get a record
+ *		- Your own implementation for providing metadata records.
+ *
+ * It also initiates:
+ *	- PDO datbase connection object $db.
+ *	- ANDS_XML XML document handler $outputObj.  
+ *
+ * \todo <b>Remember:</b> to define your own classess for generating metadata records.
+ * In common cases, you have to implement your own code to act fully and correctly.
+ * For generic usage, you can try the ANDS_Response_XML defined in xml_creater.php.
+ */
+
+if(function_exists("date_default_timezone_set")) {
+    date_default_timezone_set("Europe/Athens");
+}
+ 
+// Report all errors except E_NOTICE
+// This is the default value set in php.ini
+// If anything else, try them.
+// error_reporting (E_ALL ^ E_NOTICE);
+
+/**
+ * An array for collecting erros which can be reported later. It will be checked before a new action is taken.
+ */
+$errors = array();
+
+/**
+ * Supported attributes associate to verbs.
+ */
+$attribs = array ('from', 'identifier', 'metadataPrefix', 'set', 'resumptionToken', 'until');
+
+if (in_array($_SERVER['REQUEST_METHOD'],array('GET','POST'))) {
+		$args = $_REQUEST;
+} else {
+	$errors[] = oai_error('badRequestMethod', $_SERVER['REQUEST_METHOD']);
+}
+
+require_once('oaidp-util.php');
+// Always using htmlentities() function to encodes the HTML entities submitted by others.
+// No one can be trusted.
+foreach ($args as $key => $val) {
+	$checking = htmlspecialchars(stripslashes($val));
+	if (!is_valid_attrb($checking)) {
+		$errors[] = oai_error('badArgument', $checking);
+	} else {$args[$key] = $checking; }
+}
+if (!empty($errors)) {	oai_exit(); }
+
+foreach($attribs as $val) {
+	unset($$val);
+}
+
+require_once('oaidp-config.php');
+
+// Create a PDO object
+try {
+    $db = new PDO($DSN, $DB_USER, $DB_PASSWD, $DB_OPTION);
+} catch (PDOException $e) {
+    exit('Connection failed: ' . $e->getMessage());
+}
+
+// For generic usage or just trying:
+// require_once('xml_creater.php');
+// In common cases, you have to implement your own code to act fully and correctly.
+require_once('eclass_oaidc.php');
+
+// Default, there is no compression supported
+$compress = FALSE;
+if (isset($compression) && is_array($compression)) {
+	if (in_array('gzip', $compression) && ini_get('output_buffering')) {
+		$compress = TRUE;
+	}
+}
+
+if (SHOW_QUERY_ERROR) {
+	echo "Args:\n"; print_r($args);
+}
+
+if (isset($args['verb'])) {
+	switch ($args['verb']) {
+
+		case 'Identify':
+			// we never use compression in Identify
+			$compress = FALSE;
+			if(count($args)>1) {
+				foreach($args as $key => $val) {
+					if(strcmp($key,"verb")!=0) {
+						$errors[] = oai_error('badArgument', $key, $val);
+					}	
+				}
+			}
+			if (empty($errors)) include 'identify.php';
+			break;
+
+		case 'ListMetadataFormats':
+			$checkList = array("ops"=>array("identifier"));
+			checkArgs($args, $checkList);
+			if (empty($errors)) include 'listmetadataformats.php';
+			break;
+
+		case 'ListSets':
+			if(isset($args['resumptionToken']) && count($args) > 2) {
+					$errors[] = oai_error('exclusiveArgument');
+			}
+			$checkList = array("ops"=>array("resumptionToken"));
+			checkArgs($args, $checkList);
+			if (empty($errors)) include 'listsets.php';
+			break;
+
+		case 'GetRecord':
+			$checkList = array("required"=>array("metadataPrefix","identifier"));
+			checkArgs($args, $checkList);
+			if (empty($errors)) include 'getrecord.php';
+			break;
+
+		case 'ListIdentifiers':
+		case 'ListRecords':
+			if(isset($args['resumptionToken'])) {
+				if (count($args) > 2) {
+					$errors[] = oai_error('exclusiveArgument');
+				}
+				$checkList = array("ops"=>array("resumptionToken"));
+			} else {
+				$checkList = array("required"=>array("metadataPrefix"),"ops"=>array("from","until","set"));
+			}
+			checkArgs($args, $checkList);
+			if (empty($errors)) include 'listrecords.php';
+			break;
+
+		default:
+			// we never use compression with errors
+			$compress = FALSE;
+			$errors[] = oai_error('badVerb', $args['verb']);
+	} /*switch */
+} else {
+	$errors[] = oai_error('noVerb');
+}
+
+if (!empty($errors)) {	oai_exit(); }
+
+if ($compress) {
+	ob_start('ob_gzhandler');
+}
+
+header(CONTENT_TYPE);
+
+if(isset($outputObj)) {
+	$outputObj->display();
+} else {
+	exit("There is a bug in codes");
+}
+
+	if ($compress) {
+		ob_end_flush();
+	}
+
+?>
