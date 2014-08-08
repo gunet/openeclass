58c97c54d7037378427a8757930b9fa813849c2e
Ananias Tsalouchidis <atsaloux@it.auth.gr>
2014-08-08 11:37:44 +0300

    Session form title validation

--- modules/bbb/index.php
+++ modules/bbb/index.php
@@ -57,6 +57,9 @@ load_js('jquery');
 load_js('jquery-ui');
 load_js('jquery-ui-timepicker-addon.min.js');
 
+$head_content .= '<script type="text/javascript">var langEmptyGroupName = "' .
+        $langΒΒΒAlertTitle . '";</script>';
+
 $head_content .= "<link rel='stylesheet' type='text/css' href='{$urlAppend}js/jquery-ui-timepicker-addon.min.css'>
 <script type='text/javascript'>
 $(function() {
