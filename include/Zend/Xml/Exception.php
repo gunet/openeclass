af3126259fa5f4536c7a9eaebd6f9408e6ae5a7f
Thanos Kyritsis <atkyritsis@upnet.gr>
2014-09-02 14:00:45 +0300

    upgrade zend lucene to 1.12.8

--- include/Zend/Xml/Exception.php
+++ include/Zend/Xml/Exception.php
@@ -13,37 +13,24 @@
  * to license@zend.com so we can send you a copy immediately.
  *
  * @category   Zend
- * @package    Zend_Search_Lucene
- * @subpackage Analysis
+ * @package    Zend_Xml
  * @copyright  Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  * @version    $Id$
  */
 
 
-/** Zend_Search_Lucene_Analysis_Analyzer_Common_Utf8Num */
-require_once 'Zend/Search/Lucene/Analysis/Analyzer/Common/Utf8Num.php';
-
-/** Zend_Search_Lucene_Analysis_TokenFilter_LowerCaseUtf8 */
-require_once 'Zend/Search/Lucene/Analysis/TokenFilter/LowerCaseUtf8.php';
+/**
+ * @see Zend_Exception
+ */
+require_once 'Zend/Exception.php';
 
 
 /**
  * @category   Zend
- * @package    Zend_Search_Lucene
- * @subpackage Analysis
+ * @package    Zend_Xml
  * @copyright  Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
-
-
-class Zend_Search_Lucene_Analysis_Analyzer_Common_Utf8Num_CaseInsensitive extends Zend_Search_Lucene_Analysis_Analyzer_Common_Utf8Num
-{
-    public function __construct()
-    {
-        parent::__construct();
-
-        $this->addFilter(new Zend_Search_Lucene_Analysis_TokenFilter_LowerCaseUtf8());
-    }
-}
-
+class Zend_Xml_Exception extends Zend_Exception
+{}
