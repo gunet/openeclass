64e35d6b359e5af43637b7f6c023fae3b54d3993
Thanos Kyritsis <atkyritsis@upnet.gr>
2014-09-04 20:36:09 +0300

    Upgrade TinyMCE to 4.1.4, TinyMCE PHP Compressor 4.0.2 and Langs: cs 2014-08-27, de 2014-06-18 17:09, el 2014-06-18 16:16, es 2014-07-07 13:29, fr 2014-08-28 15:11, it 2014-08-18 17:47

--- js/tinymce.popup.urlgrabber.src.js
+++ js/tinymce.popup.urlgrabber.src.js
@@ -2,24 +2,15 @@ $(document).ready(function() {
 
     $("a.fileURL").click(function() {
         var URL = $(this).attr('href');
-        var win = tinyMCEPopup.getWindowArg("window");
-
+        var win = top.tinymce.activeEditor.windowManager.getParams().window;
+        var inputId = top.tinymce.activeEditor.windowManager.getParams().input;
+        
         // insert information now
-        win.document.getElementById(tinyMCEPopup.getWindowArg("input")).value = URL;
-
-        // are we an image browser
-        if (typeof(win.ImageDialog) != "undefined") {
-            // we are, so update image dimensions...
-            if (win.ImageDialog.getImageData)
-                win.ImageDialog.getImageData();
-
-            // ... and preview if necessary
-            if (win.ImageDialog.showPreviewImage)
-                win.ImageDialog.showPreviewImage(URL);
-        }
-
+        var ele = win.document.getElementById(inputId);
+        ele.value = URL;
+        
         // close popup window
-        tinyMCEPopup.close();
+        top.tinymce.activeEditor.windowManager.close();
         return false;
     });
 });
