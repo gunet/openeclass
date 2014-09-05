cf9a7348df8df5024db52af695a1b3a47f5b6ab3
Alexandros Diamantidis <adia@noc.uoa.gr>
2014-09-05 13:11:07 +0300

    Allow registration to courses with password = NULL

--- modules/auth/course_submit.php
+++ modules/auth/course_submit.php
@@ -43,7 +43,7 @@ if ($q) {
     $course_password = $q->password;
     if ($state == 'true') {
         if (($visible == COURSE_OPEN or $visible == COURSE_REGISTRATION) and
-                $password === $course_password) {
+                ($password === $course_password or $course_password === null)) {
             Database::get()->query("INSERT IGNORE INTO `course_user` (`course_id`, `user_id`, `status`, `reg_date`)
                                          VALUES (?d, ?d, " . USER_STUDENT . ", CURDATE())", $cid, $uid);
             Log::record($cid, MODULE_ID_USERS, LOG_INSERT, array('uid' => $uid, 'right' => 5));
