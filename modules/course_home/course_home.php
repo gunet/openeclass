eef2146ec68a25c07f736cc6d88e077996f4a2e4
Ioannis Feneris <ifeneris@ccf.auth.gr>
2014-09-19 22:26:01 +0300

    Fix: Fix style of buttons
    --HG--
    branch : new-ui

--- modules/course_home/course_home.php
+++ modules/course_home/course_home.php
@@ -464,7 +464,7 @@ $tool_content .= "
     <div class ='col-md-12'>
         <div class='toolbox pull-right'>
 
-            <div type='button' class='button dropdown open-on-hover'>
+            <div type='button' class='btn-default-eclass dropdown open-on-hover'>
                 <span class='txt' rel='tooltip' data-toggle='tooltip' data-placement='bottom'>Πληροφορίες Μαθήματος</span>
                 <span class='fa fa-caret-down'></span>
                 <ul class='dropdown-menu'>
@@ -481,7 +481,7 @@ $tool_content .= "
                 if ($receive_mail) {
                     $tool_content .= "
                         <a href='../../modules/contact/index.php?course=$course_code' id='email_btn'>
-                            <button class='button hover-blue' title='$langContactProf' >
+                            <button class='btn-default-eclass' title='$langContactProf' >
                                 <i class='fa fa-envelope'></i>
                             </button>
                         </a>";
@@ -492,7 +492,7 @@ $tool_content .= "
             // Button: star - bookmark the page
             $tool_content .= "
                         <a href='$_SERVER[SCRIPT_NAME]' title='" . q($title) . "' class='jqbookmark'>
-                            <button class='button hover-blue' title='$langAddAsBookmark'>
+                            <button class='btn-default-eclass' title='$langAddAsBookmark'>
                                 <i class='fa fa-star'></i>
                             </button>
                         </a>";
@@ -503,7 +503,7 @@ $tool_content .= "
             {
                 $tool_content .= "
                         <a href='${urlServer}modules/announcements/rss.php?c=$course_code'>
-                            <button class='button hover-blue' title='" . q($langRSSFeed) . "'>
+                            <button class='btn-default-eclass' title='" . q($langRSSFeed) . "'>
                                 <i class='fa fa-rss'></i>
                             </button>
                         </a>";
@@ -511,7 +511,7 @@ $tool_content .= "
 
             // Button: toggle student view
             $tool_content .= "
-                        <button class='button hover-blue' title='$langAddAsBookmark'>
+                        <button class='btn-default-eclass' title='$langAddAsBookmark'>
                                 $toggle_student_view_close
                                 $toggle_student_view
                         </button>";
@@ -583,7 +583,7 @@ if (!$alter_layout){
             <div class='align-right'>
                 <div class='toolbox margin-bottom-thin margin-top-thin'>
                     <a href='{$urlServer}modules/units/info.php?course=$course_code' rel='tooltip' data-toggle='tooltip' data-placement='right' title ='$langAddUnit' class='btn btn-default-eclass color-green'>
-                        <i class='fa fa-plus-circle'></i>
+                        <i class='fa fa-plus-circle space-after-icon'></i>
                         $langAddUnit
                     </a>
                 </div>                          
