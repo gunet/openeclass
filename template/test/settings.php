3ec4005a71e0228e876de4e772a6a6d58211092a
Alexandros Diamantidis <adia@noc.uoa.gr>
2014-09-25 18:00:12 +0300

    Removing "test" template

--- template/test/settings.php
+++
@@ -1,9 +0,0 @@
-<?php
-$icon_map = array(
-    'arrow' => 'link-icon',
-    'calendar' => 'calendar-icon',
-    'links' => 'link-icon',
-    'docs' => 'documents-icon',
-    'videos' => 'media-icon',
-    'announcements' => 'announcements-icon',
-);
