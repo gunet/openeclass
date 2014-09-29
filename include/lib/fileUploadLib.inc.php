d36a06ef6042e8fcb3333519c9c81e4835e2a961
nikos vavoulas <nikos.va@gmail.com>
2014-09-29 16:12:52 +0300

    Show quota page using Bootstrap elements
    --HG--
    branch : new-ui

--- include/lib/fileUploadLib.inc.php
+++ include/lib/fileUploadLib.inc.php
@@ -381,7 +381,8 @@ function get_max_upload_size($maxFilledSpace, $baseWorkDir) {
   This scipt uses the 'gaugebar.php' class for the graphic gauge bar
   =============================================================== */
 
-require_once 'include/lib/gaugebar.php';
+//to be removed
+//require_once 'include/lib/gaugebar.php';
 
 function showquota($quota, $used) {
 
@@ -392,9 +393,9 @@ function showquota($quota, $used) {
 
     // diamorfwsh ths grafikhs mparas xrhsimopoioumenou kai eleftherou xwrou (me vash ta quotas)
     // kai ypologismos statistikwn stoixeiwn
-    $oGauge = new myGauge();
-    $oGauge->MaxVal = $quota; //maximum value
-    $oGauge->CurVal = $used; //current value
+//    $oGauge = new myGauge();
+//    $oGauge->MaxVal = $quota; //maximum value
+//    $oGauge->CurVal = $used; //current value
     // pososto xrhsimopoioumenou xorou se %
     $diskUsedPercentage = round(($used / $quota) * 100) . "%";
     // morfopoihsh tou synolikou diathesimou megethous tou quota
@@ -417,24 +418,31 @@ function showquota($quota, $used) {
                    <li><a href=$link>" . $langBack . "</a></li>
                  </ul>
                </div>";
-
-    $retstring .= "
-                <table class='tbl_border'>
-                <tr>
-                  <th>$langQuotaUsed:</th>
-              <td>$used</td>
-                </tr>
-                <tr>
-                  <th>$langQuotaPercentage:</th>
-              <td align='center'>";
-    $retstring .= $oGauge->display();
-    $retstring .= "$diskUsedPercentage</td>
-                </tr>
-                <tr>
-                  <th>$langQuotaTotal:</th>
-              <td>$quota</td>
-                </tr>
-                </table>";
+$retstring .= "
+<form class='form-horizontal' role='form'>
+  <div class='form-group'>
+    <label class='col-sm-2'>$langQuotaUsed:</label>
+    <div class='col-sm-10'>
+      <input type='text' class='form-control' value='$used' disabled>
+    </div>
+  </div>
+  <div class='form-group'>
+    <label class='col-sm-2'>$langQuotaPercentage:</label>
+    <div class='col-sm-10'>
+        <div class='progress'>
+          <div class='progress-bar progress-bar-striped active' role='progressbar' aria-valuenow='".str_replace('%','',$diskUsedPercentage)."' aria-valuemin='0' aria-valuemax='100' style='width: $diskUsedPercentage;'>
+            $diskUsedPercentage
+          </div>
+        </div>
+    </div>
+  </div>
+  <div class='form-group'>
+    <label class='col-sm-2'>$langQuotaTotal:</label>
+    <div class='col-sm-10'>
+      <input type='text' class='form-control' value='$quota' disabled>
+    </div>
+  </div>  
+</form>";
     $tmp_cwd = getcwd();
 
     return $retstring;
