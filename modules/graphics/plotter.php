7f3715df4a12dcda3cb986f5ac83ff269768b542
Yannis Exidaridis <jexi@noc.uoa.gr>
2014-10-02 13:55:41 +0300

    correct fix unicode

--- modules/graphics/plotter.php
+++ modules/graphics/plotter.php
@@ -79,7 +79,7 @@ class Plotter {
 
             $dataset = '[';
             foreach ($this->data as $name => $value) {
-                $name = mb_strlen($name) > 17 ? (mb_substr($name, 0, 17) . "...") : $name;
+                $name = mb_strlen($name, "UTF-8") > 17 ? (mb_substr($name, 0, 17, "UTF-8") . "...") : $name;
                 $dataset .= '["' . $name . '", ' . $value . "], ";
             }
             if (strlen($dataset) > 1) {
