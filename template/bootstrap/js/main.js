f001db770a61bf3f057aa72cc79ff4d327dee56a
Alexandros Diamantidis <adia@noc.uoa.gr>
2014-10-07 18:51:00 +0300

    Make student view toggle work.
    --HG--
    branch : new-ui

--- template/bootstrap/js/main.js
+++ template/bootstrap/js/main.js
@@ -22,6 +22,10 @@ $(document).ready( function () {
     // Btn-toggle
     $('.btn-toggle').on('click',function(){
         $(this).toggleClass('btn-toggle-on');
+        $('#student-view-form').append($('<input>', {
+                    'name': 'next',
+                    'value': window.location.pathname + window.location.search,
+                    'type': 'hidden'})).submit();
     });
 
     // Lesson accordion - jQuery ui
