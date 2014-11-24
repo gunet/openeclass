d93213592d17ad6f6e5046299d3530bb4946aa32
Theodore Schizas <tioschizas@gmail.com>
2014-11-24 17:58:41 +0200

    Conflict fixes
    --HG--
    branch : tschizas

--- template/default/js/main.js
+++ template/default/js/main.js
@@ -64,7 +64,7 @@ $(document).ready( function () {
                 },
                 complete: function () {
                     $("#toggle-sidebar").toggleClass("toggle-active");
-                    $("#toggle-sidebar i").toggleClass("fa-rotate-180");
+                    //$("#toggle-sidebar i").toggleClass("fa-rotate-180");
                     if ($("#sidebar").hasClass("in"))
                     {
                         $("#sidebar-container").css({"display":"none"});
