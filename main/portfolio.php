f5d43e4eee5569a784646e65dc27cab1223c8456
Theodore Schizas <tioschizas@gmail.com>
2014-12-24 08:41:12 +0200

    Changes in portfolio
    --HG--
    branch : tschizas

--- main/portfolio.php
+++ main/portfolio.php
@@ -242,28 +242,25 @@ $tool_content .= "
                         <h4>".q("$_SESSION[givenname] $_SESSION[surname]")."</h4>
                         <h5 class='not_visible'>(-".q($_SESSION['uname'])."-)</h5>
                         <span class='tag'>$langProfileMemberSince : </span><span class='tag-value'>$userdata->registered_at</span><br>
-                        <span class='tag'>Τελευταία επίσκεψη :". $lastVisit[1]->when."</span>
+                        <span class='tag'>$langProfileLastVisit :". $lastVisit[1]->when."</span>
                     </div>
                     <div class='col-xs-12 col-sm-5'>
                         <ul class='list-group'>
                             <li class='list-group-item'>
                               <span class='badge'>$student_courses_count</span>
-                              Μαθήματα που παρακολουθώ
+                              $langSumCoursesEnrolled
                             </li>
                             <li class='list-group-item'>
                               <span class='badge'>$teacher_courses_count</span>
-                              Μαθήματα που διαχειρίζομαι
+                              $langSumCoursesSupport
                             </li>
                             <!--<li class='list-group-item'>
                               <span class='badge'>$numUsersRegistered</span>
                               Συνολικός αριθμός φοιτητών στα μαθήματά μου
                             </li>-->
                         </ul>
+                        <div class='quick-change-pwd'><a href='".$urlSecure."main/profile/password.php'>$langProfileQuickPassword</a></div>
                     </div>
-                    
-                    <!--<div class='col-sm-9'>
-                        <div class='stats'>".courseVisitsPlot()."</div>
-                    </div>--> 
                 </div>
             </div>
         </div>
