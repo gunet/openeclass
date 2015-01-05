f5a36f739f6be55e616d2921f49977380036874f
Yannis Exidaridis <jexi@noc.uoa.gr>
2015-01-05 14:28:13 +0200

    add missing fields in poll upgrade

--- upgrade/upgrade.php
+++ upgrade/upgrade.php
@@ -1319,6 +1319,8 @@ $mysqlMainDb = ' . quote($mysqlMainDb) . ';
                             `start_date` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
                             `end_date` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
                             `active` INT(11) NOT NULL DEFAULT 0,
+                            `description` MEDIUMTEXT NOT NULL,
+                            `end_message` MEDIUMTEXT NOT NUll,
                             `anonymized` INT(1) NOT NULL DEFAULT 0)
                             $charset_spec");
         Database::get()->query("CREATE TABLE IF NOT EXISTS `poll_answer_record` (
