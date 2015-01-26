c7a588fa72a88931288a9bebe76c4d28c7df75a3
Alexandros Diamantidis <adia@noc.uoa.gr>
2015-01-26 16:20:05 +0200

    Update manual links to point to docs.openeclass.org

--- info/manual.php
+++ info/manual.php
@@ -119,6 +119,6 @@ if (isset($uid) and $uid) {
  * @return type
  */
 function manlink($desc, $link, $language) {
-    $url = 'http://wiki.openeclass.org/3.0/doku.php';
-    return "<a href='$url?id=$language:$link' target='_blank' class='mainpage'>$desc</a>";
+    $url = 'http://docs.openeclass.org/3.0';
+    return "<a href='$url/$language:$link' target='_blank' class='mainpage'>$desc</a>";
 }
