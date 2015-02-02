e7c643ab7006b3195cd92a4968418d2e43b296e5
Yannis Exidaridis <jexi@noc.uoa.gr>
2015-02-02 11:14:58 +0200

    fix error while replying in forums

--- modules/forum/reply.php
+++ modules/forum/reply.php
@@ -146,7 +146,7 @@ if (isset($_POST['submit'])) {
                                 <a href='{$urlServer}$course_code'>{$urlServer}$course_code</a>";
     $plain_body_topic_notify = "$langBodyTopicNotify $langInForum '" . q($topic_title) . "' $langOfForum " . q($forum_name) . "' $langInCat '" . q($cat_name) . "' $langTo $langCourseS '$c' \n\n$plain_forum_message \n\n$gunet\n<a href='{$urlServer}$course_code'>{$urlServer}$course_code</a>";
     $linkhere = "&nbsp;<a href='${urlServer}main/profile/emailunsubscribe.php?cid=$course_id'>$langHere</a>.";
-    $unsubscribe = "<br /><br />$langNote: " . sprintf($langLinkUnsubscribe, $title);
+    $unsubscribe = "<br /><br />$langNote: " . sprintf($langLinkUnsubscribe, course_id_to_title($course_id));
     $plain_body_topic_notify .= $unsubscribe . $linkhere;
     $body_topic_notify .= $unsubscribe . $linkhere;
     foreach ($sql as $r) {
