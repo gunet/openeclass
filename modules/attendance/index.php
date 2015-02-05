60bc02a139f0af3d2d6616538dc9778bd82471c8
Theodore Schizas <none@none>
2015-02-05 15:11:48 +0200

    small presenatition fix in attendance module

--- modules/attendance/index.php
+++ modules/attendance/index.php
@@ -623,7 +623,7 @@ if ($is_editor) {
                 <div class='form-wrapper'>
                     <form class='form-horizontal' role='form' method='post' action='$_SERVER[SCRIPT_NAME]?course=$course_code&editUsers=1' onsubmit=\"return checkrequired(this, 'antitle');\">
                         <fieldset>
-                            <div class='form-group'><label class='col-sm-offset-1'>$langRefreshList</label><small class='help-block'>($langAttendanceInfoForUsers)</small></div>
+                            <div class='form-group'><label class='col-xs-12'>$langRefreshList<small class='help-block'>($langAttendanceInfoForUsers)</small></label></div>
                             <div class='form-group'>
                                 <div class='col-sm-12'>
                                     <select name='usersLimit' class='form-control'>                
@@ -634,7 +634,7 @@ if ($is_editor) {
                                 </div>
                             </div>
                             <div class='form-group'>
-                                <div class='col-sm-offset-2 col-sm-10'>
+                                <div class='col-sm-12'>
                                     <input class='btn btn-primary' type='submit' name='resetAttendance' value='$langAttendanceUpdate' />
                                 </div>
                             </div>
@@ -654,15 +654,14 @@ if ($is_editor) {
                 <div class='form-wrapper'>
                     <form class='form-horizontal' role='form' method='post' action='$_SERVER[SCRIPT_NAME]?course=$course_code' onsubmit=\"return checkrequired(this, 'antitle');\">
                         <fieldset>
-                        <div class='form-group'><label class='col-sm-offset-1'>$langAttendanceLimitTitle</label></div>
+                        <div class='form-group'><label class='col-xs-12'>$langAttendanceLimitNumber:</label></div>
                             <div class='form-group'>
-                                <label for='limit' class='col-sm-2 control-label'>$langAttendanceLimitNumber:</label>
-                                <div class='col-sm-10'>
+                                <div class='col-sm-12'>
                                     <input class='form-control' type='text' name='limit' value='$attendance_limit'/>
                                 </div>
                             </div>
                             <div class='form-group'>
-                                <div class='col-sm-offset-2 col-sm-10'>
+                                <div class='col-sm-12'>
                                     <input class='btn btn-primary' type='submit' name='submitAttendanceLimit' value='$langAttendanceUpdate' />
                                 </div>
                             </div>
