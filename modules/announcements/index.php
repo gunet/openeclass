2aaf9e871d1aa2fd072a7d3b85ab87e684a32014
Theodore Schizas <none@none>
2015-02-16 13:49:32 +0200

    add date in single announcement

--- modules/announcements/index.php
+++ modules/announcements/index.php
@@ -540,6 +540,7 @@ if (isset($_GET['an_id'])) {
         $navigation[] = array("url" => "$_SERVER[SCRIPT_NAME]?course=$course_code", "name" => $langAnnouncements);
         $tool_content .= "<div class='panel'>";
         $tool_content .= "<div class='panel-body'>";
+        $tool_content .= "<p class='not_visible'>$langDate: $row->date</p>";
         $tool_content .= $row->content;
         $tool_content .= "</div></div>";
     }
