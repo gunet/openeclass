a6d6c5626e888c6d2e3c861a6f0f48369739ec7b
Alexandros Diamantidis <adia@noc.uoa.gr>
2015-02-19 18:15:05 +0200

    Set admin language to current language during installation

--- install/install_db.php
+++ install/install_db.php
@@ -1121,12 +1121,12 @@ $db->query("CREATE TABLE IF NOT EXISTS `user_department` (
 $hasher = new PasswordHash(8, false);
 $password_encrypted = $hasher->HashPassword($passForm);
 $admin_uid = $db->query("INSERT INTO `user`
-    (`givenname`, `surname`, `username`, `password`, `email`, `status`,
+    (`givenname`, `surname`, `username`, `password`, `email`, `status`, `lang`
      `registered_at`,`expires_at`, `verified_mail`, `whitelist`, `description`)
     VALUES (?s, ?s, ?s, ?s, ?s, ?d, " . DBHelper::timeAfter() . ", " .
         DBHelper::timeAfter(5*365*24*60*60) . ", ?d, ?s, ?s)",
-    $nameForm, '', $loginForm, $password_encrypted, $emailForm, 1, 1,
-        '*,,', 'Administrator')->affectedRows;
+    $nameForm, '', $loginForm, $password_encrypted, $emailForm, 1, 1, $lang,
+        '*,,', 'Administrator')->lastInsertID;
 $db->query("INSERT INTO loginout (`id_user`, `ip`, `when`, `action`)
     VALUES (?d, ?s, " . DBHelper::timeAfter() . ", ?s)",
     $admin_uid, $_SERVER['REMOTE_ADDR'], 'LOGIN');
