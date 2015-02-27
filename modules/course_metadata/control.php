7b065b9ee58e8abfbbcdad65c67c983b93203716
Theodore Schizas <none@none>
2015-02-27 17:01:32 +0200

    bootstrapping open course metadata

--- modules/course_metadata/control.php
+++ modules/course_metadata/control.php
@@ -144,14 +144,22 @@ $disabledALevel = (!$looksA && !$checkedALevel) ? "disabled='disabled'" : '';
 $disabledAPlusLevel = (!$looksAPlus && !$checkedAPlusLevel) ? "disabled='disabled'" : '';
 
 // images
-$openAccessImg = ($hasOpenAccess) ? 'tick' : 'delete';
-$mandatoryMetadataImg = ($hasMandatoryMetadata) ? 'tick' : 'delete';
-$licenseImg = ($hasLicense) ? 'tick' : 'delete';
-$teacherConfirmImg = ($hasTeacherConfirm) ? 'tick' : 'delete';
-$docsImg = ($numDocs > 0) ? 'tick' : 'delete';
-$unitsImg = ($numUnits > 0) ? 'tick' : 'delete';
-$mediaImg = ($numMedia > 0) ? 'tick' : 'delete';
-$teacherConfirmVideoImg = ($hasTeacherConfirmVideo) ? 'tick' : 'delete';
+$openAccessImg = ($hasOpenAccess) ? 'fa-check' : 'fa-times';
+$openAccessImgBadge = ($hasOpenAccess) ? 'valid' : 'not-valid';
+$mandatoryMetadataImg = ($hasMandatoryMetadata) ? 'fa-check' : 'fa-times';
+$mandatoryMetadataImgBadge = ($hasMandatoryMetadata) ? 'valid' : 'not-valid';
+$licenseImg = ($hasLicense) ? 'fa-check' : 'fa-times';
+$licenseImgBadge = ($hasLicense) ? 'valid' : 'not-valid';
+$teacherConfirmImg = ($hasTeacherConfirm) ? 'fa-check' : 'fa-times';
+$teacherConfirmImgBadge = ($hasTeacherConfirm) ? 'valid' : 'not-valid';
+$docsImg = ($numDocs > 0) ? 'fa-check' : 'fa-times';
+$docsImgBadge = ($numDocs > 0) ? 'valid' : 'not-valid';
+$unitsImg = ($numUnits > 0) ? 'fa-check' : 'fa-times';
+$unitsImgBadge = ($numUnits > 0) ? 'valid' : 'not-valid';
+$mediaImg = ($numMedia > 0) ? 'fa-check' : 'fa-times';
+$mediaImgBadge = ($numMedia > 0) ? 'valid' : 'not-valid';
+$teacherConfirmVideoImg = ($hasTeacherConfirmVideo) ? 'fa-check' : 'fa-times';
+$teacherConfirmVideoImgBadge = ($hasTeacherConfirmVideo) ? 'valid' : 'not-valid';
 
 // parse last submission date
 $lastSubmission = '';
@@ -161,70 +169,90 @@ if (isset($xmlData['course_lastLevelConfirmation']) &&
     $lastSubmission = '<p><small>' . $langLastSubmission . ': ' . date('j/n/Y H:i:s', $ts) . '</small></p>';
 }
 
-$tool_content .= "<form method='post' action='" . $_SERVER['SCRIPT_NAME'] . "?course=$course_code'>";
+$tool_content .= action_bar(array(
+        array('title' => $langBack,
+            'url' => "index.php?course=$course_code",
+            'icon' => 'fa-reply',
+            'level' => 'primary-label')),false);
+$tool_content .= "
+    <div class='row oc-metedata-list' >
+        <div class='col-xs-12'>
+            <ul class='list-group'>
+                <li class='list-group-item list-header'>
+                    <h4>$langOpenCoursesCharacteristics</h4>
+                </li>
+                <li class='list-group-item'>
+                    <span class='badge $openAccessImgBadge'><i class='fa $openAccessImg'></i></span>
+                    $langOpenCoursesOpenAccess
+                </li>
+                <li class='list-group-item'>
+                    <span class='badge $mandatoryMetadataImgBadge'><i class='fa $mandatoryMetadataImg'></i></span>
+                    $langOpenCoursesMandatoryMetadata
+                </li>
+                <li class='list-group-item'>
+                    <span class='badge $licenseImgBadge'><i class='fa $licenseImg'></i></span>
+                    $langOpenCoursesLicense
+                </li>
+                <li class='list-group-item'>
+                    <span class='badge $teacherConfirmImgBadge'><i class='fa $teacherConfirmImg'></i></span>
+                    $langOpenCoursesTeacherConfirm
+                </li>
+                <li class='list-group-item'>
+                    <span class='badge $docsImgBadge'><i class='fa $docsImg'></i></span>
+                    $langOpenCoursesHasDocuments ($numDocs $langDoc)
+                </li>
+                <li class='list-group-item'>
+                    <span class='badge $unitsImgBadge'><i class='fa $unitsImg'></i></span>
+                    $langOpenCoursesHasUnits ($numUnits $langSections)
+                </li>
+                <li class='list-group-item'>
+                    <span class='badge $mediaImgBadge'><i class='fa $mediaImg'></i></span>
+                    $langOpenCoursesHasMediaFiles ($numMedia $langOpenCoursesFiles)
+                </li>
+                <li class='list-group-item'>
+                    <span class='badge $teacherConfirmVideoImgBadge'><i class='fa $teacherConfirmVideoImg'></i></span>
+                    $langOpenCoursesTeacherConfirmVideo
+                </li>
+            </ul>
+        </div>
+    </div>
+        ";
+
+$tool_content .= "<div class='row'>
+                    <div class='col-sm-12'>
+                        <div class='list-group'>
+                            <form class='horizontal-form' role='form' method='post' action='" . $_SERVER['SCRIPT_NAME'] . "?course=$course_code'>";
 $tool_content .= <<<EOF
-    <table class="tbl_courseid" width="100%">
-    <tbody><tr>
-      <td class="title1" colspan="2">$langOpenCoursesCharacteristics</td>
-    </tr>
-    <tr>
-      <td class="smaller"><img src="$themeimg/arrow.png" alt="">&nbsp;$langOpenCoursesOpenAccess</td> 
-      <td align="right"><img src="$themeimg/$openAccessImg.png" alt=""></td>
-    </tr>
-    <tr>
-      <td class="smaller"><img src="$themeimg/arrow.png" alt="">&nbsp;$langOpenCoursesMandatoryMetadata</td>
-      <td align="right"><img src="$themeimg/$mandatoryMetadataImg.png" alt=""></td>
-    </tr>
-    <tr>
-      <td class="smaller"><img src="$themeimg/arrow.png" alt="">&nbsp;$langOpenCoursesLicense</td>
-      <td align="right"><img src="$themeimg/$licenseImg.png" alt=""></td>
-    </tr>
-    <tr>
-      <td class="smaller"><img src="$themeimg/arrow.png" alt="">&nbsp;$langOpenCoursesTeacherConfirm</td>
-      <td align="right"><img src="$themeimg/$teacherConfirmImg.png" alt=""></td>
-    </tr>
-    <tr>
-      <td class="smaller"><img src="$themeimg/arrow.png" alt="">&nbsp;$langOpenCoursesHasDocuments ($numDocs $langDoc)</td>
-      <td align="right"><img src="$themeimg/$docsImg.png" alt=""></td>
-    </tr>
-    <tr>
-      <td class="smaller"><img src="$themeimg/arrow.png" alt="">&nbsp;$langOpenCoursesHasUnits ($numUnits $langSections)</td>
-      <td align="right"><img src="$themeimg/$unitsImg.png" alt=""></td>
-    </tr>
-    <tr>
-      <td class="smaller"><img src="$themeimg/arrow.png" alt="">&nbsp;$langOpenCoursesHasMediaFiles ($numMedia $langOpenCoursesFiles)</td>
-      <td align="right"><img src="$themeimg/$mediaImg.png" alt=""></td>
-    </tr>
-    <tr>
-      <td class="smaller"><img src="$themeimg/arrow.png" alt="">&nbsp;$langOpenCoursesTeacherConfirmVideo</td>
-      <td align="right"><img src="$themeimg/$teacherConfirmVideoImg.png" alt=""></td>
-    </tr>
-    </tbody></table>
-    <br/>
-    <table class="tbl_courseid" width="100%">
-    <tbody><tr>
-      <td class="title1" colspan="2">$langOpenCoursesCharacter</td>
-    </tr>
-    <tr>
-      <td class="smaller"><img src="$themeimg/arrow.png" alt="">&nbsp;$langOpenCoursesType <strong>$langOpenCoursesIsAMinusLevel</strong></td>
-      <td align="right"><input type="checkbox" id="check_AMinus" name="course_confirmAMinusLevel" value="true" $checkedAMinusLevel $disabledAMinusLevel/>
-    </tr>
-    <tr>
-      <td class="smaller"><img src="$themeimg/arrow.png" alt="">&nbsp;$langOpenCoursesType <strong>$langOpenCoursesIsALevel</strong></td>
-      <td align="right"><input type="checkbox" id="check_A" name="course_confirmALevel" value="true" $checkedALevel $disabledALevel/>
-    </tr>
-    <tr>
-      <td class="smaller"><img src="$themeimg/arrow.png" alt="">&nbsp;$langOpenCoursesType <strong>$langOpenCoursesIsAPlusLevel</strong></td>
-      <td align="right"><input type="checkbox" id="check_APlus" name="course_confirmAPlusLevel" value="true" $checkedAPlusLevel $disabledAPlusLevel/>
-    </tr>
-    </tbody></table>
-    <br/>
-    <p class='right'><input class='btn btn-primary' type='submit' name='submit' value='$langSubmit'></p>
+                            <ul class='list-group'>
+                                <li class='list-group-item list-header'>
+                                    <h4>$langOpenCoursesCharacter</h4>
+                                </li>
+                                <li class='list-group-item'>
+                                    <label for='check_AMinus'>$langOpenCoursesType <strong>$langOpenCoursesIsAMinusLevel</strong></label>
+                                    <span class='pull-right'><input type="checkbox" id="check_AMinus" name="course_confirmAMinusLevel" value="true" $checkedAMinusLevel $disabledAMinusLevel/></span>
+                                </li>
+                                <li class='list-group-item'>
+                                    <label for='check_A'>$langOpenCoursesType <strong>$langOpenCoursesIsALevel</strong></label>
+                                    <span class='pull-right'><input type="checkbox" id="check_A" name="course_confirmALevel" value="true" $checkedALevel $disabledALevel/></span>
+                                </li>
+                                <li class='list-group-item' style='border-bottom:none;'>
+                                    <label for='check_APlus'>$langOpenCoursesType <strong>$langOpenCoursesIsAPlusLevel</strong></label>
+                                    <span class='pull-right'><input type="checkbox" id="check_APlus" name="course_confirmAPlusLevel" value="true" $checkedAPlusLevel $disabledAPlusLevel/></span>
+                                </li>
+                                <li class='list-group-item' style='border-top:none;'>
+                                    <input class='btn btn-primary' type='submit' name='submit' value='$langSubmit'>
+                                </li>
+                            </ul>
+                        </form>
+                    </div>
+                </div>
+            </div>
+    
+    
+    
+    
     </form>
-    <br/><br/>
     $lastSubmission
-    <p>&laquo; <a href='{$urlServer}modules/course_info/index.php?course=$course_code'>$langBack</a></p>
-    <p>&laquo; <a href='{$urlServer}courses/$course_code/index.php'>$langBackCourse</a></p>
 EOF;
 
 $head_content .= <<<EOF
