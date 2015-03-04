4b3327c04d252f23a92098128443517dd55099c3
Theodore Schizas <none@none>
2015-03-04 12:40:55 +0200

    extapp minor changes

--- modules/admin/extapp.php
+++ modules/admin/extapp.php
@@ -62,24 +62,26 @@ if ($appName) {
         redirect_to_home_page('modules/admin/extapp.php?edit=' . $appName);
     }
     if ($shouldEdit) {
-        $tool_content .= "\n<div class='form-wrapper'>\n";
-        $tool_content .= "  <form class='form-horizontal' role='form' action='extapp.php?update=" . $appName . "' method='post'>\n";
-        $tool_content .= "    <fieldset>\n";
+        $tool_content .= "\n<div class='row extapp'>\n<div class='col-xs-12'>\n";
+        $tool_content .= "  <div class='form-wrapper'>\n";
+        $tool_content .= "    <form class='form-horizontal' role='form' action='extapp.php?update=" . $appName . "' method='post'>\n";
+        $tool_content .= "      <fieldset>\n";
 
         foreach ($app->getParams() as $param) {
-            $tool_content .= "      <div class='form-group'>\n";
-            $tool_content .= "        <label for='" . $param->name() . "' class='col-sm-2 control-label'>" . $param->display() . "</label>\n";
-            $tool_content .= "        <div class='col-sm-10'><input class='FormData_InputText form-control' type='text' name='" . $param->name() . "' value='" . $param->value() . "'></div>";
-            $tool_content .= "      </div>\n";
+            $tool_content .= "        <div class='form-group'>\n";
+            $tool_content .= "          <label for='" . $param->name() . "' class='col-sm-2 control-label'>" . $param->display() . "</label>\n";
+            $tool_content .= "          <div class='col-sm-10'><input class='FormData_InputText form-control' type='text' name='" . $param->name() . "' value='" . $param->value() . "'></div>";
+            $tool_content .= "        </div>\n";
         }
 
-        $tool_content .= "      <div class='col-sm-offset-2 col-sm-10'><input class='btn btn-primary' type='submit' name='submit' value='$langModify'></div>\n";
-        $tool_content .= "    </fieldset>\n";
-        $tool_content .= "  </form>\n</div>\n";
+        $tool_content .= "        <div class='col-sm-offset-2 col-sm-10'><input class='btn btn-primary' type='submit' name='submit' value='$langModify'></div>\n";
+        $tool_content .= "      </fieldset>\n";
+        $tool_content .= "    </form>\n</div>\n</div>\n";
         $tool_content.=$app->getLongDescription();
     }
 } else {
-    $tool_content .="<table class=\"table-default dataTable no-footer\">\n";
+    $tool_content .= "<div class=\"row extapp\">\n<div class='col-xs-12'>\n";
+    $tool_content .="<table class=\"table-default dataTable no-footer extapp-table\">\n";
     $tool_content.="<thead class='list-header'><td>$langExtAppName</td><td>$langExtAppDescription</td></thead>\n";
     $tool_content.="\n";
     /*foreach (ExtAppManager::getApps() as $app) {
@@ -91,11 +93,12 @@ if ($appName) {
         $tool_content .="</tr>\n";
     }*/
     $tool_content .="<tr>\n";
-    $tool_content .= "<td style=\"width:90px; padding: 20px;\"><a href=\"bbbmoduleconf.php\"><img class=\"img-responsive\" src=\"../../template/icons/bigbluebutton.png\"/>BigBlueButton</a></td>\n";
-    $tool_content .= "<td style=\"padding: 20px;\"><p>Το υποσύστημα <b>Τηλεσυνεργασίας</b> της πλατφόρμας Open eClass υποστηρίζεται λειτουργικά από το ανοικτό λογισμικό <a href=\"http://www.bigbluebutton.org/\" target=\"_blank\">BigBlueButton</a> το οποίο αποτελεί  ένα πλήρες σύστημα άμεσης web-based επικοινωνίας και συνεργασίας εκπαιδευτών και εκπαιδευόμενων.</p><p>Το BigBlueButton ανήκει στις εφαρμογές τηλεδιάσκεψης / σύγχρονης τηλεκπαίδευσης που δεν απαιτούν την εγκατάσταση πρόσθετου υλικού (Software ή Hardware). Η εφαρμογή εκτελείται απευθείας από τον πλοηγό διαδικτύου (Internet Explorer, Firefox, Chrome, Safari, κ.α) με χρήση του Adobe Flash Player. Για τη σύνδεση του Open eClass με μία εγκατεστημένη πλατφόρμα BigBlueButton επιλέξτε <a href=\"bbbmoduleconf.php\">Ρυθμίσεις</a>.</p></td>\n";
+    $tool_content .= "<td style=\"width:90px;\"><a href=\"bbbmoduleconf.php\"><img class=\"img-responsive\" src=\"../../template/icons/bigbluebutton.png\"/>BigBlueButton</a></td>\n";
+    $tool_content .= "<td class=\"text-muted\"><p>Το υποσύστημα <b>Τηλεσυνεργασίας</b> της πλατφόρμας Open eClass υποστηρίζεται λειτουργικά από το ανοικτό λογισμικό <a href=\"http://www.bigbluebutton.org/\" target=\"_blank\">BigBlueButton</a> το οποίο αποτελεί  ένα πλήρες σύστημα άμεσης web-based επικοινωνίας και συνεργασίας εκπαιδευτών και εκπαιδευόμενων.</p><p>Το BigBlueButton ανήκει στις εφαρμογές τηλεδιάσκεψης / σύγχρονης τηλεκπαίδευσης που δεν απαιτούν την εγκατάσταση πρόσθετου υλικού (Software ή Hardware). Η εφαρμογή εκτελείται απευθείας από τον πλοηγό διαδικτύου (Internet Explorer, Firefox, Chrome, Safari, κ.α) με χρήση του Adobe Flash Player. Για τη σύνδεση του Open eClass με μία εγκατεστημένη πλατφόρμα BigBlueButton επιλέξτε <a href=\"bbbmoduleconf.php\">Ρυθμίσεις</a>.</p></td>\n";
     $tool_content .="</tr>\n";
 
     $tool_content.="</table>\n";
+    $tool_content .= "</div>\n</div>\n";
 }
 
 draw($tool_content, 3, null, $head_content);
