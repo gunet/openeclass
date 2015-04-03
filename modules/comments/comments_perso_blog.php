1c935e74306da788e46c3ef95e84f336202eea09
plessas <none@none>
2015-04-03 08:57:08 +0300

    prevent anonymous users from commenting
    --HG--
    branch : personal_blog

--- modules/comments/comments_perso_blog.php
+++ modules/comments/comments_perso_blog.php
@@ -29,50 +29,57 @@ if (get_config('personal_blog_commenting')) {
     $response = array();
     
     if ($_POST['action'] == 'new') {
-        $comment = new Comment();
-        if ($comment->create($_POST['commentText'], $uid, $_POST['rtype'], intval($_POST['rid']))) {
-            $post_actions = '<div class="pull-right">';
-            $post_actions .= '<a href="javascript:void(0)" onclick="xmlhttpPost(\''.$urlServer.'modules/comments/comments_perso_blog.php\', \'editLoad\', '.$_POST['rid'].', \''.$_POST['rtype'].'\', \'\', '.$comment->getId().')">';
-            $post_actions .= icon('fa-edit', $langModify).'</a> ';
-            $post_actions .= '<a href="javascript:void(0)" onclick="xmlhttpPost(\''.$urlServer.'modules/comments/comments_perso_blog.php\', \'delete\', '.$_POST['rid'].', \''.$_POST['rtype'].'\', \''.$langCommentsDelConfirm.'\', '.$comment->getId().')">';
-            $post_actions .= icon('fa-times', $langDelete).'</a>';
-            $post_actions .='</div>';   
-            
-            $response[0] = 'OK';
-            $response[1] = "<div class='alert alert-success'>".$langCommentsSaveSuccess."</div>";
-            $response[2] = $comment->getId();
-            $response[3] = "
-             <div class='row margin-bottom-thin margin-top-thin comment' id='comment-".$comment->getId()."'>
-                <div class='col-xs-12'>
-                    <div class='media'>
-                        <a class='media-left' href='#'>
-                            ". profile_image($comment->getAuthor(), IMAGESIZE_SMALL) ."
-                        </a>
-                        <div class='media-body bubble'>
-                            <div class='label label-success media-heading'>".nice_format($comment->getTime(), true).'</div>'.
-                                "<small>".$langBlogPostUser.display_user($comment->getAuthor(), false, false)."</small>".
-                                $post_actions
-                                ."<div class='margin-top-thin' id='comment_content-".$comment->getId()."'>". q($comment->getContent()) ."</div>
+        if (get_config('personal_blog_commenting') && isset($_SESSION['uid'])) {
+            $comment = new Comment();
+            if ($comment->create($_POST['commentText'], $uid, $_POST['rtype'], intval($_POST['rid']))) {
+                $post_actions = '<div class="pull-right">';
+                $post_actions .= '<a href="javascript:void(0)" onclick="xmlhttpPost(\''.$urlServer.'modules/comments/comments_perso_blog.php\', \'editLoad\', '.$_POST['rid'].', \''.$_POST['rtype'].'\', \'\', '.$comment->getId().')">';
+                $post_actions .= icon('fa-edit', $langModify).'</a> ';
+                $post_actions .= '<a href="javascript:void(0)" onclick="xmlhttpPost(\''.$urlServer.'modules/comments/comments_perso_blog.php\', \'delete\', '.$_POST['rid'].', \''.$_POST['rtype'].'\', \''.$langCommentsDelConfirm.'\', '.$comment->getId().')">';
+                $post_actions .= icon('fa-times', $langDelete).'</a>';
+                $post_actions .='</div>';   
+                
+                $response[0] = 'OK';
+                $response[1] = "<div class='alert alert-success'>".$langCommentsSaveSuccess."</div>";
+                $response[2] = $comment->getId();
+                $response[3] = "
+                 <div class='row margin-bottom-thin margin-top-thin comment' id='comment-".$comment->getId()."'>
+                    <div class='col-xs-12'>
+                        <div class='media'>
+                            <a class='media-left' href='#'>
+                                ". profile_image($comment->getAuthor(), IMAGESIZE_SMALL) ."
+                            </a>
+                            <div class='media-body bubble'>
+                                <div class='label label-success media-heading'>".nice_format($comment->getTime(), true).'</div>'.
+                                    "<small>".$langBlogPostUser.display_user($comment->getAuthor(), false, false)."</small>".
+                                    $post_actions
+                                    ."<div class='margin-top-thin' id='comment_content-".$comment->getId()."'>". q($comment->getContent()) ."</div>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>                    
-            ";
+                </div>                    
+                ";
+            } else {
+                $response[0] = 'ERROR';
+                $response[1] = "<div class='alert alert-warning'>".$langCommentsSaveFail."</div>";
+            }
         } else {
             $response[0] = 'ERROR';
-            $response[1] = "<div class='alert alert-warning'>".$langCommentsSaveFail."</div>";
+            $response[1] = "<div class='alert alert-warning'>".$langCommentsNewNoPerm."</div>";
         }
         echo json_encode($response);
     } else if ($_POST['action'] == 'delete') {
         $comment = new Comment();
         if ($comment->loadFromDB(intval($_POST['cid']))) {
             $permEdit = false;
-            if ($comment->getAuthor() == $uid) {
-                $permEdit = true;
-            }
-            $blog_user = Database::get()->querySingle("SELECT user_id FROM blog_post WHERE id = ?d", $comment->getRid());
-            if ($blog_user->user_id == $uid) {
-                $permEdit = true;
+            if (isset($_SESSION['uid'])) {
+                if ($comment->getAuthor() == $uid) {
+                    $permEdit = true;
+                }
+                $blog_user = Database::get()->querySingle("SELECT user_id FROM blog_post WHERE id = ?d", $comment->getRid());
+                if ($blog_user->user_id == $uid) {
+                    $permEdit = true;
+                }
             }
             if ($permEdit) {
                 if ($comment->delete()) {
@@ -95,12 +102,14 @@ if (get_config('personal_blog_commenting')) {
         $comment = new Comment();
         if ($comment->loadFromDB(intval($_POST['cid']))) {
             $permEdit = false;
-            if ($comment->getAuthor() == $uid) {
-                $permEdit = true;
-            }
-            $blog_user = Database::get()->querySingle("SELECT user_id FROM blog_post WHERE id = ?d", $comment->getRid());
-            if ($blog_user->user_id == $uid) {
-                $permEdit = true;
+            if (isset($_SESSION['uid'])) {
+                if ($comment->getAuthor() == $uid) {
+                    $permEdit = true;
+                }
+                $blog_user = Database::get()->querySingle("SELECT user_id FROM blog_post WHERE id = ?d", $comment->getRid());
+                if ($blog_user->user_id == $uid) {
+                    $permEdit = true;
+                }
             }
             if ($permEdit) {
                 $response[0] = 'OK';
@@ -120,12 +129,14 @@ if (get_config('personal_blog_commenting')) {
         $comment = new Comment();
         if ($comment->loadFromDB(intval($_POST['cid']))) {
             $permEdit = false;
-            if ($comment->getAuthor() == $uid) {
-                $permEdit = true;
-            }
-            $blog_user = Database::get()->querySingle("SELECT user_id FROM blog_post WHERE id = ?d", $comment->getRid());
-            if ($blog_user->user_id == $uid) {
-                $permEdit = true;
+            if (isset($_SESSION['uid'])) {
+                if ($comment->getAuthor() == $uid) {
+                    $permEdit = true;
+                }
+                $blog_user = Database::get()->querySingle("SELECT user_id FROM blog_post WHERE id = ?d", $comment->getRid());
+                if ($blog_user->user_id == $uid) {
+                    $permEdit = true;
+                }
             }
             if ($permEdit) {
                 if ($comment->edit($_POST['commentText'])) {
