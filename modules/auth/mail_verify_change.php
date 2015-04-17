d94a9c6d5f5a6fea16baa26a0077ec94cc8f7608
Yannis Exidaridis <jexi@noc.uoa.gr>
2015-04-17 11:28:11 +0300

    don't ask email verification is autoregister is enabled

--- modules/auth/mail_verify_change.php
+++ modules/auth/mail_verify_change.php
@@ -33,7 +33,7 @@ $require_valid_uid = true;
 $mail_ver_excluded = true;
 include '../../include/baseTheme.php';
 include 'include/sendMail.inc.php';
-$pageName = $langMailVerify;
+$toolName = $langMailVerify;
 
 $uid = (isset($_SESSION['uid']) && !empty($_SESSION['uid'])) ? $_SESSION['uid'] : NULL;
 
@@ -42,15 +42,15 @@ if (empty($uid)) {
     draw($tool_content, 0);
     exit;
 }
-
-// user might already verified mail account or verification is no more needed
+// user might already verified mail account or verification is no more needed (e.g. autoregister is enabled)
 if (!get_config('email_verification_required') or
-        get_mail_ver_status($uid) == EMAIL_VERIFIED) {
-    if (isset($_SESSION['mail_verification_required'])) {
-        unset($_SESSION['mail_verification_required']);
-    }
-    header("Location:" . $urlServer);
-    exit;
+    get_mail_ver_status($uid) == EMAIL_VERIFIED or		
+    get_config('alt_auth_stud_reg') == 2) {
+    	if (isset($_SESSION['mail_verification_required'])) {
+        	unset($_SESSION['mail_verification_required']);
+    	}
+    	header("Location:" . $urlServer);
+    	exit;
 }
 
 if (!empty($_POST['submit'])) {
@@ -84,22 +84,23 @@ if (!empty($_POST['submit'])) {
 }
 
 if (empty($_POST['email']) or !email_seems_valid($_POST['email'])) {
-    $tool_content .= "<br /><br /><form method='post' action='$_SERVER[SCRIPT_NAME]'>
+    $tool_content .= "<div class='form-wrapper'>
+    	<form class='form-horizontal' method='post' role='form' action='$_SERVER[SCRIPT_NAME]'>
         <fieldset>
-                <legend>$langUserData</legend>
-                <table class='tbl' with='100%'>                
-                <tr>
-                        <th class='left'>$lang_email:</th>
-                        <td><input type='text' name='email' size='30' maxlength='40' value='" . q($_SESSION['email']) . "' /></td>
-                        <td><small>($langMailVerificationAddrChange)</small></td>
-                </tr>
-                <tr>
-                        <th class='left'>&nbsp;</th>
-                        <td colspan='2'><input class='btn btn-primary' type='submit' name='submit' value='$langMailVerificationNewCode' /></td>
-                </tr>
-                </table>               
+			<div class='form-group'>
+                <label class='col-sm-2'>$lang_email:</label>
+                <div class='col-sm-10'>
+					<input class='form-control' type='text' name='email' size='30' maxlength='40' value='" . q($_SESSION['email']) . "' placeholder='$langMailVerificationAddrChange'>
+				</div>
+			</div>
+			<div class='form-group'>
+				<div class='col-sm-offset-2 col-sm-10'>
+					<input class='btn btn-primary' type='submit' name='submit' value='$langMailVerificationNewCode'>
+				</div>
+			</div>
         </fieldset>
-    </form>";
+    </form>
+    </div>";
 }
 
 if (isset($_GET['from_profile'])) {
