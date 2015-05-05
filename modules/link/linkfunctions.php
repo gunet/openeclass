9b772324a5b434f874ece18e6a55ba7a77d6b637
Theodore Schizas <none@none>
2015-05-05 10:10:09 +0300

    Link / Questionaire - action_button text change & optimazation

--- modules/link/linkfunctions.php
+++ modules/link/linkfunctions.php
@@ -61,7 +61,7 @@ function showlinksofcategory($catid) {
     global $is_editor, $course_id, $urlview, $tool_content,
     $urlServer, $course_code,
     $langLinkDelconfirm, $langDelete, $langUp, $langDown,
-    $langModify, $is_in_tinymce, $links_num;
+    $langEditChange, $is_in_tinymce, $links_num;
 
     $tool_content .= "<tr>";
     $result = Database::get()->queryArray("SELECT * FROM `link`
@@ -86,7 +86,7 @@ function showlinksofcategory($catid) {
                 $editlink .= "&amp;category=$category";
             }
             $tool_content .= action_button(array(
-                array('title' => $langModify,
+                array('title' => $langEditChange,
                       'icon' => 'fa-edit',
                       'url' => $editlink),
                 array('title' => $langUp,
@@ -136,7 +136,7 @@ function showcategoryadmintools($categoryid) {
 
     $basecaturl = "$_SERVER[SCRIPT_NAME]?course=$course_code&amp;id=$categoryid&amp;urlview=$urlview&amp;";
     $tool_content .= action_button(array(
-                array('title' => $langModify,
+                array('title' => $langEditChange,
                       'icon' => 'fa-edit',
                       'url' => "$basecaturl" . "action=editcategory"),
                 array('title' => $langUp,
