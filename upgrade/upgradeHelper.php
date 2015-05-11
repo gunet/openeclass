3470e9d7c22dc3a4a0c23a01a76e7a476bf16fd1
nikos vavoulas <nikos.va@gmail.com>
2015-05-11 16:20:53 +0300

    commented line removal

--- upgrade/upgradeHelper.php
+++ upgrade/upgradeHelper.php
@@ -1,7 +1,6 @@
 <?php
 
 function getInfoAreas() {
-    //return '<div id="progressbar-outer"><div id="progressbar-inner"></div></div><div id="progressbar-info"></div>';
     return "<div class='progress'>
       <div id='progress-bar' class='progress-bar progress-bar-striped active' role='progressbar' aria-valuenow='45' aria-valuemin='0' aria-valuemax='100' style='width: 100%'>
         <span class='sr-only'>45% Complete</span>
