dba34de92f5c14cc6b3f83416ad2698a4f3e752b
Theodore Schizas <tioschizas@gmail.com>
2015-07-07 14:21:10 +0300

    questionaire - small fix for showing results

--- modules/questionnaire/index.php
+++ modules/questionnaire/index.php
@@ -212,7 +212,7 @@ function printPolls() {
         $tool_content .= "
                     <div class='table-repsonsive'>
 		      <table class='table-default'>
-		      <tr>
+		      <tr class='list-header'>
 			<th><div align='left'>&nbsp;$langTitle</div></th>
 			<th class='text-center'>$langPollStart</th>
 			<th class='text-center'>$langPollEnd</th>";
@@ -345,15 +345,17 @@ function printPolls() {
                         }
                     }
                     $tool_content .= "</td>";
-                     $tool_content .= "
-                        <td class='text-center option-btn-cell'>" .action_button(array(
-                            array(
-                                'title' => $langUsage,
-                                'icon' => 'fa-line-chart',
-                                'url' => "pollresults.php?course=$course_code&pid=$pid",
-                                'show' => $has_participated
-                            )         
-                        ))."</td></tr>";
+                    $line_chart_link = ($has_participated && $thepoll->show_results)? "<a href='pollresults.php?course=$course_code&pid=$pid'><span class='fa fa-line-chart'></span></a>" : "<span class='fa-stack fa-sm'><span class='fa fa-line-chart fa-stack-1x'></span><span class='fa fa-ban fa-stack-2x fa-rotate-90'></span></span>" ;
+                    $tool_content .= "<td class='text-center option-btn-cell'><div style='padding-top:7px;padding-bottom:7px;'>$line_chart_link</div></td></tr>";
+//                    $tool_content .= "
+//                        <td class='text-center option-btn-cell'>" .action_button(array(
+//                            array(
+//                                'title' => $langUsage,
+//                                'icon' => 'fa-line-chart',
+//                                'url' => "pollresults.php?course=$course_code&pid=$pid",
+//                                'show' => $has_participated
+//                            )         
+//                        ))."</td></tr>";
                 }
             }
             $index_aa ++;
