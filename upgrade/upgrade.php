2be2152c6237577470f1b1f15353e1a520a3eab1
Nikos Chatzimpontozis <none@none>
2015-07-15 21:04:19 +0300

    [opencourses] bugfix metadata for non-greek platform setups

--- upgrade/upgrade.php
+++ upgrade/upgrade.php
@@ -2436,6 +2436,10 @@ $mysqlMainDb = ' . quote($mysqlMainDb) . ';
             updateInfo(-1, $langUpgCourse . " " . $row->code . " 3.1.3");
             upgrade_course_3_0_rc2($row->code, $row->id);
         }
+        if (version_compare($oldversion, '3.1.4', '<')) {
+            updateInfo(-1, $langUpgCourse . " " . $row->code . " 3.1.4");
+            upgrade_course_3_0_rc2($row->code, $row->id);
+        }
         $i++;
     }
 
