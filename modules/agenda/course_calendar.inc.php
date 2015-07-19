e5456fff5db4e650d12f2717d0a5747f07339284
Ioannis Stais <istais@di.uoa.gr>
2015-07-19 16:42:28 +0300

    Issue #247

--- modules/agenda/course_calendar.inc.php
+++ modules/agenda/course_calendar.inc.php
@@ -462,6 +462,9 @@ require_once 'include/lib/references.class.php';
        $eventlist = get_course_events("month", $fromdatetime, $todatetime);       
        $events = array();
        foreach($eventlist as $event){
+           $event->title = q($event->title);
+           $event->content = q($event->content);
+           $event->course = q($event->course);
            $startdatetime = new DateTime($event->start);
            $event->start = $startdatetime->getTimestamp()*1000;
            $event->start_hour = $startdatetime->format("H:i");
