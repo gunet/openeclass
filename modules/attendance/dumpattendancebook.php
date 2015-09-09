47a977f062585a56126c88762413d61cb14a4a17
Yannis Exidaridis <jexi@noc.uoa.gr>
2015-09-09 13:52:08 +0300

    fix csv separator while downloading results in windows.

--- modules/attendance/dumpattendancebook.php
+++ modules/attendance/dumpattendancebook.php
@@ -20,25 +20,25 @@
  * ======================================================================== */
 
 $require_current_course = true;
-$require_course_admin = true;
+$require_editor = true;
 
 include '../../include/init.php';
 
 if (isset($_GET['enc']) and $_GET['enc'] == '1253') {
     $charset = 'Windows-1253';
+    $sendSep = true;
 } else {
     $charset = 'UTF-8';
+    $sendSep = false;
 }
 $crlf = "\r\n";
-
-if (!$is_editor) {
-    Session::Messages($langForbidden);
-    redirect_to_home_page('modules/glossary/index.php?course=' . $course_code);
-}
 
 header("Content-Type: text/csv; charset=$charset");
 header("Content-Disposition: attachment; filename=listattendanceusers.csv");
 
+if ($sendSep) {
+    echo 'sep=;', $crlf;
+}
 
 $sql = Database::get()->queryArray("SELECT id, title FROM attendance_activities WHERE attendance_id = ?d", $_GET['attendance_id']);
 foreach ($sql as $act) {
--- modules/gradebook/dumpgradebook.php
+++ modules/gradebook/dumpgradebook.php
@@ -20,24 +20,26 @@
  * ======================================================================== */
 
 $require_current_course = true;
-$require_course_admin = true;
+$require_editor = true;
 
 include '../../include/init.php';
 
 if (isset($_GET['enc']) and $_GET['enc'] == '1253') {
     $charset = 'Windows-1253';
+    $sendSep = true;
 } else {
     $charset = 'UTF-8';
+    $sendSep = false;
 }
 $crlf = "\r\n";
 
-if (!$is_editor) {
-    Session::Messages($langForbidden);
-    redirect_to_home_page();
-}
 
 header("Content-Type: text/csv; charset=$charset");
 header("Content-Disposition: attachment; filename=list_gradebook_users.csv");
+
+if ($sendSep) {
+    echo 'sep=;', $crlf;
+}
 
 $sql = Database::get()->queryArray("SELECT id, title FROM gradebook_activities WHERE gradebook_id = ?d", getDirectReference($_GET['gradebook_id']));
 foreach ($sql as $act) {
