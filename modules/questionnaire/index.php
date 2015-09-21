60e7277d472d69bcef0a7a676c71ea836a5929d6
nikos vavoulas <nikos.va@gmail.com>
2015-09-21 12:26:14 +0300

    small poll cloning fix

--- modules/questionnaire/index.php
+++ modules/questionnaire/index.php
@@ -87,7 +87,7 @@ if ($is_editor) {
                     continue;
                 }
             }
-            if ($ok) {
+            if ($ok || $is_editor) {
                 $poll = Database::get()->querySingle("SELECT * FROM poll WHERE pid = ?d", $pid);
                 $questions = Database::get()->queryArray("SELECT * FROM poll_question WHERE pid = ?d ORDER BY q_position", $pid);
 
