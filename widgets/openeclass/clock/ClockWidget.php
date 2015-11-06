164d54ae2b2a87b278425672501d11eaef23d9ed
nikos vavoulas <nikos.va@gmail.com>
2015-11-06 15:34:51 +0200

    More widget fixes and improvements

--- widgets/openeclass/clock/ClockWidget.php
+++ widgets/openeclass/clock/ClockWidget.php
@@ -32,13 +32,6 @@ class ClockWidget extends Widget implements WidgetInterface {
     public function __construct() {  
         parent::__construct();
         
-        /* Supported languages
-         * [el] => Ελληνικά, [en] => English, [es] => Español, [cs] => Česky, [sq] => Shqip,
-         * [bg] => Български, [ca] => Català, [da] => Dansk, [nl] => Nederlands, [fi] => Suomi,
-         * [fr] => Français [de] => Deutsch [is] => Íslenska [it] => Italiano [jp] => 日本語 [pl] => Polski [ru] => Русский [tr] => Türkçe [sv] => Svenska
-         * 
-         * Fallback language is English
-         */        
         $this->name = array(
             'en' => 'Analog Clock',
             'el' => 'Αναλογικό Ρολόι'     
@@ -51,35 +44,27 @@ class ClockWidget extends Widget implements WidgetInterface {
     
     public static function install()
     {
-        /* START CUSTOM CODE */
-
-        /* END CUSTOM CODE */
         return self::register_widget();
     }
     
     public static function uninstall()
-    {
-        /* START CUSTOM CODE */
-
-        /* END CUSTOM CODE */        
+    {  
         return self::unregister_widget();
     }
     public function run($widget_widget_area_id)
     {
         $this->initialize_widget_data($widget_widget_area_id);
-        /* START CUSTOM CODE */
+
         widget_css_link('clock.css', $this->folder);
         widget_js_link('clock.js', $this->folder);
-        /* END CUSTOM CODE */
+
         return widget_view("run", $this->view_data);
 
     }
     public function getOptionsForm($widget_widget_area_id)
     {
         $this->initialize_widget_data($widget_widget_area_id);
-        //START CUSTOM CODE
 
-        //END CUSTOM CODE
         return widget_view("options", $this->view_data);
     }
 
--- widgets/openeclass/text/TextWidget.php
+++ widgets/openeclass/text/TextWidget.php
@@ -31,14 +31,7 @@ class TextWidget extends Widget implements WidgetInterface {
    
     public function __construct() {  
         parent::__construct();
-        
-        /* Supported languages
-         * [el] => Ελληνικά, [en] => English, [es] => Español, [cs] => Česky, [sq] => Shqip,
-         * [bg] => Български, [ca] => Català, [da] => Dansk, [nl] => Nederlands, [fi] => Suomi,
-         * [fr] => Français [de] => Deutsch [is] => Íslenska [it] => Italiano [jp] => 日本語 [pl] => Polski [ru] => Русский [tr] => Türkçe [sv] => Svenska
-         * 
-         * Fallback language is English
-         */        
+           
         $this->name = array(
             'en' => 'Text / HTML',
             'el' => 'Text / HTML'     
@@ -51,38 +44,33 @@ class TextWidget extends Widget implements WidgetInterface {
     
     public static function install()
     {
-        /* START CUSTOM CODE */
 
-        /* END CUSTOM CODE */
         return self::register_widget();
     }
     
     public static function uninstall()
     {
-        /* START CUSTOM CODE */
-
-        /* END CUSTOM CODE */        
+        
         return self::unregister_widget();
     }
     public function run($widget_widget_area_id)
     {       
         $this->initialize_widget_data($widget_widget_area_id);
-        /* START CUSTOM CODE */
+
         global $language;
         $this->view_data['language'] = $language;
-        /* END CUSTOM CODE */
+
         return widget_view("run", $this->view_data);
     }
     public function getOptionsForm($widget_widget_area_id)
     {
-        $this->initialize_widget_data($widget_widget_area_id);
-        //START CUSTOM CODE
         global $native_language_names_init;
+        $this->initialize_widget_data($widget_widget_area_id);
 
         $this->view_data['widget_widget_area_id'] = $widget_widget_area_id;
         $this->view_data['active_ui_languages'] = explode(' ', get_config('active_ui_languages'));
         $this->view_data['native_language_names_init'] = $native_language_names_init;
-        //END CUSTOM CODE
+
         return widget_view("options", $this->view_data);
     }
 
