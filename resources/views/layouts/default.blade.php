7a63b1796d72ec5e7bfad44b010ccbcd5b8292ad
nikos vavoulas <nikos.va@gmail.com>
2015-11-27 10:09:21 +0200

    small template fix

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -238,7 +238,7 @@
                         </a>
                         <div id="collapse{{ $key }}" class="panel-collapse list-group collapse{{ $tool_group[0]['class'] }}">
                             @foreach ($tool_group[1] as $key2 => $tool)
-                            <a href="{{ $tool_group[2][$key2] }}" class="list-group-item {{ module_path($tool_group[2][$key2]) == $current_module_dir ? " active" : ""}}" {{ is_external_link($tool_group[2][$key2]) or $tool_group[3][$key2] == 'fa-external-link' ? ' target="_blank"' : "" }}>
+                            <a href="{{ $tool_group[2][$key2] }}" class="list-group-item {{ module_path($tool_group[2][$key2]) == $current_module_dir ? " active" : ""}}" {{ is_external_link($tool_group[2][$key2]) || $tool_group[3][$key2] == 'fa-external-link' ? ' target="_blank"' : "" }}>
                                 <span class="fa {{ $tool_group[3][$key2] }} fa-fw"></span>
                                 <span>{!! $tool !!}</span>
                             </a>
