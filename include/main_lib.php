600df246b5edc5a71e574767fa3db70bd90a7537
Alexandros Diamantidis <adia@noc.uoa.gr>
2015-12-04 17:52:33 +0200

    Bump version to 3.2.3
    --HG--
    branch : 3.2

--- include/main_lib.php
+++ include/main_lib.php
@@ -21,7 +21,7 @@
  * Standard header included by all eClass files
  * Defines standard functions and validates variables
  */
-define('ECLASS_VERSION', '3.2.2');
+define('ECLASS_VERSION', '3.2.3');
 
 // better performance while downloading very large files
 define('PCLZIP_TEMPORARY_FILE_RATIO', 0.2);
