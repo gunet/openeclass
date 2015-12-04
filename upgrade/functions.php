99e823f6ebeebbaea999ba0884a4d9e53245bc2c
nikos vavoulas <nikos.va@gmail.com>
2015-12-04 14:12:55 +0200

    upgrade fix for anonymized polls 2.x to 3.x
    --HG--
    branch : 3.2

--- upgrade/functions.php
+++ upgrade/functions.php
@@ -792,9 +792,9 @@ function upgrade_course_3_0($code, $course_id) {
                      FROM poll AS old ORDER by pid");
 
         $ok = (Database::get()->query("INSERT INTO `$mysqlMainDb`.poll
-                         (`pid`, `course_id`, `creator_id`, `name`, `creation_date`, `start_date`, `end_date`, `active`)
+                         (`pid`, `course_id`, `creator_id`, `name`, `creation_date`, `start_date`, `end_date`, `active`, `anonymized`)
                          SELECT `pid` + $pollid_offset, $course_id, `creator_id`, `name`, `creation_date`, `start_date`,
-                                `end_date`, `active`
+                                `end_date`, `active`, `anonymized`
                            FROM poll ORDER BY pid") != null);
 
         // ----- poll_question DB Table ----- //
