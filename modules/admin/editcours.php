b61be46086c41ecf08eab52aa29912d1ccf52a67
Alexandros Diamantidis <adia@noc.uoa.gr>
2016-01-04 13:45:55 +0200

    HTML formatting

--- modules/admin/editcours.php
+++ modules/admin/editcours.php
@@ -157,11 +157,11 @@ if (isset($c)) {
     // Users list
     $tool_content .= "
 	<tr>
-	  <td><a href='listusers.php?c=" . $cId . "'>" . $langListUsersActions . "</a></td>
+	  <td><a href='listusers.php?c=$cId'>$langListUsersActions</a></td>
 	</tr>";    
     // Backup course
     $tool_content .= "<tr>
-	  <td><a href='../course_info/archive_course.php?c=" . q($c). "&" .generate_csrf_token_link_parameter() . "'>" . $langTakeBackup . "</a></td>
+	  <td><a href='../course_info/archive_course.php?c=" . q($c). '&amp;' .generate_csrf_token_link_parameter() . "'>" . $langTakeBackup . "</a></td>
 	</tr>";
     // Course metadata 
     if (get_config('course_metadata')) {
