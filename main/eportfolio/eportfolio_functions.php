6bb0533b737750978d50f98835c22575355fb113
plessas <none@none>
2016-02-02 19:30:01 +0200

    add more eportfolio fields functions
    --HG--
    branch : eportfolio

--- main/eportfolio/eportfolio_functions.php
+++ main/eportfolio/eportfolio_functions.php
@@ -27,8 +27,8 @@ define('EPF_LINK', 5);
 
 
 /**
- * Render custom profile fields content when viewing profile
- * @param array $context
+ * Render e-portfolio fields content when viewing e-portfolio
+ * @param $uid
  * @return string
  */
 function render_eportfolio_fields_content($uid) {
@@ -91,4 +91,167 @@ function render_eportfolio_fields_content($uid) {
 
     }
     return $return_str;
+}
+
+/**
+ * Render e-portfolio fields in e-portfolio form
+ * @return string
+ */
+function render_eportfolio_fields_form() {
+    global $uid, $langOptional, $langCompulsory;
+
+    $return_string = "";
+
+    $result = Database::get()->queryArray("SELECT id, name FROM eportfolio_fields_category ORDER BY sortorder DESC");
+
+    foreach ($result as $c) {
+
+        $res = Database::get()->queryArray("SELECT id, name, shortname, description, required, datatype, data
+                                            FROM eportfolio_fields WHERE categoryid = ?d ORDER BY sortorder DESC", $c->id);
+
+        if (count($res) > 0) {
+            foreach ($res as $f) {
+
+                if (isset($fdata)) {
+                    unset($fdata);
+                }
+
+                if (Session::hasError('epf_'.$f->shortname)) {
+                    $form_class = 'form-group has-error';
+                    $help_block = '<span class="help-block">' . Session::getError('epf_'.$f->shortname) . '</span>';
+                } else {
+                    $form_class = 'form-group';
+                    $help_block = '';
+                }
+                
+                $return_string .= '<div class="'.$form_class.'">';
+                $return_string .= '<label class="col-sm-2 control-label" for="'.$f->shortname.'">'.q($f->name).'</label>';
+                $return_string .= '<div class="col-sm-10">';
+
+                //get data to prefill fields
+                $data_res = Database::get()->querySingle("SELECT data FROM eportfolio_fields_data
+                                                      WHERE field_id = ?d AND user_id = ?d", $f->id, $uid);
+                if ($data_res) {
+                    $fdata = $data_res->data;
+                }
+
+                if (Session::has('epf_'.$f->shortname)) {
+                    $fdata = Session::get('epf_'.$f->shortname);
+                }
+
+                $val = '';
+                $placeholder = '';
+
+                switch ($f->datatype) {
+                    case EPF_TEXTBOX:
+                        if (isset($fdata) && $fdata != '') {
+                            $val = 'value="'.q($fdata).'"';
+                        } elseif (isset($_REQUEST['epf_'.$f->shortname]) && isset($_REQUEST['epf_'.$f->shortname]) != '') {
+                            $val = 'value="'.q($_REQUEST['epf_'.$f->shortname]).'"';
+                        }
+                        if ($f->required == 0) {
+                            $placeholder = 'placeholder="'.$langOptional.'"';
+                        } else {
+                            $placeholder = 'placeholder="'.$langCompulsory.'"';
+                        }
+                        $return_string .= '<input class="form-control" '.$val.' type="text" '.$placeholder.' name="epf_'.$f->shortname.'">';
+                        break;
+                    case EPF_TEXTAREA:
+                        if (isset($fdata) && $fdata != '') {
+                            $val = $fdata;
+                        } elseif (isset($_REQUEST['epf_'.$f->shortname]) && isset($_REQUEST['epf_'.$f->shortname]) != '') {
+                            $val = $_REQUEST['epf_'.$f->shortname];
+                        }
+                        $return_string .= rich_text_editor('epf_'.$f->shortname, 8, 20, $val);
+                        $req_label = $langCompulsory;
+                        break;
+                    case EPF_DATE:
+                        if (isset($fdata) && $fdata != '') {
+                            $val = 'value="'.q($fdata).'"';
+                        } elseif (isset($_REQUEST['epf_'.$f->shortname]) && isset($_REQUEST['epf_'.$f->shortname]) != '') {
+                            $val = 'value="'.q($_REQUEST['epf_'.$f->shortname]).'"';
+                        }
+                        if ($f->required == 0) {
+                            $placeholder = 'placeholder="'.$langOptional.'"';
+                        } else {
+                            $placeholder = 'placeholder="'.$langCompulsory.'"';
+                        }
+                        load_js('bootstrap-datepicker');
+                        $return_string .= '<input class="form-control" '.$val.' type="text" '.$placeholder.' name="epf_'.$f->shortname.'" data-provide="datepicker" data-date-format="dd-mm-yyyy">';
+                        break;
+                    case EPF_MENU:
+                        if (isset($fdata) && $fdata != '') {
+                            $def_selection = intval($fdata);
+                        } elseif (isset($_REQUEST['epf_'.$f->shortname]) && isset($_REQUEST['epf_'.$f->shortname]) != '') {
+                            $def_selection = intval($_REQUEST['epf_'.$f->shortname]);
+                        } else {
+                            $def_selection = 0;
+                        }
+                        $options = unserialize($f->data);
+                        $options = array_combine(range(1, count($options)), array_values($options));
+                        $options[0] = "";
+                        ksort($options);
+                        $return_string .= selection($options, 'epf_'.$f->shortname, $def_selection);
+                        $req_label = $langCompulsory;
+                        break;
+                    case EPF_LINK:
+                        if (isset($fdata) && $fdata != '') {
+                            $val = 'value="'.q($fdata).'"';
+                        } elseif (isset($_REQUEST['epf_'.$f->shortname]) && isset($_REQUEST['epf_'.$f->shortname]) != '') {
+                            $val = 'value="'.q($_REQUEST['epf_'.$f->shortname]).'"';
+                        }
+                        if ($f->required == 0) {
+                            $placeholder = 'placeholder="'.$langOptional.'"';
+                        } else {
+                            $placeholder = 'placeholder="'.$langCompulsory.'"';
+                        }
+                        $return_string .= '<input class="form-control" '.$val.' type="text" '.$placeholder.' name="epf_'.$f->shortname.'">';
+                        break;
+                }
+                if (!empty($f->description)) {
+                    $return_string .= '<small><em>'.standard_text_escape($f->description);
+                    if (isset($req_label)) {
+                        $return_string .= $req_label;
+                    }
+                    $return_string .= '</em></small>';
+                } elseif (isset($req_label)) {
+                    $return_string .= '<small><em>'.$req_label.'</em></small>';
+                }
+                $return_string .= $help_block.'</div></div>';
+                unset($req_label);
+            }
+        }
+    }
+    return $return_string;
+}
+
+/**
+ * Process e-portfolio fields values after submit
+ * @return boolean $updated
+ */
+function process_eportfolio_fields_data() {
+    global $uid;
+    
+    $updated = false;
+    
+    foreach ($_POST as $key => $value) {
+        if (substr($key, 0, 4) == 'epf_') { //e-portfolio fields input names start with epf_
+            $field_name = substr($key, 4);
+            $result = Database::get()->querySingle("SELECT id, required FROM eportfolio_fields WHERE shortname = ?s", $field_name);
+            $field_id = $result->id;
+            $required = $result->required;
+            //delete old values if exist
+            if ($required == 1 && empty($value)) {
+                continue;
+            } else {
+                Database::get()->query("DELETE FROM eportfolio_fields_data WHERE field_id = ?d AND user_id = ?d", $field_id, $uid);
+            }
+            
+            if (!empty($value)) {
+                Database::get()->query("INSERT INTO eportfolio_fields_data (user_id, field_id, data) VALUES (?d,?d,?s)", $uid, $field_id, $value);
+            }
+            $updated = true;
+        }
+    }
+    return $updated;
 }
