52e71c10dfacd7d5ff70f9d83b7a9370ef7e559b
nikos vavoulas <nikos.va@gmail.com>
2016-02-19 14:33:11 +0200

    Converted admin tools index page to blade

--- modules/admin/index.php
+++ modules/admin/index.php
@@ -28,246 +28,49 @@ $pageName = $langAdmin;
 define('HIDE_TOOL_TITLE', 1);
 
 // Construct a table with platform identification info
-$tool_content .= action_bar(array(
-        array('title' => $langBack,
-            'url' => "${urlServer}main/portfolio.php",
-            'icon' => 'fa-reply',
-            'level' => 'primary-label')),false);
-$tool_content .= "
-    <div class='row'>
-        <div class='col-md-12'>
-        <div class='panel'><div class='panel-body'>
-            $langOnlineUsers : <b>" . getOnlineUsers() . "</b>
-                </div></div>
-        </div>
-    </div>
-    <div class='row'>
-        <div class='col-md-12'>
-            <div class='panel panel-default'>
-                <div class='panel-heading'>
-                    <div class='panel-title h3'>$langPlatformIdentity</div>
-                </div>                
-                <div class='panel-body'>                
-                    <div class='row margin-bottom-thin'>
-                    <div class='col-sm-3'>
-                        " . icon('fa-check') . " <strong>$langWebServerVersion</strong> 
-                    </div>
-                    <div class='col-sm-9'>
-                        <em>" . q($_SERVER['SERVER_SOFTWARE']) . "</em>
-                    </div>
-                </div>
-                <div class='row margin-bottom-thin'>
-                <div class='col-sm-3'>";
-                if (version_compare(PHP_VERSION, '5.4.0') >= 0) {
-                    $info_icon = icon('fa-check');
-                    $info_text = '';
-                } else {
-                    $info_icon = icon('fa-ban');
-                    $info_text = "<div class='row margin-bottom-thin'>
-                                    <div class='col-sm-12'>
-                                        <div class='alert alert-danger'>$langWarnAboutPHP</div>
-                                    </div>
-                                  </div>";
-                }
-                $tool_content .= "$info_icon <strong>$langPHPVersion</strong></div>";                
-                $tool_content .= "<div class='col-sm-9'>";
-                $tool_content .= "<em>" . PHP_VERSION . "</em>";                
-                $tool_content .= "</div></div>";
-                $tool_content .= $info_text;
-                $tool_content .= "<div class='row margin-bottom-thin'>
-                    <div class='col-sm-3'>" . icon('fa-check') . "
-                        <strong>$langMySqlVersion</strong>
-                    </div>
-                    <div class='col-sm-9'>
-                        <em>" . Database::get()->attributes()->serverVersion() . "</em>
-                    </div>
-                </div>
-                <div class='row margin-bottom-thin'>
-                    <div class='col-sm-3'>
-                        <strong>$langVersion:</strong>
-                    </div>
-                    <div class='col-sm-9'>
-                        <em>$siteName " . ECLASS_VERSION . "</em>
-                    </div>
-                </div>
-            </div>
-        </div>";
+$data['action_bar'] = action_bar(array(
+    array('title' => $langBack,
+        'url' => "${urlServer}main/portfolio.php",
+        'icon' => 'fa-reply',
+        'level' => 'primary-label')),false);
 
+$data['validPHP'] = version_compare(PHP_VERSION, '5.4.0') >= 0;
+$data['serverVersion'] = Database::get()->attributes()->serverVersion();
+$data['siteName'] = $siteName;
 
 // Count prof requests with status = 1
-$count_prof_requests = Database::get()->querySingle("SELECT COUNT(*) AS cnt FROM user_request WHERE state = 1 AND status = 1")->cnt;
-if ($count_prof_requests > 0) {
-    $prof_request_msg = "$langThereAre $count_prof_requests $langOpenRequests";
-} else {
-    $prof_request_msg = $langNoOpenRequests;
-}
-
+$data['count_prof_requests'] = Database::get()->querySingle("SELECT COUNT(*) AS cnt FROM user_request WHERE state = 1 AND status = 1")->cnt;
 // Find last course created
-$myrow = Database::get()->querySingle("SELECT code, title, prof_names FROM course ORDER BY id DESC LIMIT 0, 1");
-if ($myrow) {
-    $last_course_info = "<b>" . q($myrow->title) . "</b> " . q($myrow->code) . ", " . q($myrow->prof_names) . ")";
-} else {
-    $last_course_info = $langNoCourses;
-}
-
+$data['lastCreatedCourse'] = Database::get()->querySingle("SELECT code, title, prof_names FROM course ORDER BY id DESC LIMIT 0, 1");
 // Find last prof registration
-$myrow = Database::get()->querySingle("SELECT givenname, surname, username, registered_at FROM user WHERE status = 1 ORDER BY id DESC LIMIT 0,1");
-$last_prof_info = "<b>" . q($myrow->givenname) . " " . q($myrow->surname) . "</b> (" . q($myrow->username) . ", " . date("j/n/Y H:i", strtotime($myrow->registered_at)) . ")";
-
+$data['lastProfReg'] = Database::get()->querySingle("SELECT givenname, surname, username, registered_at FROM user WHERE status = 1 ORDER BY id DESC LIMIT 0,1");
 // Find last stud registration
-$myrow = Database::get()->querySingle("SELECT givenname, surname, username, registered_at FROM user WHERE status = 5 ORDER BY id DESC LIMIT 0,1");
-if ($myrow) {
-    $last_stud_info = "<b>" . q($myrow->givenname) . " " . q($myrow->surname) . "</b> (" . q($myrow->username) . ", " . date("j/n/Y H:i", strtotime($myrow->registered_at)) . ")";
-} else {
-    // no student is yet registered
-    $last_stud_info = $langLastStudNone;
-}
-
+$data['lastStudReg'] = Database::get()->querySingle("SELECT givenname, surname, username, registered_at FROM user WHERE status = 5 ORDER BY id DESC LIMIT 0,1");
 // Find admin's last login
 $lastadminloginres = Database::get()->querySingle("SELECT `when` FROM loginout WHERE id_user = ?d AND action = 'LOGIN' ORDER BY `when` DESC LIMIT 1,1", $uid);
-$lastregisteredprofs = 0;
-$lastregisteredstuds = 0;
+$data['lastregisteredprofs'] = 0;
+$data['lastregisteredstuds'] = 0;
 if ($lastadminloginres && $lastadminloginres->when) {
     $lastadminlogin = $lastadminloginres->when;
     // Count profs registered after last login
-    $lastregisteredprofs = Database::get()->querySingle("SELECT COUNT(*) AS cnt FROM user WHERE status = 1 AND registered_at > ?t", $lastadminlogin)->cnt;
+    $data['lastregisteredprofs'] = Database::get()->querySingle("SELECT COUNT(*) AS cnt FROM user WHERE status = 1 AND registered_at > ?t", $lastadminlogin)->cnt;
     // Count studs registered after last login
-    $lastregisteredstuds = Database::get()->querySingle("SELECT COUNT(*) AS cnt FROM user WHERE status = 5 AND registered_at > ?t", $lastadminlogin)->cnt;
+    $data['lastregisteredstuds'] = Database::get()->querySingle("SELECT COUNT(*) AS cnt FROM user WHERE status = 5 AND registered_at > ?t", $lastadminlogin)->cnt;
 }
-
-
-$tool_content .= "
-    <div class='panel panel-default'>
-        <div class='panel-heading'>
-            <div class='panel-title h3'>$langInfoAdmin</div>
-        </div>
-        <div class='panel-body'>
-            <div class='row margin-bottom-thin'>
-                <div class='col-sm-4'>
-                <strong>$langOpenRequests:</strong>
-                </div>
-                <div class='col-sm-8'>
-                    $prof_request_msg
-                </div>                        
-            </div>
-            <div class='row margin-bottom-thin'>
-                <div class='col-sm-4'>
-                <strong>$langLastLesson</strong>
-                </div>
-                <div class='col-sm-8'>
-                    $last_course_info
-                </div>                        
-            </div>
-            <div class='row margin-bottom-thin'>
-                <div class='col-sm-4'>
-                <strong>$langLastProf</strong>
-                </div>
-                <div class='col-sm-8'>
-                    $last_prof_info
-                </div>                        
-            </div>
-            <div class='row margin-bottom-thin'>
-                <div class='col-sm-4'>
-                <strong>$langLastStud</strong>
-                </div>
-                <div class='col-sm-8'>
-                    $last_stud_info
-                </div>                        
-            </div>
-            <div class='row margin-bottom-thin'>
-                <div class='col-sm-4'>
-                <strong>$langAfterLastLoginInfo</strong>
-                </div>
-                <div class='col-sm-8'>
-                $langAfterLastLogin
-                    <ul class='custom_list'>
-                      <li><b>" . $lastregisteredprofs . "</b> $langTeachers</li>
-                      <li><b>" . $lastregisteredstuds . "</b> $langStudents </li>
-                    </ul>
-                </div>                        
-            </div>                 
-        </div>
-    </div>";
-
-
 // INDEX RELATED
 if (get_config('enable_indexing')) {
     require_once 'modules/search/indexer.class.php';
     $idx = new Indexer();
 
-    $numDocs = $idx->getIndex()->numDocs();
-    $isOpt = (!$idx->getIndex()->hasDeletions()) ? $m['yes'] : $m['no'];
-
+    $data['numDocs'] = $idx->getIndex()->numDocs();
+    $data['isOpt'] = (!$idx->getIndex()->hasDeletions()) ? $m['yes'] : $m['no'];
+    $data['idxHasDeletions'] = $idx->getIndex()->hasDeletions();
     
-    $tool_content .= "
-    <div class='panel panel-default'>
-        <div class='panel-heading'>
-            <div class='panel-title h3'>$langIndexInfo</div>
-        </div>
-        <div class='panel-body'>
-            <div class='row margin-bottom-thin'>
-                <div class='col-sm-3'>
-                <strong>$langIndexNumDocs:</strong>
-                </div>
-                <div class='col-sm-9'>
-                    $numDocs
-                </div>                        
-            </div>
-            <div class='row margin-bottom-thin'>
-                <div class='col-sm-3'>
-                <strong>$langIndexIsOptimized</strong>
-                </div>
-                <div class='col-sm-9'>
-                    $isOpt
-                </div>
-            </div>";
-if ($idx->getIndex()->hasDeletions()) {   
-    $tool_content .= "        
-            <div class='row margin-bottom-thin'>
-                <div class='col-sm-9 col-sm-offset-3'>
-                    <a href='../search/optpopup.php' onclick=\"return optpopup('../search/optpopup.php', 600, 500)\">$langOptimize</a>
-                </div>            
-            </div>";
-}
-$tool_content .= "
-            <div class='row margin-bottom-thin'>
-                <div class='col-sm-9 col-sm-offset-3'>
-                    <a id='reindex_link' href='../search/idxpopup.php?reindex'>$langReindex</a>
-                </div>            
-            </div>    
-        </div>
-    </div>";
-    $tool_content .= modalConfirmation('confirmReindexDialog', 'confirmReindexLabel', $langConfirmEnableIndexTitle, $langConfirmEnableIndex, 'confirmReindexCancel', 'confirmReindexOk');
-}
 
-// CRON RELATED
-$tool_content .= "<img src='cron.php' width='2' height='1' alt=''/>";
-$res = Database::get()->queryArray("SELECT name, last_run FROM cron_params");
-if (count($res) >= 1) {
-    $tool_content .= "
-    <div class='panel panel-default'>
-        <div class='panel-heading'>
-            <div class='panel-title h3'>$langCronInfo</div>
-        </div>
-        <div class='panel-body'>
-            <div class='row margin-bottom-thin'>
-                <div class='col-sm-3'>
-                <strong>$langCronName</strong>
-                </div>
-                <div class='col-sm-9'>
-                    $langCronLastRun
-                    <div class='row'>";
-                
-foreach ($res as $row) {
-    $tool_content .= "<div class='col-xs-6'>" . $row->name . "</div><div class='col-xs-6'>" . $row->last_run . "</div>";
-}    
-$tool_content .= "</div>
-                </div>
-            </div>        
-        </div>
-    </div>";
+    $data['idxModal'] = modalConfirmation('confirmReindexDialog', 'confirmReindexLabel', $langConfirmEnableIndexTitle, $langConfirmEnableIndex, 'confirmReindexCancel', 'confirmReindexOk');
 }
-$tool_content .= "</div></div>";
+// CRON RELATED
+$data['cronParams'] = Database::get()->queryArray("SELECT name, last_run FROM cron_params");
 
 $head_content = <<<EOF
 <script type='text/javascript'>
@@ -335,4 +138,6 @@ $(document).ready(function() {
 /* ]]> */
 </script>
 EOF;
-draw($tool_content, 3, null, $head_content);
+
+$data['menuTypeID'] = 3;
+view('admin.index', $data);
