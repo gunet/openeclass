1e3e7db13adbd45f5feaec7e8d2f534d5f3bb2f3
Alexandros Diamantidis <adia@noc.uoa.gr>
2016-03-09 13:58:02 +0200

    Create course: reinstate course access labels

--- modules/create_course/create_course.php
+++ modules/create_course/create_course.php
@@ -217,7 +217,6 @@ if ($allow_only_defaults) {
 $departments = isset($_POST['department']) ? arrayValuesDirect($_POST['department']) : array();
 $deps_valid = true;
 
-
 foreach ($departments as $dep) {
     if ($allow_only_defaults && !in_array($dep, $allowables)) {
         $deps_valid = false;
@@ -349,29 +348,29 @@ if (!isset($_POST['create_course'])) {
                 <div class='col-sm-10'>
                     <div class='radio'>
                       <label>
-                        <input id='courseopen' type='radio' name='formvisible' value='2' checked>
-                        <img src='$themeimg/lock_open.png' alt='$langOpenCourse' title='$langOpenCourse' width='16'>&nbsp;$langOpenCourse
+                        <input id='courseopen' type='radio' name='formvisible' value='2' checked>".
+                        $course_access_icons[COURSE_OPEN]." $langOpenCourse
                         <span class='help-block'><small>$langPublic</small></span>
                       </label>
                     </div>
                     <div class='radio'>
                       <label>
-                        <input id='coursewithregistration' type='radio' name='formvisible' value='1'>
-                        <img src='$themeimg/lock_registration.png' alt='$m[legrestricted]' title='$m[legrestricted]' width='16'>&nbsp;$m[legrestricted]
+                        <input id='coursewithregistration' type='radio' name='formvisible' value='1'>".
+                        $course_access_icons[COURSE_REGISTRATION]." $m[legrestricted]
                         <span class='help-block'><small>$langPrivOpen</small></span>
                       </label>
                     </div>
                     <div class='radio'>
                       <label>
-                        <input id='courseclose' type='radio' name='formvisible' value='0'>
-                        <img src='$themeimg/lock_closed.png' alt='$langClosedCourse' title='$langClosedCourse' width='16'>&nbsp;$langClosedCourse
+                        <input id='courseclose' type='radio' name='formvisible' value='0'>".
+                        $course_access_icons[COURSE_CLOSED]." $langClosedCourse
                         <span class='help-block'><small>$langClosedCourseShort</small></span>
                       </label>
                     </div>
                     <div class='radio'>
                       <label>
-                        <input id='courseinactive' type='radio' name='formvisible' value='3'>
-                        <img src='$themeimg/lock_inactive.png' alt='$langInactiveCourse' title='$langInactiveCourse' width='16'>&nbsp;$langInactiveCourse
+                        <input id='courseinactive' type='radio' name='formvisible' value='3'>".
+                        $course_access_icons[COURSE_INACTIVE]." $langInactiveCourse
                         <span class='help-block'><small>$langCourseInactiveShort</small></span>
                       </label>
                     </div>                   
