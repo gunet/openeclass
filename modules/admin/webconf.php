181fb82ee61dcc9373ac52a26532ae8eeb522b2f
nikos vavoulas <nikos.va@gmail.com>
2016-04-22 12:25:53 +0300

    Blading "web conf" configuration admin tool

--- modules/admin/webconf.php
+++ modules/admin/webconf.php
@@ -34,48 +34,12 @@ load_js('validation.js');
 
 $available_themes = active_subdirs("$webDir/template", 'theme.html');
 
-$wc_server = isset($_GET['edit_server']) ?  $_GET['edit_server'] : '';
-
-if (isset($_GET['add_server'])) {
-    $pageName = $$langAddWebConfServer;
-    $toolName = $langWebConf;
-    $navigation[] = array('url' => 'webconf.php', 'name' => $langWebConf);
-    $tool_content .= action_bar(array(
-        array('title' => $langBack,
-            'url' => "webconf.php",
-            'icon' => 'fa-reply',
-            'level' => 'primary-label')));
-    $tool_content .= "<div class='form-wrapper'>";
-    $tool_content .= "<form class='form-horizontal' role='form' name='serverForm' action='$_SERVER[SCRIPT_NAME]' method='post'>";
-    $tool_content .= "<fieldset>";
-    $tool_content .= "<div class='form-group'>";
-    $tool_content .= "<label for='host' class='col-sm-3 control-label'>$langWebConfServer:</label>
-                    <div class='col-sm-9'><input class='form-control' id='host' type='text' name='hostname_form'></div>";
-    $tool_content .= "</div>";
-    $tool_content .= "<div class='form-group'>";
-    $tool_content .= "<label class='col-sm-3 control-label'>$langActivate:</label>
-            <div class='col-sm-9 radio'><label><input  type='radio' id='enabled_false' name='enabled' checked='false' value='false'>$langNo</label></div>
-            <div class='col-sm-offset-3 col-sm-9 radio'><label><input  type='radio' id='enabled_true' name='enabled' checked='true' value='true'>$langYes</label></div>
-        </div>";
-    $tool_content .= "<div class='form-group'><div class='col-sm-offset-3 col-sm-9'><input class='btn btn-primary' type='submit' name='submit' value='$langAddModify'></div></div>";
-    $tool_content .= "</fieldset></form></div>";
-
-    $tool_content .='<script language="javaScript" type="text/javascript">
-        //<![CDATA[
-            var chkValidator  = new Validator("serverForm");
-            chkValidator.addValidation("hostname_form","req","' . $langWebConfServerAlertHostname . '");
-        //]]></script>';
-
-} else if (isset($_GET['delete_server'])) {
+if (isset($_GET['delete_server'])) {
     $id = $_GET['delete_server'];
     Database::get()->querySingle("DELETE FROM wc_servers WHERE id=?d", $id);
-    // Display result message
-    $tool_content .= "<div class='alert alert-success'>$langFileUpdatedSuccess</div>";    
-    $tool_content .= action_bar(array(
-        array('title' => $langBack,
-            'url' => "webconf.php",
-            'icon' => 'fa-reply',
-            'level' => 'primary-label')));
+    // Display result message   
+    Session::Messages($langFileUpdatedSuccess, 'alert-success');
+    redirect_to_home_page('modules/admin/webconf.php');
 }
 // Save new config.php
 else if (isset($_POST['submit'])) {
@@ -92,101 +56,52 @@ else if (isset($_POST['submit'])) {
         (?s,?s)", $hostname, $enabled);
     }    
     // Display result message
-    $tool_content .= "<div class='alert alert-success'>$langFileUpdatedSuccess</div>";
-    // Display link to go back to index.php
-    $tool_content .= action_bar(array(
-        array('title' => $langBack,
-            'url' => "webconf.php",
-            'icon' => 'fa-reply',
-            'level' => 'primary-label')));
+    Session::Messages($langFileUpdatedSuccess, 'alert-success');
+    redirect_to_home_page('modules/admin/webconf.php');
 } // end of if($submit)
-// Display config.php edit form
-else {    
-    if (isset($_GET['edit_server'])) {
-        $pageName = $langEdit;
-        $tool_content .= action_bar(array(
-        array('title' => $langBack,
-            'url' => "webconf.php",
-            'icon' => 'fa-reply',
-            'level' => 'primary-label')));
-        
-        $server = Database::get()->querySingle("SELECT * FROM wc_servers WHERE id = ?d", $wc_server);
-        
-        $tool_content .= "<div class='form-wrapper'>";
-        $tool_content .= "<form class='form-horizontal' role='form' name='serverForm' action='$_SERVER[SCRIPT_NAME]' method='post'>";
-        $tool_content .= "<fieldset>";        
-        $tool_content .= "<div class='form-group'>";
-        $tool_content .= "<label for='host' class='col-sm-3 control-label'>$langWebConfServer:</label>
-                        <div class='col-sm-9'><input class='form-control' id='host' type='text' name='hostname_form' value='$server->hostname'></div>";
-        $tool_content .= "</div>";
-        $tool_content .= "<label class='col-sm-3 control-label'>$langActivate:</label>";
-        if ($server->enabled == "false") {
-            $checkedfalse2 = " checked='false' ";
-        } else $checkedfalse2 = '';
-        
-        $tool_content .= "<div class='col-sm-9 radio'><label><input  type='radio' id='enabled_false' name='enabled' $checkedfalse2 value='false'>$langNo</label></div>";
-        
-        if ($server->enabled == "true") {
-            $checkedtrue2 = " checked='false' ";
-        } else $checkedtrue2 = '';
-        
-        $tool_content .= "<div class='col-sm-offset-3 col-sm-9 radio'><label><input  type='radio' id='enabled_true' name='enabled' $checkedtrue2 value='true'>$langYes</label></div>";
-        $tool_content .= "<input class='form-control' type = 'hidden' name = 'id_form' value='$wc_server'>";
-        $tool_content .= "<div class='form-group'><div class='col-sm-offset-3 col-sm-9'><input class='btn btn-primary' type='submit' name='submit' value='$langAddModify'></div></div>";
-        $tool_content .= "</fieldset></form></div>";
 
-        $tool_content .='<script language="javaScript" type="text/javascript">
-        //<![CDATA[
-            var chkValidator  = new Validator("serverForm");
-            chkValidator.addValidation("hostname_form","req","' . $langWebConfServerAlertHostname . '");
-        //]]></script>';
-        
-    } else {
-        //display available WebConf servers
-        $tool_content .= action_bar(array(
-            array('title' => $langAddWebConfServer,
-                'url' => "webconf.php?add_server",
-                'icon' => 'fa-plus-circle',
-                'level' => 'primary-label',
-                'button-class' => 'btn-success'),
-            array('title' => $langBack,
-                'url' => "extapp.php",
+if (isset($_GET['add_server']) || isset($_GET['edit_server'])) {
+    $pageName = isset($_GET['add_server']) ? $langAddWebConfServer : $langEdit;
+    $toolName = $langWebConf;
+    $navigation[] = array('url' => 'webconf.php', 'name' => $langWebConf);
+    $data['action_bar'] = action_bar([
+            [
+                'title' => $langBack,
+                'url' => "webconf.php",
                 'icon' => 'fa-reply',
-                'level' => 'primary-label')));
-
-        $q = Database::get()->queryArray("SELECT * FROM wc_servers");
-        if (count($q)>0) {
-            $tool_content .= "<div class='table-responsive'>";
-            $tool_content .= "<table class='table-default'>
-                <thead>
-                <tr><th class = 'text-center'>$langWebConfServer</th>
-                    <th class = 'text-center'>$langBBBEnabled</th>
-                    <th class = 'text-center'>".icon('fa-gears')."</th></tr>
-                </thead>";
-            foreach ($q as $srv) {
-                $enabled_bbb_server = ($srv->enabled=='true')? $langYes : $langNo;
-
-                $tool_content .= "<tr>";
-                $tool_content .= "<td>$srv->hostname</td>";
-                $tool_content .= "<td class = 'text-center'>$enabled_bbb_server</td>";
-
-                $tool_content .= "<td class='option-btn-cell'>".action_button(array(
-                                                    array('title' => $langEditChange,
-                                                          'url' => "$_SERVER[SCRIPT_NAME]?edit_server=$srv->id",
-                                                          'icon' => 'fa-edit'),
-                                                    array('title' => $langDelete,
-                                                          'url' => "$_SERVER[SCRIPT_NAME]?delete_server=$srv->id",
-                                                          'icon' => 'fa-times',
-                                                          'class' => 'delete',
-                                                          'confirm' => $langConfirmDelete)
-                                                    ))."</td>";
-                $tool_content .= "</tr>";
-            }            	
-            $tool_content .= "</table></div>";
-        } else {
-             $tool_content .= "<div class='alert alert-warning'>Δεν υπάρχουν διαθέσιμοι εξυπηρετητές.</div>";
+                'level' => 'primary-label'
+            ]
+        ]);
+    $data['enabled'] = true;
+    if (isset($_GET['edit_server'])) {
+        $data['wc_server'] = $_GET['edit_server'];
+        $data['server'] = Database::get()->querySingle("SELECT * FROM wc_servers WHERE id = ?d", $data['wc_server']);
+        if ($data['server']->enabled == "false") {
+            $data['enabled'] = false;
         }
     }
+    $view = 'admin.other.extapps.webconf.create';
+
+
+// Display config.php edit form
+} else {    
+
+    //display available WebConf servers
+    $data['action_bar'] = action_bar(array(
+        array('title' => $langAddWebConfServer,
+            'url' => "webconf.php?add_server",
+            'icon' => 'fa-plus-circle',
+            'level' => 'primary-label',
+            'button-class' => 'btn-success'),
+        array('title' => $langBack,
+            'url' => "extapp.php",
+            'icon' => 'fa-reply',
+            'level' => 'primary-label')));
+
+    $data['wc_servers'] = Database::get()->queryArray("SELECT * FROM wc_servers");
+    $view = 'admin.other.extapps.webconf.index';
 }
 
-draw($tool_content, 3, null, $head_content);
+$data['menuTypeID'] = 3;
+view($view, $data);
+
---
+++ resources/views/admin/other/extapps/webconf/index.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.default')
+
+@section('content')
+    {!! isset($action_bar) ?  $action_bar : '' !!}
+    @if (count($wc_servers) > 0)
+        <div class='table-responsive'>
+            <table class='table-default'>
+                <thead>
+                    <tr>
+                        <th class = 'text-center'>{!! trans('langWebConfServer') !!}</th>
+                        <th class = 'text-center'>{!! trans('langBBBEnabled') !!}</th>
+                        <th class = 'text-center'>{!! icon('fa-gears') !!}</th>
+                    </tr>
+                </thead>
+                @foreach ($wc_servers as $wc_server)
+                    <tr>
+                        <td>{{ $wc_server->hostname }}</td>
+                        <td class='text-center'>
+                            {!! $wc_server->enabled=='true' ? trans('langYes') : trans('langNo') !!}
+                        </td>
+                        <td class='option-btn-cell'>
+                            {!! action_button([
+                                        [
+                                            'title' => trans('langEditChange'),
+                                            'url' => "$_SERVER[SCRIPT_NAME]?edit_server=$wc_server->id",
+                                            'icon' => 'fa-edit'
+                                        ],
+                                        [
+                                            'title' => trans('langDelete'),
+                                            'url' => "$_SERVER[SCRIPT_NAME]?delete_server=$wc_server->id",
+                                            'icon' => 'fa-times',
+                                            'class' => 'delete',
+                                            'confirm' => trans('langConfirmDelete')
+                                        ]
+                                    ]) !!}
+                        </td>
+                    </tr>
+                @endforeach            	
+            </table>
+        </div>
+    @else
+        <div class='alert alert-warning'>Δεν υπάρχουν διαθέσιμοι εξυπηρετητές.</div>
+    @endif    
+@endsection
