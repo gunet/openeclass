cd08de74b440c130c5171a6a03ac6c4beaca67cf
nikos vavoulas <nikos.va@gmail.com>
2016-05-06 15:22:32 +0300

    Blading stats monthly report

--- modules/admin/monthlyReport.php
+++ modules/admin/monthlyReport.php
@@ -34,84 +34,31 @@ $toolName = $langMonthlyReport;
 $navigation[] = array('url' => 'index.php', 'name' => $langAdmin);
 $navigation[] = array("url" => "../usage/index.php?t=a", "name" => $langUsage);
 
-$tool_content .= action_bar(array(                
+$data['action_bar'] = action_bar(array(                
                 array('title' => $langBack,
                     'url' => "../usage/index.php?t=a",
                     'icon' => 'fa-reply',
                     'level' => 'primary-label')));
 
-$months = "";
-for ($i = 0; $i < 12; $i++) {
-    $mon = mktime(0, 0, 0, date('m') - $i - 1, date('d'), date('Y'));
-    $mval = date('m Y', $mon);
-    $months .= "<option value='$mval'>" . $langMonths[date('m', $mon)] . date(' Y', $mon);
-}
 
-$tool_content .= '<div class="form-wrapper">
-    <form class="form-horizontal" role="form" method="post">
-    <div class="form-group"><div class="col-sm-10"><select name="selectedMonth" class="form-control">' . $months . '</select></div>
-        <input class="btn btn-primary" type="submit" name="btnUsage" value="' . $langSubmit . '">
-    </div>
-    </form>
-    </div>';
+$data['option_date'] = new Datetime();
 
 if (isset($_POST["selectedMonth"])) {
     $month = q($_POST["selectedMonth"]);
-    list($m, $y) = explode(' ', $month);  //only month
-
-    $coursNum = '';
-    $row = Database::get()->querySingle("SELECT profesNum, studNum, visitorsNum, coursNum, logins, details
+    list($m, $data['y']) = explode(' ', $month);  //only month
+    
+    $data['monthly_data'] = Database::get()->querySingle("SELECT profesNum, studNum, visitorsNum, coursNum, logins, details
                        FROM monthly_summary WHERE `month` = ?s", $month);
-    if ($row) {
-        $profesNum = $row->profesNum;
-        $studNum = $row->studNum;
-        $visitorsNum = $row->visitorsNum;
-        $coursNum = $row->coursNum;
-        $logins = $row->logins;
-        $details = $row->details;
-    }
 
     if (isset($localize) and $localize == 'greek') {
-        $msg_of_month = substr($langMonths[$m], 0, -1);
+        $data['msg_of_month'] = substr($langMonths[$m], 0, -1);
     } else {
-        $msg_of_month = $langMonths[$m];
+        $data['msg_of_month'] = $langMonths[$m];
     }
 
-    if ($coursNum) {
-        $tool_content .= '
-		<table class="table-default">
-		<tbody>		
-		<tr>
-		<th colspan="2" class="text-center">' . $langReport . ': ' . $msg_of_month . ' ' . $y . '</th>
-		</tr>
-		<tr>
-		<th class="left">' . $langNbProf . ': </th>
-		<td>' . $profesNum . '</td>
-		</tr>
-		<tr>
-		<th class="left">' . $langNbStudents . ': </th>
-		<td>' . $studNum . '</td>
-		</tr>
-		<tr>
-		<th class="left">' . $langNbVisitors . ': </th>
-		<td>' . $visitorsNum . '</td>
-		</tr>
-		<tr>
-		<th class="left">' . $langNbCourses . ':  </th>
-		<td>' . $coursNum . '</td>
-		</tr>
-		<tr>
-		<th class="left">' . $langNbLogin . ': </th>
-		<td>' . $logins . '</td>
-		</tr>
-		<tr>
-		<td colspan="2">' . $details . '</td>
-		</tr>
-		</tbody>
-		</table>';           // $details includes an html table with all details
-    } else {
-        $tool_content .= '<div class="alert alert-warning">' . $langNoReport . ': ' . $msg_of_month . ' ' . $y . '</div>';
-    }
+
 }
 load_js('tools.js');
-draw($tool_content, 3, 'admin', $head_content);
+$data['menuTypeID'] = 3;
+view('admin.other.stats.monthlyReport', $data);
+//draw($tool_content, 3, 'admin', $head_content);
