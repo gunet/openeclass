ba4ffe2326dcefaf0e4793aa5e7b7d15088566ab
Ananias Tsalouchidis <none@none>
2016-05-23 09:50:35 +0300

    small fix

--- modules/bbb/webconf/webconf.php
+++ modules/bbb/webconf/webconf.php
@@ -39,7 +39,7 @@ echo '
             var flashvars = {};
 			flashvars.videoconf_url = "rtmpe://'.base64_decode($_GET['webconf_server']).'/avchat/'.$_GET['meeting_id'].'"
 			flashvars.chat_url = "rtmpe://'.base64_decode($_GET['webconf_server']).'/textchat/'.$_GET['meeting_id'].'"
-			flashvars.jnlp_url = "'. base64_decode($_GET['base_url']) . '/openeclass/modules/bbb/webconf/rooms/'.$_GET['meeting_id'].'.jnlp"
+			flashvars.jnlp_url = "'. base64_decode($_GET['base_url']) . '/modules/bbb/webconf/rooms/'.$_GET['meeting_id'].'.jnlp"
 			flashvars.base_screen_url = "rtmp://'.base64_decode($_GET['screenshare_server']).'/screenshare"
 			flashvars.screen_streamname = "'.$_GET['meeting_id'].'"
 			flashvars.username = "'. $_GET['user'] . '"
