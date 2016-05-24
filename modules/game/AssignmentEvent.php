bb1f3e7618c5b8ccab1dc484f6e8f52564989ec0
Thanos Kyritsis <atkyritsis@upnet.gr>
2016-05-24 10:44:29 +0300

    initial import of gamification classes and phpunit tests
    --HG--
    branch : gamification

--- modules/game/AssignmentEvent.php
+++ modules/game/AssignmentEvent.php
@@ -1,3 +1,5 @@
+<?php
+
 /* ========================================================================
  * Open eClass 
  * E-learning and Course Management System
@@ -17,3 +19,24 @@
  *                  e-mail: info@openeclass.org
  * ======================================================================== 
  */
+
+require_once 'BasicEvent.php';
+
+class AssignmentEvent extends BasicEvent {
+    
+    const ACTIVITY = 'assignment';
+    const NEWGRADE = 'assignment-graded';
+    
+    public function __construct() {
+        parent::__construct();
+        
+        $this->on(self::NEWGRADE, function($data) {
+            $this->setEventData($data);
+            
+            // TODO: fetch data from DB: SELECT ASSIGNMENT GRADE FOR USER $data->uid
+            $this->context['threshold'] = 8.6;
+            $this->emit(parent::PREPARERULES);
+        });
+    }
+    
+}
--- modules/game/LearningPathEvent.php
+++ modules/game/LearningPathEvent.php
@@ -1,3 +1,5 @@
+<?php
+
 /* ========================================================================
  * Open eClass 
  * E-learning and Course Management System
@@ -17,3 +19,23 @@
  *                  e-mail: info@openeclass.org
  * ======================================================================== 
  */
+
+require_once 'BasicEvent.php';
+
+class LearningPathEvent extends BasicEvent {
+    
+    const ACTIVITY = 'learning path';
+    
+    public function __construct() {
+        parent::__construct();
+        
+        $this->on('learning-path-accessed', function($data) {
+            $this->setEventData($data);
+            
+            // TODO: fetch data from DB: SELECT LP USER GRADE FOR USER $data->uid
+            $this->context['threshold'] = 80;
+            $this->emit(parent::PREPARERULES);
+        });
+    }
+    
+}
