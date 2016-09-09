427b4fb153154ee1675bd306fdf65b413a986033
Yannis Exidaridis <jexi@noc.uoa.gr>
2016-09-09 16:34:59 +0300

    Fix course license during course cloning / restoring.
    --HG--
    branch : 3.4

--- modules/course_info/restore_functions.php
+++ modules/course_info/restore_functions.php
@@ -371,6 +371,7 @@ function course_details_form($code, $title, $prof, $lang, $type, $vis, $desc, $f
 
 function create_restored_course(&$tool_content, $restoreThis, $course_code, $course_lang, $course_title, $course_desc, $course_vis, $course_prof) {
     global $webDir, $urlServer, $urlAppend, $langEnter, $langBack, $currentCourseCode;
+    
     require_once 'modules/create_course/functions.php';
     require_once 'modules/course_info/restorehelper.class.php';
     require_once 'include/lib/fileManageLib.inc.php';
@@ -420,14 +421,15 @@ function create_restored_course(&$tool_content, $restoreThis, $course_code, $cou
             $course_data = $course_dataArr[0];
             // update course query
             $upd_course_sql = "UPDATE course SET keywords = ?s, doc_quota = ?f, video_quota = ?f, "
-                            . " group_quota = ?f, dropbox_quota = ?f, glossary_expand = ?d ";
+                            . " group_quota = ?f, dropbox_quota = ?f, glossary_expand = ?d, course_license = ?d ";
             $upd_course_args = array(
                 $course_data[$restoreHelper->getField('course', 'keywords')],
                 floatval($course_data['doc_quota']),
                 floatval($course_data['video_quota']),
                 floatval($course_data['group_quota']),
                 floatval($course_data['dropbox_quota']),
-                intval($course_data[$restoreHelper->getField('course', 'glossary_expand')])
+                intval($course_data[$restoreHelper->getField('course', 'glossary_expand')]),
+                intval($course_data[$restoreHelper->getField('course', 'course_license')])
             );
             if (!isset($course_data['course_image'])) {
                 $course_data['course_image'] = null;
