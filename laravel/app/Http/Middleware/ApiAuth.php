b4d26deede3446f8c1e65424635ac24a1138d8a5
nikos vavoulas <nikos.va@gmail.com>
2016-09-26 15:49:29 +0300

    Added basic auth to api and endpoint that returns named routes

---
+++ laravel/app/Http/Middleware/ApiAuth.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class ApiAuth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        return \Auth::onceBasic('username') ?: $next($request);
+    }
+}
