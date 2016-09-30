ab88a99dbe7e5781b441040d70fbb2d441ce25a8
Yannis Exidaridis <jexi@noc.uoa.gr>
2016-09-30 13:26:54 +0300

    fix display optional course password warning in course admin

--- modules/course_info/index.php
+++ modules/course_info/index.php
@@ -157,8 +157,8 @@ $head_content .= <<<hContent
             }
         }).change();
         
-        $('#password').keyup(function() {
-            $('#result').html(checkStrength($('#password').val()))
+        $('#coursepassword').keyup(function() {
+            $('#result').html(checkStrength($('#coursepassword').val()))
         });
         
         displayCoursePassword();
@@ -676,6 +676,9 @@ if (isset($_POST['submit'])) {
                 <label for='coursepassword' class='col-sm-2 control-label'>$langOptPassword:</label>
                 <div class='col-sm-10'>
                       <input class='form-control' id='coursepassword' type='text' name='password' value='".@q($password)."' autocomplete='off'>
+                </div>
+                <div class='col-sm-2 text-center padding-thin'>
+                    <span id='result'></span>
                 </div>
             </div>                        
 	    <div class='form-group'>
