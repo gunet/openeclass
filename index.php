73ba5734f61b5754a6e2d95f9be8ad623ac6a183
Alexandros Diamantidis <adia@noc.uoa.gr>
2016-11-28 16:43:35 +0200

    Main index.php: check for logout before checking for login
    --HG--
    branch : 3.6

--- index.php
+++ index.php
@@ -65,22 +65,6 @@ if (isset($dbname)) {
 }
 unset($dbname);
 
-// if we try to login... then authenticate user.
-$warning = '';
-if (isset($_SESSION['shib_uname'])) {
-    // authenticate via shibboleth
-    shib_cas_login('shibboleth');
-} elseif (isset($_SESSION['cas_uname']) && !isset($_GET['logout'])) {
-    // authenticate via cas
-    shib_cas_login('cas');
-} elseif (isset($_GET['provider'])) {
-    //hybridauth authentication (Facebook, Twitter, Google, Yahoo, Live, LinkedIn)
-    hybridauth_login();
-} else {
-    // normal authentication
-    process_login();
-}
-
 if (isset($_SESSION['uid'])) {
     $uid = $_SESSION['uid'];
 } else {
@@ -114,6 +98,22 @@ if (isset($_GET['logout']) and $uid) {
             phpCAS::logoutWithRedirectService($urlServer);
         }
     }
+}
+
+// if we try to login... then authenticate user.
+$warning = '';
+if (isset($_SESSION['shib_uname'])) {
+    // authenticate via shibboleth
+    shib_cas_login('shibboleth');
+} elseif (isset($_SESSION['cas_uname']) && !isset($_GET['logout'])) {
+    // authenticate via cas
+    shib_cas_login('cas');
+} elseif (isset($_GET['provider'])) {
+    //hybridauth authentication (Facebook, Twitter, Google, Yahoo, Live, LinkedIn)
+    hybridauth_login();
+} else {
+    // normal authentication
+    process_login();
 }
 
 // if the user logged in include the correct language files
