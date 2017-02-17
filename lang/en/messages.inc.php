0dc5f349bcd7dff03154f6be222ca862765ebfe8
Yannis Exidaridis <jexi@noc.uoa.gr>
2017-02-17 16:44:45 +0200

    fix english message
    --HG--
    branch : 3.6

--- lang/en/messages.inc.php
+++ lang/en/messages.inc.php
@@ -2029,7 +2029,7 @@ $langAdministrator = "Administrator";
 $langContinue = "Continue";
 $langDelImage = "Remove picture";
 $langGroups = "Groups";
-$langGroup = "group";
+$langGroup = "Group";
 $langIs = "is";
 $langLastname = "Last name";
 $langLegendRequiredFields = "<span class=\"required\">*</span> denotes required field";
