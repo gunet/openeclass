9dd111ca841842d60f672ac46b800bfb3a7fe22e
Yannis Exidaridis <jexi@noc.uoa.gr>
2017-07-24 13:54:29 +0300

    update rubric upgrade queries
    --HG--
    branch : 3.6

--- upgrade/upgrade.php
+++ upgrade/upgrade.php
@@ -3554,74 +3554,40 @@ $mysqlMainDb = ' . quote($mysqlMainDb) . ';
             ) $tbl_options");
 
         // Rubric tables
-        Database::get()->query("CREATE TABLE IF NOT EXISTS `rubric` (
+        Database::get()->query("CREATE TABLE IF NOT EXISTS `rubric` (            
             `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
             `name` VARCHAR(200) NOT NULL,
-            `title` varchar(255) NOT NULL,
             `scales` text NOT NULL,
             `description` text,
             `preview_rubric` tinyint(1) NOT NULL DEFAULT '0',
-            `rubric_during_evaluation` tinyint(1) NOT NULL DEFAULT '0',
-            `rubric_to_graded` tinyint(1) NOT NULL DEFAULT '0',
-            `points_during_evaluation` tinyint(1) NOT NULL DEFAULT '0',
             `points_to_graded` tinyint(1) NOT NULL DEFAULT '0',
             `course_id` int(11) NOT NULL,
-            KEY `course_id` (`course_id`)) $tbl_options");
+            KEY `course_id` (`course_id`)
+            ) $tbl_options");
 
-        Database::get()->query("CREATE TABLE IF NOT EXISTS `rubric_rel` (
-                            `id` INT(11) NOT NULL AUTO_INCREMENT,
-                            `rubric_id` INT(11) NOT NULL,
-                            `course_id` INT(11) NOT NULL,
-                            `module_id` INT(11) NOT NULL,
-                            `resource_id` INT(11) NOT NULL,
-                            PRIMARY KEY (`id`)
-                          ) $tbl_options");
-        Database::get()->query("CREATE TABLE IF NOT EXISTS `rubric_criteria` (
-                            `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-                            `rubric_id` int(11) NOT NULL,
-                            `sortorder` varchar(30) NOT NULL,
-                            `description` text,
-                            PRIMARY KEY (`id`)
-                         ) $tbl_options");
-        Database::get()->query("CREATE TABLE IF NOT EXISTS `rubric_levels` (
-                            `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-                            `rubric_id` int(11) NOT NULL,
-                            `criterionid` int(10) unsigned NOT NULL,
-                            `score` decimal(5,0) NOT NULL,
-                            `definition` text NOT NULL,
-                            PRIMARY KEY (`id`)
-                          ) $tbl_options");
-        Database::get()->query("CREATE TABLE IF NOT EXISTS `rubric_assesment` (
-                            `id` int(11) NOT NULL AUTO_INCREMENT,
-                            `as_sub_id` int(11) NOT NULL,
-                            `uid` int(11) NOT NULL,
-                            `level_chosen_id` int(11) NOT NULL,
-                            `level_feedback` varchar(60) NOT NULL,
-                            PRIMARY KEY (`id`)
-                          ) $tbl_options ");
-
+        // tc attendance tables
         Database::get()->query("CREATE TABLE IF NOT EXISTS `tc_attendance` (
-                            `id` int(11) NOT NULL DEFAULT '0',
-                            `meetingid` varchar(20) NOT NULL,
-                            `bbbuserid` varchar(20) DEFAULT NULL,
-                            `totaltime` int(11) NOT NULL DEFAULT '0',
-                            `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                            PRIMARY KEY (`id`,`meetingid`),
-                            KEY `id` (`id`),
-                            KEY `meetingid` (`meetingid`)
-                        ) $tbl_options");
+            `id` int(11) NOT NULL DEFAULT '0',
+            `meetingid` varchar(20) NOT NULL,
+            `bbbuserid` varchar(20) DEFAULT NULL,
+            `totaltime` int(11) NOT NULL DEFAULT '0',
+            `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            PRIMARY KEY (`id`,`meetingid`),
+            KEY `id` (`id`),
+            KEY `meetingid` (`meetingid`)
+        ) $tbl_options");
 
         Database::get()->query("CREATE TABLE IF NOT EXISTS `tc_log` (
-                            `id` int(11) NOT NULL,
-                            `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-                            `meetingid` varchar(20) NOT NULL,
-                            `bbbuserid` varchar(20) DEFAULT NULL,
-                            `fullName` varchar(200) DEFAULT NULL,
-                            `type` varchar(255) default 'bbb',
-                            PRIMARY KEY (`id`),
-                            KEY `userid` (`bbbuserid`),
-                            KEY `fullName` (`fullName`)
-                        ) $tbl_options");
+                `id` int(11) NOT NULL,
+                `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                `meetingid` varchar(20) NOT NULL,
+                `bbbuserid` varchar(20) DEFAULT NULL,
+                `fullName` varchar(200) DEFAULT NULL,
+                `type` varchar(255) default 'bbb',
+                PRIMARY KEY (`id`),
+                KEY `userid` (`bbbuserid`),
+                KEY `fullName` (`fullName`)
+            ) $tbl_options");
     }
 
     // update eclass version
