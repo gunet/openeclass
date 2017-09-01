6176f6bf1859ac3c6fb6230e74837ae585bbdb59
Yannis Exidaridis <jexi@noc.uoa.gr>
2017-09-01 11:46:55 +0300

    fix bug in admin statistics per course in csv.

--- modules/usage/faculty_stats_csv.php
+++ modules/usage/faculty_stats_csv.php
@@ -36,9 +36,9 @@ if (isset($_GET['c'])) {
     $csv->filename = course_id_to_title($_GET['c']) . '.csv';
     $code = course_id_to_code(intval($_GET['c']));
     $u_date_start = $_GET['user_date_start'];
-    $u_date_end = $_GET['user_date_end'];   
+    $u_date_end = $_GET['user_date_end'];
 
-    $name = Database::get()->querySingle("SELECT name FROM hierarchy, course, course_department WHERE hierarchy.id = course_department.department 
+    $name = Database::get()->querySingle("SELECT name FROM hierarchy, course, course_department WHERE hierarchy.id = course_department.department
                                      AND course_department.course = course.id AND course.id = ?d", $_GET['c'])->name;
     $csv->outputRecord($tree->unserializeLangField($name));
     $course = Database::get()->querySingle("SELECT title, prof_names, code, visible FROM course WHERE id = ?d", $_GET['c']);
@@ -49,35 +49,31 @@ if (isset($_GET['c'])) {
         ->outputRecord($langUsers, $users)
         ->outputRecord()
         ->outputRecord($langMonth, $langMonthlyCourseRegistrations);
-    
-    $q2 = Database::get()->queryArray("SELECT COUNT(*) AS registrations, MONTH(reg_date) AS month, YEAR(reg_date) AS year FROM course_user 
+
+    $q2 = Database::get()->queryArray("SELECT COUNT(*) AS registrations, MONTH(reg_date) AS month, YEAR(reg_date) AS year FROM course_user
                 WHERE course_id = ?d AND (reg_date BETWEEN ?s AND ?s)
-                    AND status = " . USER_STUDENT . " GROUP BY month, year ORDER BY reg_date ASC",
+                    AND status = " . USER_STUDENT . " GROUP BY month, year, reg_date ORDER BY reg_date ASC",
             $_GET['c'], $u_date_start, $u_date_end);
-    foreach ($q2 as $data) {        
+    foreach ($q2 as $data) {
         $csv->outputRecord($data->month . '-' . $data->year, $data->registrations);
     }
-    
+
     $csv->outputRecord()->outputRecord($langMonth, $langVisits, $langUsers);
-    $q3 = Database::get()->queryArray("SELECT COUNT(*) AS cnt, module_id, COUNT(DISTINCT user_id) AS users FROM actions_daily 
-            WHERE day BETWEEN ?s AND ?s AND course_id = ?d
-            GROUP BY module_id", $u_date_start, $u_date_end, $_GET['c']);
+    $q3 = Database::get()->queryArray("SELECT MONTH(day) AS month, YEAR(day) AS year, COUNT(*) AS visits, COUNT(DISTINCT user_id) AS users FROM actions_daily
+                        WHERE (day BETWEEN ?s AND ?s) AND course_id = ?d GROUP BY month, year ORDER BY year, month ASC", $u_date_start, $u_date_end, $_GET['c']);    
+    $total_visits = $total_users = 0;
     foreach ($q3 as $data) {
-        if ($data->module_id > 0) {
-            if ($data->module_id == MODULE_ID_UNITS) { // course_units
-                $mod_id = $static_modules[$data->module_id];
-            } else {
-                $mod_id = $modules[$data->module_id];
-            }
-            $csv->outputRecord($mod_id['title'], $data->cnt, $data->users);
-        }
+            $csv->outputRecord($data->month . '-' . $data->year, $data->visits, $data->users);
+            $total_visits += $data->visits;
+            $total_users += $data->users;
     }
-    
+    $csv->outputRecord($langTotal, $total_visits, $total_users);
+
     $csv->outputRecord()->outputRecord($langModule, $langVisits, $langUsers);
-    $q3 = Database::get()->queryArray("SELECT COUNT(*) AS cnt, module_id, COUNT(DISTINCT user_id) AS users FROM actions_daily 
+    $q4 = Database::get()->queryArray("SELECT COUNT(*) AS cnt, module_id, COUNT(DISTINCT user_id) AS users FROM actions_daily
             WHERE (day BETWEEN ?s AND ?s) AND course_id = ?d
             GROUP BY module_id", $u_date_start, $u_date_end, $_GET['c']);
-    foreach ($q3 as $data) {
+    foreach ($q4 as $data) {
         if ($data->module_id > 0) {
             if ($data->module_id == MODULE_ID_UNITS) { // course_units
                 $mod_id = $static_modules[$data->module_id];
@@ -88,3 +84,4 @@ if (isset($_GET['c'])) {
         }
     }
 }
+
