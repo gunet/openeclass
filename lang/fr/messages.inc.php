31be666f14cdfbd54b4e7ab03e5ae6dab159b938
Yannis Exidaridis <jexi@noc.uoa.gr>
2017-09-28 15:10:01 +0300

    cosmetic changes in assignment

--- lang/fr/messages.inc.php
+++ lang/fr/messages.inc.php
@@ -2285,7 +2285,7 @@ $m['plainview'] = "Liste concise de devoirs et de notes d'evaluation";
 $m['WorkInfo'] = 'Informations sur le devoir'
 ;
 $m['WorkView'] = 'Devoirs du cours';
-$m['WorkDelete'] = 'Supprimer le devoir';
+$langDeleteSubmission = 'Supprimer le devoir';
 $m['WorkEdit'] = 'Modifier le devoir';
 $m['SubmissionWorkInfo'] = 'Informations sur la soumission du devoir'
 ;
