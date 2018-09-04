6b80c924febdbede8808f92e3b9dbe5652dbc348
Yannis Exidaridis <jexi@noc.uoa.gr>
2018-09-04 14:22:55 +0300

    add missing file (about privacy policy)
    --HG--
    branch : 3.6

---
+++ info/privacy_policy.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once '../include/baseTheme.php';
+
+$toolName = $langPrivacyPolicy;
+
+$tool_content = action_bar(array(
+                                array('title' => $langBack,
+                                      'url' => $urlServer,
+                                      'icon' => 'fa-reply',
+                                      'level' => 'primary-label',
+                                      'button-class' => 'btn-default')
+                            ),false);
+
+$tool_content .= "<div class='row'>"
+                    . "<div class='col-xs-12'>"
+                    . "<div class='panel'>"
+                    . "<div class='panel-body'>";
+
+$tool_content .= get_config('privacy_policy_text');
+
+$tool_content .= "</div>"
+            . "</div>"
+            . "</div>"
+            . "</div>";
+
+if (isset($_SESSION['uid'])) {
+    draw($tool_content, 1);
+} else {
+    draw($tool_content, 0);
+}
+
