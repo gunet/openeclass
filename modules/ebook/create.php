c19c15e7709a7e262f4aa5e1c58d968fb377e9f9
Yannis Exidaridis <jexi@noc.uoa.gr>
2019-05-10 15:12:47 +0300

    ebook: Replace pclzip with ZipArchive

--- modules/ebook/create.php
+++ modules/ebook/create.php
@@ -1,10 +1,10 @@
 <?php
 
 /* ========================================================================
- * Open eClass 4.0
+ * Open eClass 3.0
  * E-learning and Course Management System
  * ========================================================================
- * Copyright 2003-2016  Greek Universities Network - GUnet
+ * Copyright 2003-2014  Greek Universities Network - GUnet
  * A full copyright notice can be read in "/info/copyright.txt".
  * For a full list of contributors, see "credits.txt".
  *
@@ -19,7 +19,9 @@
  *                  e-mail: info@openeclass.org
  * ======================================================================== */
 
+
 $require_current_course = true;
+$require_editor = true;
 
 require_once '../../include/baseTheme.php';
 require_once 'include/lib/fileUploadLib.inc.php';
@@ -30,43 +32,69 @@ $pageName = $langEBookCreate;
 $navigation[] = array('url' => 'index.php?course=' . $course_code, 'name' => $langEBook);
 define('EBOOK_DOCUMENTS', true);
 
-if (!$is_editor) {
-    redirect_to_home_page();
-} else {
-    $title = trim(@$_POST['title']);
-    if (empty($title)) {
-        Session::Messages($langFieldsMissing, 'alert-danger');
-        redirect_to_home_page("modules/ebook/index.php?course=$course_code&create=1");
-    }
-    if (isset($_FILES['file']['name']) and !$_FILES['file']['error']) {
-        if (!preg_match('/\.zip$/i', $_FILES['file']['name'])) {
-            Session::Messages("$langUnwantedFiletype: " . $_FILES['file']['name'], 'alert-danger');
-            redirect_to_home_page("modules/ebook/index.php?course=$course_code&create=1");
-        }
-        validateUploadedFile($_FILES['file']['name'], 2);
-        $zipFile = new PclZip($_FILES['file']['tmp_name']);
-        validateUploadedZipFile($zipFile->listContent(), 2);
-    }
-
-    $order = Database::get()->querySingle("SELECT COALESCE(MAX(`order`), 1) AS `order` FROM ebook WHERE course_id = ?d", $course_id)->order;
-    $ebook_id = Database::get()->query("INSERT INTO ebook SET `order` = ?d, `course_id` = ?d, `title` = ?s, `visible` = 1", $order + 1, $course_id, $title)->lastInsertID;
-    Database::get()->query("INSERT INTO ebook_section SET ebook_id = ?d,
-                                                    public_id = ?s,
-                                                    title = ?s"
-            , $ebook_id, '1', $langSection.' 1');
-    // Initialize document subsystem global variables
-    doc_init();
-
-    if (!make_dir($basedir)) {
-        Database::get()->query("DELETE FROM ebook WHERE course_id = ?d AND id = ?d", $course_id, $ebook_id);
-        Session::Messages($langImpossible, 'alert-danger');
-        redirect_to_home_page("modules/ebook/index.php?course=$course_code&create=1");
-    }
-
-    if (isset($zipFile)) {
-        chdir($basedir);
-        $realFileSize = 0;
-        $zipFile->extract(PCLZIP_CB_PRE_EXTRACT, 'process_extracted_file');
-    }
-    redirect_to_home_page("modules/ebook/edit.php?course=$course_code&id=$ebook_id");
+if (isset($_POST['title'])) {
+    $title = trim($_POST['title']);
 }
+
+if (empty($title)) {
+    Session::Messages($langFieldsMissing, 'alert-danger');
+    redirect_to_home_page("modules/ebook/index.php?course=$course_code&create=1");
+}
+
+$order = Database::get()->querySingle("SELECT COALESCE(MAX(`order`), 1) AS `order` FROM ebook WHERE course_id = ?d", $course_id)->order;
+$ebook_id = Database::get()->query("INSERT INTO ebook SET `order` = ?d, `course_id` = ?d, `title` = ?s, `visible` = 1", $order + 1, $course_id, $title)->lastInsertID;
+Database::get()->query("INSERT INTO ebook_section SET ebook_id = ?d,
+                                                public_id = ?s,
+                                                title = ?s"
+        , $ebook_id, '1', $langSection.' 1');
+// Initialize document subsystem global variables
+doc_init();
+
+if (!make_dir($basedir)) {
+    Database::get()->query("DELETE FROM ebook WHERE course_id = ?d AND id = ?d", $course_id, $ebook_id);
+    Session::Messages($langImpossible, 'alert-danger');
+    redirect_to_home_page("modules/ebook/index.php?course=$course_code&create=1");
+}
+
+if (isset($_FILES['file']['name']) and !$_FILES['file']['error']) {
+    if (!preg_match('/\.zip$/i', $_FILES['file']['name'])) {
+        Session::Messages("$langUnwantedFiletype: " . $_FILES['file']['name'], 'alert-danger');
+        redirect_to_home_page("modules/ebook/index.php?course=$course_code&create=1");
+    }
+    validateUploadedFile($_FILES['file']['name'], 2);
+    $userFile = $_FILES['file']['tmp_name'];
+    $realFileSize = 0;
+    $files_in_zip = array();
+    $zipFile = new ZipArchive();
+    if ($zipFile->open($userFile) == TRUE) {
+        $diskUsed = dir_total_space($basedir);
+        $diskQuotaDocument = Database::get()->querySingle("SELECT doc_quota AS quotatype FROM course WHERE id = ?d", $course_id)->quotatype;
+        // check for file type in zip contents
+        for ($i = 0; $i < $zipFile->numFiles; $i++) {
+            $stat = $zipFile->statIndex($i);
+            $files_in_zip[$i] = $stat['name'];
+            if (!empty(my_basename($files_in_zip[$i]))) {
+                validateUploadedFile(my_basename($files_in_zip[$i]), 2);
+            }
+        }
+        for ($i = 0; $i < $zipFile->numFiles; $i++) {
+            $stat = $zipFile->statIndex($i);
+            $realFileSize += $stat["size"]; // check for free space
+            if ($diskUsed + $realFileSize > $diskQuotaDocument) {
+                Session::Messages($langNoSpace, 'alert-danger');
+                redirect_to_home_page("modules/ebook/index.php?course=$course_code&create=1");
+            }
+            $extracted_file_name = process_extracted_file($stat);
+            if (!is_null($extracted_file_name)) {
+                $zipFile->renameName($stat["name"], $extracted_file_name);
+                $zipFile->extractTo("$basedir/", $extracted_file_name);
+            }
+        }
+        $zipFile->close();
+    } else {
+        Session::Messages($langErrorFileMustBeZip, 'alert-warning');
+        redirect_to_home_page("modules/ebook/index.php?course=$course_code&create=1");
+    }
+}
+
+redirect_to_home_page("modules/ebook/edit.php?course=$course_code&id=$ebook_id");
