3558e197b1c7992356a4ce464fe73c3cfb7a0225
Yannis Exidaridis <jexi@noc.uoa.gr>
2020-06-19 14:21:42 +0300

    Exercises: minor fix in results.

--- modules/exercise/exercise_result.php
+++ modules/exercise/exercise_result.php
@@ -199,13 +199,11 @@ $showScore = $displayScore == 1
 if (isset($user)) { // user details
     $tool_content .= "<div class='alert alert-info'>" . q($user->surname) . " " . q($user->givenname);
     if ($user->am) {
-        $tool_content .= "($langAm:" . q($user->am) . ")";
+        $tool_content .= " ($langAm:" . q($user->am) . ")";
     }
     if ($showScore) {
         $tool_content .= "<h5>$langYourTotalScore: <span><strong>$exercise_user_record->total_score</span> / $exercise_user_record->total_weighting</strong></h5>";
     }
-    $tool_content .= "<h5>$langStart: <em>" . nice_format($exercise_user_record->record_start_date, true) . "</em></h5>";
-    $tool_content .= "<h5>$langDuration: <em>" . format_time_duration($exercise_user_record->time_duration) . "</em></h5>";
     /*if ($exerciseAttemptsAllowed > 0) {
         $tool_content .= "<h5>$langAttempt: <em>$exerciseAttemptsAllowed</em></h5>";
     }*/
