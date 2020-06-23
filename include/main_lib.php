58f6a615021fa5cef07ea7e0c71934395ee5f772
Alexandros Diamantidis <adia@noc.uoa.gr>
2020-06-23 16:43:30 +0300

    Revert incomplete change to include/main_lib.php get/set_config()
    --HG--
    branch : 3.9

--- include/main_lib.php
+++ include/main_lib.php
@@ -1799,17 +1799,11 @@ function deleteUser($id, $log) {
  * @return type
  */
 function get_config($key, $default = null) {
-    if (!isset($_SESSION['configCache'][$key])) {
-        $r = Database::get()->querySingle("SELECT `value` FROM config WHERE `key` = ?s", $key);
-        if ($r) {
-            $_SESSION['configCache'][$key] = $r->value;
-            return $r->value;
-        } else {
-            $_SESSION['configCache'][$key] = null;
-            return $default;
-        }
-    } elseif ($_SESSION['configCache'][$key]) {
-        return $_SESSION['configCache'][$key];
+
+    $r = Database::get()->querySingle("SELECT `value` FROM config WHERE `key` = ?s", $key);
+    if ($r) {
+        $row = $r->value;
+        return $row;
     } else {
         return $default;
     }
@@ -1821,7 +1815,7 @@ function get_config($key, $default = null) {
  * @param type $value
  */
 function set_config($key, $value) {
-    $_SESSION['configCache'][$key] = $value;
+
     Database::get()->query("REPLACE INTO config (`key`, `value`) VALUES (?s, ?s)", $key, $value);
 }
 
