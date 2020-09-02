061b01be513b76f16b1909944819db0c6087baa2
Yannis Exidaridis <jexi@noc.uoa.gr>
2020-09-02 11:48:29 +0300

    User Groups: respect group visibility in exercises.
    --HG--
    branch : 3.9

--- modules/exercise/exercise_admin.inc.php
+++ modules/exercise/exercise_admin.inc.php
@@ -29,12 +29,15 @@ require_once 'modules/tags/moduleElement.class.php';
 if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
     if (isset($_POST['assign_type'])) {
         if ($_POST['assign_type'] == 2) {
-            $data = Database::get()->queryArray("SELECT name,id FROM `group` WHERE course_id = ?d ORDER BY name", $course_id);
+            $data = Database::get()->queryArray("SELECT name,id FROM `group` 
+                                          WHERE course_id = ?d 
+                                          AND visible = 1 
+                                      ORDER BY name", $course_id);
         } elseif ($_POST['assign_type'] == 1) {
             $data = Database::get()->queryArray("SELECT user.id AS id, surname, givenname
                                     FROM user, course_user
                                     WHERE user.id = course_user.user_id
-                                    AND course_user.course_id = ?d AND course_user.status = 5
+                                    AND course_user.course_id = ?d AND course_user.status = " . USER_STUDENT . "
                                     AND user.id ORDER BY surname", $course_id);
         }
         echo json_encode($data);
@@ -161,8 +164,10 @@ if (isset($_POST['submitExercise'])) {
         if ($objExercise->selectAssignToSpecific() == 2) {
             $assignees = Database::get()->queryArray("SELECT `group`.id AS id, `group`.name
                 FROM exercise_to_specific, `group`
-                WHERE `group`.id = exercise_to_specific.group_id AND exercise_to_specific.exercise_id = ?d", $exerciseId);
-            $all_groups = Database::get()->queryArray("SELECT name,id FROM `group` WHERE course_id = ?d", $course_id);
+                WHERE `group`.id = exercise_to_specific.group_id
+                    AND `group`.visible = 1 
+                    AND exercise_to_specific.exercise_id = ?d", $exerciseId);
+            $all_groups = Database::get()->queryArray("SELECT name, id FROM `group` WHERE course_id = ?d AND visible = 1", $course_id);
             foreach ($assignees as $assignee_row) {
                 $assignee_options .= "<option value='".$assignee_row->id."'>".$assignee_row->name."</option>";
             }
@@ -176,13 +181,14 @@ if (isset($_POST['submitExercise'])) {
             }
         } else {
             $assignees = Database::get()->queryArray("SELECT user.id AS id, surname, givenname
-                FROM exercise_to_specific, user
-                WHERE user.id = exercise_to_specific.user_id AND exercise_to_specific.exercise_id = ?d", $exerciseId);
+                                    FROM exercise_to_specific, user
+                                        WHERE user.id = exercise_to_specific.user_id 
+                                        AND exercise_to_specific.exercise_id = ?d", $exerciseId);
             $all_users = Database::get()->queryArray("SELECT user.id AS id, user.givenname, user.surname
-                FROM user, course_user
-                WHERE user.id = course_user.user_id
-                AND course_user.course_id = ?d AND course_user.status = 5
-                AND user.id", $course_id);
+                                    FROM user, course_user
+                                        WHERE user.id = course_user.user_id
+                                        AND course_user.course_id = ?d AND course_user.status = " . USER_STUDENT . "
+                                        AND user.id", $course_id);
             foreach ($assignees as $assignee_row) {
                 $assignee_options .= "<option value='$assignee_row->id'>$assignee_row->surname $assignee_row->givenname</option>";
             }
