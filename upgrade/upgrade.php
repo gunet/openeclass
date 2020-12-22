2529b3f1bf16e35d940142a7f882787f55b87cba
Yannis Exidaridis <jexi@noc.uoa.gr>
2020-12-22 13:52:24 +0200

    Upgrade: add missing query

--- upgrade/upgrade.php
+++ upgrade/upgrade.php
@@ -1998,7 +1998,8 @@ $mysqlMainDb = ' . quote($mysqlMainDb) . ';
 
         // upgrade queries for version 3.10
         updateInfo(-1, sprintf($langUpgForVersion, '3.10'));
-
+        
+        Database::get()->query("ALTER TABLE exercise_with_questions CHANGE question_id question_id INT DEFAULT 0");
         if (!DBHelper::fieldExists('exercise_with_questions', 'random_criteria')) {
             Database::get()->query("ALTER TABLE exercise_with_questions ADD `random_criteria` TEXT");
             Database::get()->query("ALTER TABLE exercise_with_questions DROP PRIMARY KEY");
