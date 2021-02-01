3e8fa95cf96ecd66e616709d778667fb47b2c182
Alexandros Diamantidis <adia@noc.uoa.gr>
2021-02-01 15:45:51 +0200

    Assignments: pass file downloads through indirect reference, fix fileModal links
    --HG--
    branch : 3.10

--- modules/work/index.php
+++ modules/work/index.php
@@ -115,6 +115,7 @@ if (isset($_GET['get']) or isset($_GET['getcomment'])) {
         }
         $get = $_GET['get'];
     }
+    $get = getDirectReference($get);
     if (!send_file(intval($get), $file_type)) {
         Session::Messages($langFileNotFound, 'alert-danger');
     }
@@ -1035,7 +1036,7 @@ function edit_assignment($id) {
             $file_name = $row->file_name;
         } else {
             validateUploadedFile($_FILES['userfile']['name'], 2);
-            $student_name = trim(uid_to_name($user_id));
+            $student_name = trim(uid_to_name($uid));
             $local_name = !empty($student_name)? $student_name : uid_to_name($user_id, 'username');
             $am = Database::get()->querySingle("SELECT am FROM user WHERE id = ?d", $uid)->am;
             if (!empty($am)) {
@@ -1046,6 +1047,7 @@ function edit_assignment($id) {
             $secret = $row->secret_directory;
             $ext = get_file_extension($_FILES['userfile']['name']);
             $filename = "$secret/$local_name" . (empty($ext) ? '' : '.' . $ext);
+            make_dir("$workPath/admin_files/$secret");
             if (move_uploaded_file($_FILES['userfile']['tmp_name'], "$workPath/admin_files/$filename")) {
                 @chmod("$workPath/admin_files/$filename", 0644);
                 $file_name = $_FILES['userfile']['name'];
@@ -3034,7 +3036,7 @@ function show_edit_assignment($id) {
                 <div class='form-group'>
                     <label for='userfile' class='col-sm-2 control-label'>$langWorkFile:</label>
                     <div class='col-sm-10'>
-                      ".(($row->file_name)? "<a href='$_SERVER[SCRIPT_NAME]?course=$course_code&amp;get=$row->id&amp;file_type=1'>".q($row->file_name)."</a>"
+                      ".(($row->file_name)? "<a href='$_SERVER[SCRIPT_NAME]?course=$course_code&amp;get=".getIndirectReference($row->id)."&amp;file_type=1'>".q($row->file_name)."</a>"
                 . "<a href='$_SERVER[SCRIPT_NAME]?course=$course_code&amp;id=$id&amp;choice=do_delete_file' onClick='return confirmation(\"$m[WorkDeleteAssignmentFileConfirm]\");'>
                                      <img src='$themeimg/delete.png' title='$m[WorkDeleteAssignmentFile]' /></a>" : "<input type='file' id='userfile' name='userfile' />")."
                     </div>
@@ -3210,7 +3212,7 @@ function show_edit_assignment($id) {
                         }
                         $tool_content .= "<div class='radio $class_not_visible'>
                           <label $label>
-                            <input type='radio' id='rubrics_button' name='grading_type' value='2'". ($grading_type==ASSIGNMENT_RUBRIC_GRADE ? " checked" : "") ." $lti_group_disabled $addon>                
+                            <input type='radio' id='rubrics_button' name='grading_type' value='2'". ($grading_type==ASSIGNMENT_RUBRIC_GRADE ? " checked" : "") ." $lti_group_disabled $addon>
                             $langGradeRubrics
                           </label>
                         </div>
@@ -4542,11 +4544,12 @@ function assignment_details($id, $row, $x =false) {
                 </div>
             </div>";
         }
+        $file_indirect = getIndirectReference($row->id);
         if (isset($_GET['unit'])) {
             $unit = intval($_GET['unit']);
-            $fileUrl = "{$urlServer}modules/units/view.php?course=$course_code&amp;res_type=assignment&amp;get=$row->id&amp;file_type=1&amp;id=$unit";
+            $fileUrl = "{$urlServer}modules/units/view.php?course=$course_code&amp;res_type=assignment&amp;get=$file_indirect&amp;file_type=1&amp;id=$unit";
         } else {
-            $fileUrl = "{$urlServer}modules/work/?course=$course_code&amp;get=$row->id&amp;file_type=1";
+            $fileUrl = "{$urlServer}modules/work/?course=$course_code&amp;get=$file_indirect&amp;file_type=1";
         }
         if (!empty($row->file_name)) {
             $filelink = MultimediaHelper::chooseMediaAhrefRaw($fileUrl, $fileUrl, $row->file_name, $row->file_name);
@@ -4885,7 +4888,7 @@ function show_assignment($id, $display_graph_results = false) {
                             } else {
                                 $filename = $item->file_name;
                             }
-                            $url = "{$urlAppend}modules/work/index.php?course=$course_code&amp;get=$item->id";
+                            $url = "{$urlAppend}modules/work/index.php?course=$course_code&amp;get=" . getIndirectReference($item->id);
                             return MultimediaHelper::chooseMediaAhrefRaw($url, $url, $item->file_name, $filename);
                         }, $allFiles));
                     }
@@ -4976,10 +4979,10 @@ function show_assignment($id, $display_graph_results = false) {
                     } else {
                         $grade_comments = "&nbsp;<span>" . nl2br($grade_comments) . "</span>&nbsp;&nbsp;";
                     }
-                    $comments = '<strong>'.$m['gradecomments'] . '</strong>:' . $grade_comments . "
-                            <span class='small'>
-                                <a href='$_SERVER[SCRIPT_NAME]?course=$course_code&amp;getcomment=$row->id'>" . q($row->grade_comments_filename) . "</a>
-                            </span>";
+                    $comment_file_indirect = getIndirectReference($row->id);
+                    $fileUrl = "$_SERVER[SCRIPT_NAME]?course=$course_code&amp;getcomment=$comment_file_indirect";
+                    $fileLink = MultimediaHelper::chooseMediaAhrefRaw($fileUrl, $fileUrl, $row->grade_comments_filename, $row->grade_comments_filename);
+                    $comments = '<strong>'.$m['gradecomments'] . '</strong>:' . $grade_comments . "<span class='small'>$fileLink</span>";
                 }
                 $tool_content .= "<div style='padding-top: .5em;'>$comments $label</div>";
                 if($autojudge->isEnabled() and $auto_judge_enabled_assign) {
