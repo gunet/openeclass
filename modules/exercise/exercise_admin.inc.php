e4015a45a3497f7ed604973a205794c6c460ec7a
Yannis Exidaridis <jexi@noc.uoa.gr>
2021-03-18 12:55:15 +0200

    Exercises: fix link in 'exercises settings'
    --HG--
    branch : 3.10

--- modules/exercise/exercise_admin.inc.php
+++ modules/exercise/exercise_admin.inc.php
@@ -639,7 +639,7 @@ if (isset($_GET['modifyExercise']) or isset($_GET['NewExercise'])) {
           'level' => 'primary-label',
           'button-class' => 'btn-danger' ],
         [ 'title' => $langGroupProperties,
-          'url' => "$_SERVER[SCRIPT_NAME]?$course_code&amp;exerciseId=$exerciseId&amp;modifyExercise=yes",
+          'url' => "$_SERVER[SCRIPT_NAME]?course=$course_code&amp;exerciseId=$exerciseId&amp;modifyExercise=yes",
           'icon' => 'fa-edit',
           'level' => 'primary-label',
           'button-class' => 'btn btn-success' ],
