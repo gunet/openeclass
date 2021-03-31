00bdb661473231639ff9ffd7f27c0dc1b59c904a
Alexandros Diamantidis <adia@noc.uoa.gr>
2021-03-31 15:25:47 +0300

    phpInfo: improve styling (add padding to td, th)
    --HG--
    branch : 3.10

--- modules/admin/phpInfo.php
+++ modules/admin/phpInfo.php
@@ -42,28 +42,27 @@ $tool_content .= action_bar(array(
 $tool_content .= "<div class='alert alert-info'>";
 ob_start();
 phpinfo();
-$tool_content .= standard_text_escape(ob_get_contents());
+$tool_content .= preg_replace('/,([a-z])/', ', \1', standard_text_escape(ob_get_contents()));
 ob_end_clean();
 $tool_content .= '</div>';
 
-$local_head_contents = '<style type="text/css">
-        pre {margin: 0px; font-family: monospace;}
-        table {border-collapse: collapse;}
-        .center {text-align: center;}
-        .center table { margin-left: auto; margin-right: auto; text-align: left;}
+$local_head_contents = '
+    <style type="text/css">
+        pre { margin: 0px; font-family: monospace; }
+        table { border-collapse: collapse; margin: 5px; }
+        .center { text-align: center; }
+        .center table { margin-left: auto; margin-right: auto; text-align: left; }
         .center th { text-align: center !important; }
-        td, th { border: 1px solid #000000; font-size: 75%; vertical-align: baseline;}
-        h1 {font-size: 150%;}
-        h2 {font-size: 125%;}
-        .p {text-align: left;}
-        .e {background-color: #ccccff; font-weight: bold; color: #000000;}
-        .h {background-color: #9999cc; font-weight: bold; color: #000000;}
-        .v {background-color: #cccccc; color: #000000;}
-        .vr {background-color: #cccccc; text-align: right; color: #000000;}
-        img {float: right; border: 0px;}
-        hr {width: 600px; background-color: #cccccc; border: 0px; height: 1px; color: #000000;}
-        </style>
-        ';
+        td, th { border: 1px solid #000000; font-size: 75%; vertical-align: baseline; }
+        h1 { font-size: 150%; padding-left: 2em; padding-right: 2em; }
+        h2 { font-size: 125%; padding-left: 2em; padding-right: 2em; }
+        .p { text-align: left; }
+        .e { background-color: #ccccff; font-weight: bold; color: #000000; padding: 0 0.3em; }
+        .h { background-color: #9999cc; font-weight: bold; color: #000000; }
+        .v { background-color: #cccccc; color: #000000; padding: 0 0.3em; }
+        .vr { background-color: #cccccc; text-align: right; color: #000000; }
+        .h th, .v td { padding: 0 0.3em; }
+        hr { width: 600px; background-color: #cccccc; border: 0px; height: 1px; color: #000000; }
+    </style>';
 
 draw($tool_content, 3, '', $local_head_contents);
-
