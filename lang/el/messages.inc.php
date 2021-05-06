43fb4d559eb3c201e83465cd789a44ef633592c8
Yannis Exidaridis <jexi@noc.uoa.gr>
2021-05-06 13:59:20 +0300

    Exercises: change message 'ταίριασμα' το 'αντιστοίχιση' (grafted from 5cbcb4849ea46887fcbe167c80fff1e1a65915fe)

--- lang/el/messages.inc.php
+++ lang/el/messages.inc.php
@@ -1904,7 +1904,7 @@ $langFillBlanksStrict = "Αυστηρή Ταυτοποίηση";
 $langFillBlanksTolerant = "Χαλαρή Ταυτοποίηση";
 $langFillBlanksStrictExample = "(Αθήνα ≠ αθηνα ≠ ΑΘΗΝΑ)";
 $langFillBlanksTolerantExample = "(Αθήνα = αθηνα = ΑΘΗΝΑ)";
-$langMatching = "Ταίριασμα";
+$langMatching = "Αντιστοίχιση";
 $langTrueFalse = "Σωστό / Λάθος";
 $langFreeText = "Ελεύθερου Κειμένου";
 $langScale = "Κλίμακας";
