cae290d9428fe5128b2e1a69b14eb3d7a21ff576
Thanos Kyritsis <atkyritsis@upnet.gr>
2021-09-08 17:13:01 +0300

    h5p improve delete action
    --HG--
    branch : h5p_3.x

--- modules/h5p/delete.php
+++ modules/h5p/delete.php
@@ -19,12 +19,10 @@
  * For a full list of contributors, see "credits.txt".
  */
 
+$require_login = true;
+
 require_once 'test.php';
 
-$content_id = $_GET['id'];
-
-echo "This is delete.php<br>";
-
-if (delete_content($content_id)) {
-    header("location: index.php");
-}
+delete_content($_GET['id']);
+Session::Messages($langH5pDeleteSuccess, 'alert-success');
+redirect($urlAppend . 'modules/h5p/?course=' . $course_code);
