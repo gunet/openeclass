dcbe20510a8caaa89dfa112c33915b9c98533999
Thanos Kyritsis <atkyritsis@upnet.gr>
2021-09-20 17:14:06 +0300

    h5p proper handle editor uploaded files
    --HG--
    branch : h5p_3.x

--- modules/h5p/ajax.php
+++ modules/h5p/ajax.php
@@ -63,8 +63,82 @@ switch ($_GET['action']) {
         $editor->ajax->action(H5PEditorEndpoints::TRANSLATIONS, $language);
         break;
 
+    // Handle file upload through the editor.
+    // This endpoint needs a token that only users with H5P editor access could get.
+    case H5PEditorEndpoints::FILES:
+        $token = $_GET['token'];
+        $contentid = $_POST['contentId'];
+
+        $maxsize = getMaxUploadFileSize();
+        // Check size of each uploaded file.
+        foreach ($_FILES as $uploadedfile) {
+            if ($uploadedfile['size'] > $maxsize) {
+                $filename = cleanFilename($uploadedfile['name']);
+                H5PCore::ajaxError('maxbytesfile error for file: ' . $filename . " and size: " . $uploadedfile['size']);
+                return;
+            }
+        }
+
+        $editor->ajax->action(H5PEditorEndpoints::FILES, $token, $contentid);
+        break;
+
     // do nothing if AJAX action is not handled.
     default:
-        throw new Exception("Unhandled AJAX");
+        throw new Exception("Unhandled AJAX action: " . $_GET['action']);
         break;
+}
+
+function getMaxUploadFileSize(): int {
+    if (!$filesize = ini_get('upload_max_filesize')) {
+        $filesize = '5M';
+    }
+    $minimumsize = getRealSize($filesize);
+
+    if ($postsize = ini_get('post_max_size')) {
+        $postsize = getRealSize($postsize);
+        if ($postsize < $minimumsize) {
+            $minimumsize = $postsize;
+        }
+    }
+
+    return $minimumsize;
+}
+
+/**
+ * Converts numbers like 10M into bytes.
+ *
+ * @param string $size The size to be converted
+ * @return int
+ */
+function getRealSize(string $size): int {
+    if (!$size) {
+        return 0;
+    }
+
+    static $binaryprefixes = array(
+        'K' => 1024 ** 1,
+        'k' => 1024 ** 1,
+        'M' => 1024 ** 2,
+        'm' => 1024 ** 2,
+        'G' => 1024 ** 3,
+        'g' => 1024 ** 3,
+        'T' => 1024 ** 4,
+        't' => 1024 ** 4,
+        'P' => 1024 ** 5,
+        'p' => 1024 ** 5,
+    );
+
+    if (preg_match('/^([0-9]+)([KMGTP])/i', $size, $matches)) {
+        return $matches[1] * $binaryprefixes[$matches[2]];
+    }
+
+    return (int) $size;
+}
+
+function cleanFilename(string $val): string {
+    $val = preg_replace('~[[:cntrl:]]|[&<>"`\|\':\\\\/]~u', '', $val);
+    if ($val === '.' || $val === '..') {
+        $val = '';
+    }
+    return $val;
 }
