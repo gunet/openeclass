a319b398e25c1b1b789d0a9e02ccc3eed884136f
Yannis Exidaridis <jexi@noc.uoa.gr>
2021-11-18 11:00:04 +0200

    Assignments: fix error in assignments with grading scale
    --HG--
    branch : 3.12

--- include/main_lib.php
+++ include/main_lib.php
@@ -3976,14 +3976,14 @@ function closest($search, $arr) {
  *
  * @param $key string
  * @param $arr array
- * @return null|string|array
+ * @return array
  */
 function array_value_recursive($key, array $arr){
     $val = array();
     array_walk_recursive($arr, function($v, $k) use($key, &$val){
         if($k == $key) array_push($val, $v);
     });
-    return count($val) > 1 ? $val : array_pop($val);
+    return $val;
 }
 
 /**
