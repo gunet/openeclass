8b8f76ad51af576fe495c0fb51fd91b14b69394a
Yannis Exidaridis <jexi@noc.uoa.gr>
2021-11-22 13:33:24 +0200

    Admin / statistics: display users with most registered courses.
    --HG--
    branch : 3.12

--- modules/admin/otheractions.php
+++ modules/admin/otheractions.php
@@ -23,8 +23,8 @@
 /**
  * @file otheractions.php
  * @brief display other actions
- */ 
-  
+ */
+
 $require_admin = TRUE;
 require_once '../../include/baseTheme.php';
 require_once 'include/log.class.php';
@@ -74,8 +74,9 @@ $tool_content .= "<div class='table-responsive'>
                     <tr><td><a href='../usage/displaylog.php?from_other=TRUE'>$langSystemActions</a></td></tr>
                     <tr><td><a href='$_SERVER[SCRIPT_NAME]?stats=failurelogin'>$langLoginFailures</a><small> ($langLast15Days)</small></td></tr>
                     <tr><td><a href='$_SERVER[SCRIPT_NAME]?stats=musers'>$langMultipleUsers</a></td></tr>
-                    <tr><td><a href='$_SERVER[SCRIPT_NAME]?stats=memail'>$langMultipleAddr e-mail</a></td></tr>
+                    <tr><td><a href='$_SERVER[SCRIPT_NAME]?stats=memail'>$langMultipleAddr e-mail</a></td></tr>                    
                     <tr><td><a href='$_SERVER[SCRIPT_NAME]?stats=mlogins'>$langMultiplePairs LOGIN - PASS</a></td></tr>
+                    <tr><td><a href='$_SERVER[SCRIPT_NAME]?stats=cusers'>$langMultipleCourseUsers</a><small> ($langLast30Entries)</small></td></tr>
                     <tr><td><a href='$_SERVER[SCRIPT_NAME]?stats=vmusers'>$langMailVerification</a></td></tr>
                     <tr><td><a href='$_SERVER[SCRIPT_NAME]?stats=unregusers'>$langUnregUsers</a><small> ($langLastMonth)</small></td></tr>
                 </table>            
@@ -103,7 +104,7 @@ if (isset($_GET['stats'])) {
             $page_link = "&amp;stats=unregusers";
             $log = new Log();
             $log->display(0, -1, 0, LOG_DELETE_USER, $date_start, $date_end, $_SERVER['SCRIPT_NAME'], $limit, $page_link);
-            break;        
+            break;
         case 'musers':
             $tool_content .= "<div class='table-responsive'>
                             <table class='table-default'>";
@@ -119,11 +120,11 @@ if (isset($_GET['stats'])) {
                 $tool_content .= "<tr><td class='right' colspan='2'>" . ok_message() . "</td></tr>";
             }
             $tool_content .= "</table></div>";
-            break;        
+            break;
         case 'memail':
             $sqlLoginDouble = "SELECT DISTINCT email, COUNT(*) AS nb FROM user GROUP BY email
 				HAVING nb > 1 ORDER BY nb DESC";
-            $loginDouble = list_ManyResult($sqlLoginDouble, 'email');            
+            $loginDouble = list_ManyResult($sqlLoginDouble, 'email');
             $tool_content .= "<div class='table-responsive'>
                             <table class='table-default'>
                             <tr class='list-header'>
@@ -184,6 +185,31 @@ if (isset($_GET['stats'])) {
                         </ul>
                         </div></div>";
             break;
+        case 'cusers':
+            $q = Database::get()->queryArray("SELECT surname, givenname, username, user_id, count(course_id) AS num_of_courses 
+                                FROM course_user 
+                                    JOIN user 
+                                ON course_user.user_id = user.id 
+                                WHERE course_user.status=5 
+                                    GROUP BY user_id 
+                                    ORDER BY num_of_courses 
+                                    DESC 
+                                    LIMIT 0,30");
+            $tool_content .= "<div class='table-responsive'>
+                        <table class='table-default'>
+                        <tr>
+                            <th class='list-header'><strong>$langUsers</strong></th>
+                            <th class='text-center'><strong>$langResult</strong></th>
+                        </tr>";
+                        foreach ($q as $data) {
+                            $link = $urlServer . "modules/admin/edituser.php?u=" . $data->user_id;
+                            $tool_content .= "<tr>
+                            <td>" . $data->surname . " " . $data->givenname . " (<a href='$link'>" . $data->username . "</a>)</td>
+                            <td class='text-center'>" . $data->num_of_courses . "</td>
+                            </tr>";
+                        }
+                        $tool_content .= "</table></div>";
+                break;
         default:
             break;
     }
@@ -193,18 +219,16 @@ if (isset($_GET['stats'])) {
 draw($tool_content, 3, null, $head_content);
 
 /**
- * @brief output a <tr> with an array 
- * @param type $table
- * @param type $search
+ * @brief output a <tr> with an array
  * @return string
  */
 function tablize($table, $search) {
-    
+
     global $urlServer;
-    
-    $ret = "";    
+
+    $ret = "";
     if (is_array($table)) {
-        foreach ($table as $key => $thevalue) {            
+        foreach ($table as $key => $thevalue) {
             $ret .= "<tr>";
             switch($search) {
                 case 'email' : $link = $urlServer . "modules/admin/listusers.php?uname=&fname=&lname=&email=" . urlencode($key) . "&am=&user_type=0"
@@ -214,11 +238,11 @@ function tablize($table, $search) {
                 case 'username':
                 case 'pair': $link = $urlServer . "modules/admin/listusers.php?uname=" . urlencode($key) . "&fname=&lname=&email=&am=&user_type=0"
                                         . "&auth_type=0&reg_flag=1&user_registered_at=&verified_mail=3"
-                                        . "&department=0&search_type=contains";                                        
-                    break;                
+                                        . "&department=0&search_type=contains";
+                    break;
                 default : $link = '';
             }
-            
+
             $ret .= "<td style='font-size: 90%'><a href='$link'>" . $key . "</a></td>";
             $ret .= "<td class='right'><strong>" . $thevalue . "</strong></td></tr>";
         }
@@ -227,9 +251,8 @@ function tablize($table, $search) {
 }
 
 /**
- * @brief ok message 
- * @global type $langNotExist
- * @return type
+ * @brief ok message
+ * @return string
  */
 function ok_message() {
     global $langNotExist;
@@ -239,8 +262,7 @@ function ok_message() {
 
 /**
  * @brief error message
- * @global type $langExist
- * @return type
+ * @return string
  */
 function error_message($count) {
     global $langExist, $langRegisterActions;
@@ -249,17 +271,17 @@ function error_message($count) {
 }
 
 /**
- * 
+ *
  * @param type $sql
  * @param type $fieldname
- * @return type
+ * @return array
  */
 function list_ManyResult($sql, $fieldname) {
-    
+
     $resu = array();
     $res = Database::get()->queryArray($sql);
-    foreach ($res as $resA) {     
-        $name = $resA->$fieldname;     
+    foreach ($res as $resA) {
+        $name = $resA->$fieldname;
         $resu[$name] = $resA->nb;
     }
     return $resu;
