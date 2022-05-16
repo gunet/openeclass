8f6fe57e77b19cfafca8d63fd2a8d0718be6980f
Yannis Exidaridis <jexi@noc.uoa.gr>
2022-05-16 11:17:06 +0300

    Upgrade: Reorganize make asynchronous upgrade process.
    --HG--
    branch : 3.13

--- upgrade/index.php
+++ upgrade/index.php
@@ -38,7 +38,7 @@ if ($language == 'el') {
 }
 
 // check PHP version
-if (version_compare(PHP_VERSION, '7.2') < 0) {
+if (version_compare(PHP_VERSION, '7.3') < 0) {
     $tool_content .= "<div class='alert alert-danger'>$langWarnAboutPHP</div>";
 }
 
