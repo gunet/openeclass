c9bcc92423049f3c627efd4c17fb89406b63a14a
Yannis Exidaridis <jexi@noc.uoa.gr>
2022-06-29 10:41:10 +0300

    Merge changes from 4.0-gui branch

--- resources/views/modern/info/about.blade.php
+++ resources/views/modern/info/about.blade.php
@@ -12,13 +12,29 @@
 
                 <div class="row p-5">
 
-                    @include('layouts.common.breadcrumbs', ['breadcrumbs' => [0 => ['bread_href' => 'about.php', 'bread_text' => trans('langInfo') ]]])
+                    <div class='col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mt-3'>
+                        <nav class="navbar navbar-expand-lg navrbar_menu_btn">
+                            <a type="button" id="getTopicButton" class="d-none d-sm-block d-md-none d-lg-block ms-2 btn btn-primary btn btn-primary" href="{{$urlAppend}}modules/help/help.php?language={{$language}}&topic={{$helpTopic}}&subtopic={{$helpSubTopic}}" style='margin-top:-10px'>
+                                <i class="fas fa-question"></i>
+                            </a>
+                        </nav>
+                        @include('layouts.common.breadcrumbs', ['breadcrumbs' => $breadcrumbs])
+                    </div>
+
+                    <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mt-3">
+                        <legend class="float-none w-auto py-2 px-4 notes-legend"><span style="margin-left:-20px;"><i class="fa fa-credit-card fa-fw"></i> {{trans('langPlatformIdentity')}}</span></legend>
+                        <div class="row p-2"></div>
+                    </div>
+
+
 
                     <div class='col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mt-3'>
                         <div class='row'>
+                            <div class='text-start text-secondary'>{{trans('langEclass')}} - {{trans('langInfo')}}</div>
                             {!! $action_bar !!}
                         </div>
                     </div>
+
 
                     <div class='col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mt-3'>
                         <div class='panel shadow-lg p-3 mb-5 bg-body rounded bg-primary'>
