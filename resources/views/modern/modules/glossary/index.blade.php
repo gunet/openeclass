40072f46f8dd91c2ff1764f625b8782b83c6dcb0
Yannis Exidaridis <jexi@noc.uoa.gr>
2022-07-13 14:50:20 +0300

    Glossary: fix errors in view

--- resources/views/modern/modules/glossary/index.blade.php
+++ resources/views/modern/modules/glossary/index.blade.php
@@ -1,8 +1,3 @@
-<?php 
-    $lesson = Database::get()->querySingle("SELECT * FROM `course` WHERE `course`.`title`='{$title_course}' ");
-    $course_code_title = $lesson->code;  
-    $course_Teacher = $lesson->prof_names;
-?>
 @extends('layouts.default')
 
 @section('content')
@@ -13,14 +8,14 @@
 
             <div class="row">
 
-                <div id="background-cheat-leftnav" class="col-xl-2 col-lg-2 col-md-0 col-sm-0 col-0 justify-content-center col_sidebar_active"> 
+                <div id="background-cheat-leftnav" class="col-xl-2 col-lg-2 col-md-0 col-sm-0 col-0 justify-content-center col_sidebar_active">
                     <div class="d-none d-sm-block d-sm-none d-md-block d-md-none d-lg-block">
                         @include('layouts.partials.sidebar',['is_editor' => $is_editor])
                     </div>
                 </div>
 
                 <div class="col-xl-10 col-lg-10 col-md-12 col-sm-12 col-12 justify-content-center col_maincontent_active">
-                    
+
                         <div class="row p-lg-5 p-md-5 ps-1 pe-2 pt-5 pb-5">
 
                                     <nav class="navbar navbar-expand-lg navrbar_menu_btn">
@@ -28,8 +23,8 @@
                                             <i class="fas fa-align-left"></i>
                                             <span></span>
                                         </button>
-                                        
-                                    
+
+
                                         <a class="btn btn-primary d-lg-none mr-auto" type="button" data-bs-toggle="offcanvas" href="#collapseTools" role="button" aria-controls="collapseTools" style="margin-top:-10px;">
                                             <i class="fas fa-tools"></i>
                                         </a>
@@ -56,7 +51,7 @@
 
                                             <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 bg-white">{!! isset($action_bar) ?  $action_bar : '' !!}</div>
                                             <div class="row p-2"></div>
-                                        
+
                                     @endif
 
                                     @if(Session::has('message'))
@@ -68,16 +63,16 @@
                                     </div>
                                     @endif
 
-                                    
+
 
                                     @if ($is_editor == 1 && $expand_glossary && $total_glossary_terms > $max_glossary_terms)
                                         <div class='alert alert-warning'>{!! trans('langGlossaryOverLimit',["<b>$max_glossary_terms</b>"]) !!}</div>
-                                    @endif   
+                                    @endif
 
                                     @if ($glossary_index && count($prefixes) > 1)
                                     <div class="row p-2"></div>
                                     <nav>
-                                        
+
                                         <ul class="pagination">
                                             <small>{{trans('langGlossaryIndex')}}:</small>
                                         @foreach ($prefixes as $key => $letter)
@@ -86,15 +81,15 @@
                                         @endforeach
                                         </ul>
                                     </nav>
-                                    
+
                                     @endif
 
-                                
+
                                     @if ($glossary_terms)
-                                    
+
                                         <div class='table-responsive glossary-categories' style="">
                                             <table class='table' id="glossary_table" style="overflow: inherit">
-                                            
+
                                                 <thead class="notes_thead text-light">
                                                     <tr>
                                                         <th scope="col"><span class="notes_th_comment">#</span></th>
@@ -104,17 +99,15 @@
                                                         <th scope="col"><span class="notes_th_comment">URL</span></th>
                                                         <th scope="col"><span class="notes_th_comment">{{ trans("langComments") }}</span></th>
                                                         @if($is_editor == 1)
-                                                            
+
                                                                 <th scope="col"><span class="notes_th_comment"><i class='fas fa-cogs'></i></span></th>
-                                                            
+
                                                         @endif
                                                     </tr>
                                                 </thead>
                                                 <tbody>
                                                     <?php $i=0; ?>
                                                     @foreach ($glossary_terms as $glossary_term)
-                                                    <?php $tmp_edit_id = Database::get()->querySingle("SELECT * FROM `glossary` WHERE `glossary`.`term`='{$glossary_term->term}' ");?>
-                                                    <?php $edit_id = $tmp_edit_id->id; ?>
                                                     <?php $i++; ?>
                                                     <tr>
                                                             <th scope="row">{{$i}}</th>
@@ -130,32 +123,21 @@
                                                             </td>
                                                             <td>
                                                                 @if ($glossary_term->category_id)
-                                                                
-                                                                    
-                                                                    <a href='{{ $base_url }}&amp;cat={{ getIndirectReference($glossary_term->category_id) }}'> 
+
+
+                                                                    <a href='{{ $base_url }}&amp;cat={{ getIndirectReference($glossary_term->category_id) }}'>
                                                                         {{ $categories[$glossary_term->category_id] }}
                                                                     </a>
-                                                                
+
                                                                 @endif
                                                             </td>
                                                             <td>
                                                                 @if ($glossary_term->url)
-                                                                    <!-- <div>
-                                                                    
-                                                                                <a href='{{ $glossary_term->url }}' target='_blank'>
-                                                                                    {{ $glossary_term->url }}&nbsp;&nbsp;<i class='fas fa-external-link' style='color:#444;'></i>
-                                                                                </a>
-                                                                        
-                                                                    </div>       -->
-                                                                    
-                                                                    
 
                                                                     <a class="content-truncate-announcement" data-bs-toggle="modal" role="button" aria-expanded="false" data-bs-target="#ModalUrl{{$i}}">
-                                                                        <?php $content_myann = strip_tags($myann->content); ?>
                                                                         <span class="d-inline-block text-truncate" style="max-width: 180px;"><i class="fas fa-arrow-down"></i>{{ $glossary_term->url }}</span>
                                                                     </a>
 
-                                                                    
                                                                     <div class="modal fade modalAnnouncement" id="ModalUrl{{$i}}" tabindex="-1" aria-labelledby="ModalUrl{{$i}}" aria-hidden="true">
                                                                         <div class="modal-dialog modal-xl">
                                                                             <div class="modal-content">
@@ -170,22 +152,17 @@
                                                                             </div>
                                                                         </div>
                                                                     </div>
-
-
-
                                                                 @endif
                                                             </td>
                                                             <td>
                                                                 @if ($glossary_term->notes)
-                                                                    
                                                                         {!! standard_text_escape($glossary_term->notes) !!}
-                                                                    
                                                                 @endif
                                                             </td>
                                                             @if($is_editor)
                                                             <td>
-                                                       
-                                                                {!! 
+
+                                                                {!!
                                                                     action_button(array(
                                                                         array('title' => trans('langEditChange'),
                                                                             'url' => $base_url ."&amp;edit=". getIndirectReference($glossary_term->id),
@@ -196,17 +173,12 @@
                                                                             'class' => 'delete',
                                                                             'confirm' => trans('langConfirmDelete'))
                                                                         )
-                                                                    ) 
-                                                                !!}                          
-                                                                   
-                                                    
+                                                                    )
+                                                                !!}
 
                                                             </td>
                                                             @endif
-                                                        
                                                     </tr>
-                                                
-                                                        
                                                     @endforeach
                                                 </tbody>
                                             </table>
@@ -214,11 +186,8 @@
                                     @else
                                         <div class='xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12'><div class='alert alert-warning'>{{trans('langAnalyticsNotAvailable')}} {{trans('langGlossary')}}.</div></div>
                                     @endif
-                                   
-                                    
-                            
                         </div>
-                    
+
                 </div>
             </div>
         </div>
