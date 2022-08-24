4d62111c3ea0480e000da3cbcd885d6f1d08c041
Yannis Exidaridis <jexi@noc.uoa.gr>
2022-08-24 11:00:13 +0300

    Install / Upgrade: Chech for required 'intl' php module

--- upgrade/upgrade.php
+++ upgrade/upgrade.php
@@ -393,6 +393,7 @@ if (!isset($_POST['submit2']) and isset($_SESSION['is_admin']) and $_SESSION['is
     warnIfExtNotLoaded('pcre');
     warnIfExtNotLoaded('curl');
     warnIfExtNotLoaded('zip');
+    warnIfExtNotLoaded('intl');
     $tool_content .= "</ul><h5>$langOptionalPHP</h5>";
     $tool_content .= "<ul class='list-unstyled'>";
     warnIfExtNotLoaded('soap');
