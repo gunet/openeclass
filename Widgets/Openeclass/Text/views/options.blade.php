b15f459f05da06bf478092e95503144305636061
Yannis Exidaridis <jexi@noc.uoa.gr>
2022-12-16 12:24:21 +0200

    Widget: fix `text/html` widget.

--- Widgets/Openeclass/Text/views/options.blade.php
+++ Widgets/Openeclass/Text/views/options.blade.php
@@ -1,16 +1,16 @@
-                            <form id="optionsForm{{ $widget_widget_area_id }}">
-                                <ul class="nav nav-tabs" role="tablist">
-                                    @foreach ($active_ui_languages as $key => $active_ui_language)
-                                      <li role="presentation" class="{{ $key == 0 ? 'active' : '' }}">
-                                          <a href="#{{ $active_ui_language . $widget_widget_area_id }}" id="home-tab" role="tab" data-toggle="tab" aria-controls="{{ $active_ui_language . $widget_widget_area_id}}" aria-expanded="{{ $key == 0 ? 'true' : 'false' }}">{{ $native_language_names_init[$active_ui_language] }}</a>
-                                      </li>
-                                    @endforeach
-                                </ul>
-                                 <div class="tab-content">
-                                    @foreach ($active_ui_languages as $key => $active_ui_language)
-                                        <div role="tabpanel" class="tab-pane fade{{ $key == 0 ? ' active in' : '' }}" id="{{ $active_ui_language . $widget_widget_area_id }}" aria-labelledby="{{ $active_ui_language . $widget_widget_area_id}}-tab">
-                                            <textarea class='form-control' name="text_{{ $active_ui_language }}">{{ isset(${'text_'.$active_ui_language}) ? ${'text_'.$active_ui_language} : '' }}</textarea>
-                                        </div>
-                                    @endforeach
-                                </div>                           
-                            </form>
+<form id="optionsForm{{ $widget_widget_area_id }}">
+    <ul class="nav nav-pills" role="tablist">
+        @foreach ($active_ui_languages as $key => $active_ui_language)
+          <li class="nav-item" role="presentation">
+              <button class="nav-link {{ $key == 0 ? 'active' : '' }}" id="{{ $active_ui_language . $widget_widget_area_id}}-tab" data-bs-toggle="tab" data-bs-target="#{{ $active_ui_language . $widget_widget_area_id }}-tab-pane" type="button" role="tab" aria-controls="{{ $active_ui_language . $widget_widget_area_id}}-tab-pane" aria-selected="true">{{ $native_language_names_init[$active_ui_language] }}</button>
+          </li>
+        @endforeach
+    </ul>
+     <div class="tab-content">
+        @foreach ($active_ui_languages as $key => $active_ui_language)
+            <div class="tab-pane fade {{ $key == 0 ? 'show active' : '' }}" id="{{ $active_ui_language . $widget_widget_area_id }}-tab-pane" role="tabpanel"  aria-labelledby="{{ $active_ui_language . $widget_widget_area_id}}-tab" tabindex="0">
+                <textarea class='form-control' rows='3' cols='40' name="text_{{ $active_ui_language }}">{{ isset(${'text_'.$active_ui_language}) ? ${'text_'.$active_ui_language} : '' }}</textarea>
+            </div>
+        @endforeach
+    </div>
+</form>
