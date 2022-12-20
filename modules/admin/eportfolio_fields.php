f278911c4cde6aad712054de545986c45562b7e1
Yannis Exidaridis <jexi@noc.uoa.gr>
2022-12-20 12:45:49 +0200

    Admin tool - ePortfolio: fix notices

--- modules/admin/eportfolio_fields.php
+++ modules/admin/eportfolio_fields.php
@@ -1,7 +1,7 @@
 <?php
 
 /* ========================================================================
- * Open eClass 
+ * Open eClass
  * E-learning and Course Management System
  * ========================================================================
  * Copyright 2003-2014  Greek Universities Network - GUnet
@@ -17,7 +17,7 @@
  *                  Network Operations Center, University of Athens,
  *                  Panepistimiopolis Ilissia, 15784, Athens, Greece
  *                  e-mail: info@openeclass.org
- * ======================================================================== 
+ * ========================================================================
  */
 
 define('EPF_TEXTBOX', 1);
@@ -34,9 +34,9 @@ $navigation[] = array('url' => 'index.php', 'name' => $langAdmin);
 
 if (isset($_GET['add_cat'])) { //add a new category form
     load_js('validation.js');
-    
+
     $pageName = $langCategoryAdd;
-    
+
     $tool_content .= action_bar(array(
         array('title' => $langBack,
               'url' => "eportfolio_fields.php",
@@ -76,17 +76,17 @@ if (isset($_GET['add_cat'])) { //add a new category form
     //delete category
     Database::get()->query("DELETE FROM eportfolio_fields_category WHERE id = ?d", $catid);
     //Session::Messages($langEPFCatDelSuccess, 'alert-success');
-    Session::flash('message',$langEPFCatDelSuccess); 
+    Session::flash('message',$langEPFCatDelSuccess);
     Session::flash('alert-class', 'alert-success');
     redirect_to_home_page("modules/admin/eportfolio_fields.php");
 } elseif (isset($_GET['edit_cat'])) { //edit category form
     $catid = intval(getDirectReference($_GET['edit_cat']));
     $cat_name = Database::get()->querySingle("SELECT name FROM eportfolio_fields_category WHERE id = ?d", $catid)->name;
-    
+
     load_js('validation.js');
-    
+
     $pageName = $langCategoryMod;
-    
+
     $tool_content .= action_bar(array(
         array('title' => $langBack,
               'url' => "eportfolio_fields.php",
@@ -115,17 +115,17 @@ if (isset($_GET['add_cat'])) { //add a new category form
     //]]></script>';
 } elseif (isset($_GET['add_field'])) { //add new field form (first step)
     $catid = intval(getDirectReference($_GET['add_field']));
-    
+
     $pageName = $langAddField;
-    
+
     $tool_content .= action_bar(array(
             array('title' => $langBack,
                   'url' => "eportfolio_fields.php",
                   'icon' => 'fa-reply',
                   'level' => 'primary-label')));
-    
+
     $field_types = array(EPF_TEXTBOX => $langCPFText, EPF_TEXTAREA => $langCPFTextarea, EPF_DATE => $langCPFDate, EPF_MENU => $langCPFMenu, EPF_LINK =>$langCPFLink );
-    
+
     $tool_content .= "
     <div class='row'>
     <div class='col-lg-6 col-12 d-none d-md-none d-lg-block'>
@@ -142,25 +142,25 @@ if (isset($_GET['add_cat'])) { //add a new category form
     $tool_content .= "</div>";
     $tool_content .= "<div class='col-12 mt-5 d-flex justify-content-center align-items-center'><input class='btn submitAdminBtn' type='submit' name='add_field_proceed_step2' value='$langNext'></div>";
     $tool_content .= "</fieldset>". generate_csrf_token_form_field() ."</form></div></div></div>";
-    
+
 } elseif (isset($_POST['add_field_proceed_step2'])) { //add new field form 2nd step
     if (!isset($_POST['token']) || !validate_csrf_token($_POST['token'])) csrf_token_error();
     $catid = intval(getDirectReference($_POST['catid']));
-    
+
     load_js('validation.js');
-    
+
     $pageName = $langAddField;
-    
+
     $tool_content .= action_bar(array(
         array('title' => $langBack,
               'url' => "eportfolio_fields.php?add_field=" . getIndirectReference($catid),
               'icon' => 'fa-reply',
               'level' => 'primary-label')));
-    
+
     $yes_no = array(0 => $langNo, 1 => $langYes);
-    
+
     $datatype = intval($_POST['datatype']);
-    
+
     $tool_content .= "
     <div class='row'>
     <div class='col-lg-6 col-12 d-none d-md-none d-lg-block'>
@@ -180,8 +180,11 @@ if (isset($_GET['add_cat'])) { //add a new category form
     $tool_content .= "<label for='shortname' class='col-sm-12 control-label-notes'>$langCPFShortName <small>($langEPFUniqueShortname)</small></label>
                       <div class='col-sm-12'><input id='shortname' class='form-contro' type='text' name='field_shortname'></div>";
     $tool_content .= "</div>";
-    $tool_content .= "<div class='form-group mt-4'><label for='fielddescr' class='col-sm-12 control-label-notes'>$langCPFFieldDescr</label>
+
+    $tool_content .= "<div class='form-group mt-4'><label for='fielddescr' class='col-sm-12 control-label-notes'>$langDescription</label>
                       <div class='col-sm-12'>".rich_text_editor('fielddescr', 8, 20, '')."</div>";
+
+
     $tool_content .= "</div>";
     $tool_content .= "<div class='form-group mt-4'>";
     $tool_content .= "<label for='required' class='col-sm-12 control-label-notes'>$langCPFFieldRequired</label>
@@ -195,7 +198,7 @@ if (isset($_GET['add_cat'])) { //add a new category form
     }
     $tool_content .= "<div class='col-12 mt-5 d-flex justify-content-center align-items-center'>".showSecondFactorChallenge()."<input class='btn submitAdminBtn' type='submit' name='submit_field' value='$langAdd'></div>";
     $tool_content .= "</fieldset>". generate_csrf_token_form_field() ."</form></div></div></div>";
-    
+
     $tool_content .='<script language="javaScript" type="text/javascript">
         //<![CDATA[
             var chkValidator  = new Validator("fieldForm");
@@ -204,9 +207,9 @@ if (isset($_GET['add_cat'])) { //add a new category form
         ';
     if ($datatype == EPF_MENU) {
         $tool_content .= 'chkValidator.addValidation("options","req","' . $langCPFMenuOptionsAlert . '");';
-    }        
+    }
     $tool_content .= '//]]></script>';
-                
+
 } elseif (isset($_POST['submit_field'])) {
     if (!isset($_POST['token']) || !validate_csrf_token($_POST['token'])) csrf_token_error();
     checkSecondFactorChallenge();
@@ -225,10 +228,10 @@ if (isset($_GET['add_cat'])) { //add a new category form
     } else {
         $data = '';
     }
-    
+
     if (isset($_POST['field_id'])) { //save edited field
         $fieldid = intval(getDirectReference($_POST['field_id']));
-        
+
         //check for unique shortname
         $is_unique = true;
         $old_shortname = Database::get()->querySingle("SELECT shortname FROM eportfolio_fields WHERE id = ?d", $fieldid)->shortname;
@@ -238,7 +241,7 @@ if (isset($_GET['add_cat'])) { //add a new category form
                 $is_unique = false;
             }
         }
-        
+
         if ($is_unique) {
             Database::get()->query("UPDATE eportfolio_fields SET name = ?s,
                                     shortname = ?s,
@@ -248,39 +251,39 @@ if (isset($_GET['add_cat'])) { //add a new category form
                                     data = ?s
                                     WHERE id = ?d", $name, $shortname, $description, $datatype, $required, $data, $fieldid);
             //Session::Messages($langEPFFieldEditSuccess, 'alert-success');
-            Session::flash('message',$langEPFFieldEditSuccess); 
+            Session::flash('message',$langEPFFieldEditSuccess);
             Session::flash('alert-class', 'alert-success');
             redirect_to_home_page("modules/admin/eportfolio_fields.php");
         } else {
             //Session::Messages($langEPFEditUniqueShortnameError, 'alert-danger');
-            Session::flash('message',$langEPFEditUniqueShortnameError); 
+            Session::flash('message',$langEPFEditUniqueShortnameError);
             Session::flash('alert-class', 'alert-danger');
             redirect_to_home_page("modules/admin/eportfolio_fields.php");
         }
     } else { //save new field
-        
+
         //check for unique shortname
         $count = Database::get()->querySingle("SELECT COUNT(*) AS c FROM eportfolio_fields WHERE shortname = ?s", $shortname)->c;
         if ($count == 0) { //shortname is unique, proceed
-        
+
             $catid = intval(getDirectReference($_POST['catid']));
-            
+
             $result = Database::get()->querySingle("SELECT MIN(sortorder) AS m FROM eportfolio_fields WHERE categoryid = ?d", $catid);
             if (!is_null($result->m)) {
                 $sortorder = $result->m - 1;
-            } else { 
+            } else {
                 $sortorder = 0;
             }
-            
+
             Database::get()->query("INSERT INTO eportfolio_fields (shortname, name, description, datatype, categoryid, sortorder, required, data) 
                                     VALUES (?s, ?s, ?s, ?d, ?d, ?d, ?d, ?s)", $shortname, $name, $description, $datatype, $catid, $sortorder, $required, $data);
             //Session::Messages($langEPFFieldAddSuccess, 'alert-success');
-            Session::flash('message',$langEPFFieldAddSuccess); 
+            Session::flash('message',$langEPFFieldAddSuccess);
             Session::flash('alert-class', 'alert-success');
             redirect_to_home_page("modules/admin/eportfolio_fields.php");
         } else { //shortname is not unique, abort
             //Session::Messages($langCPFCreateUniqueShortnameError, 'alert-danger');
-            Session::flash('message',$langCPFCreateUniqueShortnameError); 
+            Session::flash('message',$langCPFCreateUniqueShortnameError);
             Session::flash('alert-class', 'alert-danger');
             redirect_to_home_page("modules/admin/eportfolio_fields.php");
         }
@@ -294,21 +297,21 @@ if (isset($_GET['add_cat'])) { //add a new category form
     //delete field
     Database::get()->query("DELETE FROM eportfolio_fields WHERE id = ?d", $fieldid);
     //Session::Messages($langEPFFieldDelSuccess, 'alert-success');
-    Session::flash('message',$langEPFFieldDelSuccess); 
+    Session::flash('message',$langEPFFieldDelSuccess);
     Session::flash('alert-class', 'alert-success');
     redirect_to_home_page("modules/admin/eportfolio_fields.php");
 } elseif (isset($_GET['edit_field'])) { //save edited field
     $pageName = $langCPFFieldEdit;
-    
+
     $tool_content .= action_bar(array(
         array('title' => $langBack,
               'url' => "eportfolio_fields.php",
               'icon' => 'fa-reply',
               'level' => 'primary-label')));
-              
+
     $fieldid = intval(getDirectReference($_GET['edit_field']));
     $result = Database::get()->querySingle("SELECT * FROM eportfolio_fields WHERE id = ?d", $fieldid);
-   
+
     if ($result) {
         $name = q($result->name);
         $shortname = q($result->shortname);
@@ -316,7 +319,7 @@ if (isset($_GET['add_cat'])) { //add a new category form
         $datatype = $result->datatype;
         $required = $result->required;
         $data = $result->data;
-        
+
         if ($datatype == EPF_MENU) {
             $data = unserialize($data);
             $textarea_val = '';
@@ -325,12 +328,12 @@ if (isset($_GET['add_cat'])) { //add a new category form
             }
             $textarea_val = substr($textarea_val, 0, strlen($textarea_val)-1);
         }
-        
+
         load_js('validation.js');
-        
+
         $field_types = array(EPF_TEXTBOX => $langCPFText, EPF_TEXTAREA => $langCPFTextarea, EPF_DATE => $langCPFDate, EPF_MENU => $langCPFMenu, EPF_LINK =>$langCPFLink);
         $yes_no = array(0 => $langNo, 1 => $langYes);
-        
+
         $tool_content .= "
         <div class='row'>
         <div class='col-lg-6 col-12 d-none d-md-none d-lg-block'>
@@ -350,8 +353,9 @@ if (isset($_GET['add_cat'])) { //add a new category form
         $tool_content .= "<label for='shortname' class='col-sm-12 control-label-notes'>$langCPFShortName <small>($langEPFUniqueShortname)</small></label>
                           <div class='col-sm-12'><input id='shortname' class='form-control' type='text' name='field_shortname' value='$shortname'></div>";
         $tool_content .= "</div>";
-        $tool_content .= "<div class='form-group mt-4'><label for='fielddescr' class='col-sm-12 control-label-notes'>$langCPFFieldDescr</label>
+        $tool_content .= "<div class='form-group mt-4'><label for='fielddescr' class='col-sm-12 control-label-notes'>$langDescription</label>
                           <div class='col-sm-12'>".rich_text_editor('fielddescr', 8, 20, $description)."</div>";
+
         $tool_content .= "</div>";
         $tool_content .= "<div class='form-group mt-4'>";
         $tool_content .= "<label for='datatype' class='col-sm-12 control-label-notes'>$langCPFFieldDatatype</label>
@@ -369,7 +373,7 @@ if (isset($_GET['add_cat'])) { //add a new category form
         }
         $tool_content .= "<div class='col-12 mt-5 d-flex justify-content-center align-items-center'><input class='btn submitAdminBtn' type='submit' name='submit_field' value='$langSave'></div>";
         $tool_content .= "</fieldset>". generate_csrf_token_form_field() ."</form></div></div></div>";
-        
+
         $tool_content .='<script language="javaScript" type="text/javascript">
                         //<![CDATA[
                             var chkValidator  = new Validator("fieldForm");
@@ -381,7 +385,7 @@ if (isset($_GET['add_cat'])) { //add a new category form
         }
         $tool_content .= '//]]></script>';
     } else {
-        
+
     }
 } elseif (isset($_POST['submit_cat'])) {
     if (!isset($_POST['token']) || !validate_csrf_token($_POST['token'])) csrf_token_error();
@@ -390,7 +394,7 @@ if (isset($_GET['add_cat'])) { //add a new category form
         $catid = intval(getDirectReference($_POST['cat_id']));
         Database::get()->query("UPDATE eportfolio_fields_category SET name = ?s WHERE id = ?d", $_POST['cat_name'], $catid);
         //Session::Messages($langEPFCatModSuccess, 'alert-success');
-        Session::flash('message',$langEPFCatModSuccess); 
+        Session::flash('message',$langEPFCatModSuccess);
         Session::flash('alert-class', 'alert-success');
         redirect_to_home_page("modules/admin/eportfolio_fields.php");
     } else { //save new category
@@ -403,30 +407,30 @@ if (isset($_GET['add_cat'])) { //add a new category form
         }
         Database::get()->query("INSERT INTO eportfolio_fields_category (name, sortorder) VALUES (?s, ?d)", $_POST['cat_name'], $sortorder);
         //Session::Messages($langEPFCatAddedSuccess, 'alert-success');
-        Session::flash('message',$langEPFCatAddedSuccess); 
+        Session::flash('message',$langEPFCatAddedSuccess);
         Session::flash('alert-class', 'alert-success');
         redirect_to_home_page("modules/admin/eportfolio_fields.php");
     }
 } elseif (isset($_POST['cats'])) { //save sort order
     $cats_counter = count($_POST['cats']);
     $fields_counter = count($_POST['fields']);
-    
+
     foreach ($_POST['cats'] as $cat) {
         $cat_id = getDirectReference(substr($cat, 4));
         Database::get()->query("UPDATE eportfolio_fields_category SET sortorder = ?d WHERE id = ?d", $cats_counter, $cat_id);
         $cats_counter--;
     }
-    
+
     foreach ($_POST['fields'] as $field) {
         $field_id = getDirectReference(substr($field, 6));
         Database::get()->query("UPDATE eportfolio_fields SET sortorder = ?d, categoryid=?d WHERE id = ?d", $fields_counter, getDirectReference(substr($_POST['fields_cat'][$field], 4)), $field_id);
         $fields_counter--;
     }
-    
+
     exit;
 } else { //show categories and fields list
     load_js('sortable');
-    
+
     $tool_content .= action_bar(array(
         array('title' => $langCategoryAdd,
               'url' => "eportfolio_fields.php?add_cat",
@@ -444,11 +448,11 @@ if (isset($_GET['add_cat'])) { //add a new category form
         <div class='col-12'><div class='alert alert-warning'>$langEPFNoCats</div></div>";
     } else {
         $form_data_array = array(); //array used to build the sortorder form
-        
+
         $tool_content .= "<div id='multi'>"; //container for sorting
         foreach ($result as $res) {
             $form_data_array[$res->id] = array();
-            
+
             $head_content .= "<style>
                                 .tile__name {
                                     cursor: move;
@@ -459,7 +463,7 @@ if (isset($_GET['add_cat'])) { //add a new category form
                               </style>";
             $tool_content .= "<div id='cat_".getIndirectReference($res->id)."' class='table-responsive tile' style='margin-bottom:30px;'><table class='table-default'>";
             $tool_content .= "<caption class='tile__name ps-1 pe-1'><strong>$langCategory :</strong> $res->name<div class='float-end'>";
-            
+
             $dyntools = array(
                 array(
                         'title' => $langEPFNewField,
@@ -479,9 +483,9 @@ if (isset($_GET['add_cat'])) { //add a new category form
                         'confirm' => $langEPFConfirmCatDelete
                 )
             );
-            
+
             $tool_content .= action_button($dyntools)."</div></caption>";
-            
+
             $tool_content .= "<thead><tr class='notes_thead'>
                 <td class='TextBold text-white'>$langName</td>
                 <td class='TextBold text-white'>$langCPFShortName</td>
@@ -490,17 +494,17 @@ if (isset($_GET['add_cat'])) { //add a new category form
                 <td class='TextBold text-white'>$langCPFFieldRequired</td>
                 <td class='TextBold text-white'>" . icon('fa-gears') . "</td>
 		        </tr></thead>";
-            
+
             $q = Database::get()->queryArray("SELECT * FROM eportfolio_fields WHERE categoryid = ?d ORDER BY sortorder DESC", $res->id);
             if (count($q) == 0) {
                 $tool_content .= "<tbody class='tile__list'>";
                 $tool_content .= "<tr class='ignore-item'><td colspan='9' class='text-center'><span class='not_visible'>".$langCPFNoFieldinCat."</td></tr>";
                 $tool_content .= "</tbody>";
             } else {
-                
+
                 $field_types = array(EPF_TEXTBOX => $langCPFText, EPF_TEXTAREA => $langCPFTextarea, EPF_DATE => $langCPFDate, EPF_MENU => $langCPFMenu, EPF_LINK =>$langCPFLink);
                 $yes_no = array(0 => $langNo, 1 => $langYes);
-                
+
                 $tool_content .= "<tbody class='tile__list'>";
                 foreach ($q as $f) {
                     $form_data_array[getIndirectReference($res->id)][] = getIndirectReference($f->id);
@@ -516,7 +520,7 @@ if (isset($_GET['add_cat'])) { //add a new category form
                               'confirm' => $langEPFConfirmFieldDelete
                         )
                     );
-                    
+
                     $tool_content .= "<tr id='field_" . getIndirectReference($f->id) . "'>";
                     $tool_content .= "<td>".q($f->name)."</td>";
                     $tool_content .= "<td>".q($f->shortname)."</td>";
@@ -528,7 +532,7 @@ if (isset($_GET['add_cat'])) { //add a new category form
                 }
                 $tool_content .= "</tbody>";
             }
-            
+
             $tool_content .= "</table></div>";
         }
         $tool_content .= "</div>";
@@ -536,7 +540,7 @@ if (isset($_GET['add_cat'])) { //add a new category form
         $tool_content .= generate_csrf_token_form_field() ."</form>";
         $tool_content .= "<script src='custom_profile_fields.js'></script>";
     }
-    
+
 }
 
 draw($tool_content, 3, null, $head_content);
