800e1fab64604e320f559cd4bf4ac6a1e8871993
Alexandros Diamantidis <adia@noc.uoa.gr>
2023-01-25 15:17:32 +0200

    Upgrade: add 3.14 to list of upgrades
    --HG--
    branch : 3.14

--- upgrade/upgrade.php
+++ upgrade/upgrade.php
@@ -185,7 +185,7 @@ if ($command_line or $ajax_call) {
     }
 
     $oldversion = get_config('version');
-    $versions = ['3.1', '3.2', '3.3', '3.4', '3.5', '3.6', '3.7', '3.8', '3.9', '3.10', '3.11', '3.12', '3.13'];
+    $versions = ['3.1', '3.2', '3.3', '3.4', '3.5', '3.6', '3.7', '3.8', '3.9', '3.10', '3.11', '3.12', '3.13', '3.14'];
     if (isset($_SESSION['upgrade_step'])) {
         $step = $_SESSION['upgrade_step'];
     }
