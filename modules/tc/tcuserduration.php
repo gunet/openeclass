5d85e3dd2f6aafb54647a0375f647470941980b9
Yannis Exidaridis <jexi@noc.uoa.gr>
2024-01-16 14:15:54 +0200

    TC: Export results in `xls` format.
    --HG--
    branch : 3.15

--- modules/tc/tcuserduration.php
+++ modules/tc/tcuserduration.php
@@ -1,7 +1,7 @@
 <?php
 
 /* ========================================================================
- * Open eClass 3.14
+ * Open eClass 3.15
  * E-learning and Course Management System
  * ========================================================================
  * Copyright 2003-2023  Greek Universities Network - GUnet
@@ -23,6 +23,10 @@
  * @file tcuserduration.php
  * @brief display various teleconference participation reports
  */
+
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
 $require_current_course = true;
 $require_login = true;
 $require_help = true;
@@ -40,19 +44,17 @@ $navigation[] = array('url' => 'index.php?course=' . $course_code, 'name' => $la
 
 if (isset($_GET['id'])) {
     $meetingid = get_tc_meeting_id($_GET['id']);
-    if (!isset($_GET['pdf'])) {
+    if (!isset($_GET['pdf']) and !isset($_GET['xls'])) {
         $tool_content .= action_bar(array(
-            array('title' => "$langExport",
-                'url' => "dumptcuserduration.php?course=$course_code&amp;meeting_id=$meetingid",
-                'icon' => 'fa-file-archive-o',
-                'level' => 'primary-label',
-                'button-class' => 'btn-success',
-                'show' => $is_course_reviewer),
             array('title' => $langDumpPDF,
                 'url' => "tcuserduration.php?course=$course_code&amp;id=$_GET[id]&amp;pdf=true",
                 'icon' => 'fa-file-pdf-o',
                 'level' => 'primary-label',
-                'button-class' => 'btn-success',
+                'show' => $is_course_reviewer),
+            array('title' => $langDumpUser,
+                'url' => "tcuserduration.php?course=$course_code&amp;id=$_GET[id]&amp;xls=true",
+                'icon' => 'fa-download',
+                'level' => 'primary-label',
                 'show' => $is_course_reviewer),
             array('title' => $langBack,
                 'url' => "index.php?course=$course_code",
@@ -61,15 +63,18 @@ if (isset($_GET['id'])) {
         ));
     }
 } else {
-    if (!isset($_GET['pdf'])) {
+    if (!isset($_GET['pdf']) and !isset($_GET['xls'])) {
         if (isset($_GET['per_user'])) {
             $url = "tcuserduration.php?course=$course_code&per_user=true&pdf=true";
+            $xls_url = "tcuserduration.php?course=$course_code&per_user=true&xls=true";
             $back_url = "index.php?course=$course_code";
         } else if (isset($_GET['u'])) {
             $url = "tcuserduration.php?course=$course_code&u=$_GET[u]&pdf=true";
+            $xls_url = "tcuserduration.php?course=$course_code&u=$_GET[u]&xls=true";
             $back_url = "tcuserduration.php?course=$course_code&per_user=true";
         } else {
             $url = "tcuserduration.php?course=$course_code&pdf=true";
+            $xls_url = "tcuserduration.php?course=$course_code&xls=true";
             $back_url = "index.php?course=$course_code";
         }
         $tool_content .= action_bar(array(
@@ -77,7 +82,11 @@ if (isset($_GET['id'])) {
                 'url' => "$url",
                 'icon' => 'fa-file-pdf-o',
                 'level' => 'primary-label',
-                'button-class' => 'btn-success',
+                'show' => $is_course_reviewer),
+            array('title' => $langDumpUser,
+                'url' => "$xls_url",
+                'icon' => 'fa-download',
+                'level' => 'primary-label',
                 'show' => $is_course_reviewer),
             array('title' => $langBack,
                 'url' => "$back_url",
@@ -97,6 +106,11 @@ if (isset($_GET['per_user']) or isset($_GET['u'])) { // all users participation 
         }
         $u = $_GET['u'];
         $bbb_name = uid_to_name($u, 'username');
+
+        if (isset($_GET['xls'])) {
+            $data[] = [ get_config('site_name') . " - " . q($currentCourseName) . " - " . q($langParticipate) ];
+        }
+
         $result = Database::get()->queryArray("SELECT title, start_date, meetingid, bbbuserid, totaltime, date FROM tc_attendance, tc_session 
                                             WHERE tc_attendance.meetingid = tc_session.meeting_id
                                             AND tc_session.course_id = ?d 
@@ -112,18 +126,30 @@ if (isset($_GET['per_user']) or isset($_GET['u'])) { // all users participation 
             $tool_content .= "<div class='panel-heading'><strong>" . uid_to_name($u, 'surname') . " " . uid_to_name($u, 'givenname') . "</strong></div>";
             $tool_content .= "<div class='panel-body'><em>$langTotalDuration:</em> <strong>" . format_time_duration(0 + 60 * $total_time, 24, false) . "</strong></span></div><br>";
             $tool_content .= "</div>";
+
+            if (isset($_GET['xls'])) {
+                $data[] = [ uid_to_name($u, 'surname') . " " . uid_to_name($u, 'givenname') .  " -- $langTotalDuration: " . format_time_duration(0 + 60 * $total_time, 24, false) ];
+            }
+            $data[] = [];
+
             $tool_content .= "<table class='table-default'>";
-            $tool_content .= "<tr class='list-header'>                                  
+            $tool_content .= "<tr class='list-header'>
                               <th>$langBBB</th>
                               <th>$langLogIn</th>
                               <th>$langDuration</th>
                            </tr>";
+            if (isset($_GET['xls'])) {
+                $data[] = [$langBBB, $langLogIn, $langDuration];
+            }
             foreach ($result as $row) {
-                $tool_content .= "<tr>                            
+                $tool_content .= "<tr>
                         <td>$row->title</td>
                         <td>" . format_locale_date(strtotime($row->date), 'full') . "</td>
                         <td>" . format_time_duration(0 + 60 * $row->totaltime, 24, false) . "</td>
                     </tr>";
+                if (isset($_GET['xls'])) {
+                    $data[] = [ $row->title, format_locale_date(strtotime($row->date), 'full'), format_time_duration(0 + 60 * $row->totaltime, 24, false)];
+                }
             }
             $tool_content .= "</table>";
         } else {
@@ -140,19 +166,25 @@ if (isset($_GET['per_user']) or isset($_GET['u'])) { // all users participation 
                       <th class='text-center'>" . icon('fa-gears') . "</th>
                     </tr>";
 
+        if (isset($_GET['xls'])) {
+            $data[] = [ get_config('site_name') . " - " . q($currentCourseName) . " - " . q($langParticipate) ];
+            $data[] = [];
+            $data[] = [];
+            $data[] = [ $langSurnameName , $langAm, $langGroup, $langTotalDuration ];
+        }
         $users = Database::get()->queryArray("SELECT user.id, surname, givenname, am                           
                                 FROM course_user, user
                                 WHERE user.id = course_user.user_id
                                 AND course_user.course_id = ?d 
                                 ORDER BY surname, givenname",
-                $course_id);
+                    $course_id);
         foreach ($users as $user) {
             $bbb_name = uid_to_name($user->id, 'username');
             $result = Database::get()->querySingle("SELECT SUM(totaltime) AS totaltime FROM tc_attendance, tc_session 
                                                     WHERE tc_attendance.meetingid = tc_session.meeting_id
                                                     AND tc_session.course_id = ?d 
                                                     AND tc_attendance.bbbuserid = ?s", $course_id, $bbb_name);
-            if (isset($_GET['pdf'])) {
+            if (isset($_GET['pdf']) or isset($_GET['xls'])) {
                 $link_to_user = "$user->surname" . " " . "$user->givenname";
                 $grp_name = user_groups($course_id, $user->id, false);
             } else {
@@ -167,6 +199,10 @@ if (isset($_GET['per_user']) or isset($_GET['u'])) { // all users participation 
                         <td>" . format_time_duration(0 + 60 * $result->totaltime, 24, false) . "</td>
                         <td>" . icon('fa-line-chart', $langDetails, $link_to_user) . "</td>
                     </tr>";
+
+            if (isset($_GET['xls'])) {
+                $data[] = [ "$user->surname  $user->givenname", $user->am, $grp_name, format_time_duration(0 + 60 * $result->totaltime, 24, false) ];
+            }
         }
         $tool_content .= "</table>";
     }
@@ -191,8 +227,13 @@ if (isset($_GET['per_user']) or isset($_GET['u'])) { // all users participation 
 
     // display results
     if (count($result) > 0) {
+        if (isset($_GET['xls'])) {
+            $data[] = [ get_config('site_name') . " - " . q($currentCourseName) . " - " . q($langParticipate) ];
+            $data[] = [];
+            $data[] = [];
+            $data[] = [ $langSurnameName, $langBBB, $langLogIn, $langTotalDuration ];
+        }
         $tool_content .= "<table class='table-default'>";
-
         $temp_date = null;
         $first = true;
         foreach ($result as $row) {
@@ -217,6 +258,9 @@ if (isset($_GET['per_user']) or isset($_GET['u'])) { // all users participation 
                             <td>" . format_locale_date(strtotime($row->date), 'full') . "</td>
                             <td>" . format_time_duration(0 + 60 * $row->totaltime, 24, false) . "</td>
                             </tr>";
+            if (isset($_GET['xls'])) {
+                $data[] = [ $user_full_name, $row->title, format_locale_date(strtotime($row->date), 'full'), format_time_duration(0 + 60 * $row->totaltime, 24, false) ];
+            }
         }
         $tool_content .= "</table>";
     } else {
@@ -278,6 +322,25 @@ if (isset($_GET['pdf']) and $is_course_reviewer) {
     $mpdf->SetAuthor(course_id_to_prof($course_id));
     $mpdf->WriteHTML($pdf_content);
     $mpdf->Output("$course_code tc_report.pdf", 'I'); // 'D' or 'I' for download / inline display
-} else {
+} else if (isset($_GET['xls']) and $is_course_reviewer) { // xls output
+    $spreadsheet = new Spreadsheet();
+    $sheet = $spreadsheet->getActiveSheet();
+    $sheet->setTitle($langParticipate);
+    $sheet->getDefaultColumnDimension()->setWidth(25);
+    $filename = $course_code . "_tc_report.xlsx";
+    $sheet->mergeCells("A1:B1");
+    for ($i=1; $i<=4; $i++) {
+        $sheet->getCellByColumnAndRow($i, 4)->getStyle()->getFont()->setBold(true);
+    }
+    // create spreadsheet
+    $sheet->fromArray($data, NULL);
+    // file output
+    $writer = new Xlsx($spreadsheet);
+    header("Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+    set_content_disposition('attachment', $filename);
+    $writer->save("php://output");
+    exit;
+
+}else {
     draw($tool_content, 2, null, $head_content);
 }
