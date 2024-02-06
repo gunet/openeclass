9eb05e535034e2cdf9d545137b09288c2ec92052
Yannis Exidaridis <jexi@noc.uoa.gr>
2024-02-06 16:10:30 +0200

    Delete Course: Notify course admin (via email) about course deletion.
    --HG--
    branch : 3.15

--- modules/course_info/delete_course.php
+++ modules/course_info/delete_course.php
@@ -1,10 +1,10 @@
 <?php
 
 /* ========================================================================
- * Open eClass 3.0
+ * Open eClass 3.15
  * E-learning and Course Management System
  * ========================================================================
- * Copyright 2003-2014  Greek Universities Network - GUnet
+ * Copyright 2003-2023  Greek Universities Network - GUnet
  * A full copyright notice can be read in "/info/copyright.txt".
  * For a full list of contributors, see "credits.txt".
  *
@@ -22,6 +22,7 @@
 $require_current_course = TRUE;
 $require_course_admin = TRUE;
 require_once '../../include/baseTheme.php';
+require_once 'include/sendMail.inc.php';
 require_once 'include/log.class.php';
 require_once 'archive_functions.php';
 
@@ -46,12 +47,57 @@ if (isset($_POST['delete'])) {
     touch("$garbage/index.html");
     rename($zipfile, "$target/$course_code.zip");
 
-    delete_course($course_id);
+    // send email to course admins
+    $profs = Database::get()->queryArray("SELECT user.id AS prof_uid, user.email AS email,
+                              user.surname, user.givenname
+                           FROM course_user JOIN user ON user.id = course_user.user_id
+                           WHERE course_id = ?d AND course_user.status = " . USER_TEACHER, $course_id);
 
+    $subject = "$langCourseDeleted " . q($currentCourseName) . " ($course_code)";
+
+    $mailHeader = "
+    <!-- Header Section -->
+	<div id='mail-header'>
+		<div>
+			<br>
+			<div id='header-title'>$langCourseDeleted '" . q($currentCourseName) . " ($course_code)'</div>
+		</div>
+	</div>";
+
+    $mailMain = "
+    <!-- Body Section -->
+	<div id='mail-body-inner'>
+		<br>
+		<div>$langCourseDeletedBy <strong>" . uid_to_name($uid) . "</strong>.</div>
+		<br>		
+	</div>";
+
+    $mailFooter = "    
+	<div id='mail-footer'>
+		<br>
+		<div><small class='notice'>$langNoticeCourseDeleted</small></div>
+	</div>";
+
+    $message = $mailHeader.$mailMain.$mailFooter;
+    $plainMessage = html2text($message);
+    foreach ($profs as $prof) {
+        if (!get_user_email_notification_from_courses($prof->prof_uid) or (!get_user_email_notification($prof->prof_uid, $course_id))) {
+            continue;
+        } else {
+            $to_name = $prof->givenname . ' ' . $prof->surname;
+            if (!send_mail_multipart('', '', '', $prof->email, $subject, $plainMessage, $message)) {
+                $tool_content .= "<div class='alert alert-warning'>$GLOBALS[langErrorSendingMessage]</div>";
+            }
+        }
+    }
+
+    // delete course
+    delete_course($course_id);
     // logging
     Log::record(0, 0, LOG_DELETE_COURSE, array('id' => $course_id,
                                                'code' => $course_code,
                                                'title' => $currentCourseName));
+
     Session::Messages("$langTheCourse <b>" . q($currentCourseName) . " ($course_code)</b> $langHasDel", 'alert-info');
     unset($_SESSION['dbname']);
     redirect_to_home_page('main/portfolio.php');
