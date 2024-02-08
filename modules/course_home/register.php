8cae7ac5cea6cb9122a6e8255aea11af2704223d
Alexandros Diamantidis <adia@noc.uoa.gr>
2024-02-08 16:38:59 +0200

    Course register from course home: initialize document_timestamp correctly
    --HG--
    branch : 3.15

--- modules/course_home/register.php
+++ modules/course_home/register.php
@@ -81,8 +81,8 @@ if (isset($_POST['register'])) {
             }
         }
 
-        Database::get()->query("INSERT IGNORE INTO `course_user` (`course_id`, `user_id`, `status`, `reg_date`)
-            VALUES (?d, ?d, " . USER_STUDENT . ", NOW())", $course_id, $uid);
+        Database::get()->query("INSERT IGNORE INTO `course_user` (`course_id`, `user_id`, `status`, `reg_date`, `document_timestamp`)
+            VALUES (?d, ?d, " . USER_STUDENT . ", NOW(), NOW())", $course_id, $uid);
         Log::record($course_id, MODULE_ID_USERS, LOG_INSERT, array('uid' => $uid, 'right' => USER_STUDENT));
         Session::Messages($langNotifyRegUser1, 'alert-success');
         redirect_to_home_page("courses/$course_code/");
@@ -183,4 +183,3 @@ $tool_content .= action_bar(array(
 
 
 draw($tool_content, 1, null, $head_content);
-
