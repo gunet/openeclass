02a64d512d6f1d4e34cf0b1059f7ada02b03f4e5
Lazaros Psarokostas <lazarospsa@hotmail.com>
2024-02-21 10:25:20 +0200

    semicolon

--- resources/views/modern/portfolio/privacy_policy_modal.blade.php
+++ resources/views/modern/portfolio/privacy_policy_modal.blade.php
@@ -2,17 +2,21 @@
     <div class='modal-dialog' role='document'>
         <div class='modal-content'>
             <div class='modal-header'>
-                <button type='button' class='close' data-bs-dismiss='modal' aria-label='Close'><span aria-hidden='true'>&times;</span></button>
+                <button type='button' class='close' data-bs-dismiss='modal' aria-label='Close'><span
+                        aria-hidden='true'>&times;</span></button>
                 <h4 class='modal-title' id='consentModalLabel'>{{ trans('langUserConsent') }}</h4>
             </div>
             <div class='modal-body' style='margin-left:20px; margin-right:20px;'>
-                {!!  get_config('privacy_policy_text_' . $session->language); !!}
+                {!! get_config('privacy_policy_text_' . $session->language) !!}
             </div>
             <form method='post' action='{{ $_SERVER['SCRIPT_NAME'] }}'>
                 <div class='modal-footer'>
-                    <button type='submit' class='btn submitAdminBtn' role='button' name='accept_policy' value='yes'>{{ trans('langAccept') }}</button>
-                    <button type='submit' class='btn deleteAdminBtn ms-1' role='button' name='accept_policy' value='no'>{{ trans('langRejectRequest') }}</button>
-                    <button type='submit' class='btn cancelAdminBtn' role='button' name='accept_policy' value='later'>{{ trans('langLater') }}</button>
+                    <button type='submit' class='btn submitAdminBtn' role='button' name='accept_policy'
+                        value='yes'>{{ trans('langAccept') }}</button>
+                    <button type='submit' class='btn deleteAdminBtn ms-1' role='button' name='accept_policy'
+                        value='no'>{{ trans('langRejectRequest') }}</button>
+                    <button type='submit' class='btn cancelAdminBtn' role='button' name='accept_policy'
+                        value='later'>{{ trans('langLater') }}</button>
                 </div>
             </form>
         </div>
