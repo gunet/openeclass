e5243c5deeaf1ec5fcf8e96565282ca9a43630c4
ntsagkas <n.tsagkas@gmail.com>
2024-03-04 09:28:42 +0000

    Documents (rec_video, rec_audio): UI improvements

--- resources/views/modern/modules/document/rec_audio.blade.php
+++ resources/views/modern/modules/document/rec_audio.blade.php
@@ -19,43 +19,72 @@
                 @else
                     <div class="col-12">
                 @endif
-                <div class="row">
-
-                    @include('layouts.common.breadcrumbs', ['breadcrumbs' => $breadcrumbs])
-
-                    @if($course_code)
-                        <div class="offcanvas offcanvas-start d-lg-none" tabindex="-1" id="collapseTools">
-                            <div class="offcanvas-header">
-                                <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                            </div>
-                            <div class="offcanvas-body">
-                                @include('layouts.partials.sidebar',['is_editor' => $is_editor])
+                        <div class="row">
+                            @include('layouts.common.breadcrumbs', ['breadcrumbs' => $breadcrumbs])
+                            @if($course_code)
+                                <div class="offcanvas offcanvas-start d-lg-none" tabindex="-1" id="collapseTools">
+                                    <div class="offcanvas-header">
+                                        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                                    </div>
+                                    <div class="offcanvas-body">
+                                        @include('layouts.partials.sidebar',['is_editor' => $is_editor])
+                                    </div>
+                                </div>
+                            @endif
+                            @include('layouts.partials.legend_view')
+                            @if(Session::has('message'))
+                                <div class='col-12 all-alerts'>
+                                    <div class="alert {{ Session::get('alert-class', 'alert-info') }} alert-dismissible fade show" role="alert">
+                                        @php 
+                                            $alert_type = '';
+                                            if(Session::get('alert-class', 'alert-info') == 'alert-success'){
+                                                $alert_type = "<i class='fa-solid fa-circle-check fa-lg'></i>";
+                                            }elseif(Session::get('alert-class', 'alert-info') == 'alert-info'){
+                                                $alert_type = "<i class='fa-solid fa-circle-info fa-lg'></i>";
+                                            }elseif(Session::get('alert-class', 'alert-info') == 'alert-warning'){
+                                                $alert_type = "<i class='fa-solid fa-triangle-exclamation fa-lg'></i>";
+                                            }else{
+                                                $alert_type = "<i class='fa-solid fa-circle-xmark fa-lg'></i>";
+                                            }
+                                        @endphp
+                                        
+                                        @if(is_array(Session::get('message')))
+                                            @php $messageArray = array(); $messageArray = Session::get('message'); @endphp
+                                            {!! $alert_type !!}<span>
+                                            @foreach($messageArray as $message)
+                                                {!! $message !!}
+                                            @endforeach</span>
+                                        @else
+                                            {!! $alert_type !!}<span>{!! Session::get('message') !!}</span>
+                                        @endif
+                                        
+                                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                    </div>
+                                </div>
+                            @endif
+                            {!! $backButton !!}
+                            <div class='@if(isset($module_id) and $module_id) d-lg-flex gap-4 @else row m-auto @endif mt-4'>
+                                <div class='@if(isset($module_id) and $module_id) flex-grow-1 @else col-lg-6 col-12 px-0 @endif'>
+                                    <div class='col-12 d-flex justify-content-start align-items-center gap-2 flex-wrap'>
+                                        <button class='btn submitAdminBtnDefault' id='button-start-recording'>{{ trans('langStart') }}</button>
+                                        <button class='btn deleteAdminBtn' id='button-stop-recording' disabled>{{ trans('langStopRecording') }}</button>
+                                        <button class='btn submitAdminBtn' id='button-release-microphone' disabled>{{ trans('langReleaseMic') }}</button>
+                                        <button class='btn submitAdminBtnDefault' id='button-download-recording' disabled>{{ trans('langSaveInDoc') }}</button>
+                                    </div>
+                                    <div class='col-12 d-flex justify-content-start align-items-center mt-2'>
+                                        <span class='help-block'>{{ trans('langMaxRecAudioTime') }}</span>
+                                    </div>
+                                    <div class='col-12 d-flex justify-content-start align-item-center mt-4'>
+                                        <audio controls autoplay playsinline></audio>
+                                    </div>
+                                </div>
+                                <div class='@if(isset($module_id) and $module_id) form-content-modules @else col-lg-6 col-12 @endif d-none d-lg-block'>
+                                    <img class='form-image-modules' src='{!! get_form_image() !!}' alt='form-image'>
+                                </div>
                             </div>
                         </div>
-                    @endif
-
-                    @include('layouts.partials.legend_view')
-
-                    {!! $backButton !!}
-
-                </div>
-
-                <div class='col-12 d-flex justify-content-center align-items-center p-2'>
-                    <button class='btn successAdminBtn me-2' id='button-start-recording'>{{ trans('langStart') }}</button>
-                    <button class='btn deleteAdminBtn me-2' id='button-stop-recording' disabled>{{ trans('langStopRecording') }}</button>
-                    <button class='btn submitAdminBtn me-2' id='button-release-microphone' disabled>{{ trans('langReleaseMic') }}</button>
-                    <button class='btn successAdminBtn me-2' id='button-download-recording' disabled>{{ trans('langSaveInDoc') }}</button>
-                </div>
-                <div class='col-12 d-flex justify-content-center p-1'>
-                    <span class='help-block'>{{ trans('langMaxRecAudioTime') }}</span>
-                </div>
-
-                <div class='col-12 d-flex justify-content-center p-5'>
-                    <audio controls autoplay playsinline></audio>
-                </div>
-
                     </div>
-                </div>
+                
             </div>
         </div>
     </div>
--- resources/views/modern/modules/document/rec_video.blade.php
+++ resources/views/modern/modules/document/rec_video.blade.php
@@ -6,57 +6,86 @@
         <div class='{{ $container }}  @if($course_code) module-container py-lg-0 @else main-container @endif'>
             <div class="@if($course_code) course-wrapper d-lg-flex align-items-lg-strech w-100 @else row m-auto @endif">
 
-                @if($course_code)
-                    <div id="background-cheat-leftnav" class="col_sidebar_active d-flex justify-content-start align-items-strech ps-lg-0 pe-lg-0">
-                        <div class="d-none d-sm-block d-sm-none d-md-block d-md-none d-lg-block ContentLeftNav">
-                            @include('layouts.partials.sidebar',['is_editor' => $is_editor])
-                        </div>
-                    </div>
-                @endif
-
-                @if($course_code)
-                    <div class="col_maincontent_active">
+                        @if($course_code)
+                            <div id="background-cheat-leftnav" class="col_sidebar_active d-flex justify-content-start align-items-strech ps-lg-0 pe-lg-0">
+                                <div class="d-none d-sm-block d-sm-none d-md-block d-md-none d-lg-block ContentLeftNav">
+                                    @include('layouts.partials.sidebar',['is_editor' => $is_editor])
+                                </div>
+                            </div>
+                        @endif
+                        @if($course_code)
+                            <div class="col_maincontent_active">
                         @else
                             <div class="col-12">
                         @endif
 
-                        <div class="row">
-
-                            @include('layouts.common.breadcrumbs', ['breadcrumbs' => $breadcrumbs])
-
-                            @if($course_code)
-                                <div class="offcanvas offcanvas-start d-lg-none" tabindex="-1" id="collapseTools">
-                                    <div class="offcanvas-header">
-                                        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                                    </div>
-                                    <div class="offcanvas-body">
-                                        @include('layouts.partials.sidebar',['is_editor' => $is_editor])
+                                <div class="row">
+                                    @include('layouts.common.breadcrumbs', ['breadcrumbs' => $breadcrumbs])
+                                    @if($course_code)
+                                        <div class="offcanvas offcanvas-start d-lg-none" tabindex="-1" id="collapseTools">
+                                            <div class="offcanvas-header">
+                                                <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                                            </div>
+                                            <div class="offcanvas-body">
+                                                @include('layouts.partials.sidebar',['is_editor' => $is_editor])
+                                            </div>
+                                        </div>
+                                    @endif
+                                    @include('layouts.partials.legend_view')
+                                    @if(Session::has('message'))
+                                        <div class='col-12 all-alerts'>
+                                            <div class="alert {{ Session::get('alert-class', 'alert-info') }} alert-dismissible fade show" role="alert">
+                                                @php 
+                                                    $alert_type = '';
+                                                    if(Session::get('alert-class', 'alert-info') == 'alert-success'){
+                                                        $alert_type = "<i class='fa-solid fa-circle-check fa-lg'></i>";
+                                                    }elseif(Session::get('alert-class', 'alert-info') == 'alert-info'){
+                                                        $alert_type = "<i class='fa-solid fa-circle-info fa-lg'></i>";
+                                                    }elseif(Session::get('alert-class', 'alert-info') == 'alert-warning'){
+                                                        $alert_type = "<i class='fa-solid fa-triangle-exclamation fa-lg'></i>";
+                                                    }else{
+                                                        $alert_type = "<i class='fa-solid fa-circle-xmark fa-lg'></i>";
+                                                    }
+                                                @endphp
+                                                
+                                                @if(is_array(Session::get('message')))
+                                                    @php $messageArray = array(); $messageArray = Session::get('message'); @endphp
+                                                    {!! $alert_type !!}<span>
+                                                    @foreach($messageArray as $message)
+                                                        {!! $message !!}
+                                                    @endforeach</span>
+                                                @else
+                                                    {!! $alert_type !!}<span>{!! Session::get('message') !!}</span>
+                                                @endif
+                                                
+                                                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                            </div>
+                                        </div>
+                                    @endif
+                                    {!! $backButton !!}
+                                
+                                    <div class='@if(isset($module_id) and $module_id) d-lg-flex gap-4 @else row m-auto @endif mt-4'>
+                                        <div class='@if(isset($module_id) and $module_id) flex-grow-1 @else col-lg-6 col-12 px-0 @endif'>
+                                            <div class='col-12 d-flex justify-content-start align-items-center gap-2 flex-wrap'>
+                                                <button class='btn submitAdminBtnDefault' id='button-open-camera'>{{ trans('langOpenCamera') }}</button>
+                                                <button class='btn submitAdminBtn' id='button-start-recording' disabled>{{ trans('langStart') }}</button>
+                                                <button class='btn deleteAdminBtn' id='button-stop-recording' disabled>{{ trans('langStopRecording') }}</button>
+                                                <button class='btn submitAdminBtnDefault' id='button-download-recording' disabled>{{ trans('langSaveInDoc') }}</button>
+                                            </div>
+                                            <div class='col-12 d-flex justify-content-start align-items-center mt-2'>
+                                                <span class='help-block'>{{ trans('langMaxRecVideoTime') }}</span>
+                                            </div>
+                                            <div class='col-12 d-flex justify-content-start align-item-center mt-4'>
+                                                <video controls autoplay playsinline></video>
+                                            </div>
+                                        </div>
+                                        <div class='@if(isset($module_id) and $module_id) form-content-modules @else col-lg-6 col-12 @endif d-none d-lg-block'>
+                                            <img class='form-image-modules' src='{!! get_form_image() !!}' alt='form-image'>
+                                        </div>
                                     </div>
                                 </div>
-                            @endif
-
-                            @include('layouts.partials.legend_view')
-
-                            {!! $backButton !!}
-
-                        </div>
-
-                        <div class='col-12 d-flex justify-content-center align-items-center p-2'>
-                            <button class='btn successAdminBtn me-2' id='button-open-camera'>{{ trans('langOpenCamera') }}</button>
-                            <button class='btn submitAdminBtn me-2' id='button-start-recording' disabled>{{ trans('langStart') }}</button>
-                            <button class='btn deleteAdminBtn me-2' id='button-stop-recording' disabled>{{ trans('langStopRecording') }}</button>
-                            <button class='btn successAdminBtn me-2' id='button-download-recording' disabled>{{ trans('langSaveInDoc') }}</button>
-                        </div>
-                        <div class='col-12 d-flex justify-content-center p-1'>
-                            <span class='help-block'>{{ trans('langMaxRecVideoTime') }}</span>
-                        </div>
-
-                        <div class='col-12 d-flex justify-content-center p-5'>
-                            <video controls autoplay playsinline></video>
-                        </div>
-
-                    </div>
-                </div>
+                            </div>
+                
             </div>
         </div>
     </div>
