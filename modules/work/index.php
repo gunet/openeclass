f2ad0ab2a4c2f1578c0b5b59355492f7c2c6b5f6
Alexandros Diamantidis <adia@noc.uoa.gr>
2024-05-29 13:26:47 +0300

    Download assignment submissions: make sure we are at the correct subdirectory
    --HG--
    branch : 3.16

--- modules/work/index.php
+++ modules/work/index.php
@@ -6081,8 +6081,8 @@ function download_assignments($id) {
         $temp_online_text_path = "$webDir/courses/temp/{$course_code}_work_$id";
         $zip = new ZipArchive();
         $zip->open($filepath, ZipArchive::CREATE);
+        chdir($workPath);
         if ($sub_type == 1) { // free text assignment
-            chdir($workPath);
             create_zip_index("$secret/index.html", $id);
             if (!is_dir($temp_online_text_path)) {
                 mkdir($temp_online_text_path);
