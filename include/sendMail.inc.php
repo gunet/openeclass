30e017b159fb803e68cc3229b52dfd57b099cfb9
Alexandros Diamantidis <adia@noc.uoa.gr>
2024-08-23 18:04:45 +0300

    send_mail(): fix typo
    --HG--
    branch : 3.16

--- include/sendMail.inc.php
+++ include/sendMail.inc.php
@@ -30,7 +30,7 @@ function send_mail($from, $from_address, $to, $to_address, $subject, $body) {
     if (is_array($to_address) and count($to_address) > 1) {
         $message->setBcc($to_address);
     } else {
-        if (is_array($to_adderss)) {
+        if (is_array($to_address)) {
             $to_address = $to_address[0];
         }
         $message->setTo(array($to_address => $to));
