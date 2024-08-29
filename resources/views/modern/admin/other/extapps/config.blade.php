d47865ccc1c1d4334c4ecfa579a379a4e3ee3940
Alexandros Diamantidis <adia@noc.uoa.gr>
2024-08-29 10:42:56 +0300

    Admin: Ext app config: remove stray whitespace from template

--- resources/views/modern/admin/other/extapps/config.blade.php
+++ resources/views/modern/admin/other/extapps/config.blade.php
@@ -7,7 +7,7 @@
         <div class="row m-auto">
 
                     @include('layouts.common.breadcrumbs', ['breadcrumbs' => $breadcrumbs])
-                    
+
 
                     @include('layouts.partials.legend_view')
 
@@ -17,18 +17,18 @@
                         <div class='mt-4'></div>
                     @endif
 
-                    @include('layouts.partials.show_alert') 
+                    @include('layouts.partials.show_alert')
 
-                    @if ($app->getName() == 'turnitin') 
+                    @if ($app->getName() == 'turnitin')
                         <div class='col-12'>
                            <div class='alert alert-warning'><i class='fa-solid fa-triangle-exclamation fa-lg'></i><span>{{ trans('langNoInfoAvailable')}}</span></div>
                         </div>
                     @else
-                        
-                            
+
+
                             <div class='col-lg-6 col-12'>
                                 <div class='form-wrapper form-edit border-0 px-0'>
-                                    
+
                                     <form class='form-horizontal' role='form' action='extapp.php?edit={{ $appName }}' method='post'>
                                         <fieldset>
                                         <legend class='mb-0' aria-label="{{ trans('langForm') }}"></legend>
@@ -37,13 +37,11 @@
                                             @if ($param->getType() == ExtParam::TYPE_BOOLEAN)
                                                 <?php $boolean_fields[] = $param; ?>
                                             @elseif ($param->getType() == ExtParam::TYPE_MULTILINE)
-                                                
+
                                                 <div class='form-group mt-4'>
                                                     <label for='{{ $param->name() }}' class='col-sm-12 control-label-notes'>{{ $param->display() }}</label>
                                                     <div class='col-sm-12'>
-                                                        <textarea id='{{ $param->name() }}' class='form-control' rows='3' cols='40' name='{{ $param->name() }}'>
-                                                            {{ $param->value() }}
-                                                        </textarea>
+                                                        <textarea id='{{ $param->name() }}' class='form-control' rows='3' cols='40' name='{{ $param->name() }}'>{{ $param->value() }}</textarea>
                                                     </div>
                                                 </div>
                                             @else
@@ -60,7 +58,7 @@
                                                     <div class='col-sm-offset-2 col-sm-10'>
                                                         <div class='checkbox'>
                                                         <label class='label-container' aria-label="{{ trans('langSettingSelect') }}">
-                                                                <input type='checkbox' name='{{ $param->name() }}'{!! $param->value() == 1 ? " value='0' checked" : " value='1'" !!}> 
+                                                                <input type='checkbox' name='{{ $param->name() }}'{!! $param->value() == 1 ? " value='0' checked" : " value='1'" !!}>
                                                                 <span class='checkmark'></span>{{ $param->display() }}
                                                             </label>
                                                         </div>
@@ -71,7 +69,7 @@
                                                 <div class='col-12 d-flex justify-content-end align-items-center'>
                                                     <button class='btn submitAdminBtn' type='submit' name='submit' value='{{ trans('langModify') }}'>
                                                         {{ trans('langModify') }}
-                                                    </button> 
+                                                    </button>
                                                     <button class='btn deleteAdminBtn ms-1' type='submit' name='submit' value='clear'>
                                                         {{ trans('langClearSettings') }}
                                                     </button>
@@ -85,10 +83,10 @@
                             <div class='col-lg-6 col-12 d-none d-md-none d-lg-block text-end'>
                             <img class='form-image-modules' src='{!! get_form_image() !!}' alt="{{ trans('langImgFormsDes') }}">
                             </div>
-                        
+
                     @endif
-                
+
         </div>
-</div>   
+</div>
 </div>
 @endsection
