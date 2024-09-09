5faef2dc73915cfe04745f24afa3986d416f3161
Alexandros Diamantidis <adia@noc.uoa.gr>
2024-09-09 15:40:13 +0300

    Portfolio calendar view: remove duplicate JS navgation functions

--- resources/views/modern/portfolio/index.blade.php
+++ resources/views/modern/portfolio/index.blade.php
@@ -39,21 +39,6 @@
                 }
             });
 
-            $(".btn-group button[data-calendar-nav]").each(function() {
-                var $this = $(this);
-                $this.click(function() {
-                    calendar.navigate($this.data("calendar-nav"));
-                });
-            });
-
-            $(".btn-group button[data-calendar-view]").each(function() {
-                var $this = $(this);
-                $this.click(function() {
-                    calendar.view($this.data("calendar-view"));
-                });
-            });
-
-
         var idCoursePortfolio = '';
         var btnPortfolio = '';
         var modal_portfolio = '';
