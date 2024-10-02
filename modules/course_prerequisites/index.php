84ec95c6ecd53315924a7f0ab7b2f637fc769d6b
Yannis Exidaridis <jexi@noc.uoa.gr>
2024-10-02 12:46:07 +0300

    Course Prerequisities: Render with `blade`.

--- modules/course_prerequisites/index.php
+++ modules/course_prerequisites/index.php
@@ -25,43 +25,7 @@ $require_help = true;
 $helpTopic = 'prequesities';
 
 require_once '../../include/baseTheme.php';
-
-$head_content .= <<<hContent
-<script type="text/javascript">
-/* <![CDATA[ */
-
-$(document).ready(function () {
-    $(document).on("click", ".delete_btn", function(e) {
-        var link = $(this).attr("href");
-        e.preventDefault();
-       
-        bootbox.confirm({ 
-            closeButton: false,
-            title: "<div class=\"icon-modal-default\"><i class=\"fa-regular fa-trash-can fa-xl Accent-200-cl\"></i></div><div class=\"modal-title-default text-center mb-0\">$langConfirmDelete</div>",
-            message: "<p class=\"text-center\">$langDelWarnCoursePrerequisite</p>",
-            buttons: {
-                cancel: {
-                    label: "$langCancel",
-                    className: "cancelAdminBtn position-center"
-                },
-                confirm: {
-                    label: "$langDelete",
-                    className: "deleteAdminBtn position-center",
-                }
-            },
-            callback: function (result) {
-                if(result) {
-                    document.location.href = link;
-                }
-            }
-        });
-
-    });
-});
-
-/* ]]> */
-</script>
-hContent;
+load_js('select2');
 
 $prereqs_url = array('url' => "$_SERVER[SCRIPT_NAME]?course=$course_code", 'name' => $langCoursePrerequisites);
 $toolName = $langCoursePrerequisites;
@@ -69,7 +33,8 @@ $toolName = $langCoursePrerequisites;
 if (isset($_GET['add'])) {
     $pageName = $langNewCoursePrerequisite;
     $navigation[] = $prereqs_url;
-    new_prereq();
+
+    view('modules.course_prerequisites.new');
 } else {
     if (isset($_POST['addcommit'])) {
         $prereqId = intval($_POST['prerequisite_course']);
@@ -79,85 +44,28 @@ if (isset($_GET['add'])) {
         $prereqId = intval($_GET['del']);
         del_prereq($prereqId);
     }
+    $data['result'] = $result = Database::get()->queryArray("SELECT c.*
+                                 FROM course_prerequisite cp 
+                                 JOIN course c on (c.id = cp.prerequisite_course) 
+                                 WHERE cp.course_id = ?d 
+                                 ORDER BY c.title", $course_id);
 
-    show_prereqs();
-}
-
-draw($tool_content, 2, null, $head_content);
-
-/////////////////
-// FUNCTIONS  //
-///////////////
-
-function new_prereq() {
-    global $tool_content, $head_content, $urlServer, $course_code,
-           $langBack, $langCourse, $langSubmit, $langCancel, $langNote, $langNewCoursePrerequisiteHelp2, 
-           $urlAppend, $langImgFormsDes, $langForm;
-
-    $tool_content .= action_bar(array(
-        array('title' => $langBack,
-            'url' => "{$urlServer}modules/course_prerequisites/index.php?course=" . $course_code,
-            'icon' => 'fa-reply',
-            'level' => 'primary')
+    $action_bar = action_bar(array(
+        array('title' => $langNewCoursePrerequisite,
+            'url' => "$_SERVER[SCRIPT_NAME]?course=$course_code&amp;add=1",
+            'button-class' => 'btn-success',
+            'icon' => 'fa-plus-circle',
+            'level' => 'primary-label')
     ));
 
-    load_js('select2');
+    $data['action_bar'] = $action_bar;
 
-    $head_content .= <<<hContent
-        <script type="text/javascript">
-        /* <![CDATA[ */
-        
-        $(document).ready(function () {
-            $('#courses-select').select2({
-                minimumInputLength: 2,
-                tags: true,
-                ajax: {
-                  url: '{$urlServer}modules/course_prerequisites/coursefeed.php',
-                  dataType: 'json'
-                }
-            });
-        });
-        
-        /* ]]> */
-        </script>
-hContent;
-
-    $tool_content .= "
-
-    <div class='d-lg-flex gap-4 mt-5'>
-        <div class='flex-grow-1'>
-            <div class='form-wrapper form-edit rounded'>
-                <div class='alert alert-info'><i class='fa-solid fa-circle-info fa-lg'></i><span>$langNewCoursePrerequisiteHelp2</span></div>
-                <form role='form' class='form-horizontal' method='post' action='index.php?course=" . $course_code . "'>
-                    <input type='hidden' name='addcommit' value='1'>
-                    <fieldset>
-                        <legend class='mb-0' aria-label='$langForm'></legend>
-                        <div class='form-group'>
-                        <label for='courses-select' class='col-sm-12 control-label-notes'>$langCourse:</label>
-                        <div class='col-sm-12'>
-                            <select id='courses-select' class='form-select' name='prerequisite_course'></select>
-                        </div>
-                        </div>
-                        <div class='form-group mt-4'>
-                        <div class='col-12 d-flex justify-content-end align-items-center gap-2'>
-                            <input class='btn submitAdminBtn' type='submit' name='submit' value='" . q($langSubmit) . "'>
-                            <a href='index.php?course=" . $course_code . "' class='btn cancelAdminBtn'>$langCancel</a>
-                        </div>
-                        </div>
-                    </fieldset>
-                    ". generate_csrf_token_form_field() ."
-                </form>
-            </div>
-        </div>
-        <div class='d-none d-lg-block'>
-            <img class='form-image-modules' src='".get_form_image()."' alt='$langImgFormsDes'>
-        </div>
-    </div>
-    ";
+    view('modules.course_prerequisites.index', $data);
 }
 
 function add_prereq($prereqId) {
-    global $course_id, $langNewCoursePrerequisiteSuccess, $langNewCoursePrerequisiteFailInvalid, $langNewCoursePrerequisiteFailSelf, $langNewCoursePrerequisiteFailAlreadyIn,
+    global $course_id, $langNewCoursePrerequisiteSuccess, $langNewCoursePrerequisiteFailInvalid,
+           $langNewCoursePrerequisiteFailSelf, $langNewCoursePrerequisiteFailAlreadyIn,
            $langNewCoursePrerequisiteFailBadgeMissing;
 
     // check invalid
@@ -202,68 +110,12 @@ function add_prereq($prereqId) {
 }
 
 function del_prereq($prereqId) {
-    global $course_id, $langDelCoursePrerequisiteSuccess, $langSettingSelect;
+    global $course_id, $langDelCoursePrerequisiteSuccess;
 
     if ($prereqId <= 0) {
         return;
     }
-
     Session::flash('message',$langDelCoursePrerequisiteSuccess);
     Session::flash('alert-class', 'alert-success');
     Database::get()->query("DELETE FROM course_prerequisite WHERE course_id = ?d AND prerequisite_course = ?d", $course_id, $prereqId);
-}
-
-function show_prereqs() {
-    global $tool_content, $course_id, $course_code, $action_bar,
-           $langTitle, $langRemovePrerequisite, $langNoCoursePrerequisites,
-           $langNewCoursePrerequisite, $langBack;
-
-    $action_bar = action_bar(array(
-        array('title' => $langNewCoursePrerequisite,
-            'url' => "$_SERVER[SCRIPT_NAME]?course=$course_code&amp;add=1",
-            'button-class' => 'btn-success',
-            'icon' => 'fa-plus-circle',
-            'level' => 'primary-label')
-    ));
-
-    $tool_content .= $action_bar;
-
-    $result = Database::get()->queryArray("SELECT c.*
-                                 FROM course_prerequisite cp 
-                                 JOIN course c on (c.id = cp.prerequisite_course) 
-                                 WHERE cp.course_id = ?d 
-                                 ORDER BY c.title", $course_id);
-
-    if (count($result) > 0) {
-        $tool_content .= "
-            <div class='row'><div class='col-sm-12'>
-            <div class='table-responsive'><table class='table-default'>
-                                  <thead><tr class='list-header'>
-                                      <th>$langTitle</th>
-                                      <th aria-label='$langSettingSelect'>".icon('fa-gears')."</th>
-                                  </tr></thead>";
-        foreach ($result as $row) {
-
-            $cid = intval($row->id);
-            $course_title = q($row->title . " (" . $row->public_code . ")");
-
-            $tool_content .= "<tr>
-            <td>$course_title</td>
-            <td class='option-btn-cell text-end'>" . action_button(
-                    array(
-                        array(
-                            'title' => $langRemovePrerequisite,
-                            'level' => 'primary',
-                            'url' => "$_SERVER[SCRIPT_NAME]?course=$course_code&amp;del=" . $cid,
-                            'icon' => 'fa-xmark Accent-200-cl',
-                            'btn_class' => 'delete_btn deleteAdminBtn'
-                        )
-                    )
-                ) . "</td>
-            </tr>" ;
-        }
-        $tool_content .= '</table></div></div></div>';
-    } else {
-        $tool_content .= "<div class='col-sm-12'><div class='alert alert-warning'><i class='fa-solid fa-triangle-exclamation fa-lg'></i><span>$langNoCoursePrerequisites</span></div></div>";
-    }
 }
