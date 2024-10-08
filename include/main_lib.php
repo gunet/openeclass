6fced7f5a7fbaa85653579c8361d4cb2ea064d84
Alexandros Diamantidis <adia@noc.uoa.gr>
2024-10-08 11:25:54 +0300

    Bump version to 3.16.3
    --HG--
    branch : 3.16

--- include/main_lib.php
+++ include/main_lib.php
@@ -27,7 +27,7 @@
  * Standard header included by all eClass files
  * Defines standard functions and validates variables
  */
-define('ECLASS_VERSION', '3.16.2');
+define('ECLASS_VERSION', '3.16.3');
 
 // mPDF library temporary file path and font path
 if (isset($webDir)) { // needed for avoiding 'notices' in some files
