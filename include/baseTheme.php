325f1e8eb56b291e2dc72fed13a2611a7ca69ee0
ntsagkas <n.tsagkas@gmail.com>
2024-10-17 19:50:27 +0000

    UI improvements regarding the height of main content.

--- include/baseTheme.php
+++ include/baseTheme.php
@@ -457,12 +457,12 @@ function view($view_file, $view_data = array()) {
             $styles_str .= "
                 @media(min-width:992px){
                     .jumbotron.jumbotron-login{
-                        min-height: calc(100vh - 80px + 80px - 80px);
+                        min-height: calc(100vh - 80px);
                     }
                 }
                 @media(max-width:991px){
                     .jumbotron.jumbotron-login{
-                        min-height: calc(100vh - 56px - 122px);
+                        min-height: calc(100vh - 56px);
                     }
                 }
             ";
