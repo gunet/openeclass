b22a0b5c27f02cb03279effc1738161d1b7a6b2c
Alexandros Diamantidis <adia@noc.uoa.gr>
2024-10-25 16:29:10 +0300

    Document upload: build local bundle for Uppy with required plugins
    CSS still loaded from CDN

---
+++ js/build/uppy.js
@@ -0,0 +1,12 @@
+import Uppy from '@uppy/core'
+import Dashboard from '@uppy/dashboard'
+import DragDrop from '@uppy/drag-drop'
+import XHRUpload from '@uppy/xhr-upload'
+import English from '@uppy/locales/lib/en_US'
+import French from '@uppy/locales/lib/fr_FR'
+import German from '@uppy/locales/lib/de_DE'
+import Italian from '@uppy/locales/lib/it_IT'
+import Spanish from '@uppy/locales/lib/es_ES'
+import Greek from '@uppy/locales/lib/el_GR'
+
+export { Uppy, Dashboard, DragDrop, XHRUpload, English, French, German, Italian, Spanish, Greek }
