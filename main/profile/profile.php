22b289140039c1287732c6a19ff868b24329dccd
Yannis Exidaridis <jexi@noc.uoa.gr>
2024-11-12 11:40:13 +0200

    User Profile: remove stray `{` character from 'displaying profile` view.

--- main/profile/profile.php
+++ main/profile/profile.php
@@ -17,25 +17,6 @@
  *
  */
 
-/* ========================================================================
- * Open eClass 4.0
- * E-learning and Course Management System
- * ========================================================================
- * Copyright 2003-2023  Greek Universities Network - GUnet
- * A full copyright notice can be read in "/info/copyright.txt".
- * For a full list of contributors, see "credits.txt".
- *
- * Open eClass is an open platform distributed in the hope that it will
- * be useful (without any warranty), under the terms of the GNU (General
- * Public License) as published by the Free Software Foundation.
- * The full license can be read in "/info/license/license_gpl.txt".
- *
- * Contact address: GUnet Asynchronous eLearning Group,
- *                  Network Operations Center, University of Athens,
- *                  Panepistimiopolis Ilissia, 15784, Athens, Greece
- *                  e-mail: info@openeclass.org
- * ======================================================================== */
-
 $require_login = true;
 $require_valid_uid = true;
 $require_help = true;
@@ -601,5 +582,4 @@ $data['SecFactorChallenge'] = showSecondFactorChallenge();
 
 $data['allProviders'] = $allProviders;
 
-$data['menuTypeID'] = 1;
 view('main.profile.edit', $data);
