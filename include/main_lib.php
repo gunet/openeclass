4358f18fbf5a4ff5eca46fcde6b1b26a1451ff51
Alexandros Diamantidis <adia@noc.uoa.gr>
2024-11-21 15:40:45 +0200

    Bump version to 4.0

--- include/main_lib.php
+++ include/main_lib.php
@@ -25,7 +25,7 @@
  * Defines standard functions and validates variables
  */
 
-define('ECLASS_VERSION', '4.0-beta2');
+define('ECLASS_VERSION', '4.0');
 
 // mPDF library temporary file path and font path
 if (isset($webDir)) { // needed for avoiding 'notices' in some files
