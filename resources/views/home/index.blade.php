55ab67b5068740565010b28eea2440850bd410a1
ntsagkas <n.tsagkas@gmail.com>
2025-01-20 10:50:05 +0000

    Homepage: minor ui improvement

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -35,8 +35,17 @@
                                 <div class='card bg-transparent card-transparent border-0'>
                                     <div class='card-header border-0 bg-transparent d-flex justify-content-between align-items-center px-0 py-0 gap-3 flex-wrap'>
                                         <div class='jumbotron-intro-text'>
-                                            <h1 class='eclass-title'>{!! get_config('homepage_title_'.$language_code) !!}</h1>
-                                            <p class='eclassInfo'>{!! get_config('homepage_intro_'.$language_code) !!}</p>
+                                            @if(get_config('homepage_title_'.$language_code))
+                                                <h1 class='eclass-title' aria-label="{{ trans('langEclass') }}">{!! get_config('homepage_title_'.$language_code) !!}</h1>
+                                            @else
+                                                <h1 class='eclass-title' aria-label="{{ trans('langEclass') }}">{{ trans('langEclass') }}</h1>
+                                            @endif
+
+                                            @if(get_config('homepage_intro_'.$language_code))
+                                                <p class='eclassInfo mb-0' aria-label="{{ trans('langInfo') }}">{!! get_config('homepage_intro_'.$language_code) !!}</p>
+                                            @else
+                                                <p class='eclassInfo mb-0' aria-label="{{ trans('langInfo') }}">{{ trans('langEclassInfo')}}</p>
+                                            @endif
                                         </div>
                                     </div>
                                     <div class='card-body px-0'>
