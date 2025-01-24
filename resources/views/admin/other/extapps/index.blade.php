e5b835f1e7d069ba5017362a6d0bbcaef4cac555
sotavgeris <none@none>
2025-01-24 15:35:24 +0200

    extapp css added class

--- resources/views/admin/other/extapps/index.blade.php
+++ resources/views/admin/other/extapps/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <div class="col-12 main-section">
-    <div class='{{ $container }} main-container'>
+    <div class='{{ $container }} main-container extapps-container'>
         <div class="row m-auto">
 
                     @include('layouts.common.breadcrumbs', ['breadcrumbs' => $breadcrumbs])
