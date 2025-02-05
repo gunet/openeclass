faaeb63c5facc9ae80f4502a4af01f12729998ef
Yannis Exidaridis <jexi@noc.uoa.gr>
2025-02-05 14:29:04 +0200

    Exercise: small improvement in results.

--- modules/exercise/exercise.lib.php
+++ modules/exercise/exercise.lib.php
@@ -464,8 +464,8 @@ function display_exercise($exercise_id): void
                                 $icon_choice = "fa-regular fa-square";
                             }
                             $tool_content .= icon($icon_choice) . "</div>";
-                            $tool_content .= "</td><td style='width: 200px;'>" . standard_text_escape($answerTitle) . " <strong><small>($langScore: $answerWeighting)</small></strong></td>
-                                               <td>" . $answerComment . "</td>
+                            $tool_content .= "</td><td>" . standard_text_escape($answerTitle) . " <strong><small>($langScore: $answerWeighting)</small></strong></td>
+                                               <td style='width: 30%;'>" . $answerComment . "</td>
                                         </tr>";
                         } elseif ($answerType == FILL_IN_BLANKS || $answerType == FILL_IN_BLANKS_TOLERANT) {
                             $tool_content .= "<tr><td>" . standard_text_escape(nl2br($answerTitle)) . " <strong><small>($langScore: " . preg_replace('/,/', ' : ', "$answerWeighting") . ")</small></strong>
