476d72616fa58bc10692b6801d375512ca5fa155
Yannis Exidaridis <jexi@noc.uoa.gr>
2025-02-27 11:55:24 +0200

    Change in forms button message.

--- modules/group/group_settings.php
+++ modules/group/group_settings.php
@@ -107,7 +107,7 @@ if (isset($_POST['submit'])) {
                                 ".form_buttons(array(
                                     array(
                                         'class' => 'submitAdminBtn',
-                                        'text' => $langSave,
+                                        'text' => $langSubmit,
                                         'name' => 'submit',
                                         'value'=> $langSubmit
                                     ),
