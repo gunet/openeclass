79f5c4a5b349677641a95418cac664b873d9cffa
Yannis Exidaridis <jexi@noc.uoa.gr>
2025-02-27 12:45:46 +0200

    Course Units: Assign course unit to specific users or groups.
    --HG--
    branch : 4.1

--- modules/course_home/course_home.php
+++ modules/course_home/course_home.php
@@ -113,6 +113,7 @@ if ($is_editor) {
         if ($course_info->view_type == 'units') {
             Database::get()->query('DELETE FROM course_units WHERE id = ?d', $id);
             Database::get()->query('DELETE FROM unit_resources WHERE unit_id = ?d', $id);
+            Database::get()->query("DELETE FROM course_units_to_specific WHERE unit_id = ?d", $id);
             Indexer::queueAsync(Indexer::REQUEST_REMOVE, Indexer::RESOURCE_UNIT, $id);
             Indexer::queueAsync(Indexer::REQUEST_REMOVEBYUNIT, Indexer::RESOURCE_UNITRESOURCE, $id);
             Indexer::queueAsync(Indexer::REQUEST_STORE, Indexer::RESOURCE_COURSE, $course_id);
@@ -469,9 +470,9 @@ if ($is_editor) {
     if ($last_id) {
         $last_id = $last_id->id;
     }
-    $query = "SELECT id, title, start_week, finish_week, comments, visible, public, `order` FROM course_units WHERE course_id = ?d AND `order` >= 0 ORDER BY `order`";
+    $query = "SELECT id, title, start_week, finish_week, comments, visible, public, `order`, assign_to_specific FROM course_units WHERE course_id = ?d AND `order` >= 0 ORDER BY `order`";
 } else {
-    $query = "SELECT id, title, start_week, finish_week, comments, visible, public, `order` FROM course_units WHERE course_id = ?d AND visible = 1 AND `order` >= 0 ORDER BY `order`";
+    $query = "SELECT id, title, start_week, finish_week, comments, visible, public, `order`, assign_to_specific FROM course_units WHERE course_id = ?d AND visible = 1 AND `order` >= 0 ORDER BY `order`";
 }
 
 $data['all_units'] = $all_units = Database::get()->queryArray($query, $course_id);
@@ -660,17 +661,16 @@ if ($total_cunits > 0) {
         foreach ($all_units as $cu) {
             $not_shown = false;
             $icon = '';
-                // check if unit has started
             if (!$is_editor) {
-                if (!(is_null($cu->start_week)) and (date('Y-m-d') < $cu->start_week)) {
+                if (!has_access_to_units($cu->id, $cu->assign_to_specific, $uid)) { // unit has assigned to users or groups ?
+                    $not_shown = true;
+                } else if (!(is_null($cu->start_week)) and (date('Y-m-d') < $cu->start_week)) { // unit has started ?
                     $not_shown = true;
                     $icon = icon('fa-clock fa-md', $langUnitNotStarted);
-                // or has completed units (if any)
-                } else if (!in_array($cu->id, $visible_units_id)) {
+                } else if (!in_array($cu->id, $visible_units_id)) { //  has completed units (if any) ?
                     $not_shown = true;
                     $icon = icon('fa-minus-circle fa-md', $langUnitNotCompleted);
                 } else {
-
                     if (in_array($cu->id, $visible_units_id)) {
                         $sql_badge = Database::get()->querySingle("SELECT id FROM badge WHERE course_id = ?d AND unit_id = ?d", $course_id, $cu->id);
                         if ($sql_badge) {
@@ -785,17 +785,17 @@ if ($total_cunits > 0) {
             $counter_hr++;
             $not_shown = false;
             $icon = '';
-                // check if unit has started
+
             if (!$is_editor) {
-                if (!(is_null($cu->start_week)) and (date('Y-m-d') < $cu->start_week)) {
+                if (!has_access_to_units($cu->id, $cu->assign_to_specific, $uid)) { // unit has assigned to users or groups ?
+                    $not_shown = true;
+                } else if (!(is_null($cu->start_week)) and (date('Y-m-d') < $cu->start_week)) { // unit has started ?
                     $not_shown = true;
                     $icon = icon('fa-clock fa-md', $langUnitNotStarted);
-                // or has completed units (if any)
-                } else if (!in_array($cu->id, $visible_units_id)) {
+                } else if (!in_array($cu->id, $visible_units_id)) { //  has completed units (if any) ?
                     $not_shown = true;
                     $icon = icon('fa-minus-circle fa-md', $langUnitNotCompleted);
                 } else {
-
                     if (in_array($cu->id, $visible_units_id)) {
                         $sql_badge = Database::get()->querySingle("SELECT id FROM badge WHERE course_id = ?d AND unit_id = ?d", $course_id, $cu->id);
                         if ($sql_badge) {
@@ -996,6 +996,37 @@ if($course_info->view_type == 'sessions' && isset($_SESSION['uid'])){
                                                             ORDER BY start ASC",$course_id,1);
 }
 view('modules.course.home.index', $data);
+
+/**
+ * @brief check if user has access to unit if it is assigned to specific users or groups
+ * @param $unit_id
+ * @return bool
+ */
+function has_access_to_units($unit_id, $assign_to_specific, $user_id)
+{
+    switch ($assign_to_specific) {
+        case 0:
+            return true;
+        case 1:
+            $q = Database::get()->querySingle("SELECT user_id FROM course_units_to_specific WHERE unit_id = ?d AND user_id = ?d", $unit_id, $user_id);
+            if ($q) {
+                return true;
+            } else {
+                return false;
+            }
+        case 2:
+            $unit_to_group_id = Database::get()->querySingle("SELECT group_id FROM course_units_to_specific WHERE unit_id = ?d", $unit_id)->group_id;
+            $q = Database::get()->queryArray("SELECT user_id FROM group_members WHERE group_id = ?d", $unit_to_group_id);
+            foreach ($q as $group_data) {
+                if ($group_data->user_id == $user_id) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+    }
+}
+
 
 /**
  * @brief fetch course announcements
--- modules/units/info.php
+++ modules/units/info.php
@@ -30,6 +30,22 @@ $helpTopic = 'AddCourseUnits';
 require_once '../../include/baseTheme.php';
 require_once 'modules/tags/moduleElement.class.php';
 
+if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+    if (isset($_POST['assign_type'])) {
+        if ($_POST['assign_type'] == 2) {
+            $data = Database::get()->queryArray("SELECT name,id FROM `group` WHERE course_id = ?d ORDER BY name", $course_id);
+        } elseif ($_POST['assign_type'] == 1) {
+            $data = Database::get()->queryArray("SELECT user.id AS id, surname, givenname
+                                    FROM user, course_user
+                                    WHERE user.id = course_user.user_id
+                                    AND course_user.course_id = ?d AND course_user.status = " . USER_STUDENT . "
+                                    AND user.id ORDER BY surname", $course_id);
+        }
+        echo json_encode($data);
+        exit;
+    }
+}
+
 load_js('tools.js');
 load_js('select2');
 load_js('bootstrap-datepicker');
@@ -38,9 +54,10 @@ $data['start_week'] = $data['finish_week'] = '';
 if (isset($_GET['edit'])) {
     $id = intval($_GET['edit']);
     $pageName = $langEditUnit;
-    $cu = Database::get()->querySingle("SELECT id, title, comments, 
-                                        start_week, finish_week
-                                    FROM course_units WHERE id = ?d AND course_id = ?d",
+    $cu = Database::get()->querySingle("SELECT id, title, comments, start_week, finish_week, assign_to_specific 
+                                            FROM course_units 
+                                            WHERE id = ?d 
+                                            AND course_id = ?d",
                                         $id, $course_id);
     $navigation[] = [ 'name' => $cu->title, 'url' => "index.php?course=$course_code&id=$id"];
     if (!$cu) {
@@ -58,7 +75,61 @@ if (isset($_GET['edit'])) {
     $data['unitTitle'] = $cu->title;
     $data['unitId'] = $cu->id;
 
+    $unassigned_options = $assignee_options = '';
+    if ($cu->assign_to_specific) {
+        //preparing options in select boxes for assigning to specific users/groups
+        $assignee_options='';
+        $unassigned_options='';
+        if ($cu->assign_to_specific == 2) {
+            $assignees = Database::get()->queryArray("SELECT `group`.id AS id, `group`.name
+                FROM course_units_to_specific, `group`
+                WHERE `group`.id = course_units_to_specific.group_id                    
+                    AND course_units_to_specific.unit_id = ?d", $id);
+            $all_groups = Database::get()->queryArray("SELECT name, id FROM `group` WHERE course_id = ?d AND visible = 1", $course_id);
+            foreach ($assignees as $assignee_row) {
+                $assignee_options .= "<option value='".$assignee_row->id."'>".$assignee_row->name."</option>";
+            }
+            $unassigned = array_udiff($all_groups, $assignees,
+                function ($obj_a, $obj_b) {
+                    return $obj_a->id - $obj_b->id;
+                }
+            );
+            foreach ($unassigned as $unassigned_row) {
+                $unassigned_options .= "<option value='$unassigned_row->id'>$unassigned_row->name</option>";
+            }
+        } else {
+            $assignees = Database::get()->queryArray("SELECT user.id AS id, surname, givenname
+                FROM course_units_to_specific, user
+                WHERE user.id = course_units_to_specific.user_id AND course_units_to_specific.unit_id = ?d", $id);
+            $all_users = Database::get()->queryArray("SELECT user.id AS id, user.givenname, user.surname
+                FROM user, course_user
+                WHERE user.id = course_user.user_id
+                AND course_user.course_id = ?d
+                AND course_user.status = " . USER_STUDENT . "
+                AND user.id", $course_id);
+            foreach ($assignees as $assignee_row) {
+                $assignee_options .= "<option value='$assignee_row->id'>$assignee_row->surname $assignee_row->givenname</option>";
+            }
+            $unassigned = array_udiff($all_users, $assignees,
+                function ($obj_a, $obj_b) {
+                    return $obj_a->id - $obj_b->id;
+                }
+            );
+            foreach ($unassigned as $unassigned_row) {
+                $unassigned_options .= "<option value='$unassigned_row->id'>$unassigned_row->surname $unassigned_row->givenname</option>";
+            }
+        }
+    }
+
+    $data['unitAssignToSpecific'] = $cu->assign_to_specific;
+    $data['unassigned_options'] = $unassigned_options;
+    $data['assignee_options'] = $assignee_options;
+
+
 } else {
+    $data['unitAssignToSpecific'] = $unitAssignToSpecific = 0;
+    $data['unassigned_options'] = $unassigned_options = '';
+    $data['assignee_options'] = $assignee_options = '';
     $pageName = $langAddUnit;
     $unitDescr = '';
     $data['unitTitle'] = $data['unitId'] = null;
