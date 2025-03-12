751cc696ca5cd255b2baba6a0976d6733e3a1238
Yannis Exidaridis <jexi@noc.uoa.gr>
2025-03-12 15:38:45 +0200

    Assignemnts: Render with Blade (almost completed ...)
    --HG--
    branch : 4.1

---
+++ resources/views/modules/work/not_submitted.blade.php
@@ -0,0 +1,122 @@
+@include('layouts.partials.show_alert')
+
+@extends('layouts.default')
+
+@section('content')
+
+    <div class="col-12 main-section">
+        <div class='{{ $container }} module-container py-lg-0'>
+            <div class="course-wrapper d-lg-flex align-items-lg-strech w-100">
+
+                @include('layouts.partials.left_menu')
+
+                <div class="col_maincontent_active">
+                    <div class="row">
+                        @include('layouts.common.breadcrumbs', ['breadcrumbs' => $breadcrumbs])
+
+                        @include('layouts.partials.legend_view')
+
+                        <div id='operations_container'>
+                            {!! $action_bar !!}
+                        </div>
+
+                        @include('layouts.partials.show_alert')
+
+                        @include('modules.work.assignment_details')
+
+                        @if ($group_submissions)
+                            @if ($num_results > 0)
+                                <div class='row'>
+                                    <div class='col-sm-12'>
+                                        <div class='alert alert-info'>
+                                            <i class='fa-solid fa-triangle-exclamation fa-lg'></i>
+                                            @if ($num_results == 1)
+                                                {{ trans('langOneNonSubmission') }}
+                                            @else
+                                                {!! sprintf(trans('langMoreSubmissions'), $num_results) !!}
+                                            @endif
+                                        </div>
+                                        <div class='table-responsive'>
+                                            <table class='table-default sortable'>
+                                                <thead>
+                                                    <tr class='list-header'>
+                                                        <th class='count-col' aria-label='{{ trans('langSettingSelect') }}'>#</th>
+                                                        {!! sort_link(trans('langGroup'), 'username') !!}
+                                                    </tr>
+                                                </thead>
+                                                @foreach ($groups as $row => $value)
+                                                    <tr>
+                                                        <td class='count-col' style='width: 5%'>
+                                                            {{ $loop->iteration }}.
+                                                        </td>
+                                                        <td>
+                                                            <a href='../group/group_space.php?course={{ $course_code }}&group_id={{ $row }}'>{{ $value }}</a>
+                                                        </td>
+                                                    </tr>
+                                                @endforeach
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            @else
+                                <p class='sub_title1 mt-3'>{{ trans('langWorkGroupNoSubmission') }}:</p>
+                                <div class='col-sm-12'>
+                                    <div class='alert alert-warning'>
+                                        <i class='fa-solid fa-triangle-exclamation fa-lg'></i><span>{{ trans('langNoneWorkGroupNoSubmission') }}</span>
+                                    </div>
+                                </div>
+                            @endif
+                        @else
+                            <div class='row'>
+                                <div class='col-sm-12'>
+                                    @if ($num_results > 0)
+                                        <div class='alert alert-warning mt-4'>
+                                            <i class='fa-solid fa-triangle-exclamation fa-lg'></i>
+                                            @if ($num_results == 1)
+                                                {{ trans('langOneNonSubmission') }}
+                                            @else
+                                                {!! sprintf(trans('langMoreNonSubmissions'), $num_results) !!}
+                                            @endif
+                                        </div>
+                                    <div class='table-responsive mt-1'>
+                                        <table class='table-default'>
+                                            <thead>
+                                                <tr class='list-header'>
+                                                    <th class='count-col' aria-label='{{ trans('langSettingSelect') }}'>#</th>
+                                                    <th>{{ trans('langSurnameName') }}</th>
+                                                </tr>
+                                            </thead>
+                                            @foreach ($users as $row => $value)
+                                                <tr>
+                                                    <td class='count-col' style='width: 5%'>
+                                                        {{ $loop->iteration }}.
+                                                    </td>
+                                                    <td> {!! display_user($row) !!}
+                                                        @if (!is_null(uid_to_am($row)))
+                                                            <div class='text-heading-h6'>
+                                                                {{ trans('langAmShort') }}: {{ uid_to_am($row) }}
+                                                            </div>
+                                                        @endif
+                                                    </td>
+                                                </tr>
+                                            @endforeach
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                            @else
+                                <p class='sub_title1 mt-3'>{{ trans('langWorkUserNoSubmission') }}:</p>
+                                <div class='col-sm-12'>
+                                    <div class='alert alert-warning'>
+                                        <i class='fa-solid fa-triangle-exclamation fa-lg'></i><span>{{ trans('langNoneWorkUserNoSubmission') }}</span>
+                                    </div>
+                                </div>
+                            @endif
+                        @endif
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
