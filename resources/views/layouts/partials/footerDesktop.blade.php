3c24dcb1d5cdf5716c9f434a2a0bf46ccb992f65
swtav@sotos <swtav@sotos>
2025-04-25 14:22:30 +0300

    LTI iframe changes
    --HG--
    branch : 4.1

--- resources/views/layouts/partials/footerDesktop.blade.php
+++ resources/views/layouts/partials/footerDesktop.blade.php
@@ -21,14 +21,14 @@
                 <div class='col-12 d-flex d-flex justify-content-center align-items-center gap-3 flex-wrap mt-3'>
                     @if (!get_config('dont_display_about_menu'))
                         <div>
-                            <a class="a_tools_site_footer" href="{{$urlAppend}}info/about.php">
+                            <a class="a_tools_site_footer" href="{{$urlAppend}}info/about.php" @if($_SESSION['provider'] == 'lti_publish') target="_blank" @endif>
                                 {{ trans('langPlatformIdentity') }}
                             </a>
                         </div>
                     @endif
                     @if (!get_config('dont_display_contact_menu'))
                         <div>
-                            <a class="a_tools_site_footer" href="{{$urlAppend}}info/contact.php">
+                            <a class="a_tools_site_footer" href="{{$urlAppend}}info/contact.php" @if($_SESSION['provider'] == 'lti_publish') target="_blank" @endif>
                                 {{ trans('langContact') }}
                             </a>
                         </div>
@@ -36,26 +36,26 @@
 
                     @if (!get_config('dont_display_manual_menu'))
                         <div>
-                            <a class="a_tools_site_footer" href="{{$urlAppend}}info/manual.php">
+                            <a class="a_tools_site_footer" href="{{$urlAppend}}info/manual.php" @if($_SESSION['provider'] == 'lti_publish') target="_blank" @endif>
                                 {{ trans('langManuals') }}
                             </a>
                         </div>
                     @endif
                     <div>
-                        <a class="a_tools_site_footer" href="{{$urlAppend}}info/terms.php">
+                        <a class="a_tools_site_footer" href="{{$urlAppend}}info/terms.php" @if($_SESSION['provider'] == 'lti_publish') target="_blank" @endif>
                             {{ trans('langUsageTerms') }}
                         </a>
                     </div>
                     @if (get_config('activate_privacy_policy_text'))
                         <div>
-                            <a class="a_tools_site_footer" href="{{$urlAppend}}info/privacy_policy.php">
+                            <a class="a_tools_site_footer" href="{{$urlAppend}}info/privacy_policy.php" @if($_SESSION['provider'] == 'lti_publish') target="_blank" @endif>
                                 {{ trans('langPrivacyPolicy') }}
                             </a>
                         </div>
                     @endif
                 </div>
                 <div class="col-12 d-flex justify-content-center align-items-center flex-wrap gap-5 mt-3 pb-3">
-                    <a class="copyright" href='{{$urlAppend}}info/copyright.php'>Copyright © {{ date('Y') }} All rights reserved</a>
+                    <a class="copyright" href='{{$urlAppend}}info/copyright.php' @if($_SESSION['provider'] == 'lti_publish') target="_blank" @endif>Copyright © {{ date('Y') }} All rights reserved</a>
                     @if(get_config('enable_social_sharing_links'))
                         <div class='d-flex gap-3 justify-content-end'>
                             @if (get_config('link_fb'))
@@ -87,21 +87,21 @@
                 <nav class='col-12 d-flex justify-content-between align-items-center'>
                     <ul class="container-items-footer nav">
                         @if (!get_config('dont_display_about_menu'))
-                            <li class="nav-item"><a class="nav-link menu-item a_tools_site_footer ps-2 pe-3" href="{{ $urlAppend }}info/about.php">{{ trans('langPlatformIdentity') }}</a></li>
+                            <li class="nav-item"><a class="nav-link menu-item a_tools_site_footer ps-2 pe-3" href="{{ $urlAppend }}info/about.php"  @if($_SESSION['provider'] == 'lti_publish') target="_blank" @endif>{{ trans('langPlatformIdentity') }}</a></li>
                         @endif
                         @if (!get_config('dont_display_contact_menu'))
-                            <li class="nav-item"><a class="nav-link menu-item a_tools_site_footer px-3" href="{{ $urlAppend }}info/contact.php">{{ trans('langContact') }}</a></li>
+                            <li class="nav-item"><a class="nav-link menu-item a_tools_site_footer px-3" href="{{ $urlAppend }}info/contact.php"  @if($_SESSION['provider'] == 'lti_publish') target="_blank" @endif>{{ trans('langContact') }}</a></li>
                         @endif
                         @if (!get_config('dont_display_manual_menu'))
-                            <li class="nav-item"><a class="nav-link menu-item a_tools_site_footer px-3" href="{{ $urlAppend }}info/manual.php">{{ trans('langManuals') }}</a></li>
+                            <li class="nav-item"><a class="nav-link menu-item a_tools_site_footer px-3" href="{{ $urlAppend }}info/manual.php"  @if($_SESSION['provider'] == 'lti_publish') target="_blank" @endif>{{ trans('langManuals') }}</a></li>
                         @endif
-                        <li class="nav-item"><a class="nav-link menu-item a_tools_site_footer px-3" href="{{ $urlAppend }}info/terms.php">{{ trans('langUsageTerms') }}</a></li>
+                        <li class="nav-item"><a class="nav-link menu-item a_tools_site_footer px-3" href="{{ $urlAppend }}info/terms.php"  @if($_SESSION['provider'] == 'lti_publish') target="_blank" @endif>{{ trans('langUsageTerms') }}</a></li>
                         @if (get_config('activate_privacy_policy_text'))
-                            <li class="nav-item"><a class="nav-link menu-item a_tools_site_footer px-3" href="{{ $urlAppend }}info/privacy_policy.php">{{ trans('langPrivacyPolicy') }}</a>
+                            <li class="nav-item"><a class="nav-link menu-item a_tools_site_footer px-3" href="{{ $urlAppend }}info/privacy_policy.php"  @if($_SESSION['provider'] == 'lti_publish') target="_blank" @endif>{{ trans('langPrivacyPolicy') }}</a>
                         @endif
                     </ul>
                     <div class='d-flex justify-content-start align-items-center'>
-                        <a class="copyright px-2" href='{{ $urlAppend }}info/copyright.php'>Copyright © {{ date('Y') }} All rights reserved</a>
+                        <a class="copyright px-2" href='{{ $urlAppend }}info/copyright.php' @if($_SESSION['provider'] == 'lti_publish') target="_blank" @endif>Copyright © {{ date('Y') }} All rights reserved</a>
                         @if(get_config('enable_social_sharing_links'))
                             @if (get_config('link_fb'))
                                 <a class='a_tools_site_footer' href="{!! get_config('link_fb') !!}" target="_blank" aria-label="Facebook: {{ trans('langOpenNewTab') }}">
@@ -149,14 +149,14 @@
                 <div class='col-12 d-flex d-flex justify-content-center align-items-center pb-3 gap-3 flex-wrap'>
                     @if (!get_config('dont_display_about_menu'))
                         <div>
-                            <a class="a_tools_site_footer" href="{{$urlAppend}}info/about.php">
+                            <a class="a_tools_site_footer" href="{{$urlAppend}}info/about.php" @if($_SESSION['provider'] == 'lti_publish') target="_blank" @endif>
                                 {{ trans('langPlatformIdentity') }}
                             </a>
                         </div>
                     @endif
                     @if (!get_config('dont_display_contact_menu'))
                         <div>
-                            <a class="a_tools_site_footer" href="{{$urlAppend}}info/contact.php">
+                            <a class="a_tools_site_footer" href="{{$urlAppend}}info/contact.php" @if($_SESSION['provider'] == 'lti_publish') target="_blank" @endif>
                                 {{ trans('langContact') }}
                             </a>
                         </div>
@@ -164,19 +164,19 @@
 
                     @if (!get_config('dont_display_manual_menu'))
                         <div>
-                            <a class="a_tools_site_footer" href="{{$urlAppend}}info/manual.php">
+                            <a class="a_tools_site_footer" href="{{$urlAppend}}info/manual.php" @if($_SESSION['provider'] == 'lti_publish') target="_blank" @endif>
                                 {{ trans('langManuals') }}
                             </a>
                         </div>
                     @endif
                     <div>
-                        <a class="a_tools_site_footer" href="{{$urlAppend}}info/terms.php">
+                        <a class="a_tools_site_footer" href="{{$urlAppend}}info/terms.php" @if($_SESSION['provider'] == 'lti_publish') target="_blank" @endif>
                             {{ trans('langUsageTerms') }}
                         </a>
                     </div>
                     @if (get_config('activate_privacy_policy_text'))
                         <div>
-                            <a class="a_tools_site_footer" href="{{$urlAppend}}info/privacy_policy.php">
+                            <a class="a_tools_site_footer" href="{{$urlAppend}}info/privacy_policy.php" @if($_SESSION['provider'] == 'lti_publish') target="_blank" @endif>
                                 {{ trans('langPrivacyPolicy') }}
                             </a>
                         </div>
@@ -184,7 +184,7 @@
                 </div>
                 <div class='col-12 border-bottom-footer'></div>
                 <div class="col-12 mt-auto d-flex justify-content-between align-items-center flex-wrap gap-3 pt-3">
-                    <a class="copyright" href='{{$urlAppend}}info/copyright.php'>Copyright © {{ date('Y') }} All rights reserved</a>
+                    <a class="copyright" href='{{$urlAppend}}info/copyright.php' @if($_SESSION['provider'] == 'lti_publish') target="_blank" @endif>Copyright © {{ date('Y') }} All rights reserved</a>
                     @if(get_config('enable_social_sharing_links'))
                         <div class='d-flex gap-3 justify-content-end'>
                             @if (get_config('link_fb'))
