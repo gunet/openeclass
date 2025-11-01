c8a3203a5a98a9aee0f27652fe0936b672ff458b
plessas <none@none>
2025-11-01 10:23:52 +0200

    Add function that returns info about the level progress of a user in a points game
    --HG--
    branch : 4.x_gamification

--- modules/progress/PointsGame.php
+++ modules/progress/PointsGame.php
@@ -21,15 +21,80 @@
 class PointsGame {
 
     public static function levelUpdate($uid, $gid, $points, $curlevel) {
-        $level  = Database::get()->querySingle("select id from points_game_levels 
-                                                where points_game = ?d and required_points <= ?d 
-                                                order by required_points desc limit 1", $gid, $points);
+        $level  = Database::get()->querySingle("SELECT id FROM points_game_levels 
+                                                WHERE points_game = ?d AND required_points <= ?d 
+                                                ORDER BY required_points DESC LIMIT 1", $gid, $points);
         if($level) {
             if($level->id != $curlevel) {
-                Database::get()->query("update user_points_game_points set current_level = ?d 
-                                        where user = ?d and points_game = ?d", $level->id, $uid, $gid);
+                Database::get()->query("UPDATE user_points_game_points SET current_level = ?d 
+                                        WHERE user = ?d AND points_game = ?d", $level->id, $uid, $gid);
             }
         }
+    }
+
+    public static function getNextLevelInfo($uid, $gid) {
+        $points_q = Database::get()->querySingle("SELECT total_points AS p, current_level AS l FROM user_points_game_points WHERE user=?d AND points_game=?d", $uid, $gid);
+        if($points_q) {
+            if($points_q->l) {
+                $q = Database::get()->querySingle("SELECT required_points AS p FROM points_game_levels WHERE id=?d",$points_q->l);
+            }
+
+            $performance = [
+                'points' => $points_q->p,
+                'current_level' => $points_q->l,
+                'current_level_min_points' => ($points_q->l) ? $q->p : null
+            ];
+        } else {
+            $performance = [
+                'points' => 0,
+                'current_level' => null,
+                'current_level_min_points' => null
+            ];
+        }
+
+        $next_q = Database::get()->querySingle("SELECT id, required_points FROM points_game_levels 
+                                                WHERE points_game = ?d AND required_points > ?d
+                                                ORDER BY required_points ASC LIMIT 1", $gid, $performance['points']);
+
+        //user hasn't reach first level yet
+        if(!$performance['current_level'] && $next_q) {
+            $max = (int) $next_q->required_points;
+            $progress = max(0, $performance['points']);
+            $percent = ($max > 0) ? ($progress / $max) * 100 : 100;
+            $percent = round(min(100, max(0, $percent)), 2);
+
+            return [
+                'current_level_id' => null,
+                'next_level_id' => $next_q->id,
+                'points_needed_for_next' => $max - $performance['points'],
+                'progress_percentage' => $percent
+            ];
+        }
+
+        //user is in max level
+        if($performance['current_level'] && !$next_q) {
+            return [
+                'current_level_id' => $performance['current_level'],
+                'next_level_id' => null,
+                'points_needed_for_next' => null,
+                'progress_percentage' => 100
+            ];
+        }
+
+        //typical case where user has reached one level and is progressing to the next one
+        $min = (int) $performance['current_level_min_points'];
+        $max = (int) $next_q->required_points;
+        $span = $max - $min;
+        $progress = $points - $min;
+        $percent = ($span > 0) ? ($progress / $span) * 100 : 100;
+        $percent = round(min(100, max(0, $percent)), 2);
+
+        return [
+            'current_level_id' => $performance['current_level'],
+            'next_level_id' => $next_q->id,
+            'points_needed_for_next' => $max - $points,
+            'progress_percentage' => $percent
+        ];
     }
 
 }
