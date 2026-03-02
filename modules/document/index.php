d3246659ce2215f1adeb6130f361a7460f45aaf7
Yannis Exidaridis <jexi@noc.uoa.gr>
2026-03-02 12:39:07 +0200

    Documents, Course Metadata: add while list filtering while upload audio recording or video recording in documents. Also filtering while uploading an image in course metadata.

--- modules/document/index.php
+++ modules/document/index.php
@@ -243,6 +243,7 @@ if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQ
     /* save video recorded data */
     if (isset($_FILES['video-blob'])) {
         $title = $_POST['userFile'];
+        validateUploadedFile($_FILES['video-blob']['name']);
         $file_path = '/' . safe_filename('webm');
         if (!move_uploaded_file($_FILES['video-blob']['tmp_name'], $basedir . $file_path)) {
             Session::flash('message', $langGeneralError);
@@ -253,30 +254,30 @@ if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQ
             $file_date = date('Y-m-d G:i:s');
             $file_format = 'webm';
             Database::get()->query("INSERT INTO document SET
-            course_id = ?d,
-            subsystem = ?d,
-            subsystem_id = ?d,
-            path = ?s,
-            extra_path = '',
-            filename = ?s,
-            visible = 1,
-            comment = '',
-            category = 0,
-            title = ?s,
-            creator = ?s,
-            date = ?s,
-            date_modified = ?s,
-            subject = '',
-            description = '',
-            author = ?s,
-            format = ?s,
-            copyrighted = 0,
-            editable = 0,
-            lock_user_id = ?d",
-                $course_id, $subsystem, $subsystem_id, $file_path,
-                $filename, $title, $file_creator,
-                $file_date, $file_date, $file_creator, $file_format,
-                $language, $uid);
+                course_id = ?d,
+                subsystem = ?d,
+                subsystem_id = ?d,
+                path = ?s,
+                extra_path = '',
+                filename = ?s,
+                visible = 1,
+                comment = '',
+                category = 0,
+                title = ?s,
+                creator = ?s,
+                date = ?s,
+                date_modified = ?s,
+                subject = '',
+                description = '',
+                author = ?s,
+                format = ?s,
+                copyrighted = 0,
+                editable = 0,
+                lock_user_id = ?d",
+                    $course_id, $subsystem, $subsystem_id, $file_path,
+                    $filename, $title, $file_creator,
+                    $file_date, $file_date, $file_creator, $file_format,
+                    $language, $uid);
             Session::Messages($langDownloadEnd, 'alert-success');
             exit();
         }
@@ -284,6 +285,7 @@ if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQ
     /* save audio recorded data */
     if (isset($_FILES['audio-blob'])) {
         $title = $_POST['userFile'];
+        validateUploadedFile($_FILES['audio-blob']['name']);
         $file_path = '/' . safe_filename('mka');
         if (!move_uploaded_file($_FILES['audio-blob']['tmp_name'], $basedir . $file_path)) {
             Session::flash('message', $langGeneralError);
@@ -397,9 +399,9 @@ $dialogData = [
 if (isset($_GET['mindmap'])) {
     $mindmap = $_GET['mindmap'];
     $title = $_GET['mindtitle'];
-
     $file_path = '/' . safe_filename('jm');
-    if (!file_put_contents($basedir . $file_path, $_GET['mindmap'])) {
+    validateUploadedFile($file_path);
+    if (!file_put_contents($basedir . $file_path, $mindmap)) {
         Session::flash('message', $langGeneralError);
         Session::flash('alert-class', 'alert-danger');
     } else {
