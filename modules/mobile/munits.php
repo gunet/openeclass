e0ce32e5997136000c7f2e3be6c04ec4e35b370a
Yannis Exidaridis <jexi@noc.uoa.gr>
2026-03-11 17:09:31 +0200

    Fix wrong commit

--- modules/mobile/munits.php
+++ modules/mobile/munits.php
@@ -61,27 +61,3 @@ function createCourseUnitsDom($course_units) {
     $dom->formatOutput = true;
     return array($dom, $retroot);
 }
-
-<units>
-  <unit id="101" name="Εισαγωγή" description=""/>
-  <unit id="102" name="Game#1 - Knight Rider" description=""/>
-  <unit id="103" name="Game#2 - Downloading bar" description=""/>
-  <unit id="104" name="Game#3 - Φανάρια κυκλοφορίας" description=""/>
-  <unit id="105" name="Game#4 - Φανάρια αγώνων ταχύτητας" description=""/>
-  <unit id="106" name="Game#5 - Πέτρα, ψαλίδι, χαρτί (μέσω σειριακής οθόνης)" description=""/>
-  <unit id="107" name="Game#6 - Πέτρα, ψαλίδι, χαρτί (με διακόπτες και LCD οθόνη)" description=""/>
-  <unit id="108" name="Game#7 - Pacman, TRex να σωθείς!" description=""/>
-  <unit id="109" name="Game#8 - Μπάρα parking (με κουμπί ενεργοποίησης)" description=""/>
-  <unit id="110" name="Game#9 - Αυτόματη μπάρα parking (με φωτοαντίσταση)" description=""/>
-  <unit id="111" name="Bonus Game" description=""/>
-  <unit id="112" name="Game#10 - Arduino hacks T-REX" description=""/>
-  <unit id="113" name="Game#11 - Ρομποτικό όχημα" description="The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for t..."/>
-  <unit id="114" name="Game#11 - Βοηθητικό σύστημα παρκαρίσματος (paktronic)" description=""/>
-  <unit id="115" name="Game#12 - Arduino Radar" description=""/>
-  <unit id="116" name="Game#13 - Otto robot" description=""/>
-  <unit id="117" name="Game#14 - Μετρητής σκορ" description=""/>
-  <unit id="118" name="Game#15 - Τηλεχειρισμός με υπέρυθρες" description=""/>
-  <unit id="119" name="Game#16 - Robotic arm" description=""/>
-  <unit id="120" name="Ardublock libraries" description=""/>
-</units>
-
