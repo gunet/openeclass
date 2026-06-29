c8e9e4a5d6fc136f092cceb2f8c09d82e35fc7b2
Yannis Exidaridis <jexi@noc.uoa.gr>
2026-06-29 12:07:05 +0300

    TC: Proper sanitization during adding a new tc session.

--- modules/attendance/index.php
+++ modules/attendance/index.php
@@ -420,8 +420,8 @@ if ($is_editor) {
                 foreach ($ug as $u) {
                     if (!in_array($u->user_id, $already_inserted_ids)) {
                         $newUsersQuery = Database::get()->query("INSERT INTO attendance_users (attendance_id, uid)
-                                SELECT $attendance_id, user_id FROM course_user
-                                WHERE course_id = ?d AND user_id = ?d", $course_id, $u->user_id);
+                                SELECT ?d, user_id FROM course_user
+                                WHERE course_id = ?d AND user_id = ?d", $attendance_id, $course_id, $u->user_id);
                         update_user_attendance_activities($attendance_id, $u->user_id);
                         $distinct_users_count++;
                     }
